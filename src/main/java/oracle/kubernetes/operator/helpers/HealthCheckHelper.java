7893ca3034d4667722d9fb612962027ad3af29e0
Antonio Nappi <antonio.nappi@cern.ch>
2018-02-07 23:07:56 +0100

    Issue-73. Fix typo
    Signed-off-by: Antonio Nappi <antonio.nappi@cern.ch>

--- src/main/java/oracle/kubernetes/operator/helpers/HealthCheckHelper.java
+++ src/main/java/oracle/kubernetes/operator/helpers/HealthCheckHelper.java
@@ -248,7 +248,7 @@ public class HealthCheckHelper {
 	while( ! minor_string.chars().allMatch( Character::isDigit )) {
           minor_string = minor_string.substring(0, minor_string.length() -1);
 	}
-        Integer minor = Integer.parseInt(info.getMinor());
+        Integer minor = Integer.parseInt(minor_string);
         if (minor < 7) {
           k8sMinVersion = false;
         } else if (minor >= 7) {
