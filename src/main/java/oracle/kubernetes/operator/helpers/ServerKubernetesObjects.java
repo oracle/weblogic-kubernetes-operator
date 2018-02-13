3f437ac17d108ce62a7991d0f3c37252c6ddc480
Ryan Eberhard <ryan.eberhard@oracle.com>
2018-02-13 15:32:00 -0500

    work in progress

--- src/main/java/oracle/kubernetes/operator/helpers/ServerKubernetesObjects.java
+++ src/main/java/oracle/kubernetes/operator/helpers/ServerKubernetesObjects.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 import io.kubernetes.client.models.V1Pod;
 import io.kubernetes.client.models.V1Service;
+import io.kubernetes.client.models.V1beta1Ingress;
 
 /**
  * Kubernetes pods and services associated with a single WebLogic server
@@ -17,6 +18,7 @@ public class ServerKubernetesObjects {
   private V1Pod pod = null;
   private V1Service service = null;
   private Map<String, V1Service> channels = null;
+  private Map<String, V1beta1Ingress> ingresses = null;
   
   /**
    * The Pod
@@ -50,10 +52,25 @@ public class ServerKubernetesObjects {
     this.service = service;
   }
   
+  /**
+   * Channel map
+   * @return Map from channel name to Service
+   */
   public Map<String, V1Service> getChannels() {
     if (channels == null) {
       channels = new HashMap<String, V1Service>();
     }
     return channels;
+  }
+  
+  /**
+   * Ingress map
+   * @return Map from cluster name to Ingress
+   */
+  public Map<String, V1beta1Ingress> getIngresses() {
+    if (ingresses == null) {
+      ingresses = new HashMap<String, V1beta1Ingress>();
+    }
+    return ingresses;
   }
 }
