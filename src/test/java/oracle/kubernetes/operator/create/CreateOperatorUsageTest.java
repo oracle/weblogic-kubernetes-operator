002a4993fbdc22763e111be924bc1e3e1bb967a5
Tom Moreau <tom.moreau@oracle.com>
2018-02-27 00:05:08 -0500

    Continue writing unit tests for the create scripts

---
+++ src/test/java/oracle/kubernetes/operator/create/CreateOperatorUsageTest.java
@@ -0,0 +1,43 @@
+// Copyright 2018, Oracle Corporation and/or its affiliates.  All rights reserved.
+package oracle.kubernetes.operator.create;
+
+import org.junit.Test;
+
+import static oracle.kubernetes.operator.create.ExecResultMatcher.allOf;
+import static oracle.kubernetes.operator.create.ExecResultMatcher.failsAndPrints;
+import static oracle.kubernetes.operator.create.ExecResultMatcher.succeedsAndPrints;
+import static oracle.kubernetes.operator.create.ExecResultMatcher.toArray;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Test the various create-weblogic-operator.sh command line options that print usage messages
+ */
+public class CreateOperatorUsageTest extends CreateOperatorTest {
+
+  private static final String[] USAGE = { "usage", "-o", "-i", "-g", "-h" };
+
+  @Test
+  public void helpOption_succeedsAndPrintsUsage() throws Exception {
+    assertThat(exexCreateOperatorResult(" -h"), succeedsAndPrints(USAGE));
+  }
+
+  @Test
+  public void noOption_failsAndPrintsErrorAndUsage() throws Exception {
+    assertThat(exexCreateOperatorResult(""), failsAndPrints(allOf(USAGE, CREATE_SCRIPT, "-o must be specified")));
+  }
+
+  @Test
+  public void missingOutputDir_failsAndPrintsErrorAndUsage() throws Exception {
+    assertThat(exexCreateOperatorResult(" -o"), failsAndPrints(USAGE, toArray("option requires an argument -- o")));
+  }
+
+  @Test
+  public void missingInputFileName_failsAndPrintsErrorAndUsage() throws Exception {
+    assertThat(exexCreateOperatorResult(" -i"), failsAndPrints(USAGE, toArray("option requires an argument -- i")));
+  }
+
+  @Test
+  public void unsupportedOption_failsAndPrintsErrorAndUsage() throws Exception {
+    assertThat(exexCreateOperatorResult(" -z"), failsAndPrints(USAGE, toArray("illegal option -- z")));
+  }
+}
---
+++ src/test/java/oracle/kubernetes/operator/create/ExecResultMatcher.java
@@ -0,0 +1,113 @@
+package oracle.kubernetes.operator.create;
+
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Matcher for testing the results of using java to execute a command
+ */
+public class ExecResultMatcher extends TypeSafeDiagnosingMatcher<ExecResult> {
+  public static final String MULTI_LINE_REGEXP_PREFIX = "(?s)";
+  private int expectedExitValue;
+  private String[] stdoutRegExps;
+  private String[] stderrRegExps;
+
+  private ExecResultMatcher(int expectedExitValue, String[] stdoutRegExps, String[] stderrRegExps) {
+    this.expectedExitValue = expectedExitValue;
+    this.stdoutRegExps = stdoutRegExps;
+    this.stderrRegExps = stderrRegExps;
+  }
+
+  public static ExecResultMatcher succeedsAndPrints(String... stdoutRegExps) {
+    return new ExecResultMatcher(0, stdoutRegExps, null);
+  }
+
+  public static ExecResultMatcher failsAndPrints(String... stdoutRegExps) {
+    return new ExecResultMatcher(1, stdoutRegExps, null);
+  }
+
+  public static ExecResultMatcher failsAndPrints(String[] stdoutRegExps, String[] stdErrRegExps) {
+    return new ExecResultMatcher(1, stdoutRegExps, stdErrRegExps);
+  }
+
+  public static String errorRegexp(String regexp) {
+    return "ERROR: " + ExecResultMatcher.toContainsRegExp(regexp);
+  }
+
+  public static String toContainsRegExp(String regexp) {
+    return ".*" + regexp + ".*";
+  }
+
+  public static String[] allOf(String[] list, String... more) {
+    String[] combined = new String[list.length + more.length];
+    System.arraycopy(list, 0, combined, 0, list.length);
+    System.arraycopy(more, 0, combined, list.length, more.length);
+    return combined;
+  }
+
+  public static String[] toArray(String ... vals) {
+    return vals;
+  }
+
+  @Override
+  protected boolean matchesSafely(ExecResult execResult, Description description) {
+    boolean rtn = true;
+    //if (hasIncorrectExitValue(description, execResult)) {
+    //  rtn = false;
+    if (hasIncorrectOutputStream(description, "stdout", execResult.stdout(), stdoutRegExps))
+      rtn = false;
+    if (hasIncorrectOutputStream(description, "stderr", execResult.stderr(), stderrRegExps))
+      rtn = false;
+    return rtn;
+  }
+
+  private boolean hasIncorrectExitValue(Description description, ExecResult execResult) {
+    if (execResult.exitValue() == expectedExitValue)
+      return true;
+    description.appendText("exit value was ").appendValue(execResult.exitValue());
+    return false;
+  }
+
+  private boolean hasIncorrectOutputStream(Description description, String streamName, String streamContent, String[] regExps) {
+    if (regExps == null) {
+      // null regExps implies that the stream should return no content
+      if (streamContent.length() != 0) {
+        description.appendText(streamName + " has ").appendValue(streamContent);
+        return true;
+      }
+    }  else {
+      List<String> missingRegexps = getMissingRegexps(streamContent, regExps);
+      if (!missingRegexps.isEmpty()) {
+        description.appendValueList("actual stdout \n'" + streamContent + "'\n is missing [", ", ", "]", missingRegexps);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private List<String> getMissingRegexps(String have, String... regExpsWant) {
+    List<String> missing = new ArrayList<>();
+    for (String regExp : regExpsWant) {
+      if (!have.matches(MULTI_LINE_REGEXP_PREFIX + toContainsRegExp(regExp)))
+        missing.add(regExp);
+    }
+    return missing;
+  }
+
+  @Override
+  public void describeTo(Description description) {
+    description.appendText("exit code ").appendValue(expectedExitValue);
+    describeRequiredOutputStream(description, "stdout", stdoutRegExps);
+    describeRequiredOutputStream(description, "stderr", stderrRegExps);
+  }
+
+  private void describeRequiredOutputStream(Description description, String streamName, String[] regExps) {
+    if (regExps == null)
+      description.appendText("\n with an empty " + streamName);
+    else
+      description.appendValueList("\n with " + streamName + " containing [", ",", "]", regExps);
+  }
+}
