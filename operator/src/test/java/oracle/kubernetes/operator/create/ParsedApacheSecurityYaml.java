78ea79e61c68c02e1f08cc86966c0712859cd62e
Anthony Lai <anthony.lai@oracle.com>
2018-04-26 13:34:38 -0700

    add unit tests for apache load balance work

---
+++ operator/src/test/java/oracle/kubernetes/operator/create/ParsedApacheSecurityYaml.java
@@ -0,0 +1,37 @@
+// Copyright 2018, Oracle Corporation and/or its affiliates.  All rights reserved.
+// Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+
+package oracle.kubernetes.operator.create;
+
+import java.nio.file.Path;
+
+import io.kubernetes.client.models.V1beta1ClusterRole;
+import io.kubernetes.client.models.V1beta1ClusterRoleBinding;
+
+/**
+ * Parses a generated weblogic-domain-apache-security.yaml file into a set of typed k8s java objects
+ */
+public class ParsedApacheSecurityYaml extends ParsedKubernetesYaml {
+
+  private CreateDomainInputs inputs;
+
+  public ParsedApacheSecurityYaml(Path yamlPath, CreateDomainInputs inputs) throws Exception {
+    super(yamlPath);
+    this.inputs = inputs;
+  }
+
+  public V1beta1ClusterRole getApacheClusterRole() {
+    return getClusterRoles().find(getApacheName());
+  }
+
+  public V1beta1ClusterRoleBinding getApacheClusterRoleBinding() {
+    return getClusterRoleBindings().find(getApacheName());
+  }
+
+  public int getExpectedObjectCount() {
+    return 2;
+  }
+
+  private String getApacheName() { return inputs.getDomainUID() + "-apache-webtier"; }
+}
+
