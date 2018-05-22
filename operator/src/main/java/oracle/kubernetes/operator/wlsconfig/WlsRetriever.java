c5f675846afe3a7d0415b944a0d03f9e3bf18eca
anthony_lai <anthony.lai@oracle.com>
2018-05-21 18:13:18 -0700

    reduce logging of repeated warning messages in WlsRetriever

--- operator/src/main/java/oracle/kubernetes/operator/wlsconfig/WlsRetriever.java
+++ operator/src/main/java/oracle/kubernetes/operator/wlsconfig/WlsRetriever.java
@@ -359,17 +359,18 @@ public class WlsRetriever {
 
         return doNext(packet);
       } catch (Throwable t) {
-        if (RequestType.CONFIG.equals(requestType)) {
-          LOGGER.warning(MessageKeys.WLS_CONFIGURATION_READ_FAILED, t);
-        } else {
-          LOGGER.warning(
-              MessageKeys.WLS_HEALTH_READ_FAILED, packet.get(ProcessingConstants.SERVER_NAME), t);
-        }
-
         // exponential back-off
         Integer retryCount = (Integer) packet.get(RETRY_COUNT);
         if (retryCount == null) {
           retryCount = 0;
+          // Log warning if this is the first try. Do not log for retries to prevent
+          // filling up the log repeatedly  with same log message
+          if (RequestType.CONFIG.equals(requestType)) {
+            LOGGER.warning(MessageKeys.WLS_CONFIGURATION_READ_FAILED, t);
+          } else {
+            LOGGER.fine(
+                MessageKeys.WLS_HEALTH_READ_FAILED, packet.get(ProcessingConstants.SERVER_NAME), t);
+          }
         }
         long waitTime = Math.min((2 << ++retryCount) * SCALE, MAX) + (R.nextInt(HIGH - LOW) + LOW);
         packet.put(RETRY_COUNT, retryCount);
