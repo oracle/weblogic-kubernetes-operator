c9bc3be9ac465bc81aa787744e4e0f2a7366920f
Russell Gold <russell.gold@oracle.com>
2018-05-31 10:27:47 -0400

    remove duplicate code

--- operator/src/main/java/oracle/kubernetes/operator/helpers/IngressHelper.java
+++ operator/src/main/java/oracle/kubernetes/operator/helpers/IngressHelper.java
@@ -11,6 +11,7 @@ import static oracle.kubernetes.operator.LabelConstants.CREATEDBYOPERATOR_LABEL;
 import static oracle.kubernetes.operator.LabelConstants.DOMAINNAME_LABEL;
 import static oracle.kubernetes.operator.LabelConstants.DOMAINUID_LABEL;
 import static oracle.kubernetes.operator.LabelConstants.RESOURCE_VERSION_LABEL;
+import static oracle.kubernetes.operator.VersionConstants.DOMAIN_V1;
 import static oracle.kubernetes.operator.Workarounds.INTORSTRING_BAD_EQUALS;
 
 import io.kubernetes.client.custom.IntOrString;
@@ -22,9 +23,9 @@ import io.kubernetes.client.models.V1beta1IngressBackend;
 import io.kubernetes.client.models.V1beta1IngressRule;
 import io.kubernetes.client.models.V1beta1IngressSpec;
 import java.util.Objects;
+import java.util.Optional;
 import oracle.kubernetes.operator.KubernetesConstants;
 import oracle.kubernetes.operator.ProcessingConstants;
-import oracle.kubernetes.operator.VersionConstants;
 import oracle.kubernetes.operator.calls.CallResponse;
 import oracle.kubernetes.operator.steps.DefaultResponseStep;
 import oracle.kubernetes.operator.work.NextAction;
@@ -66,10 +67,10 @@ public class IngressHelper {
       clusterName = (String) packet.get(ProcessingConstants.CLUSTER_NAME);
       port = (Integer) packet.get(ProcessingConstants.PORT);
 
-      if (!hasClusterData()) {
-        return doNext(packet);
-      } else {
+      if (hasClusterData()) {
         return doNext(verifyIngressStep(getNext()), packet);
+      } else {
+        return doNext(packet);
       }
     }
 
@@ -79,65 +80,15 @@ public class IngressHelper {
 
     private Step verifyIngressStep(Step next) {
       return new CallBuilder()
-          .readIngressAsync(getIngressName(), getNamespace(), new ReadIngressResponseStep(next));
+          .readIngressAsync(getName(), getNamespace(), new ReadIngressResponseStep(next));
     }
 
-    V1beta1Ingress createDesiredResource() {
-      return new V1beta1Ingress()
-          .apiVersion(KubernetesConstants.EXTENSIONS_API_VERSION)
-          .kind(KubernetesConstants.KIND_INGRESS)
-          .metadata(
-              new V1ObjectMeta()
-                  .name(getIngressName())
-                  .namespace(getNamespace())
-                  .putAnnotationsItem(CLASS_INGRESS, CLASS_INGRESS_VALUE)
-                  .putLabelsItem(RESOURCE_VERSION_LABEL, VersionConstants.DOMAIN_V1)
-                  .putLabelsItem(DOMAINUID_LABEL, getUid())
-                  .putLabelsItem(DOMAINNAME_LABEL, getDomainName())
-                  .putLabelsItem(CLUSTERNAME_LABEL, clusterName)
-                  .putLabelsItem(CREATEDBYOPERATOR_LABEL, "true"))
-          .spec(createIngressSpec());
+    private String getName() {
+      return LegalNames.toIngressName(getUid(), clusterName);
     }
 
     private String getNamespace() {
       return info.getDomain().getMetadata().getNamespace();
-    }
-
-    private String getClusterServiceName() {
-      return LegalNames.toClusterServiceName(getUid(), clusterName);
-    }
-
-    private String getDomainName() {
-      return info.getDomain().getSpec().getDomainName();
-    }
-
-    private String getUid() {
-      return info.getDomain().getSpec().getDomainUID();
-    }
-
-    String getIngressName() {
-      return LegalNames.toIngressName(getUid(), clusterName);
-    }
-
-    V1beta1IngressSpec createIngressSpec() {
-      String serviceName = getClusterServiceName();
-      return new V1beta1IngressSpec()
-          .addRulesItem(
-              new V1beta1IngressRule()
-                  .http(
-                      new V1beta1HTTPIngressRuleValue()
-                          .addPathsItem(
-                              new V1beta1HTTPIngressPath()
-                                  .path("/")
-                                  .backend(
-                                      new V1beta1IngressBackend()
-                                          .serviceName(serviceName)
-                                          .servicePort(new IntOrString(port))))));
-    }
-
-    private boolean isCompatible(V1beta1Ingress result) {
-      return VersionHelper.matchesResourceVersion(result.getMetadata(), VersionConstants.DOMAIN_V1)
-          && equalObjects(result.getSpec(), createIngressSpec());
     }
 
     private class ReadIngressResponseStep extends DefaultResponseStep<V1beta1Ingress> {
@@ -162,71 +113,96 @@ public class IngressHelper {
 
       private Step createIngressStep(Step next) {
         return new CallBuilder()
-            .createIngressAsync(
-                getNamespace(), createDesiredResource(), new CreateIngressResponseStep(next));
+            .createIngressAsync(getNamespace(), defineIngress(), new UpdateStep(next));
       }
 
       Step replaceIngressStep(Step next) {
         return new CallBuilder()
-            .replaceIngressAsync(
-                getIngressName(),
-                getNamespace(),
-                createDesiredResource(),
-                new ReplaceIngressResponseStep(next));
+            .replaceIngressAsync(getName(), getNamespace(), defineIngress(), new UpdateStep(next));
+      }
+
+      private boolean isCompatible(V1beta1Ingress result) {
+        return VersionHelper.matchesResourceVersion(result.getMetadata(), DOMAIN_V1)
+            && equalObjects(result.getSpec(), createIngressSpec());
+      }
+
+      private boolean equalObjects(V1beta1IngressSpec object1, V1beta1IngressSpec object2) {
+        return INTORSTRING_BAD_EQUALS
+            ? yamlEquals(object1, object2)
+            : Objects.equals(object1, object2);
+      }
+
+      private boolean yamlEquals(Object actual, Object expected) {
+        return Objects.equals(objectToYaml(actual), objectToYaml(expected));
+      }
+
+      private String objectToYaml(Object object) {
+        return new Yaml().dump(object);
       }
     }
 
-    private class CreateIngressResponseStep extends ResponseStep<V1beta1Ingress> {
-      CreateIngressResponseStep(Step next) {
+    V1beta1Ingress defineIngress() {
+      return new V1beta1Ingress()
+          .apiVersion(KubernetesConstants.EXTENSIONS_API_VERSION)
+          .kind(KubernetesConstants.KIND_INGRESS)
+          .metadata(
+              new V1ObjectMeta()
+                  .name(getName())
+                  .namespace(getNamespace())
+                  .putAnnotationsItem(CLASS_INGRESS, CLASS_INGRESS_VALUE)
+                  .putLabelsItem(RESOURCE_VERSION_LABEL, DOMAIN_V1)
+                  .putLabelsItem(DOMAINUID_LABEL, getUid())
+                  .putLabelsItem(DOMAINNAME_LABEL, getDomainName())
+                  .putLabelsItem(CLUSTERNAME_LABEL, clusterName)
+                  .putLabelsItem(CREATEDBYOPERATOR_LABEL, "true"))
+          .spec(createIngressSpec());
+    }
+
+    private String getUid() {
+      return info.getDomain().getSpec().getDomainUID();
+    }
+
+    private String getDomainName() {
+      return info.getDomain().getSpec().getDomainName();
+    }
+
+    V1beta1IngressSpec createIngressSpec() {
+      String serviceName = getClusterServiceName();
+      return new V1beta1IngressSpec()
+          .addRulesItem(
+              new V1beta1IngressRule()
+                  .http(
+                      new V1beta1HTTPIngressRuleValue()
+                          .addPathsItem(
+                              new V1beta1HTTPIngressPath()
+                                  .path("/")
+                                  .backend(
+                                      new V1beta1IngressBackend()
+                                          .serviceName(serviceName)
+                                          .servicePort(new IntOrString(port))))));
+    }
+
+    private String getClusterServiceName() {
+      return LegalNames.toClusterServiceName(getUid(), clusterName);
+    }
+
+    private class UpdateStep extends ResponseStep<V1beta1Ingress> {
+      UpdateStep(Step next) {
         super(next);
       }
 
       @Override
       public NextAction onFailure(Packet packet, CallResponse<V1beta1Ingress> callResponse) {
-        return super.onFailure(CreateClusterStep.this, packet, callResponse);
+        return onFailure(CreateClusterStep.this, packet, callResponse);
       }
 
       @Override
       public NextAction onSuccess(Packet packet, CallResponse<V1beta1Ingress> callResponse) {
-        V1beta1Ingress result = callResponse.getResult();
-        if (result != null) {
-          addToDomainInfo(result);
-        }
+        Optional.ofNullable(callResponse.getResult())
+            .ifPresent(CreateClusterStep.this::addToDomainInfo);
+
         return doNext(packet);
       }
     }
-
-    private class ReplaceIngressResponseStep extends ResponseStep<V1beta1Ingress> {
-
-      ReplaceIngressResponseStep(Step next) {
-        super(next);
-      }
-
-      @Override
-      public NextAction onFailure(Packet packet, CallResponse<V1beta1Ingress> callResponse) {
-        return super.onFailure(CreateClusterStep.this, packet, callResponse);
-      }
-
-      @Override
-      public NextAction onSuccess(Packet packet, CallResponse<V1beta1Ingress> callResponse) {
-        V1beta1Ingress result = callResponse.getResult();
-        if (result != null) {
-          addToDomainInfo(result);
-        }
-        return doNext(packet);
-      }
-    }
-  }
-
-  private static boolean equalObjects(V1beta1IngressSpec object1, V1beta1IngressSpec object2) {
-    return INTORSTRING_BAD_EQUALS ? yamlEquals(object1, object2) : Objects.equals(object1, object2);
-  }
-
-  private static boolean yamlEquals(Object actual, Object expected) {
-    return Objects.equals(objectToYaml(actual), objectToYaml(expected));
-  }
-
-  private static String objectToYaml(Object object) {
-    return new Yaml().dump(object);
   }
 }
