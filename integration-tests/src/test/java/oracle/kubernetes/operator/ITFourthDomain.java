f3f4ded385910712cdf25755ff3657608d05e1c4
Vanajakshi Mukkara <VANAJAKSHI.MUKKARA@ORACLE.COM>
2018-07-02 12:46:17 -0700

    adding non-quick tests domain2, domain3, operator2, domain4

--- integration-tests/src/test/java/oracle/kubernetes/operator/ITFourthDomain.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/ITFourthDomain.java
@@ -6,11 +6,8 @@ package oracle.kubernetes.operator;
 
 import java.util.Properties;
 import oracle.kubernetes.operator.utils.Domain;
-import oracle.kubernetes.operator.utils.ExecCommand;
-import oracle.kubernetes.operator.utils.ExecResult;
 import oracle.kubernetes.operator.utils.Operator;
 import oracle.kubernetes.operator.utils.TestUtils;
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -20,14 +17,14 @@ import org.junit.Test;
  * <p>This test is used for creating Operator and a single domain which the Operator manages and
  * verifies both.
  */
-public class ITSingleDomain extends BaseTest {
+public class ITFourthDomain extends BaseTest {
   public static final String TESTWEBAPP = "testwebapp";
 
   // property file used to customize operator properties for operator inputs yaml
-  private static String opPropsFile = "ITSingleDomain_op.properties";
+  private static String opPropsFile = "ITSecondOperator.properties";
 
   // property file used to customize domain properties for domain inputs yaml
-  private static String domainPropsFile = "ITSingleDomain_domain.properties";
+  private static String domainPropsFile = "ITFourthDomain.properties";
 
   // property file used to configure constants for integration tests
   private static String appPropsFile = "OperatorIT.properties";
@@ -74,38 +71,12 @@ public class ITSingleDomain extends BaseTest {
   }
 
   /**
-   * Shutdown operator and domain
+   * verify domain1 is unaffected
    *
    * @throws Exception
    */
-  @AfterClass
-  public static void staticUnPrepare() throws Exception {
-    if (System.getenv("QUICK_TEST") != null && System.getenv("QUICK_TEST").equals("true")) {
-      logger.info("+++++++++++++++++++++++++++++++++---------------------------------+");
-      logger.info("BEGIN");
-      logger.info("Run once, shutdown/deleting operator, domain, pv, etc");
-      // shutdown operator, domain and cleanup all artifacts and pv dir
-      try {
-        if (domain != null) domain.destroy();
-        if (operator != null) operator.destroy();
-      } finally {
-        String cmd =
-            "export RESULT_ROOT="
-                + getResultRoot()
-                + " export PV_ROOT="
-                + getPvRoot()
-                + " && "
-                + getProjectRoot()
-                + "/src/integration-tests/bash/cleanup.sh";
-        ExecResult result = ExecCommand.exec(cmd);
-        if (result.exitValue() != 0) {
-          logger.info(
-              "FAILED: command to call cleanup script " + cmd + " failed " + result.stderr());
-        }
-        logger.info("Command " + cmd + " returned " + result.stdout() + "\n" + result.stderr());
-      }
-      logger.info("SUCCESS");
-    }
+  public void verifyDomain1() throws Exception {
+    ITSingleDomain.getDomain().verifyDomainCreated();
   }
 
   /**
@@ -141,25 +112,6 @@ public class ITSingleDomain extends BaseTest {
       throw new RuntimeException("FAILURE: exposeAdminT3Channel is not set or false");
     }
     domain.verifyWebAppLoadBalancing(TESTWEBAPP);
-    logger.info("SUCCESS");
-  }
-
-  /**
-   * Restarting the domain should not have any impact on Operator managing the domain, web app load
-   * balancing and node port service
-   *
-   * @throws Exception
-   */
-  @Test
-  public void testDomainLifecyle() throws Exception {
-    logTestBegin("testDomainLifecyle");
-    domain.destroy();
-    domain.create();
-    operator.verifyExternalRESTService();
-    operator.verifyDomainExists(domainUid);
-    domain.verifyDomainCreated();
-    domain.verifyWebAppLoadBalancing(TESTWEBAPP);
-    domain.verifyAdminServerExternalService(BaseTest.getUsername(), BaseTest.getPassword());
     logger.info("SUCCESS");
   }
 
@@ -218,23 +170,26 @@ public class ITSingleDomain extends BaseTest {
     }
 
     domain.verifyWebAppLoadBalancing(TESTWEBAPP);
+
+    // verfiy scaling has no impact on domains1
+    ITSingleDomain.getDomain().verifyDomainCreated();
     logger.info("SUCCESS");
   }
 
   /**
-   * Restarting Operator should not impact the running domain
+   * test delete and recreate domain1 has no impact on domain4
    *
    * @throws Exception
    */
   @Test
-  public void testOperatorLifecycle() throws Exception {
-    logTestBegin("testOperatorLifecycle");
-    operator.destroy();
-    operator.create();
-    operator.verifyExternalRESTService();
-    operator.verifyDomainExists(domainUid);
+  public void testDeleteAndRecreteDomain1() throws Exception {
+
+    Domain domain1 = ITSingleDomain.getDomain();
+    domain1.destroy();
+    domain1.create();
+
+    // verify domain4 has no impact
     domain.verifyDomainCreated();
-    logger.info("SUCCESS");
   }
 
   public static Domain getDomain() {
