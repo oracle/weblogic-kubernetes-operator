b46b03b78e81598e853c15b511c7c010d60a72a5
Vanajakshi Mukkara <VANAJAKSHI.MUKKARA@ORACLE.COM>
2018-07-09 11:10:48 -0700

    fixing typo for QUICKTEST condition

--- integration-tests/src/test/java/oracle/kubernetes/operator/ITThirdDomain.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/ITThirdDomain.java
@@ -41,8 +41,8 @@ public class ITThirdDomain extends BaseTest {
   @BeforeClass
   public static void staticPrepare() throws Exception {
     Assume.assumeTrue(
-        System.getenv("QUICK_TEST") == null
-            || System.getenv("QUICK_TEST").equalsIgnoreCase("false"));
+        System.getenv("QUICKTEST") == null
+            || System.getenv("QUICKTEST").equalsIgnoreCase("false"));
     logger.info("+++++++++++++++++++++++++++++++++---------------------------------+");
     logger.info("BEGIN");
 
