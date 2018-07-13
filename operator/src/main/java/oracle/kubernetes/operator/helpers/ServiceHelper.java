1ff9ae3e13a3569cd6571866d487d771d441a800
Vanajakshi Mukkara <VANAJAKSHI.MUKKARA@ORACLE.COM>
2018-07-13 12:53:09 -0700

    replace with latest from develop

--- operator/src/main/java/oracle/kubernetes/operator/helpers/ServiceHelper.java
+++ operator/src/main/java/oracle/kubernetes/operator/helpers/ServiceHelper.java
@@ -97,6 +97,9 @@ public class ServiceHelper {
       if (isPublishNotReadyAddressesSupported()) {
         serviceSpec.setPublishNotReadyAddresses(Boolean.TRUE);
       }
+      if (nodePort == null) {
+        serviceSpec.clusterIP("None");
+      }
       return serviceSpec;
     }
 
