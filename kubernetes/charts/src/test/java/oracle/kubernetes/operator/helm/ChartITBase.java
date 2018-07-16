661ec1b84d3f656241c33245f21b78ac70193e34
Russell Gold <russell.gold@oracle.com>
2018-07-16 17:19:16 -0400

    Initial test for helm chart processing

---
+++ kubernetes/charts/src/test/java/oracle/kubernetes/operator/helm/ChartITBase.java
@@ -0,0 +1,25 @@
+// Copyright 2018, Oracle Corporation and/or its affiliates.  All rights reserved.
+// Licensed under the Universal Permissive License v 1.0 as shown at
+// http://oss.oracle.com/licenses/upl.
+
+package oracle.kubernetes.operator.helm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("SameParameterValue")
+class ChartITBase {
+  private static List<ProcessedChart> charts = new ArrayList<>();
+
+  ProcessedChart getChart(String chartName, UpdateValues updater) {
+    for (ProcessedChart chart : charts) {
+      if (chart.matches(chartName, updater)) {
+        return chart;
+      }
+    }
+
+    ProcessedChart chart = new ProcessedChart(chartName, updater);
+    charts.add(chart);
+    return chart;
+  }
+}
