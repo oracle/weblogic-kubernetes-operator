208f050254aebedddd80c1edd6ffa0b0d863277c
doxiao <dongbo.xiao@oracle.com>
2018-08-28 08:24:50 -0700

    Only test invalid characters in non-helm cases
    Signed-off-by: doxiao <dongbo.xiao@oracle.com>

--- src/integration-tests/bash/run.sh
+++ src/integration-tests/bash/run.sh
@@ -3131,8 +3131,16 @@ function test_suite {
 
     #          DOM_KEY  OP_KEY  NAMESPACE DOMAIN_UID STARTUP_CONTROL WL_CLUSTER_NAME WL_CLUSTER_TYPE  MS_BASE_NAME   ADMIN_PORT ADMIN_WLST_PORT ADMIN_NODE_PORT MS_PORT LOAD_BALANCER_WEB_PORT LOAD_BALANCER_DASHBOARD_PORT
     dom_define domain1  oper1   default   domain1    AUTO            cluster-1       DYNAMIC          managed-server 7001       30012           30701           8001    30305                  30315
-    dom_define domain2  oper1   default   domain2    AUTO            cluster-1       DYNAMIC          Managed_Server 7011       30031           30702           8021    30306                  30316
-    dom_define domain3  oper1   test1     domain3    AUTO            cluster_1       DYNAMIC          managed-Server 7021       30041           30703           8031    30307                  30317
+
+    # TODO: we need to figure out how to support invalid characters in the helm use cases
+    # for now, invalid characters are only tested in the none helm cases
+    if [ "$USE_HELM" = "true" ]; then
+      dom_define domain2  oper1   default   domain2    AUTO            cluster-1       DYNAMIC          managed_server 7011       30031           30702           8021    30306                  30316
+      dom_define domain3  oper1   test1     domain3    AUTO            cluster-1       DYNAMIC          managed-server 7021       30041           30703           8031    30307                  30317
+    else
+      dom_define domain2  oper1   default   domain2    AUTO            cluster-1       DYNAMIC          Managed_Server 7011       30031           30702           8021    30306                  30316
+      dom_define domain3  oper1   test1     domain3    AUTO            cluster_1       DYNAMIC          managed-Server 7021       30041           30703           8031    30307                  30317
+    fi
     dom_define domain4  oper2   test2     domain4    AUTO            cluster-1       CONFIGURED       managed-server 7041       30051           30704           8041    30308                  30318
     dom_define domain5  oper1   default   domain5    ADMIN           cluster-1       DYNAMIC          managed-server 7051       30061           30705           8051    30309                  30319
     dom_define domain6  oper1   default   domain6    AUTO            cluster-1       DYNAMIC          managed-server 7061       30071           30706           8061    30310                  30320
