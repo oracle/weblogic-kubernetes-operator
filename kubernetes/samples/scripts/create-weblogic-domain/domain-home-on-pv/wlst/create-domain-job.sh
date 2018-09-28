bdd5b00ea276fd9e408b6756282fe045e4bece91
doxiao <dongbo.xiao@oracle.com>
2018-09-28 10:08:54 -0700

    Minor correction in create-domain-job.sh
    Signed-off-by: doxiao <dongbo.xiao@oracle.com>

--- kubernetes/samples/scripts/create-weblogic-domain/domain-home-on-pv/wlst/create-domain-job.sh
+++ kubernetes/samples/scripts/create-weblogic-domain/domain-home-on-pv/wlst/create-domain-job.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Copyright 2017, 2018, Oracle Corporation and/or its affiliates.  All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
-#!/bin/bash
 #
 
 # Include common utility functions
