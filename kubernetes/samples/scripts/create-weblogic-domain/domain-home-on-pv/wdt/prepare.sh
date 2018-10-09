c6d41a989c41b3e5b9e807e089ce73969d6d9ed0
doxiao <dongbo.xiao@oracle.com>
2018-10-09 06:50:13 -0700

    Initial version of wdt scripts
    Signed-off-by: doxiao <dongbo.xiao@oracle.com>

---
+++ kubernetes/samples/scripts/create-weblogic-domain/domain-home-on-pv/wdt/prepare.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Copyright 2018, Oracle Corporation and/or its affiliates.  All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+#
+
+# Perform preparation based on the specified cluster type
+
+function usage {
+  echo usage: ${script} -t clusterTpe -i file [-h]
+  echo "  -i Directory, must be specified."
+  echo "  -t Cluster type, CONFIGURED or DYNAMIC, default is DYNAMIC."
+  echo "  -h Help"
+  exit $1
+}
+
+
+while getopts "hi:t:" opt; do
+  case $opt in
+    i) externalFilesTmpDir="${OPTARG}"
+    ;;
+    t) clusterType="${OPTARG}"
+    ;;
+    h) usage 0
+    ;;
+    *) usage 1
+    ;;
+  esac
+done
+
+echo clusterType = $clusterType
+
+if [ "${clusterType}" = "CONFIGURED" ]; then
+  source_model=${externalFilesTmpDir}/wdt_model_configured.yaml
+else
+  source_model=${externalFilesTmpDir}/wdt_model_dynamic.yaml
+fi
+
+echo model file $source_model
+
+cp $source_model ${externalFilesTmpDir}/wdt_model.yaml
+
