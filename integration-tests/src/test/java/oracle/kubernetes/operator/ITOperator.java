c8a7534f48d565db2a9d35d0bd09122f88d9d960
Lily He <lily.he@oracle.com>
2018-10-09 23:03:24 -0700

    use @Ignore to disable the apache test

--- integration-tests/src/test/java/oracle/kubernetes/operator/ITOperator.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/ITOperator.java
@@ -13,6 +13,7 @@ import org.junit.AfterClass;
 import org.junit.Assume;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
@@ -254,7 +255,8 @@ public class ITOperator extends BaseTest {
     logger.info("SUCCESS - test9CreateDomainOnExistingDir");
   }
 
-  // @Test
+  @Test
+  @Ignore
   public void testACreateDomainApacheLB() throws Exception {
     Assume.assumeFalse(
         System.getenv("QUICKTEST") != null && System.getenv("QUICKTEST").equalsIgnoreCase("true"));
