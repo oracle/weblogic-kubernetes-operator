14e90cf7a25b279c272cc3db789c436b27106f06
Ryan Eberhard <ryan.eberhard@oracle.com>
2018-10-19 17:00:08 -0400

    Format files

--- integration-tests/src/test/java/oracle/kubernetes/operator/utils/Operator.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/utils/Operator.java
@@ -324,7 +324,8 @@ public class Operator {
     }
 
     if (System.getenv("IMAGE_PULL_SECRET_OPERATOR") != null) {
-      operatorMap.put("imagePullSecrets", "{ name: " + System.getenv("IMAGE_PULL_SECRET_OPERATOR") + " }");
+      operatorMap.put(
+          "imagePullSecrets", "{ name: " + System.getenv("IMAGE_PULL_SECRET_OPERATOR") + " }");
       // create docker registry secrets
       TestUtils.createDockerRegistrySecret(
           System.getenv("IMAGE_PULL_SECRET_OPERATOR"),
