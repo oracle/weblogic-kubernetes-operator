303958acad44a1bf69733f76a4ce892ec17db438
Ryan Eberhard <ryan.eberhard@oracle.com>
2018-10-26 14:00:53 -0400

    Tag 19.1.0.0 images

--- integration-tests/src/test/resources/setupenv.sh
+++ integration-tests/src/test/resources/setupenv.sh
@@ -67,6 +67,9 @@ function pull_tag_images {
     docker pull wlsldi-v2.docker.oraclecorp.com/store-weblogic-12.2.1.3:latest
     docker tag wlsldi-v2.docker.oraclecorp.com/store-weblogic-12.2.1.3:latest store/oracle/weblogic:12.2.1.3
 
+    docker pull wlsldi-v2.docker.oraclecorp.com/weblogic:19.1.0.0
+    docker tag wlsldi-v2.docker.oraclecorp.com/weblogic:19.1.0.0 store/oracle/weblogic:19.1.0.0
+
     docker pull wlsldi-v2.docker.oraclecorp.com/store-serverjre-8:latest
     docker tag wlsldi-v2.docker.oraclecorp.com/store-serverjre-8:latest store/oracle/serverjre:8
 
