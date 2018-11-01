1569ce069f1abf39cc0c7d4bc226bb8a278f1ac4
anthony_lai <anthony.lai@oracle.com>
2018-11-01 14:28:56 -0700

    extract UpdateDynamicClusterStep from WlsRetriever, and replace usage of WlsRetriever.readConfig() in RestBackendImpl

--- operator/src/main/java/oracle/kubernetes/operator/wlsconfig/WlsRetriever.java
+++ operator/src/main/java/oracle/kubernetes/operator/wlsconfig/WlsRetriever.java
@@ -152,66 +152,6 @@ public class WlsRetriever {
     }
   }
 
-  /** Step for updating the cluster size of a WebLogic dynamic cluster */
-  static final class UpdateDynamicClusterStep extends Step {
-
-    final WlsClusterConfig wlsClusterConfig;
-    final int targetClusterSize;
-
-    /**
-     * Constructor
-     *
-     * @param wlsClusterConfig The WlsClusterConfig object for the WebLogic dynamic cluster to be
-     *     updated
-     * @param targetClusterSize The target dynamic cluster size
-     * @param next The next Step to be performed
-     */
-    public UpdateDynamicClusterStep(
-        WlsClusterConfig wlsClusterConfig, int targetClusterSize, Step next) {
-      super(next);
-      this.wlsClusterConfig = wlsClusterConfig;
-      this.targetClusterSize = targetClusterSize;
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public NextAction apply(Packet packet) {
-
-      String clusterName = wlsClusterConfig == null ? "null" : wlsClusterConfig.getClusterName();
-
-      if (wlsClusterConfig == null || !wlsClusterConfig.hasDynamicServers()) {
-        LOGGER.warning(MessageKeys.WLS_UPDATE_CLUSTER_SIZE_INVALID_CLUSTER, clusterName);
-      } else {
-        try {
-          LOGGER.info(MessageKeys.WLS_UPDATE_CLUSTER_SIZE_STARTING, clusterName, targetClusterSize);
-          HttpClient httpClient = (HttpClient) packet.get(HttpClient.KEY);
-          DomainPresenceInfo info = packet.getSPI(DomainPresenceInfo.class);
-
-          long startTime = System.currentTimeMillis();
-
-          String serviceURL = HttpClient.getServiceURL(info.getAdmin().getService().get());
-
-          boolean successful =
-              updateDynamicClusterSizeWithServiceURL(
-                  wlsClusterConfig, targetClusterSize, httpClient, serviceURL);
-
-          if (successful) {
-            LOGGER.info(
-                MessageKeys.WLS_CLUSTER_SIZE_UPDATED,
-                clusterName,
-                targetClusterSize,
-                (System.currentTimeMillis() - startTime));
-          } else {
-            LOGGER.warning(MessageKeys.WLS_UPDATE_CLUSTER_SIZE_FAILED, clusterName, null);
-          }
-        } catch (Throwable t) {
-          LOGGER.warning(MessageKeys.WLS_UPDATE_CLUSTER_SIZE_FAILED, clusterName, t);
-        }
-      }
-      return doNext(packet);
-    }
-  }
-
   static final class WithHttpClientStep extends Step {
     private final V1Service service;
 
@@ -398,39 +338,6 @@ public class WlsRetriever {
     }
     LOGGER.exiting(jsonResult);
     return jsonResult;
-  }
-
-  /**
-   * Static method to update the WebLogic dynamic cluster size configuration.
-   *
-   * @param wlsClusterConfig The WlsClusterConfig object of the WLS cluster whose cluster size needs
-   *     to be updated. The caller should make sure that the cluster is a dynamic cluster.
-   * @param targetClusterSize The target dynamic cluster size
-   * @param httpClient HttpClient object for issuing the REST request
-   * @param serviceURL service URL of the WebLogic admin server
-   * @return true if the request to update the cluster size is successful, false if it was not
-   *     successful
-   */
-  private static boolean updateDynamicClusterSizeWithServiceURL(
-      final WlsClusterConfig wlsClusterConfig,
-      final int targetClusterSize,
-      final HttpClient httpClient,
-      final String serviceURL) {
-    LOGGER.entering();
-
-    boolean result = false;
-    // Update the dynamic cluster size of the WebLogic cluster
-    String jsonResult =
-        httpClient
-            .executePostUrlOnServiceClusterIP(
-                wlsClusterConfig.getUpdateDynamicClusterSizeUrl(),
-                serviceURL,
-                wlsClusterConfig.getUpdateDynamicClusterSizePayload(targetClusterSize))
-            .getResponse();
-
-    result = wlsClusterConfig.checkUpdateDynamicClusterSizeJsonResult(jsonResult);
-    LOGGER.exiting(result);
-    return result;
   }
 
   /**
