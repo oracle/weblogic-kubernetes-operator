3dff0a0f226ca55634d13fd970e75c9a9b687661
Anthony Lai <anthony.lai@oracle.com>
2018-11-09 14:56:51 -0800

    use v2 Domain

--- operator/src/main/java/oracle/kubernetes/operator/helpers/ConfigMapHelper.java
+++ operator/src/main/java/oracle/kubernetes/operator/helpers/ConfigMapHelper.java
@@ -28,7 +28,7 @@ import oracle.kubernetes.operator.wlsconfig.WlsDomainConfig;
 import oracle.kubernetes.operator.work.NextAction;
 import oracle.kubernetes.operator.work.Packet;
 import oracle.kubernetes.operator.work.Step;
-import oracle.kubernetes.weblogic.domain.v1.Domain;
+import oracle.kubernetes.weblogic.domain.v2.Domain;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.joda.time.DateTime;
