fb6bdeb27535263d1a9531302350e2e0a3cc5044
vince.kraemer <vince.kraemer@oracle.com>
2018-11-08 19:56:52 -0800

    integrate the operator debug cm into the operator's livenessProbe

--- src/scripts/livenessProbe.sh
+++ src/scripts/livenessProbe.sh
@@ -2,12 +2,16 @@
 # Copyright 2017, Oracle Corporation and/or its affiliates.  All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 
+# if the livenessProbeSuccessOverride file is available, treat failures as success
+#
+RETVAL=$(test -f /operator/debug-config/livenessProbeSuccessOverride ; echo $?)
+
 test `find /operator/.alive -mmin -1`
 
 if(($?==0)); then
     exit 0
 else 
-    exit 1
+    exit $RETVAL
 fi
 
 
