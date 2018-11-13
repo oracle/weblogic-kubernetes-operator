75948f9d4397909f5f51defdd0bd7bfabe0c248b
Ryan Eberhard <ryan.eberhard@oracle.com>
2018-11-13 17:41:49 -0500

    Use server name rather than registry URL form

--- integration-tests/src/test/java/oracle/kubernetes/operator/utils/Domain.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/utils/Domain.java
@@ -799,7 +799,7 @@ public class Domain {
       // create docker registry secrets
       TestUtils.createDockerRegistrySecret(
           System.getenv("IMAGE_PULL_SECRET_WEBLOGIC"),
-          System.getenv("REPO_REGISTRY"),
+          System.getenv("REPO_SERVER"),
           System.getenv("REPO_USERNAME"),
           System.getenv("REPO_PASSWORD"),
           System.getenv("REPO_EMAIL"),
