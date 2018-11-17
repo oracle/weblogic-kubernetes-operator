8d2ea45c01052bd7150fd4c945f1d78ba65664e8
Tom Moreau <tom.moreau@oracle.com>
2018-11-17 09:14:05 -0500

    Debug wercker failure

--- kubernetes/src/test/java/oracle/kubernetes/operator/helm/CreateOperatorGeneratedFilesOptionalFeaturesEnabledIT.java
+++ kubernetes/src/test/java/oracle/kubernetes/operator/helm/CreateOperatorGeneratedFilesOptionalFeaturesEnabledIT.java
@@ -13,13 +13,13 @@ public class CreateOperatorGeneratedFilesOptionalFeaturesEnabledIT
 
   @BeforeClass
   public static void setup() throws Exception {
-try {
-    defineOperatorYamlFactory(new HelmOperatorYamlFactory());
-} catch (Exception e) {
-System.out.println("MOREAUT_DEBUG " + e);
-e.printStackTrace();
-throw e;
-}
+    try {
+      defineOperatorYamlFactory(new HelmOperatorYamlFactory());
+    } catch (Exception e) {
+      System.out.println("MOREAUT_DEBUG " + e);
+      e.printStackTrace();
+      throw e;
+    }
   }
 
   @Test
