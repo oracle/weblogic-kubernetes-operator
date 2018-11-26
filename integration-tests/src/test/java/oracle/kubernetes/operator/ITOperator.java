f9fc03c7e8cc470fa42169d3a5ef7f9b499aa758
Vanajakshi Mukkara <VANAJAKSHI.MUKKARA@ORACLE.COM>
2018-11-26 13:58:39 -0800

    kubectl create if domain created already

--- integration-tests/src/test/java/oracle/kubernetes/operator/ITOperator.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/ITOperator.java
@@ -160,9 +160,9 @@ public class ITOperator extends BaseTest {
       operator1 = TestUtils.createOperator(op1YamlFile);
     }
     if (domain1 == null) {
-    	domain1 = TestUtils.createDomain(domain1YamlFile);
+      domain1 = TestUtils.createDomain(domain1YamlFile);
     } else {
-    	domain1.create();
+      domain1.create();
     }
     logger.info("Checking if operator2 is running, if not creating");
     if (operator2 == null) {
