4dcf7717e4d22340fa61eacc40a80cb7bdfe2064
Lily He <lily.he@oracle.com>
2018-11-29 23:04:48 -0800

    fix formatting error

--- operator/src/test/java/oracle/kubernetes/operator/DomainPresenceTest.java
+++ operator/src/test/java/oracle/kubernetes/operator/DomainPresenceTest.java
@@ -4,10 +4,8 @@
 
 package oracle.kubernetes.operator;
 
-import static oracle.kubernetes.operator.DomainPresenceInfoMatcher.domain;
 import static oracle.kubernetes.operator.KubernetesConstants.DOMAIN_CONFIG_MAP_NAME;
 import static oracle.kubernetes.operator.LabelConstants.CHANNELNAME_LABEL;
-import static oracle.kubernetes.operator.LabelConstants.CLUSTERNAME_LABEL;
 import static oracle.kubernetes.operator.LabelConstants.CREATEDBYOPERATOR_LABEL;
 import static oracle.kubernetes.operator.LabelConstants.DOMAINUID_LABEL;
 import static oracle.kubernetes.operator.LabelConstants.SERVERNAME_LABEL;
@@ -16,7 +14,6 @@ import static oracle.kubernetes.operator.WebLogicConstants.READINESS_PROBE_NOT_R
 import static org.hamcrest.Matchers.anEmptyMap;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasEntry;
-import static org.hamcrest.Matchers.hasValue;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.Matchers.sameInstance;
