99ce4482ecf273bf35fe408de4b547ebc66e5b5e
doxiao <dongbo.xiao@oracle.com>
2018-12-04 18:01:51 -0800

    Better support for optional imagePullSecretName
    Signed-off-by: doxiao <dongbo.xiao@oracle.com>

--- kubernetes/samples/scripts/create-weblogic-domain/domain-home-on-pv/create-domain.sh
+++ kubernetes/samples/scripts/create-weblogic-domain/domain-home-on-pv/create-domain.sh
@@ -381,6 +381,7 @@ function createYamlFiles {
   cp ${dcrInput} ${dcrOutput}
   sed -i -e "s:%NAMESPACE%:$namespace:g" ${dcrOutput}
   sed -i -e "s:%WEBLOGIC_CREDENTIALS_SECRET_NAME%:${weblogicCredentialsSecretName}:g" ${dcrOutput}
+  sed -i -e "s:%WEBLOGIC_IMAGE_PULL_SECRET_PREFIX%:${imagePullSecretPrefix}:g" ${dcrOutput}
   sed -i -e "s:%DOMAIN_UID%:${domainUID}:g" ${dcrOutput}
   sed -i -e "s:%DOMAIN_NAME%:${domainName}:g" ${dcrOutput}
   sed -i -e "s:%DOMAIN_HOME%:${domainHome}:g" ${dcrOutput}
