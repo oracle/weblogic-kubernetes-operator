ee9f21df854d8b241bbbe0aa3613439211de32f2
Dongbo Xiao <dongbo.xiao@oracle.com>
2018-12-13 06:48:30 -0500

    Domain sample update and cleanup (#614)
    * Cleanup sample code and README plus update domain template for envs
    
    Signed-off-by: doxiao <dongbo.xiao@oracle.com>
    
    * Move envs to serverPod and serverStartStae to domain level
    
    Signed-off-by: doxiao <dongbo.xiao@oracle.com>
    
    * Move serverStartState back to admin server level to avoid empty adminServer element
    
    Signed-off-by: doxiao <dongbo.xiao@oracle.com>
    
    * Remove unused functions and update README
    
    Signed-off-by: doxiao <dongbo.xiao@oracle.com>
    
    * Minor edits to the README files
    
    Signed-off-by: doxiao <dongbo.xiao@oracle.com>
    
    * Remove only the generated files
    
    Signed-off-by: doxiao <dongbo.xiao@oracle.com>
    
    * Address a review comment
    
    Signed-off-by: doxiao <dongbo.xiao@oracle.com>
    
    * refactor the create domain scripts to share common code
    
    * Fix a merge error
    
    Signed-off-by: doxiao <dongbo.xiao@oracle.com>
    
    * Fix an error in commit 73a4df414a3ccd73ad79aae67f3a1e598bec8f96
    
    Signed-off-by: doxiao <dongbo.xiao@oracle.com>
    
    * Customize base image in domain-home-in-image sample
    
    Signed-off-by: doxiao <dongbo.xiao@oracle.com>
    
    * Correct the default base image
    
    Signed-off-by: doxiao <dongbo.xiao@oracle.com>
    
    * Fix a problem in domain home in image case
    
    Signed-off-by: doxiao <dongbo.xiao@oracle.com>
    
    * Minor correction
    
    Signed-off-by: doxiao <dongbo.xiao@oracle.com>
    
    * Revert "Minor correction"
    
    This reverts commit ec9ee5cd53f25933a7df2a84ebc372ebeab9d321.
    
    * Remove what is already in another PR
    
    Signed-off-by: doxiao <dongbo.xiao@oracle.com>

--- kubernetes/samples/scripts/create-weblogic-domain/domain-home-in-image/create-domain.sh
+++ kubernetes/samples/scripts/create-weblogic-domain/domain-home-in-image/create-domain.sh
@@ -81,51 +81,15 @@ fi
 # Function to initialize and validate the output directory
 # for the generated properties and yaml files for this domain.
 #
-function initAndValidateOutputDir {
+function initOutputDir {
   domainOutputDir="${outputDir}/weblogic-domains/${domainUID}"
   # Create a directory for this domain's output files
   mkdir -p ${domainOutputDir}
 
-  validateOutputDir \
-    ${domainOutputDir} \
-    ${valuesInputFile} \
-    create-domain-inputs.yaml \
-    domain.properties \
-    domain.yaml
-}
-
-#
-# Function to validate the domain secret
-#
-function validateDomainSecret {
-  # Verify the secret exists
-  validateSecretExists ${weblogicCredentialsSecretName} ${namespace}
-  failIfValidationErrors
-
-  # Verify the secret contains a username
-  SECRET=`kubectl get secret ${weblogicCredentialsSecretName} -n ${namespace} -o jsonpath='{.data}'| grep username: | wc | awk ' { print $1; }'`
-  if [ "${SECRET}" != "1" ]; then
-    validationError "The domain secret ${weblogicCredentialsSecretName} in namespace ${namespace} does contain a username"
-  fi
-
-  # Verify the secret contains a password
-  SECRET=`kubectl get secret ${weblogicCredentialsSecretName} -n ${namespace} -o jsonpath='{.data}'| grep password: | wc | awk ' { print $1; }'`
-  if [ "${SECRET}" != "1" ]; then
-    validationError "The domain secret ${weblogicCredentialsSecretName} in namespace ${namespace} does contain a password"
-  fi
-  failIfValidationErrors
-}
-
-#
-# Function to validate a kubernetes secret exists
-# $1 - the name of the secret
-# $2 - namespace
-function validateSecretExists {
-  echo "Checking to see if the secret ${1} exists in namespace ${2}"
-  local SECRET=`kubectl get secret ${1} -n ${2} | grep ${1} | wc | awk ' { print $1; }'`
-  if [ "${SECRET}" != "1" ]; then
-    validationError "The secret ${1} was not found in namespace ${2}"
-  fi
+  removeFileIfExists ${domainOutputDir}/${valuesInputFile}
+  removeFileIfExists ${domainOutputDir}/create-domain-inputs.yaml
+  removeFileIfExists ${domainOutputDir}/domain.properties
+  removeFileIfExists ${domainOutputDir}/domain.yaml
 }
 
 # try to execute docker to see whether docker is available
@@ -170,45 +134,9 @@ function initialize {
 
   failIfValidationErrors
 
-  # Parse the commonn inputs file
-  parseCommonInputs
+  validateCommonInputs
 
-  validateInputParamsSpecified \
-    adminServerName \
-    domainUID \
-    clusterName \
-    managedServerNameBase \
-    namespace \
-    t3PublicAddress \
-    version 
-
-  validateIntegerInputParamsSpecified \
-    adminPort \
-    configuredManagedServerCount \
-    initialManagedServerReplicas \
-    managedServerPort \
-    t3ChannelPort \
-    adminNodePort 
-
-  validateBooleanInputParamsSpecified \
-    productionModeEnabled \
-    exposeAdminT3Channel \
-    exposeAdminNodePort \
-    includeServerOutInPodLog
-
-  export requiredInputsVersion="create-weblogic-sample-domain-inputs-v1"
-  validateVersion 
-
-  validateDomainUid
-  validateNamespace
-  validateAdminServerName
-  validateManagedServerNameBase
-  validateClusterName
-  validateWeblogicCredentialsSecretName
-  initAndValidateOutputDir
-  validateServerStartPolicy
-  validateClusterType
-  failIfValidationErrors
+  initOutputDir
 
   getDockerSample
 }
@@ -347,43 +275,10 @@ function printSummary {
   echo "  ${domainOutputDir}/create-domain-inputs.yaml"
   echo "  ${domainPropertiesOutput}"
   echo "  ${dcrOutput}"
+  echo ""
+  echo "Completed"
 }
 
-#
-# Function to create the domain resource
-#
-function createDomainResource {
-  pwd
-  kubectl apply -f ${dcrOutput}
-  DCR_AVAIL=`kubectl get domain -n ${namespace} | grep ${domainUID} | wc | awk ' { print $1; } '`
-  if [ "${DCR_AVAIL}" != "1" ]; then
-    fail "The domain resource ${domainUID} was not found"
-  fi
-}
-
-#
-# Perform the following sequence of steps to create a domain
-#
-
-# Setup the environment for running this script and perform initial validation checks
-initialize
-
-# Generate the properties and yaml files for creating the domain
-createFiles
-
-# Check that the domain secret exists and contains the required elements
-validateDomainSecret
-
-# Create the WebLogic domain
-createDomainHome
-
-if [ "${executeIt}" = true ]; then
-  createDomainResource
-fi
-
-# Print a summary
-printSummary
-
-echo 
-echo Completed
+# Perform the sequence of steps to create a domain
+createDomain
 
