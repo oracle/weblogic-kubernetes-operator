5e3f23257978916c6b9a655d462c68e2e5116215
Ryan Eberhard <ryan.eberhard@oracle.com>
2018-12-13 11:56:44 -0500

    Revert "Suppress socket read timeout for watches"
    This reverts commit 2e2c9f8fb359d3dc7aa2499197e821e153e56acd.

--- operator/src/main/java/oracle/kubernetes/operator/calls/AsyncRequestStep.java
+++ operator/src/main/java/oracle/kubernetes/operator/calls/AsyncRequestStep.java
@@ -110,7 +110,6 @@ public class AsyncRequestStep<T> extends Step {
 
     AtomicBoolean didResume = new AtomicBoolean(false);
     ApiClient client = helper.take();
-    client.getHttpClient().setReadTimeout(timeoutSeconds, TimeUnit.SECONDS);
     return doSuspend(
         (fiber) -> {
           ApiCallback<T> callback =
