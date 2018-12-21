31f32572ac9043f220a3a3aea346b8b46c8f93ee
Russell Gold <russell.gold@oracle.com>
2018-12-21 14:10:38 -0500

    Add logging to explain pod recycling

--- operator/src/main/java/oracle/kubernetes/operator/helpers/PodStepContext.java
+++ operator/src/main/java/oracle/kubernetes/operator/helpers/PodStepContext.java
@@ -434,6 +434,7 @@ public abstract class PodStepContext implements StepContextConstants {
         logPodExists();
         return doNext(packet);
       } else {
+        LOGGER.info(MessageKeys.CYCLING_POD, currentPod, getPodModel());
         return doNext(replaceCurrentPod(getNext()), packet);
       }
     }
