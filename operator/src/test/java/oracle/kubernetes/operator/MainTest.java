2a2420d47313741f0a1774c781b480cabd6b0dcf
Ryan Eberhard <ryan.eberhard@oracle.com>
2018-12-22 08:34:29 -0500

    Resolve bad merge

--- operator/src/test/java/oracle/kubernetes/operator/MainTest.java
+++ operator/src/test/java/oracle/kubernetes/operator/MainTest.java
@@ -4,10 +4,7 @@
 
 package oracle.kubernetes.operator;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import io.kubernetes.client.models.V1ObjectMeta;
