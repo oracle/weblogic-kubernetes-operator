d40a25d82557f5daa0d7c4e89bfffe778af1ca91
Vanajakshi Mukkara <VANAJAKSHI.MUKKARA@ORACLE.COM>
2019-01-09 13:23:56 -0800

    changes for domain-in-image usecase

--- integration-tests/src/test/resources/callpyscript.sh
+++ integration-tests/src/test/resources/callpyscript.sh
@@ -3,7 +3,7 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 
 ARG="$*"
-ENVSCRIPT=`find /shared -name setDomainEnv.sh -print`
+ENVSCRIPT=`find /shared . -name setDomainEnv.sh -print`
 echo Sourcing $ENVSCRIPT
 . $ENVSCRIPT || exit 1
 echo "$@"
