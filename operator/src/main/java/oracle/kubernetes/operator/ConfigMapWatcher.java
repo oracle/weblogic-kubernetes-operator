b2e3a8f82a9b81342dfbf40c2daef2e8ba3c40d6
Ryan Eberhard <ryan.eberhard@oracle.com>
2019-01-09 13:01:17 -0500

    Remove unnecessary logging

--- operator/src/main/java/oracle/kubernetes/operator/ConfigMapWatcher.java
+++ operator/src/main/java/oracle/kubernetes/operator/ConfigMapWatcher.java
@@ -1,4 +1,4 @@
-// Copyright 2018, Oracle Corporation and/or its affiliates.  All rights reserved.
+// Copyright 2018, 2019, Oracle Corporation and/or its affiliates.  All rights reserved.
 // Licensed under the Universal Permissive License v 1.0 as shown at
 // http://oss.oracle.com/licenses/upl.
 
@@ -12,7 +12,6 @@ import oracle.kubernetes.operator.TuningParameters.WatchTuning;
 import oracle.kubernetes.operator.builders.WatchBuilder;
 import oracle.kubernetes.operator.builders.WatchI;
 import oracle.kubernetes.operator.watcher.WatchListener;
-import org.joda.time.DateTime;
 
 /**
  * This class handles ConfigMap watching. It receives config map change events and sends them into
@@ -46,10 +45,6 @@ public class ConfigMapWatcher extends Watcher<V1ConfigMap> {
 
   @Override
   public WatchI<V1ConfigMap> initiateWatch(WatchBuilder watchBuilder) throws ApiException {
-    // TEST
-    System.out.println(
-        "****3: ConfigMapWatcher.initiateWatch, ns=" + ns + ", time=" + DateTime.now());
-
     return watchBuilder
         .withLabelSelector(LabelConstants.CREATEDBYOPERATOR_LABEL)
         .createConfigMapWatch(ns);
