80acc2e462279b5e9fd2b7852e0a03634a292132
Marina <marina.kogan@oracle.com>
2019-01-23 15:03:28 -0800

    removed code for building terraform oci provider, added copiright info, corrected typos

--- kubernetes/samples/scripts/terraform/provider.tf
+++ kubernetes/samples/scripts/terraform/provider.tf
@@ -1,6 +1,8 @@
 /*
+ * Copyright 2018, 2019, Oracle Corporation and/or its affiliates.  All rights reserved.
+ * Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
  * This example file shows how to configure the oci provider to target the a single region.
- */
+*/
 
 // These variables would commonly be defined as environment variables or sourced in a .env file
 variable "tenancy_ocid" {}
@@ -10,6 +12,7 @@ variable "private_key_path" {}
 variable "region" { default = "us-phoenix-1" }
 
 provider "oci" {
+  version          = ">= 3.0.0"
   region = "${var.region}"
   tenancy_ocid = "${var.tenancy_ocid}"
   user_ocid = "${var.user_ocid}"
