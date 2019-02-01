05d9f82d89b0fa4a98852513ded37f7207c69c87
jchermos <jchermos@JCHERMOS-LAP.oradev.oraclecorp.com>
2019-01-31 18:55:26 -0600

    move getT3Client.sh code to setupenv.sh

--- integration-tests/src/test/resources/getT3Client.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Copyright 2019, Oracle Corporation and/or its affiliates.  All rights reserved.
-# Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
-
-SCRIPTPATH="$( cd "$(dirname "$0")" > /dev/null 2>&1 ; pwd -P )"
-
-id=$(docker create store/oracle/weblogic:19.1.0.0)
-docker cp $id:/u01/oracle/wlserver/server/lib/wlthint3client.jar $SCRIPTPATH
-docker rm -v $id
