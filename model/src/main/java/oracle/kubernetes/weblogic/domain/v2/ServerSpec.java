895f0b4ddd830d3c4a8fa8c3da2d2f9433abccc6
Mark Nelson <mark.x.nelson@oracle.com>
2019-02-21 09:39:58 -0500

    fix formatting error

--- model/src/main/java/oracle/kubernetes/weblogic/domain/v2/ServerSpec.java
+++ model/src/main/java/oracle/kubernetes/weblogic/domain/v2/ServerSpec.java
@@ -8,8 +8,8 @@ import static oracle.kubernetes.operator.KubernetesConstants.ALWAYS_IMAGEPULLPOL
 import static oracle.kubernetes.operator.KubernetesConstants.DEFAULT_IMAGE;
 import static oracle.kubernetes.operator.KubernetesConstants.IFNOTPRESENT_IMAGEPULLPOLICY;
 
-import io.kubernetes.client.models.V1EnvVar;
 import io.kubernetes.client.models.V1Container;
+import io.kubernetes.client.models.V1EnvVar;
 import io.kubernetes.client.models.V1LocalObjectReference;
 import io.kubernetes.client.models.V1PodSecurityContext;
 import io.kubernetes.client.models.V1ResourceRequirements;
