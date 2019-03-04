149bd93fa716694b2a2b386412872de7e8c21b1d
Vanajakshi Mukkara <VANAJAKSHI.MUKKARA@ORACLE.COM>
2019-03-04 12:28:08 -0800

    adding /scratch/vmukkara/depot/dev/auto_download/x86_64/jdk180131b11/jdk1.8.0_131/bin to PATH on wercker

--- integration-tests/src/test/resources/setupenv.sh
+++ integration-tests/src/test/resources/setupenv.sh
@@ -87,8 +87,8 @@ function pull_tag_images {
 	    echo "secret $IMAGE_PULL_SECRET_WEBLOGIC was not created successfully"
 	    exit 1
 	  fi
-	  docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-   	  docker pull $IMAGE_NAME_WEBLOGIC:$IMAGE_TAG_WEBLOGIC
+	  # docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+   	  # docker pull $IMAGE_NAME_WEBLOGIC:$IMAGE_TAG_WEBLOGIC
   fi
   set -x
   echo "Pull and tag the images we need"
