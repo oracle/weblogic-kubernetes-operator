dded6d57f91682e6267ad7fd8984e1a337f89434
Mark Nelson <mark.x.nelson@oracle.com>
2019-03-05 16:12:19 -0500

    update stopServer to address bugs:

--- operator/src/main/resources/scripts/stopServer.sh
+++ operator/src/main/resources/scripts/stopServer.sh
@@ -13,7 +13,7 @@ SCRIPTPATH="$( cd "$(dirname "$0")" > /dev/null 2>&1 ; pwd -P )"
 source ${SCRIPTPATH}/traceUtils.sh
 [ $? -ne 0 ] && echo "Error: missing file ${SCRIPTPATH}/traceUtils.sh" && exit 1
 
-trace "Stop server ${SERVER_NAME}"
+trace "Stop server ${SERVER_NAME}" &>> /u01/oracle/stopserver.out
 
 checkEnv SERVER_NAME || exit 1
 
@@ -24,16 +24,28 @@ fi
 
 [ ! -f "${SCRIPTPATH}/wlst.sh" ]             && trace "Error: missing file '${SCRIPTPATH}/wlst.sh'."             && exit 1
 
-${SCRIPTDIR}/wlst.sh /weblogic-operator/scripts/stop-server.py 
+trace "Before stop-server.py [${SERVER_NAME}] ${SCRIPTDIR}"  &>> /u01/oracle/stopserver.out
+${SCRIPTPATH}/wlst.sh /weblogic-operator/scripts/stop-server.py  &>> /u01/oracle/stopserver.out
+trace "After stop-server.py" &>> /u01/oracle/stopserver.out
 
-# Return status of 2 means failed to stop a server through the NodeManager.
-# Look to see if there is a server process that can be killed.
-if [ $? -eq 2 ]; then
-  pid=$(jps -v | grep '[D]weblogic.Name=${SERVER_NAME}' | awk '{print $1}')
-  if [ ! -z $pid ]; then
-    echo "Killing the server process $pid"
-    kill -15 $pid
-  fi
+# at this point node manager should have terminated the server
+# but let's try looking for the server process and
+# kill the server if the process still exists, 
+# just in case NodeManager failed to stop it
+pid=$(jps -v | grep "[D]weblogic.Name=${SERVER_NAME}" | awk '{print $1}')
+if [ ! -z $pid ]; then
+  echo "Killing the server process $pid"  &>> /u01/oracle/stopserver.out
+  kill -15 $pid
 fi
 
+# stop node manager process
+#
+trace "Stopping NodeManager"  &>> /u01/oracle/stopserver.out
+pid=$(jps | grep "NodeManager" | awk '{print $1}')
+trace "PID=[${pid}]"  &>> /u01/oracle/stopserver.out
+if [ ! -z $pid ]; then
+  echo "Killing NodeManager process $pid"  &>> /u01/oracle/stopserver.out
+  kill -15 $pid
+fi
 
+trace "Exit script"  &>> /u01/oracle/stopserver.out
