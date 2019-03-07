941f27f0b5a90f2624b05231f40098aead010885
Lenny Phan <lenny.phan@oracle.com>
2019-03-06 17:58:13 -0800

    Refactor get certificate contents to be shared between REST and Admin pod configuration

--- operator/src/main/java/oracle/kubernetes/operator/rest/RestConfigImpl.java
+++ operator/src/main/java/oracle/kubernetes/operator/rest/RestConfigImpl.java
@@ -4,26 +4,21 @@
 
 package oracle.kubernetes.operator.rest;
 
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Collection;
+import oracle.kubernetes.operator.OperatorConstants;
+import oracle.kubernetes.operator.TuningParameters;
 import oracle.kubernetes.operator.logging.LoggingFacade;
 import oracle.kubernetes.operator.logging.LoggingFactory;
 import oracle.kubernetes.operator.rest.backend.RestBackend;
 
 /** RestConfigImpl provides the WebLogic Operator REST api configuration. */
-public class RestConfigImpl implements RestConfig {
+public class RestConfigImpl implements RestConfig, OperatorConstants {
 
   private static LoggingFacade LOGGER = LoggingFactory.getLogger("Operator", "Operator");
 
   private final String principal;
   private final Collection<String> targetNamespaces;
 
-  private static final String OPERATOR_DIR = "/operator/";
-  private static final String INTERNAL_REST_IDENTITY_DIR = OPERATOR_DIR + "internal-identity/";
-  private static final String INTERNAL_CERTIFICATE =
-      INTERNAL_REST_IDENTITY_DIR + "internalOperatorCert";
   private static final String INTERNAL_CERTIFICATE_KEY =
       INTERNAL_REST_IDENTITY_DIR + "internalOperatorKey";
   private static final String EXTERNAL_REST_IDENTITY_DIR = OPERATOR_DIR + "external-identity/";
@@ -63,12 +58,12 @@ public class RestConfigImpl implements RestConfig {
 
   @Override
   public String getOperatorExternalCertificateData() {
-    return getCertificate(EXTERNAL_CERTIFICATE);
+    return TuningParameters.getInstance().getFileContents(EXTERNAL_CERTIFICATE);
   }
 
   @Override
   public String getOperatorInternalCertificateData() {
-    return getCertificate(INTERNAL_CERTIFICATE);
+    return TuningParameters.getInstance().getFileContents(INTERNAL_CERTIFICATE);
   }
 
   @Override
@@ -109,47 +104,13 @@ public class RestConfigImpl implements RestConfig {
     return result;
   }
 
-  // path - a file containing a base64 encoded string containing the operator's cert in pem format
-  private String getCertificate(String path) {
-    LOGGER.entering(path);
-    // in pem format
-    String result = null;
-    if (checkFileExists(path)) {
-      try {
-        result = new String(Files.readAllBytes(Paths.get(path)));
-      } catch (Throwable t) {
-        LOGGER.warning("Can't read " + path, t);
-      }
-    }
-    // do not include the certificate data in the log message
-    LOGGER.exiting();
-    return result;
-  }
-
   // path - a file containing the operator's private key in pem format (cleartext)
   private String getKey(String path) {
     LOGGER.entering(path);
-    if (!checkFileExists(path)) {
+    if (!TuningParameters.getInstance().checkFileExists(path)) {
       path = null;
     }
     LOGGER.exiting(path);
     return path;
-  }
-
-  private boolean checkFileExists(String path) {
-    LOGGER.entering(path);
-    File f = new File(path);
-    boolean result = false;
-    if (f.exists()) {
-      if (f.isFile()) {
-        result = true;
-      } else {
-        LOGGER.warning(path + " is not a file");
-      }
-    } else {
-      LOGGER.warning(path + " does not exist");
-    }
-    LOGGER.exiting(result);
-    return result;
   }
 }
