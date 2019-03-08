546615877b4201ac53acd78e5007e4d99d079a82
Ryan Eberhard <ryan.eberhard@oracle.com>
2019-03-08 07:29:04 -0500

    A few extra unintentional changes reverted

--- model/src/test/java/oracle/kubernetes/weblogic/domain/model/BaseConfigurationTestBase.java
+++ model/src/test/java/oracle/kubernetes/weblogic/domain/model/BaseConfigurationTestBase.java
@@ -2,10 +2,7 @@
 // Licensed under the Universal Permissive License v 1.0 as shown at
 // http://oss.oracle.com/licenses/upl.
 
-package oracle.kubernetes.weblogic.domain.model; // Copyright 2018, Oracle Corporation and/or its
-// affiliates.  All rights reserved.
-// Licensed under the Universal Permissive License v 1.0 as shown at
-// http://oss.oracle.com/licenses/upl.
+package oracle.kubernetes.weblogic.domain.model;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.not;
