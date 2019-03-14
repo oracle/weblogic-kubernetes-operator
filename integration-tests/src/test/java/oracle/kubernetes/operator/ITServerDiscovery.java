eda1a2c8d4ea200c352f1bfae2ac010210317c6a
Vanajakshi Mukkara <VANAJAKSHI.MUKKARA@ORACLE.COM>
2019-03-14 12:44:21 -0700

    fix ITServerDiscovery for werkcer

--- integration-tests/src/test/java/oracle/kubernetes/operator/ITServerDiscovery.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/ITServerDiscovery.java
@@ -124,31 +124,12 @@ public class ITServerDiscovery extends BaseTest {
    */
   @AfterClass
   public static void staticUnPrepare() throws Exception {
+    Assume.assumeFalse(QUICKTEST);
     logger.info("+++++++++++++++++++++++++++++++++---------------------------------+");
     logger.info("BEGIN");
     logger.info("Run once, release cluster lease");
 
-    StringBuffer cmd =
-        new StringBuffer("export RESULT_ROOT=$RESULT_ROOT && export PV_ROOT=$PV_ROOT && ");
-    cmd.append(BaseTest.getProjectRoot())
-        .append("/integration-tests/src/test/resources/statedump.sh");
-    logger.info("Running " + cmd);
-
-    ExecResult result = ExecCommand.exec(cmd.toString());
-    if (result.exitValue() == 0) logger.info("Executed statedump.sh " + result.stdout());
-    else
-      logger.info("Execution of statedump.sh failed, " + result.stderr() + "\n" + result.stdout());
-
-    if (domain != null && !SMOKETEST) domain.shutdownUsingServerStartPolicy();
-
-    if (JENKINS) {
-      cleanup();
-    }
-
-    if (getLeaseId() != "") {
-      logger.info("Release the k8s cluster lease");
-      TestUtils.releaseLease(getProjectRoot(), getLeaseId());
-    }
+    tearDown();
 
     logger.info("SUCCESS");
   }
