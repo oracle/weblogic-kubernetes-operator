9bbdda241eb01ef29b6f9a40785ec1eee334769a
Vanajakshi Mukkara <VANAJAKSHI.MUKKARA@ORACLE.COM>
2019-03-14 13:36:06 -0700

    adding renew lease in test

--- integration-tests/src/test/java/oracle/kubernetes/operator/ITOperator.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/ITOperator.java
@@ -87,6 +87,7 @@ public class ITOperator extends BaseTest {
       domain = TestUtils.createDomain(DOMAINONPV_WLST_YAML);
       domain.verifyDomainCreated();
       testBasicUseCases(domain);
+      TestUtils.renewK8sClusterLease(getProjectRoot(), getLeaseId());
       testAdvancedUseCasesForADomain(operator1, domain);
 
       if (!SMOKETEST) domain.testWlsLivenessProbe();
