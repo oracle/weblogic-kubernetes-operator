01afa3ab922718c6520cfc6ed2e2c99d33beb843
Marina <marina.kogan@oracle.com>
2019-03-16 15:53:27 -0700

    added Operator/Domain interaction usability testcases

--- integration-tests/src/test/java/oracle/kubernetes/operator/utils/Operator.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/utils/Operator.java
@@ -311,9 +311,9 @@ public class Operator {
         .append(" && helm upgrade ")
         .append(operatorMap.get("releaseName"))
         .append(" kubernetes/charts/weblogic-operator ")
-        .append(" --set ")
+        .append(" --set \"")
         .append(upgradeSet)
-        .append(" --reuse-values ")
+        .append("\" --reuse-values ")
         .append(" --wait --timeout 60");
     logger.info("Running " + cmd);
     ExecResult result = ExecCommand.exec(cmd.toString());
@@ -322,6 +322,22 @@ public class Operator {
     }
     String outputStr = result.stdout().trim();
     logger.info("Command returned " + outputStr);
+  }
+
+  public String getHelmValues() throws Exception {
+    StringBuffer cmd = new StringBuffer("cd ");
+    cmd.append(BaseTest.getProjectRoot())
+        .append(" && helm get values ")
+        .append(operatorMap.get("releaseName"));
+
+    logger.info("Running " + cmd);
+    ExecResult result = ExecCommand.exec(cmd.toString());
+    if (result.exitValue() != 0) {
+      reportHelmFailure(cmd.toString(), result);
+    }
+    String outputStr = result.stdout().trim();
+    logger.info("Command returned " + outputStr);
+    return outputStr;
   }
 
   private void reportHelmFailure(String cmd, ExecResult result) throws Exception {
