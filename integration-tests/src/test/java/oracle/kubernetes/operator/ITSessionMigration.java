d4d239806789c4aa2791dc376b1485dcfa7753cb
huiling_zhao <huiling.zhao@oracle.com>
2019-03-20 15:21:55 -0700

    made changes based on comments from co-workers

--- integration-tests/src/test/java/oracle/kubernetes/operator/ITSessionMigration.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/ITSessionMigration.java
@@ -1,4 +1,4 @@
-// Copyright 2018, Oracle Corporation and/or its affiliates.  All rights reserved.
+// Copyright 2019, Oracle Corporation and/or its affiliates.  All rights reserved.
 // Licensed under the Universal Permissive License v 1.0 as shown at
 // http://oss.oracle.com/licenses/upl.
 
@@ -17,7 +17,6 @@ import org.junit.AfterClass;
 import org.junit.Assume;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
@@ -29,16 +28,6 @@ import org.junit.runners.MethodSorters;
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ITSessionMigration extends BaseTest {
-
-  // property file used to customize operator properties for operator inputs yaml
-  private static String operatorYmlFile = "operator1.yaml";
-
-  // file used to customize domain properties for domain, PV and LB inputs yaml
-  private static String domainonpvwlstFile = "domainonpvwlst.yaml";
-
-  // property file used to configure constants for integration tests
-  private static String appPropsFile = "OperatorIT.properties";
-
   private static final String TESTAPPNAME = "httpsessionreplication";
   private static Map<String, String> httpAttrMap;
 
@@ -46,29 +35,6 @@ public class ITSessionMigration extends BaseTest {
 
   private static Operator operator;
   private static Domain domain;
-
-  private static boolean QUICKTEST;
-  private static boolean SMOKETEST;
-  private static boolean JENKINS;
-  private static boolean INGRESSPERDOMAIN = true;
-
-  // Set QUICKTEST env var to true to run a small subset of tests.
-  // Set SMOKETEST env var to true to run an even smaller subset
-  // of tests, plus leave domain1 up and running when the test completes.
-  // set INGRESSPERDOMAIN to false to create LB's ingress by kubectl yaml file
-  static {
-    QUICKTEST =
-        System.getenv("QUICKTEST") != null && System.getenv("QUICKTEST").equalsIgnoreCase("true");
-    SMOKETEST =
-        System.getenv("SMOKETEST") != null && System.getenv("SMOKETEST").equalsIgnoreCase("true");
-    if (SMOKETEST) QUICKTEST = true;
-    if (System.getenv("JENKINS") != null) {
-      JENKINS = new Boolean(System.getenv("JENKINS")).booleanValue();
-    }
-    if (System.getenv("INGRESSPERDOMAIN") != null) {
-      INGRESSPERDOMAIN = new Boolean(System.getenv("INGRESSPERDOMAIN")).booleanValue();
-    }
-  }
 
   /**
    * This method gets called only once before any of the test methods are executed. It does the
@@ -84,18 +50,18 @@ public class ITSessionMigration extends BaseTest {
   public static void staticPrepare() throws Exception {
     if (!QUICKTEST) {
       // initialize test properties and create the directories
-      initialize(appPropsFile);
+      initialize(APP_PROPS_FILE);
 
       // create operator1
       if (operator == null) {
         logger.info("Creating Operator & waiting for the script to complete execution");
-        operator = TestUtils.createOperator(operatorYmlFile);
+        operator = TestUtils.createOperator(OPERATOR1_YAML);
       }
 
       // create domain
       if (domain == null) {
         logger.info("Creating WLS Domain & waiting for the script to complete execution");
-        domain = TestUtils.createDomain(domainonpvwlstFile);
+        domain = TestUtils.createDomain(DOMAINONPV_WLST_YAML);
         domain.verifyDomainCreated();
 
         domain.deployWebAppViaREST(
@@ -104,18 +70,18 @@ public class ITSessionMigration extends BaseTest {
             BaseTest.getUsername(),
             BaseTest.getPassword());
 
-        // wait sometime for deployment gets ready
-        Thread.sleep(30 * 1000);
-
-        httpHeaderFile = BaseTest.getResultDir() + "/headers";
-
-        httpAttrMap = new HashMap<String, String>();
-        httpAttrMap.put("sessioncreatetime", "(.*)sessioncreatetime>(.*)</sessioncreatetime(.*)");
-        httpAttrMap.put("sessionid", "(.*)sessionid>(.*)</sessionid(.*)");
-        httpAttrMap.put("primary", "(.*)primary>(.*)</primary(.*)");
-        httpAttrMap.put("secondary", "(.*)secondary>(.*)</secondary(.*)");
-        httpAttrMap.put("count", "(.*)countattribute>(.*)</countattribute(.*)");
+        // wait some time for deployment gets ready
+        Thread.sleep(10 * 1000);
       }
+
+      httpHeaderFile = BaseTest.getResultDir() + "/headers";
+
+      httpAttrMap = new HashMap<String, String>();
+      httpAttrMap.put("sessioncreatetime", "(.*)sessioncreatetime>(.*)</sessioncreatetime(.*)");
+      httpAttrMap.put("sessionid", "(.*)sessionid>(.*)</sessionid(.*)");
+      httpAttrMap.put("primary", "(.*)primary>(.*)</primary(.*)");
+      httpAttrMap.put("secondary", "(.*)secondary>(.*)</secondary(.*)");
+      httpAttrMap.put("count", "(.*)countattribute>(.*)</countattribute(.*)");
     }
   }
 
@@ -138,8 +104,8 @@ public class ITSessionMigration extends BaseTest {
   }
 
   /**
-   * verify that after the primary server shutdown, a new and running managed server is picked up as
-   * the primary server
+   * Verify that a new and running managed server is picked up as the primary server after the
+   * primary server shut down
    *
    * @throws Exception
    */
@@ -157,63 +123,43 @@ public class ITSessionMigration extends BaseTest {
     String sessCreateTime = "sessioncreatetime";
     String primaryServ = "primary";
 
-    // Send the 1st HTTP request and save http header/sessionID info
-    logger.info("Send the first HTTP request to query Primary server name.");
-    String curlSaveHttpHeader = " -D " + httpHeaderFile;
-    String curlCmd = buildWebServiceUrl(testAppPath, curlSaveHttpHeader);
-    ExecResult result = execCurlCmd(curlCmd);
-    // logger.info("HTTP resp bf promary server is stopped:\n " + result.stdout());
+    // Send the first HTTP request and save HTTP header/sessionID info
+    ExecResult result = getHTTPResponse(testAppPath, " -D ");
 
-    // get primary server name & session create time bf primaryServName1 is stopped
+    // Get primary server name & session create time bf primaryServName1 is stopped
     String primaryServName1 = getHttpResponseAttribute(result.stdout(), primaryServ);
     String sessCreateTime1 = getHttpResponseAttribute(result.stdout(), sessCreateTime);
-    logger.info("Primary server before is: " + primaryServName1);
-    logger.info("Session created time before is: " + sessCreateTime1);
 
-    // stop primary server
-    // TODO this doesn't work. need to file a bug
-    // domain.shutdownManagedServerUsingServerStartPolicy(primaryServName1);
-    String msPodName = domainUid + "-" + primaryServName1;
-    String cmd = "kubectl delete po/" + msPodName + " -n " + domainNS;
-    logger.info("Stop managed server <" + primaryServName1 + "> using command:\n" + cmd);
-    result = ExecCommand.exec(cmd);
-    logger.info(result.stdout());
+    // Stop primary server
+    stopPrimaryServer(primaryServName1);
 
-    // Send the 2nd HTTP request by use http header/sessionID info save before
-    logger.info("Send the second HTTP request to query Primary server name.");
-    String curlUseHttpHeader = " -b " + httpHeaderFile;
-    curlCmd = buildWebServiceUrl(testAppPath, curlUseHttpHeader);
-    result = execCurlCmd(curlCmd);
-    // logger.info("HTTP resp af promary server is stopped:\n " + result.stdout());
+    // Send the second HTTP request using HTTP header/sessionID info save before
+    result = getHTTPResponse(testAppPath, " -b ");
 
-    // get primary server name & session create time af primaryServName1 is stopped
+    // Get primary server name & session create time af primaryServName1 is stopped
     String primaryServName2 = getHttpResponseAttribute(result.stdout(), primaryServ);
     String sessCreateTime2 = getHttpResponseAttribute(result.stdout(), sessCreateTime);
-    logger.info("Primary server after is: " + primaryServName2);
-    logger.info("Session created time after is: " + sessCreateTime2);
 
-    // verify that the same session info is used
+    // Verify that the same session info is used
     Assume.assumeTrue("HTTP Session should NOT change!", sessCreateTime1.equals(sessCreateTime2));
 
-    // verify that a new primary server is picked
+    // Verify that a new primary server is picked
     Assume.assumeFalse(
         "A new primary server should be picked!",
         primaryServName1.trim().equals(primaryServName2.trim()));
 
-    // restore test env
-    // TODO will use this when it works
-    // domain.restartManagedServerUsingServerStartPolicy(primaryServName1);
-    // wait sometime for ms pod gets ready
-    Thread.sleep(30 * 1000);
-    TestUtils.checkPodCreated(domainUid + "-" + primaryServName1, domainNS);
+    // Restore test env
+    // Wait some time for ms pod to become ready
+    Thread.sleep(10 * 1000);
     TestUtils.checkPodReady(domainUid + "-" + primaryServName1, domainNS);
 
-    logger.info("SUCCESS - " + testMethodName + ". ms <" + primaryServName2 + "> now is primary.");
+    logger.info(
+        "SUCCESS - " + testMethodName + ". ms <" + primaryServName2 + "> is new primary server.");
   }
 
   /**
-   * verify that after the primary server shutdown, HTTP session state is migrated to the new
-   * primary server
+   * Verify that HTTP session state is migrated to the new primary server after the primary server
+   * shut down
    *
    * @throws Exception
    */
@@ -235,93 +181,97 @@ public class ITSessionMigration extends BaseTest {
     String primaryServ = "primary";
     String count = "count";
 
-    // Send the 1st HTTP request and save http header/sessionID info
-    logger.info("Send the first HTTP request to set a count number.");
-    String curlSaveHttpHeader = " -D " + httpHeaderFile;
-    String curlCmd = buildWebServiceUrl(webServiceSetUrl, curlSaveHttpHeader);
-    ExecResult result = execCurlCmd(curlCmd);
-    // logger.info("HTTP resp bf promary server is stopped:\n " + result.stdout());
+    // Send the first HTTP request and save HTTP header/sessionID info
+    ExecResult result = getHTTPResponse(webServiceSetUrl, " -D ");
 
-    // get primary server name & count number bf primaryServName1 is stopped
+    // Get primary server name & count number bf primaryServName1 is stopped
     String primaryServName1 = getHttpResponseAttribute(result.stdout(), primaryServ);
     String countattribute1 = getHttpResponseAttribute(result.stdout(), count);
-    logger.info("Primary server before is: " + primaryServName1);
-    logger.info("count is set to: " + countattribute1);
 
-    // stop primary server
-    // TODO this doesn't work. need to file a bug
-    // domain.shutdownManagedServerUsingServerStartPolicy(primaryServName1);
-    String msPodName = domainUid + "-" + primaryServName1;
-    String cmd = "kubectl delete po/" + msPodName + " -n " + domainNS;
-    logger.info("Stop managed server <" + msPodName + "> using command:\n" + cmd);
-    result = ExecCommand.exec(cmd);
-    logger.info(result.stdout());
+    // Stop primary server
+    stopPrimaryServer(primaryServName1);
 
-    // Send the 2nd HTTP request by use http header/sessionID info save before
-    logger.info("Send the second HTTP request to get a count number.");
-    String curlUseHttpHeader = " -b " + httpHeaderFile;
-    curlCmd = buildWebServiceUrl(webServiceGetUrl, curlUseHttpHeader);
-    result = execCurlCmd(curlCmd);
-    // logger.info("HTTP response af promary server is stopped: \n" + result.stdout());
+    // Send the second HTTP request using HTTP header/sessionID info save before
+    result = getHTTPResponse(webServiceGetUrl, " -b ");
 
-    // get primary server name & count number af primaryServName1 is stopped
+    // Get primary server name & count number af primaryServName1 is stopped
     String primaryServName2 = getHttpResponseAttribute(result.stdout(), primaryServ);
     String countattribute2 = getHttpResponseAttribute(result.stdout(), count);
 
-    logger.info("Primary server after is: " + primaryServName2);
-    logger.info("Get count: " + countattribute2);
-
-    // verify that the count number is from a new primary server
+    // Verify that the count number is from a new primary server
     Assume.assumeFalse(
         "A new primary server should be picked!",
         primaryServName1.trim().equals(primaryServName2.trim()));
 
-    // verify that HTTP session state is migrated by checking the count number same
+    // Verify that HTTP session state is migrated by checking the count number same
     // bf and af primaryServName1 stopped
     Assume.assumeTrue(
         "HTTP session state is NOT migrated!", countattribute1.equals(countattribute2));
 
-    // restore test env
-    // TODO will use this when it works
-    // domain.restartManagedServerUsingServerStartPolicy(primaryServName1);
-    // wait sometime for ms pod gets ready
-    Thread.sleep(30 * 1000);
-    TestUtils.checkPodCreated(domainUid + "-" + primaryServName1, domainNS);
+    // Restore test env
+    // Wait some time for ms pod to become ready
+    Thread.sleep(10 * 1000);
     TestUtils.checkPodReady(domainUid + "-" + primaryServName1, domainNS);
 
     logger.info("SUCCESS - " + testMethodName + ". HTTP session state is migrated!");
   }
 
-  @Ignore
-  @Test
-  public void myTest1() throws Exception {
-    Assume.assumeFalse(QUICKTEST);
-    Map<String, Object> domainMap = domain.getDomainMap();
-    String domainNS = domainMap.get("namespace").toString();
+  /**
+   * Get HTTP response from the web app deployed on wls
+   *
+   * @throws Exception
+   */
+  private ExecResult getHTTPResponse(String webServiceURL, String headerOption) throws Exception {
+    // Send a HTTP request
+    String curlCmd = buildWebServiceUrl(webServiceURL, headerOption + httpHeaderFile);
+    logger.info("Send a HTTP request: " + curlCmd);
 
-    domain.shutdownManagedServerUsingServerStartPolicy("managed-server2");
-    TestUtils.checkPodDeleted("domainonpvwlst-managed-server2", domainNS);
-  }
+    ExecResult result = ExecCommand.exec(curlCmd);
 
-  @Ignore
-  @Test
-  public void myTest2() throws Exception {
-    Assume.assumeFalse(QUICKTEST);
-    Map<String, Object> domainMap = domain.getDomainMap();
-    String domainNS = domainMap.get("namespace").toString();
+    if (result.exitValue() != 0) {
+      throw new Exception(
+          "FAILURE: command "
+              + curlCmd
+              + " failed, returned "
+              + result.stderr()
+              + "\n "
+              + result.stdout());
+    }
 
-    domain.shutdownManagedServerUsingServerStartPolicy("domainonpvwlst-managed-server1");
-    TestUtils.checkPodDeleted("domainonpvwlst-managed-server1", domainNS);
+    return result;
   }
 
   /**
-   * build
+   * Stop the primary server
+   *
+   * @throws Exception
+   */
+  private void stopPrimaryServer(String primaryServerName) throws Exception {
+    Map<String, Object> domainMap = domain.getDomainMap();
+    String domainNS = domainMap.get("namespace").toString();
+    String domainUid = domain.getDomainUid();
+
+    // stop primary server
+    String msPodName = domainUid + "-" + primaryServerName;
+    String cmd = "kubectl delete po/" + msPodName + " -n " + domainNS;
+    logger.info("Stop managed server <" + msPodName + "> using command:\n" + cmd);
+
+    ExecResult result = ExecCommand.exec(cmd);
+    if (result.exitValue() != 0) {
+      throw new Exception("FAILURE: command " + cmd + " failed, returned " + result.stderr());
+    }
+
+    logger.info(result.stdout());
+  }
+
+  /**
+   * Get the value of a HTTP attribute
    *
    * @throws Exception
    */
   private String getHttpResponseAttribute(String httpResponseString, String attribute)
       throws Exception {
-    // logger.info("Looking for <" + attribute + "> in\n " + httpResponseString);
+
     String attrPatn = httpAttrMap.get(attribute);
 
     Assume.assumeNotNull(attrPatn);
@@ -334,13 +284,11 @@ public class ITSessionMigration extends BaseTest {
       httpAttribute = matcher.group(2);
     }
 
-    // logger.info("Value for HTTP attribute found.: " + attribute + " = " + httpAttribute);
-
     return httpAttribute;
   }
 
   /**
-   * build
+   * Build web server url
    *
    * @throws Exception
    */
@@ -361,13 +309,11 @@ public class ITSessionMigration extends BaseTest {
         .append(curlURLPath)
         .append(paramToAppend);
 
-    logger.info("Built web service url: " + webServiceUrl);
-
     return webServiceUrl.toString();
   }
 
   /**
-   * build
+   * Execute a given curl command and verify the results
    *
    * @throws Exception
    */
@@ -377,7 +323,7 @@ public class ITSessionMigration extends BaseTest {
     ExecResult result = ExecCommand.exec(curlCmd);
 
     if (result.exitValue() != 0) {
-      throw new RuntimeException(
+      throw new Exception(
           "FAILURE: command "
               + curlCmd
               + " failed, returned "
@@ -385,8 +331,6 @@ public class ITSessionMigration extends BaseTest {
               + "\n "
               + result.stdout());
     }
-
-    // logger.info("HTTP response is:\n " + result.stdout());
 
     return result;
   }
