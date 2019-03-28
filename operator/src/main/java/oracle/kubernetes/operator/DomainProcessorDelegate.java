d9d075157c2124e46977ecc0131bff092d8f3a6c
Russell Gold <russell.gold@oracle.com>
2019-03-28 09:57:31 -0400

    Address review comments

--- operator/src/main/java/oracle/kubernetes/operator/DomainProcessorDelegate.java
+++ operator/src/main/java/oracle/kubernetes/operator/DomainProcessorDelegate.java
@@ -10,19 +10,61 @@ import oracle.kubernetes.operator.helpers.KubernetesVersion;
 import oracle.kubernetes.operator.work.FiberGate;
 import oracle.kubernetes.operator.work.Step;
 
+/** A set of underlying services required during domain processing. */
 public interface DomainProcessorDelegate {
+  /**
+   * Returns the namespace associated with the operator itself.
+   *
+   * @return a namespace string
+   */
   String getOperatorNamespace();
 
+  /**
+   * Returns a factory that creates a step to wait for a pod in the specified namespace to be ready.
+   *
+   * @param namespace the namespace for the pod
+   * @return a step-creating factory
+   */
   PodAwaiterStepFactory getPodAwaiterStepFactory(String namespace);
 
+  /**
+   * Returns true if the namespace is running.
+   *
+   * @param namespace the namespace to check
+   * @return the 'running' state of the namespace
+   */
   boolean isNamespaceRunning(String namespace);
 
+  /**
+   * Returns the version of the Kubernetes environment in which the operator is running
+   *
+   * @return an object that represents the Kubernetes version
+   */
   KubernetesVersion getVersion();
 
+  /**
+   * Creates a new FiberGate.
+   *
+   * @return the created instance
+   */
   FiberGate createFiberGate();
 
+  /**
+   * Runs a chain of steps
+   *
+   * @param firstStep the first step to run
+   */
   void runSteps(Step firstStep);
 
+  /**
+   * Schedules the specified command to run periodically.
+   *
+   * @param command the command to run
+   * @param initialDelay the number of time units to wait before running the command the first time
+   * @param delay the number of time units to delay between successive runs
+   * @param unit the time unit for the above delays
+   * @return a future which indicates completion of the command
+   */
   ScheduledFuture<?> scheduleWithFixedDelay(
       Runnable command, long initialDelay, long delay, TimeUnit unit);
 }
