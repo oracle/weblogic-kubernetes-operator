3b0b5e08dbaa89e1dbf027b60b2eda87266e7d3b
root <root@mark.sub11201828381.certificationvc.oraclevcn.com>
2019-04-03 12:46:12 +0000

    wip create fmw infra domain

---
+++ kubernetes/samples/scripts/create-fmw-infrastructure-domain/run-rcu.sh
@@ -0,0 +1,205 @@
+#!/bin/bash
+# Copyright 2019, Oracle Corporation and/or its affiliates. All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+#
+# Description
+#  This sample script runs RCU to set up the database schemas for a Fusion Middleware domain. 
+#
+#  The following pre-requisites must be handled prior to running this script:
+#    * The kubernetes namespace must already be created
+#    * The kubernetes secret with the RCU credentials must have been created in the namespace
+#
+
+# Initialize
+script="${BASH_SOURCE[0]}"
+scriptDir="$( cd "$( dirname "${script}" )" && pwd )"
+source ${scriptDir}/../../common/utility.sh
+source ${scriptDir}/../../common/validate.sh
+
+function usage {
+  echo usage: ${script} -o dir -i file [-e] [-v] [-h]
+  echo "  -i Parameter inputs file, must be specified."
+  echo "  -h Help"
+  exit $1
+}
+
+#
+# Parse the command line options
+#
+doValidation=false
+executeIt=false
+while getopts "hi:" opt; do
+  case $opt in
+    i) valuesInputFile="${OPTARG}"
+    ;;
+    h) usage 0
+    ;;
+    *) usage 1
+    ;;
+  esac
+done
+
+if [ -z ${valuesInputFile} ]; then
+  echo "${script}: -i must be specified."
+  missingRequiredOption="true"
+fi
+
+if [ "${missingRequiredOption}" == "true" ]; then
+  usage 1
+fi
+
+#
+# Function to setup the environment to run the create domain job
+#
+function initialize {
+
+  # Validate the required files exist
+  validateErrors=false
+
+  validateKubectlAvailable
+
+  if [ -z "${valuesInputFile}" ]; then
+    validationError "You must use the -i option to specify the name of the inputs parameter file (a modified copy of kubernetes/samples/scripts/create-fmw-infrastructure-domain/create-domain-inputs.yaml)."
+  else
+    if [ ! -f ${valuesInputFile} ]; then
+      validationError "Unable to locate the input parameters file ${valuesInputFile}"
+    fi
+  fi
+
+  createJobInput="${scriptDir}/create-domain-job-template.yaml"
+  if [ ! -f ${createJobInput} ]; then
+    validationError "The template file ${createJobInput} for creating a WebLogic domain was not found"
+  fi
+
+  failIfValidationErrors
+
+  validateCommonInputs
+
+  initOutputDir
+}
+
+# create domain configmap using what is in the createDomainFilesDir
+function createRCUConfigmap {
+  # Use the default files if createDomainFilesDir is not specified
+  if [ -z "${createDomainFilesDir}" ]; then
+    createDomainFilesDir=${scriptDir}/wlst
+  elif [[ ! ${createDomainFilesDir} == /* ]]; then
+    createDomainFilesDir=${scriptDir}/${createDomainFilesDir}
+  fi
+
+  # customize the files with domain information
+  externalFilesTmpDir=$domainOutputDir/tmp
+  mkdir -p $externalFilesTmpDir
+  cp ${createDomainFilesDir}/* ${externalFilesTmpDir}/
+  if [ -d "${scriptDir}/common" ]; then
+    cp ${scriptDir}/common/* ${externalFilesTmpDir}/
+  fi
+  cp ${domainOutputDir}/create-domain-inputs.yaml ${externalFilesTmpDir}/
+ 
+  # Set the domainName in the inputs file that is contained in the configmap.
+  # this inputs file can be used by the scripts, such as WDT, that creates the WebLogic
+  # domain in the job.
+  echo domainName: $domainName >> ${externalFilesTmpDir}/create-domain-inputs.yaml
+
+  if [ -f ${externalFilesTmpDir}/prepare.sh ]; then
+   sh ${externalFilesTmpDir}/prepare.sh -i ${externalFilesTmpDir}
+  fi
+ 
+  # create the configmap and label it properly
+  local cmName=${domainUID}-create-ruc-sample-domain-job-cm
+  kubectl create configmap ${cmName} -n $namespace --from-file $externalFilesTmpDir
+
+  echo Checking the configmap $cmName was created
+  local num=`kubectl get cm -n $namespace | grep ${cmName} | wc | awk ' { print $1; } '`
+  if [ "$num" != "1" ]; then
+    fail "The configmap ${cmName} was not created"
+  fi
+
+  kubectl label configmap ${cmName} -n $namespace weblogic.resourceVersion=domain-v2 weblogic.domainUID=$domainUID weblogic.domainName=$domainName
+
+  rm -rf $externalFilesTmpDir
+}
+
+#
+# Function to run the job that creates the domain
+#
+function runRCU {
+
+  # create the config map for the job
+  createRCUConfigmap
+
+  # There is no way to re-run a kubernetes job, so first delete any prior job
+  CONTAINER_NAME="run-rcu-sample-job"
+  JOB_NAME="${domainUID}-${CONTAINER_NAME}"
+  deleteK8sObj job $JOB_NAME ${createJobOutput}
+
+  echo Creating the domain by creating the job ${createJobOutput}
+  kubectl create -f ${createJobOutput}
+
+  echo "Waiting for the job to complete..."
+  JOB_STATUS="0"
+  max=20
+  count=0
+  while [ "$JOB_STATUS" != "Completed" -a $count -lt $max ] ; do
+    sleep 30
+    count=`expr $count + 1`
+    JOBS=`kubectl get pods -n ${namespace} | grep ${JOB_NAME}`
+    JOB_ERRORS=`kubectl logs jobs/$JOB_NAME $CONTAINER_NAME -n ${namespace} | grep "ERROR:" `
+    JOB_STATUS=`echo $JOBS | awk ' { print $3; } '`
+    JOB_INFO=`echo $JOBS | awk ' { print "pod", $1, "status is", $3; } '`
+    echo "status on iteration $count of $max"
+    echo "$JOB_INFO"
+
+    # Terminate the retry loop when a fatal error has already occurred.  Search for "ERROR:" in the job log file
+    if [ "$JOB_STATUS" != "Completed" ]; then
+      ERR_COUNT=`echo $JOB_ERRORS | grep "ERROR:" | wc | awk ' {print $1; }'`
+      if [ "$ERR_COUNT" != "0" ]; then
+        echo "A failure was detected in the log file for job $JOB_NAME."
+        echo "$JOB_ERRORS"
+        echo "Check the log output for additional information."
+        fail "Exiting due to failure - the job has failed!"
+      fi
+    fi
+  done
+
+  # Confirm the job pod is status completed
+  if [ "$JOB_STATUS" != "Completed" ]; then
+    echo "The create domain job is not showing status completed after waiting 300 seconds."
+    echo "Check the log output for errors."
+    kubectl logs jobs/$JOB_NAME $CONTAINER_NAME -n ${namespace}
+    fail "Exiting due to failure - the job status is not Completed!"
+  fi
+
+  # Check for successful completion in log file
+  JOB_POD=`kubectl get pods -n ${namespace} | grep ${JOB_NAME} | awk ' { print $1; } '`
+  JOB_STS=`kubectl logs $JOB_POD $CONTAINER_NAME -n ${namespace} | grep "Successfully Completed" | awk ' { print $1; } '`
+  if [ "${JOB_STS}" != "Successfully" ]; then
+    echo The log file for the create domain job does not contain a successful completion status
+    echo Check the log output for errors
+    kubectl logs $JOB_POD $CONTAINER_NAME -n ${namespace}
+    fail "Exiting due to failure - the job log file does not contain a successful completion status!"
+  fi
+}
+
+#
+# Function to output to the console a summary of the work completed
+#
+function printSummary {
+
+  # Get the IP address of the kubernetes cluster (into K8S_IP)
+  getKubernetesClusterIP
+
+  echo ""
+  echo "RCU was run for domain ${domainName}"
+  echo ""
+  echo "The following files were generated:"
+  echo "  ${domainOutputDir}/create-domain-inputs.yaml"
+  echo "  ${createJobOutput}"
+  echo "  ${dcrOutput}"
+  echo ""
+  echo "Completed"
+}
+
+# Perform the sequence of steps to create a domain
+runRCU false
+
