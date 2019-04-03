7011ae1026152d78a7a19b17f3a91645d57bf8b3
Maggie He <maggie.he@oracle.com>
2019-04-03 13:32:18 -0700

    revise the code to supply only fragment of domain yaml file with added property

--- integration-tests/src/test/java/oracle/kubernetes/operator/utils/TestUtils.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/utils/TestUtils.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -1201,5 +1202,54 @@ public class TestUtils {
   public static void copyFile(String fromFile, String toFile) throws Exception {
     logger.info("Copying file from  " + fromFile + " to " + toFile);
     Files.copy(new File(fromFile).toPath(), Paths.get(toFile), StandardCopyOption.REPLACE_EXISTING);
+  }
+
+  /**
+   * append source file to the end of target file
+   *
+   * @param sourceFile - source file name
+   * @param targetFile - target file name
+   * @throws Exception
+   */
+  public static void concatFile(String sourceFile, String targetFile) throws Exception {
+    FileReader fr = null;
+    FileWriter fw = null;
+    try {
+      fr = new FileReader(sourceFile);
+      fw = new FileWriter(targetFile, true);
+      int c = fr.read();
+      while (c != -1) {
+        fw.write(c);
+        c = fr.read();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      if (fr != null) {
+        fr.close();
+      }
+      if (fw != null) {
+        fw.close();
+      }
+    }
+  }
+
+  /**
+   * check whether a certain property(String)is included in the file
+   *
+   * @param property
+   * @param fileName
+   * @return result of above checking
+   */
+  public static boolean checkFileIncludeProperty(String property, String fileName) {
+    boolean result = false;
+    try {
+      String content = new String(Files.readAllBytes(Paths.get(fileName)));
+      result = content.indexOf(property) >= 0;
+      logger.info("the result for checkFileIncludeProperty " + fileName + " result: " + result);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return result;
   }
 }
