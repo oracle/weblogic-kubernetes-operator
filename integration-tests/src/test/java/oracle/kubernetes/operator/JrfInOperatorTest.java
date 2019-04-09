4c26efcf6eb713b7ba6ab1a6160a56e39e09fa86
xiancao <xian.cao@oracle.com>
2019-04-09 08:30:09 -0700

    move jrf test to the develop branch

---
+++ integration-tests/src/test/java/oracle/kubernetes/operator/JrfInOperatorTest.java
@@ -0,0 +1,141 @@
+// Copyright 2019, Oracle Corporation and/or its affiliates.  All rights reserved.
+//   Licensed under the Universal Permissive License v 1.0 as shown at
+//   http://oss.oracle.com/licenses/upl.
+
+package oracle.kubernetes.operator;
+
+import oracle.kubernetes.operator.utils.JRFDomain;
+import oracle.kubernetes.operator.utils.Operator;
+import oracle.kubernetes.operator.utils.OracleDB;
+import oracle.kubernetes.operator.utils.TestUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+/**
+ * Simple JUnit test file used for testing Operator for JRF domains.
+ *
+ * <p>This test is used for creating Operator(s) and multiple JRF domains which are managed by the
+ * Operator(s).
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class JrfInOperatorTest extends BaseTest {
+
+  // property file used to customize operator properties for operator inputs yaml
+
+  private static String operator1File = "jrfoperator1.yaml";
+
+  // file used to customize domain properties for domain, PV and LB inputs yaml
+  private static String jrfdomainonpvwlstFile = "jrfdomainonpvwlst.yaml";
+
+  // property file for oracle db information
+  private static String dbPropsFile = "oracledb.properties";
+
+  // property file used to configure constants for integration tests
+  private static String appPropsFile = "OperatorIT.properties";
+
+  private static Operator operator1;
+
+  /**
+   * This method gets called only once before any of the test methods are executed. It does the
+   * initialization of the integration test properties defined in OperatorIT.properties and setting
+   * the resultRoot, pvRoot and projectRoot attributes. It also creates Oracle DB pod which used for
+   * RCU.
+   *
+   * @throws Exception - if an error occurs when load property file or create DB pod
+   */
+  @BeforeClass
+  public static void staticPrepare() throws Exception {
+    // initialize test properties and create the directories
+    initialize(appPropsFile);
+
+    // create DB used for jrf domain
+    OracleDB db = TestUtils.createOracleDB(dbPropsFile);
+
+    // populate the jrf/create-domain-script.sh
+    // copy the integration-tests/src/test/resources/domain-home-on-pv/jrf to
+    // BaseTest.getResultDir()
+    TestUtils.exec(
+        "cp -rf "
+            + BaseTest.getProjectRoot()
+            + "/integration-tests/src/test/resources/domain-home-on-pv/jrf "
+            + BaseTest.getResultDir());
+    // replace the db connection string with true value
+    String dbConnectString =
+        db.getName()
+            + "."
+            + db.getNamespace()
+            + ".svc.cluster.local:"
+            + db.getPort()
+            + "/"
+            + db.getDBPdb()
+            + "."
+            + db.getDBDomain();
+    TestUtils.replaceStringInFile(
+        BaseTest.getResultDir() + "/jrf/create-domain-script.sh",
+        "%CONNECTION_STRING%",
+        dbConnectString);
+  }
+
+  /**
+   * This method will run once after all test methods are finished. It Releases k8s cluster lease,
+   * archives result, pv directories.
+   *
+   * @throws Exception - if any error occurs
+   */
+  @AfterClass
+  public static void staticUnPrepare() throws Exception {
+    logger.info("+++++++++++++++++++++++++++++++++---------------------------------+");
+    logger.info("BEGIN");
+    logger.info("Run once, release cluster lease");
+
+    tearDown();
+
+    logger.info("SUCCESS");
+  }
+
+  /**
+   * Create operator and verify it's deployed successfully. Create jrf domain and verify domain is
+   * started. Verify liveness probe by killing managed server 1 process 3 times to kick pod
+   * auto-restart. Shutdown the domain by changing domain serverStartPolicy to NEVER.
+   *
+   * @throws Exception - if any error occurs when create operator and jrf domains
+   */
+  @Test
+  public void testJRFDomainOnPVUsingWLST() throws Exception {
+    String testMethodName = new Object() {}.getClass().getEnclosingMethod().getName();
+    logTestBegin(testMethodName);
+    logger.info("Creating Operator & waiting for the script to complete execution");
+    // create operator1
+    if (operator1 == null) {
+      operator1 = TestUtils.createOperator(operator1File);
+    }
+
+    // TODO: reconsider the logic to check the db readiness
+    // The jrfdomain can not find the db pod even the db pod shows ready, sleep more time
+    logger.info("waiting for the db to be visible to rcu script ...");
+    Thread.sleep(60000);
+
+    JRFDomain jrfdomain = null;
+    boolean testCompletedSuccessfully = false;
+
+    try {
+      jrfdomain = new JRFDomain(jrfdomainonpvwlstFile);
+      jrfdomain.verifyDomainCreated();
+
+      if (!SMOKETEST) {
+        jrfdomain.testWlsLivenessProbe();
+      }
+
+      testCompletedSuccessfully = true;
+    } finally {
+      if (jrfdomain != null && !SMOKETEST && (JENKINS || testCompletedSuccessfully)) {
+        jrfdomain.shutdownUsingServerStartPolicy();
+      }
+    }
+
+    logger.info("SUCCESS - " + testMethodName);
+  }
+}
---
+++ integration-tests/src/test/java/oracle/kubernetes/operator/utils/OracleDB.java
@@ -0,0 +1,134 @@
+// Copyright 2019, Oracle Corporation and/or its affiliates.  All rights reserved.
+// Licensed under the Universal Permissive License v 1.0 as shown at
+// http://oss.oracle.com/licenses/upl.
+
+package oracle.kubernetes.operator.utils;
+
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class OracleDB {
+
+  // DB default properties
+  public static final String DEFAULT_DB_NAME = "infradb";
+  public static final String DEFAULT_DB_NAMESPACE = "db";
+  public static final String DEFAULT_DB_IMAGE = "store/oracle/database-enterprise:12.2.0.1";
+  //    "container-registry.oracle.com/database/enterprise:12.2.0.1";
+  public static final String DEFAULT_DB_PORT = "1521";
+  public static final String DEFAULT_DB_SID = "InfraDB";
+  public static final String DEFAULT_DB_PDB = "InfraPDB1";
+  public static final String DEFAULT_DB_DOMAIN = "us.oracle.com";
+  public static final String DEFAULT_DB_BUNDLE = "basic";
+
+  // db instance variables
+  private String name;
+  private String namespace;
+  private String image;
+  private String port;
+  private String db_sid;
+  private String db_pdb;
+  private String db_domain;
+  private String db_bundle;
+
+  private static final Logger logger = Logger.getLogger("OperatorIT", "OperatorIT");
+
+  public OracleDB(String dbPropFile) throws Exception {
+    Properties dbprops = TestUtils.loadProps(dbPropFile);
+    name = dbprops.getProperty("name", DEFAULT_DB_NAME);
+    namespace = dbprops.getProperty("namespace", DEFAULT_DB_NAMESPACE);
+    image = dbprops.getProperty("image", DEFAULT_DB_IMAGE);
+    port = dbprops.getProperty("port", DEFAULT_DB_PORT);
+    db_sid = dbprops.getProperty("db_sid", DEFAULT_DB_SID);
+    db_pdb = dbprops.getProperty("db_pdb", DEFAULT_DB_PDB);
+    db_domain = dbprops.getProperty("db_domain", DEFAULT_DB_DOMAIN);
+    db_bundle = dbprops.getProperty("db_bundle", DEFAULT_DB_BUNDLE);
+
+    // clean the db env first
+    cleanDB();
+
+    String command;
+    // create the namespace
+    if (!namespace.equalsIgnoreCase("default")) {
+      command = "kubectl create ns " + namespace;
+      logger.info("Running " + command);
+      ExecCommand.exec(command);
+    }
+
+    // create db
+    command =
+        "kubectl run "
+            + name
+            + " -n "
+            + namespace
+            + " --image="
+            + image
+            + " --port="
+            + port
+            + " --env=\"DB_SID="
+            + db_sid
+            + "\" --env=\"DB_PDB="
+            + db_pdb
+            + "\" --env=\"DB_DOMAIN="
+            + db_domain
+            + "\" --env=\"DB_BUNDLE="
+            + db_bundle
+            + "\" --expose";
+    logger.info("Running " + command);
+    TestUtils.exec(command);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getNamespace() {
+    return namespace;
+  }
+
+  public String getPort() {
+    return port;
+  }
+
+  public String getImage() {
+    return image;
+  }
+
+  public String getDBSid() {
+    return db_sid;
+  }
+
+  public String getDBPdb() {
+    return db_pdb;
+  }
+
+  public String getDBDomain() {
+    return db_domain;
+  }
+
+  public String getDBBundle() {
+    return db_bundle;
+  }
+
+  private void cleanDB() throws Exception {
+    // delete the namespace if the db namespace is not default
+    String command;
+    if (!namespace.equalsIgnoreCase("default")) {
+      command = "kubectl delete ns " + namespace;
+      logger.info("Running " + command);
+      ExecCommand.exec(command);
+
+      // verify the namespace is deleted
+      TestUtils.checkNamespaceDeleted(namespace);
+    } else {
+      // delete the deployment and service
+      command = "kubectl delete deployment " + name + " -n " + namespace;
+      logger.info("Running " + command);
+      ExecCommand.exec(command);
+
+      // delete the service
+      command = "kubectl delete service " + name + " -n " + namespace;
+      logger.info("Running " + command);
+      ExecCommand.exec(command);
+    }
+  }
+}
