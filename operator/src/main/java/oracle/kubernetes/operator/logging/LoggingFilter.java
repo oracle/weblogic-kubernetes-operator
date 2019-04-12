8b5d7365e1672784cb7a87e198ab027a4c6bf7dc
Anthony Lai <anthony.lai@oracle.com>
2019-04-12 14:29:45 -0700

    Add LoggingFacade APIs that supports LoggingFilter

--- operator/src/main/java/oracle/kubernetes/operator/logging/LoggingFilter.java
+++ operator/src/main/java/oracle/kubernetes/operator/logging/LoggingFilter.java
@@ -1,25 +1,35 @@
-// Copyright 2017, Oracle Corporation and/or its affiliates.  All rights reserved.
+// Copyright 2019, Oracle Corporation and/or its affiliates.  All rights reserved.
 // Licensed under the Universal Permissive License v 1.0 as shown at
 // http://oss.oracle.com/licenses/upl.
 
 package oracle.kubernetes.operator.logging;
 
-public class LoggingFilter {
+/** A filter to control whether a log message should be logged */
+public interface LoggingFilter {
 
-  volatile boolean loggingAllowed;
+  // Constant for key for storing LoggingFilter object in Packet map
+  String LOGGING_FILTER_PACKET_KEY = "loggingFilter";
 
-  public void setCanLog(boolean canLog) {
-    loggingAllowed = canLog;
-  }
+  /**
+   * Checks if the message should be logged
+   *
+   * @param msg the message to be loggged
+   */
+  boolean canLog(String msg);
 
-  public boolean canLog() {
-    return loggingAllowed;
-  }
-
-  public static boolean canLog(LoggingFilter loggingFilter) {
+  /**
+   * Checks if the message should be logged according to an optional LoggingFilter
+   *
+   * @param loggingFilter LoggingFilter that decides whether the log message should be logged, can
+   *     be null
+   * @param msg The log message to be loggd
+   * @return the canLog() return value from the provided loggingFilter, message, and parameters, or
+   *     true if loggingFilter is null
+   */
+  static boolean canLog(LoggingFilter loggingFilter, String msg) {
     if (loggingFilter == null) {
       return true;
     }
-    return loggingFilter.canLog();
+    return loggingFilter.canLog(msg);
   }
 }
