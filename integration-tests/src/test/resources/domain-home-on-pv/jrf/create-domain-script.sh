54a23f6ea107f8c74bb2e2f11e6a72f99cf3d91a
xiancao <xian.cao@oracle.com>
2019-04-16 18:59:00 -0700

    use new fmw infra docker image

--- integration-tests/src/test/resources/domain-home-on-pv/jrf/create-domain-script.sh
+++ integration-tests/src/test/resources/domain-home-on-pv/jrf/create-domain-script.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 #
 #
-# Copyright (c) 2014-2019 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2014, 2019 Oracle and/or its affiliates. All rights reserved.
 #
 # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 #
-# If AdminServer.log does not exists, container is starting for 1st time
-# So it should start NM and also associate with AdminServer
-# Otherwise, only start NM (container restarted)
+#Define DOMAIN_HOME
+
+# get domain properties from domain.properties file
+DOMAIN_PROPERTIES_FILE=/u01/weblogic/domain.properties
+if [ -e "${DOMAIN_PROPERTIES_FILE}" ]; then
+  DOMAIN_NAME=`awk '{print $1}' $DOMAIN_PROPERTIES_FILE | grep DOMAIN_NAME | cut -d "=" -f2`
+  CONNECTION_STRING=`awk '{print $1}' $DOMAIN_PROPERTIES_FILE | grep CONNECTION_STRING | cut -d "=" -f2` 
+  RCUPREFIX=`awk '{print $1}' $DOMAIN_PROPERTIES_FILE | grep RCUPREFIX | cut -d "=" -f2` 
+  ADMIN_LISTEN_PORT=`awk '{print $1}' $DOMAIN_PROPERTIES_FILE | grep ADMIN_LISTEN_PORT | cut -d "=" -f2`
+  ADMIN_NAME=`awk '{print $1}' $DOMAIN_PROPERTIES_FILE | grep ADMIN_NAME | cut -d "=" -f2`
+  ADMINISTRATION_PORT_ENABLED=`awk '{print $1}' $DOMAIN_PROPERTIES_FILE | grep ADMINISTRATION_PORT_ENABLED | cut -d "=" -f2`
+  ADMINISTRATION_PORT=`awk '{print $1}' $DOMAIN_PROPERTIES_FILE | grep ADMINISTRATION_PORT= | cut -d "=" -f2`
+  MANAGED_NAME=`awk '{print $1}' $DOMAIN_PROPERTIES_FILE | grep MANAGED_NAME | cut -d "=" -f2`
+  MANAGEDSERVER_PORT=`awk '{print $1}' $DOMAIN_PROPERTIES_FILE | grep MANAGEDSERVER_PORT | cut -d "=" -f2`
+  PRODUCTION_MODE=`awk '{print $1}' $DOMAIN_PROPERTIES_FILE | grep PRODUCTION_MODE | cut -d "=" -f2`
+fi
+export DOMAIN_ROOT="/shared/domains"
+export DOMAIN_HOME=${DOMAIN_ROOT}/${DOMAIN_NAME}
+echo "Domain Home is: " $DOMAIN_HOME
+
 ########### SIGTERM handler ############
 function _term() {
    echo "Stopping container."
@@ -20,19 +37,6 @@ function _kill() {
    echo "SIGKILL received, shutting down the server!"
    kill -9 $childPID
 }
-#####Random Password Generation########
-function rand_pwd(){
-    while true; do
-         s=$(cat /dev/urandom | tr -dc "A-Za-z0-9" | fold -w 8 | head -n 1)
-         if [[ ${#s} -ge 8 && "$s" == *[A-Z]* && "$s" == *[a-z]* && "$s" == *[0-9]*  ]]
-         then
-             break
-         else
-             echo "Password does not Match the criteria, re-generating..." >&2
-         fi
-    done
-    echo "${s}"
-}
 
 # Set SIGTERM handler
 trap _term SIGTERM
@@ -41,160 +45,170 @@ trap _term SIGTERM
 trap _kill SIGKILL
 
 echo "Configuring Domain for first time "
-echo "If Domain has already been Configured"
 echo "Start the Admin and Managed Servers  "
 echo "====================================="
-
-export CONNECTION_STRING=%CONNECTION_STRING%
-export RCUPREFIX=%RCUPREFIX%
-export DB_PASSWORD=Oradoc_db1
-export DB_USERNAME=sys
-export ADMIN_PASSWORD=welcome1
-export DB_SCHEMA_PASSWORD=Welcome1
-export DOMAIN_NAME=%DOMAIN_NAME%
 
 # Check that the User has passed on all the details needed to configure this image
 # Settings to call RCU....
 echo "CONNECTION_STRING=${CONNECTION_STRING:?"Please set CONNECTION_STRING for connecting to the Database"}"
 echo "RCUPREFIX=${RCUPREFIX:?"Please set RCUPREFIX for the database schemas"}"
 echo "DOMAIN_NAME=${DOMAIN_NAME:?"Please set DOMAIN_NAME for creating the new Domain"}"
+echo "DOMAIN_HOME=$DOMAIN_HOME"
+
+# Settings to call domain creation ...
+echo "ADMIN_LISTEN_PORT=${ADMIN_LISTEN_PORT:-7001}"
+echo "ADMIN_NAME=${ADMIN_NAME:-AdminServer}"
+echo "ADMINISTRATION_PORT_ENABLED=${ADMINISTRATION_PORT_ENABLED:-true}"
+echo "ADMINISTRATION_PORT=${ADMINISTRATION_PORT:-9002}"
+echo "MANAGED_NAME=${MANAGED_NAME:-infraServer1}"
+echo "MANAGEDSERVER_PORT=${MANAGEDSERVER_PORT:-8001}"
+echo "PRODUCTION_MODE=${PRODUCTION_MODE:-prod}"
 
 RUN_RCU="true"
 CONTAINERCONFIG_DIR=/u01/oracle/ContainerData
 export CONNECTION_STRING=$CONNECTION_STRING
 export RCUPREFIX=$RCUPREFIX
 
-export DB_USERNAME=$DB_USERNAME
-export DB_PASSWORD=$DB_PASSWORD
-
 export jdbc_url="jdbc:oracle:thin:@"$CONNECTION_STRING
 
-#Set the password for RCU
-echo -e $DB_PASSWORD"\n"$DB_SCHEMA_PASSWORD > /u01/oracle/pwd.txt
 
 echo "Loading RCU Phase"
 echo "================="
 
 echo "CONNECTION_STRING=$CONNECTION_STRING"
 echo "RCUPREFIX=$RCUPREFIX"
-echo "DB_PASSWORD=$DB_PASSWORD"
 echo "jdbc_url=$jdbc_url"
-echo "ADMIN_PASSWORD=$ADMIN_PASSWORD"
-echo "DB_SCHEMA_PASSWORD=$DB_SCHEMA_PASSWORD"
-echo "DB_USERNAME=$DB_USERNAME"
-echo "DOMAIN_HOME: $DOMAIN_HOME"
 
 
 # Create an Infrastructure domain
 # set environments needed for the script to work
 ADD_DOMAIN=1
-export MW_HOME="/u01/oracle"
-export WL_HOME="/u01/oracle/wlserver"
-export ADMIN_USER="weblogic"
-export DOMAIN_ROOT="/shared/domains"
-export DOMAIN_HOME="${DOMAIN_ROOT}/${DOMAIN_NAME}"
 
-
-echo "MW_HOME: $MW_HOME"
-echo "WL_HOME: $WL_HOME"
-echo "ADMIN_USER: $ADMIN_USER"
-echo "DOMAIN_ROOT: $DOMAIN_ROOT"
-echo "DOMAIN HOME: $DOMAIN_HOME"
-
-if [ ! -f ${DOMAIN_HOME}/servers/AdminServer/logs/AdminServer.log ]; then
+if [ ! -f ${DOMAIN_HOME}/servers/${ADMIN_NAME}/logs/${ADMIN_NAME}.log ]; then
     ADD_DOMAIN=0
 fi
 
 # Create Domain only if 1st execution
 if [ $ADD_DOMAIN -eq 0 ];
 then
+   echo "Creating Domain 1st execution"
+   mkdir -p $ORACLE_HOME/properties
+   # Create Domain only if 1st execution
+   SEC_PROPERTIES_FILE=/u01/weblogic/domain_security.properties
 
-    # Auto generate Oracle WebLogic Server admin password
-    if [ -z ${ADMIN_PASSWORD} ]
-    then
-        # Auto generate Oracle WebLogic Server admin password
-        ADMIN_PASSWORD=$(rand_pwd)
-        echo ""
-        echo "    Oracle WebLogic Server Password Auto Generated :"
-        echo ""
-        echo "    ----> 'weblogic' admin password: $ADMIN_PASSWORD"
-        echo ""
-    fi;
+   if [ ! -e "$SEC_PROPERTIES_FILE" ]; then
+      echo "A properties file with the username and password needs to be supplied."
+      exit
+   fi
 
-    if [ -z ${DB_SCHEMA_PASSWORD} ]
-    then
-        # Auto generate Oracle Database Schema password
-        temp_pwd=$(rand_pwd)
-        #Password should not start with a number for database
-        f_str=`echo $temp_pwd|cut -c1|tr [0-9] [A-Z]`
-        s_str=`echo $temp_pwd|cut -c2-`
-        DB_SCHEMA_PASSWORD=${f_str}${s_str}
-        echo ""
-        echo "    Database Schema password Auto Generated :"
-        echo ""
-        echo "    ----> Database schema password: $DB_SCHEMA_PASSWORD"
-        echo ""
-    fi;
+   # Get Username
+   USER=`awk '{print $1}' $SEC_PROPERTIES_FILE | grep username | cut -d "=" -f2`
+   if [ -z "$USER" ]; then
+      echo "The domain username is blank.  The Admin username must be set in the properties file."
+      exit
+   fi
+   # echo "Username: $USER"
+   # Get Password
+   PASS=`awk '{print $1}' $SEC_PROPERTIES_FILE | grep password | cut -d "=" -f2`
+   if [ -z "$PASS" ]; then
+      echo "The domain password is blank.  The Admin password must be set in the properties file."
+      exit
+   fi
+   # echo "Password: $PASS"
+   # Get Database Username
+   DB_USER=`awk '{print $1}' $SEC_PROPERTIES_FILE | grep db_user | cut -d "=" -f2`
+   if [ -z "$DB_USER" ]; then
+      echo "The domain username is blank.  The Admin username must be set in the properties file."
+      exit
+   fi
+   # echo "Database Username $DB_USER"
+   # Get Database Password
+   DB_PASS=`awk '{print $1}' $SEC_PROPERTIES_FILE | grep db_pass | cut -d "=" -f2`
+   if [ -z "$DB_PASS" ]; then
+      echo "The domain password is blank.  The Admin password must be set in the properties file."
+      exit
+   fi
+   # echo "Database Password $DB_PASS"
+   # Get databasse Schema Password
+   DB_SCHEMA_PASS=`awk '{print $1}' $SEC_PROPERTIES_FILE | grep db_schema | cut -d "=" -f2`
+   if [ -z "$DB_SCHEMA_PASS" ]; then
+      echo "The databse schema password is blank.  The database schema password must be set in the properties file."
+      exit
+   fi
+   # echo "Database Schema Password: $DB_SCHEMA_PASS"
 
-    export ADMIN_PASSWORD=$ADMIN_PASSWORD
-    export DB_SCHEMA_PASSWORD=$DB_SCHEMA_PASSWORD
-    #Only call RCU the first time we create the domain
-    if [ -e $CONTAINERCONFIG_DIR/RCU.$RCUPREFIX.suc ]
-    then
-        #RCU has already been executed successfully, no need to rerun
-        RUN_RCU="false"
-        echo "SOA RCU has already been loaded.. skipping"
-    fi
 
-    if [ "$RUN_RCU" == "true" ]
-    then
-        echo "Loading SOA RCU into database"
-        # Run the RCU to load the schemas into the database
-        /u01/oracle/oracle_common/bin/rcu -silent -createRepository -databaseType ORACLE -connectString $CONNECTION_STRING -dbUser $DB_USERNAME -dbRole sysdba -useSamePasswordForAllSchemaUsers true -selectDependentsForComponents true -schemaPrefix $RCUPREFIX -component MDS -component MDS -component IAU -component IAU_APPEND -component IAU_VIEWER -component OPSS  -component WLS  -component STB -f < /u01/oracle/pwd.txt >> /u01/oracle/RCU.out
+   #Only call RCU the first time we create the domain
+   if [ -e $CONTAINERCONFIG_DIR/RCU.$RCUPREFIX.suc ]
+   then
+       #RCU has already been executed successfully, no need to rerun
+       RUN_RCU="false"
+       echo "SOA RCU has already been loaded.. skipping"
+   fi
+
+   if [ "$RUN_RCU" == "true" ]
+   then
+       #Set the password for RCU
+       echo -e ${DB_PASS}"\n"${DB_SCHEMA_PASS} > /u01/oracle/pwd.txt
+       echo "Loading SOA RCU into database"
+       # Run the RCU to load the schemas into the database
+       /u01/oracle/oracle_common/bin/rcu -silent -createRepository -databaseType ORACLE -connectString ${CONNECTION_STRING} -dbUser ${DB_USER} -dbRole sysdba -useSamePasswordForAllSchemaUsers true -selectDependentsForComponents true -schemaPrefix ${RCUPREFIX} -component MDS -component MDS -component IAU -component IAU_APPEND -component IAU_VIEWER -component OPSS  -component WLS  -component STB -f < /u01/oracle/pwd.txt >> /u01/oracle/RCU.out
+       retval=$?
+
+       if [ $retval -ne 0 ];
+       then
+           echo  "RCU has some error "
+           #RCU was already called once and schemas are in the database
+           #continue with Domain creation
+           grep -q "RCU-6016 The specified prefix already exists" "/u01/oracle/RCU.out"
+           if [ $? -eq 0 ] ; then
+             echo  "RCU has already loaded schemas into the Database"
+             echo  "RCU Ignore error"
+           else
+               echo "RCU Loading Failed.. Please check the RCU logs"
+               cat /u01/oracle/RCU.out
+               exit
+           fi
+       fi
+
+       # cleanup : remove the password file for security
+       rm -f "/u01/oracle/pwd.txt"
+   fi
+
+        echo "Domain Configuration Phase"
+        echo "=========================="
+
+        wlst.sh -skipWLSModuleScanning /u01/weblogic/createInfraDomain.py -oh ${ORACLE_HOME} -jh ${JAVA_HOME} -parent ${DOMAIN_ROOT} -name ${DOMAIN_NAME} -user ${USER} -password ${PASS} -rcuDb ${CONNECTION_STRING} -rcuPrefix ${RCUPREFIX} -rcuSchemaPwd ${DB_SCHEMA_PASS} -adminListenPort ${ADMIN_LISTEN_PORT} -adminName ${ADMIN_NAME} -adminPortEnabled ${ADMINISTRATION_PORT_ENABLED} -administrationPort ${ADMINISTRATION_PORT} -managedName ${MANAGED_NAME} -managedServerPort ${MANAGEDSERVER_PORT} -prodMode ${PRODUCTION_MODE}
+
         retval=$?
+
+        echo  "RetVal from Domain creation $retval"
 
         if [ $retval -ne 0 ];
         then
-            echo  "RCU has some error "
-            #RCU was already called once and schemas are in the database
-            #continue with Domain creation
-            grep -q "RCU-6016 The specified prefix already exists" "/u01/oracle/RCU.out"
-            if [ $? -eq 0 ] ; then
-                echo  "RCU has already loaded schemas into the Database"
-                echo  "RCU Ignore error"
-            else
-                echo "RCU Loading Failed.. Please check the RCU logs"
-                exit
-            fi
+            echo "Domain Creation Failed.. Please check the Domain Logs"
+            exit
         fi
 
-        # cleanup : remove the password file for security
-        rm -f "/u01/oracle/pwd.txt"
-    fi
+        # Create the security file to start the server(s) without the password prompt
+        mkdir -p ${DOMAIN_HOME}/servers/${ADMIN_NAME}/security/
+        echo "username=${USER}" >> ${DOMAIN_HOME}/servers/${ADMIN_NAME}/security/boot.properties
+        echo "password=${PASS}" >> ${DOMAIN_HOME}/servers/${ADMIN_NAME}/security/boot.properties
 
-    echo "Domain Configuration Phase"
-    echo "=========================="
+        ${DOMAIN_HOME}/bin/setDomainEnv.sh
+   fi
 
-    wlst.sh -skipWLSModuleScanning /u01/weblogic/createInfraDomain.py -oh $ORACLE_HOME -jh $JAVA_HOME -parent $DOMAIN_ROOT -name $DOMAIN_NAME -user $ADMIN_USER -password $ADMIN_PASSWORD -rcuDb $CONNECTION_STRING -rcuPrefix $RCUPREFIX -rcuSchemaPwd $DB_SCHEMA_PASSWORD
-    retval=$?
+  #Set Java options
+  export JAVA_OPTIONS=${JAVA_OPTIONS}
+  echo "Java Options: ${JAVA_OPTIONS}"
 
-    echo  "RetVal from Domain creation $retval"
+  echo "Starting the Admin Server"
+  echo "=========================="
 
-    if [ $retval -ne 0 ];
-    then
-        echo "Domain Creation Failed.. Please check the Domain Logs"
-        exit
-    fi
+  # Start Admin Server and tail the logs
+#  ${DOMAIN_HOME}/startWebLogic.sh
+#  touch ${DOMAIN_HOME}/servers/${ADMIN_NAME}/logs/${ADMIN_NAME}.log
+#  tail -f ${DOMAIN_HOME}/servers/${ADMIN_NAME}/logs/${ADMIN_NAME}.log &
 
-    # Create the security file to start the server(s) without the password prompt
-    mkdir -p ${DOMAIN_HOME}/servers/AdminServer/security/
-    echo "username=weblogic" > ${DOMAIN_HOME}/servers/AdminServer/security/boot.properties
-    echo "password=$ADMIN_PASSWORD" >> ${DOMAIN_HOME}/servers/AdminServer/security/boot.properties
-
-    mkdir -p ${DOMAIN_HOME}/servers/infraserver1/security/
-    echo "username=weblogic" > ${DOMAIN_HOME}/servers/infraserver1/security/boot.properties
-    echo "password="$ADMIN_PASSWORD >> ${DOMAIN_HOME}/servers/infraserver1/security/boot.properties
-
-    ${DOMAIN_HOME}/bin/setDomainEnv.sh
-fi
-
+  childPID=$!
+wait $childPID
