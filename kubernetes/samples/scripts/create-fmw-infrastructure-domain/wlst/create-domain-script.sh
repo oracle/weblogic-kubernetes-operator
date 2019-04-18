87e816869500b4a9e0cf5a52e35d3782a8a186f7
Mark Nelson <mark.x.nelson@oracle.com>
2019-04-18 07:24:14 -0400

    remove debugging code

--- kubernetes/samples/scripts/create-fmw-infrastructure-domain/wlst/create-domain-script.sh
+++ kubernetes/samples/scripts/create-fmw-infrastructure-domain/wlst/create-domain-script.sh
@@ -4,25 +4,6 @@
 
 export DOMAIN_HOME=${DOMAIN_HOME_DIR}
 
-echo 'Executing wlst.sh -skipWLSModuleScanning '
-echo '          /u01/weblogic/createFMWDomain.py '
-echo '          -oh /u01/oracle '
-echo '          -jh /usr/java/latest '
-echo "          -parent ${DOMAIN_HOME}/.. "
-echo "          -name ${CUSTOM_DOMAIN_NAME} "
-echo "          -user " `cat /weblogic-operator/secrets/username`
-echo "          -password " `cat /weblogic-operator/secrets/password`
-echo "          -rcuDb ${CUSTOM_CONNECTION_STRING} "
-echo "          -rcuPrefix ${CUSTOM_RCUPREFIX} "
-echo "          -rcuSchemaPwd " `cat /weblogic-operator/rcu-secrets/password`
-echo "          -adminListenPort ${CUSTOM_ADMIN_LISTEN_PORT} "
-echo "          -adminName ${CUSTOM_ADMIN_NAME} "
-echo "          -managedNameBase ${CUSTOM_MANAGED_BASE_NAME} "
-echo "          -managedServerPort ${CUSTOM_MANAGEDSERVER_PORT} "
-echo "          -prodMode ${CUSTOM_PRODUCTION_MODE} "
-echo "          -managedServerCount ${CUSTOM_MANAGED_SERVER_COUNT} "
-echo "          -clusterName ${CUSTOM_CLUSTER_NAME} "
-
 # Create the domain
 wlst.sh -skipWLSModuleScanning \
         /u01/weblogic/createFMWDomain.py \
