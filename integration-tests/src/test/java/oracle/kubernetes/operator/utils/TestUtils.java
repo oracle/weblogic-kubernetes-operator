083be83552bd8a5f222dae40f75af4921a1a9620
MAGGIE.HE@ORACLE.COM <lihe@slc11vis.us.oracle.com>
2019-05-01 08:52:41 -0700

    refine the code

--- integration-tests/src/test/java/oracle/kubernetes/operator/utils/TestUtils.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/utils/TestUtils.java
@@ -884,7 +884,10 @@ public class TestUtils {
     domainMap.put("exposeAdminNodePort", true);
     domainMap.put("adminNodePort", 30700 + number);
     domainMap.put("t3ChannelPort", 30000 + number);
-    domainMap.put("voyagerWebPort", 30344 + number);
+    if (System.getenv("LB_TYPE") != null && System.getenv("LB_TYPE").equalsIgnoreCase("VOYAGER")) {
+      domainMap.put("voyagerWebPort", 30344 + number);
+      logger.info("For this domain voyagerWebPort is set to: 30344 + " + number);
+    }
     return domainMap;
   }
 
