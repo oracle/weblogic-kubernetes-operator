24452f168310427cd1b24ef0bd2ac4a5e5272d65
anthony_lai <anthony.lai@oracle.com>
2019-05-13 09:43:32 -0700

    managed server should use admin port to reach admin server if admin port is enabled

--- operator/src/main/resources/scripts/startNodeManager.sh
+++ operator/src/main/resources/scripts/startNodeManager.sh
@@ -244,7 +244,11 @@ EOF
   [ ! $? -eq 0 ] && trace "Failed to create '${wl_props_file}'." && exit 1
 
   if [ ! "${ADMIN_NAME}" = "${SERVER_NAME}" ]; then
-    echo "AdminURL=http\\://${AS_SERVICE_NAME}\\:${ADMIN_PORT}" >> ${wl_props_file}
+    admin_protocol="http"
+    if [ "${ADMIN_PORT_SECURE}" = "true" ]; then
+      admin_protocol="https"
+    fi  
+    echo "AdminURL=$admin_protocol\\://${AS_SERVICE_NAME}\\:${ADMIN_PORT}" >> ${wl_props_file}
   fi
 fi
 
