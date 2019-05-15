d08329bb54febef5c492c88c8fdca0b156282208
anthony_lai <anthony.lai@oracle.com>
2019-05-15 10:07:50 -0700

    Use -Dweblogic.ssl.AcceptKSSDemoCertsEnabled=true in testJRFDomainAdminPortEnabled test

--- integration-tests/src/test/java/oracle/kubernetes/operator/JrfInOperatorAdvancedTest.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/JrfInOperatorAdvancedTest.java
@@ -286,12 +286,13 @@ public class JrfInOperatorAdvancedTest extends BaseTest {
       domain1Map.put(
           "createDomainPyScript",
           "integration-tests/src/test/resources/domain-home-on-pv/create-jrfdomain-admin-port-enabled.py");
-      // Disables host name verification, which enables you to use the demonstration digital
-      // certificates that are shipped with WebLogic Server.
-      // https://docs.oracle.com/middleware/12213/wls/ADMRF/weblogicserver.htm#ADMRF202
+      // Use -Dweblogic.ssl.AcceptKSSDemoCertsEnabled=true so that managed servers can connect
+      // to admin server using SSL without running into host name verifcation check error
+      // in default JRF domain that uses KSS demo identity and trust
+      // https://docs.oracle.com/middleware/12213/wls/SECMG/kss.htm#SECMG673tm#ADMRF202
       domain1Map.put(
           "javaOptions",
-          "-Dweblogic.StdoutDebugEnabled=false -Dweblogic.security.SSL.ignoreHostnameVerification=true");
+          "-Dweblogic.StdoutDebugEnabled=false -Dweblogic.ssl.AcceptKSSDemoCertsEnabled=true");
 
       // run RCU script to load db schema
       DBUtils.runRCU(rcuPodName, domain1Map);
