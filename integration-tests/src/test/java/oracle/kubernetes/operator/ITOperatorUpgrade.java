4da4dbf744bf59cec429be230e2735752491b964
sankar <sankarpn@gmail.com>
2019-06-21 12:37:37 -0700

    fix

--- integration-tests/src/test/java/oracle/kubernetes/operator/ITOperatorUpgrade.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/ITOperatorUpgrade.java
@@ -86,6 +86,7 @@ public class ITOperatorUpgrade extends BaseTest {
     wlstDomainMap.put("namespace", DOM_NS);
     wlstDomainMap.put("projectRoot", opUpgradeTmpDir + "/weblogic-kubernetes-operator");
     domain = TestUtils.createDomain(wlstDomainMap);
+    checkOperatorVersion("apiVersion: weblogic.oracle/v2");
     domain.verifyDomainCreated();
     testBasicUseCases(domain);
     testClusterScaling(operator20, domain);
