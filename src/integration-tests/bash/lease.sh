7754910549ead1a61c596e8ce4266db014abea7c
Rosemary Marano <rosemary.marano@oracle.com>
2019-06-21 13:29:16 -0400

    fixed copyrights

--- src/integration-tests/bash/lease.sh
+++ src/integration-tests/bash/lease.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2018, Oracle Corporation and/or its affiliates. All rights reserved.
+# Copyright 2018, 2019, Oracle Corporation and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 #
 # Usage:  See 'function usage' below, or call 'lease.sh -h'.
@@ -8,60 +8,60 @@
 
 function usage {
 cat << EOF
- 
+
   Summary:
- 
+
     Use this helper script to obtain an exclusive 10 minute lease
     on a kubernetes cluster, renew this lease, release this lease,
     force the delete of the lease, or view the current lease.
     It exits with a non-zero value on a failure.
 
   Usage:
- 
+
     Obtain       : lease.sh -o id [-t timeout]
-    Renew        : lease.sh -r id  
-    Delete       : lease.sh -d id 
-    Force Delete : lease.sh -f 
+    Renew        : lease.sh -r id
+    Delete       : lease.sh -d id
+    Force Delete : lease.sh -f
     Show         : lease.sh -s
     Help         : lease.sh -h
 
     The user is expected to call this script multiple times throughout
     the ownership of a lease:
- 
-    1) First, call 'lease.sh -o id' to try obtain a lease.  If 
+
+    1) First, call 'lease.sh -o id' to try obtain a lease.  If
        this fails, fail your script, since failure may mean some other
        owner has ownership.  This call will block up to 30 minutes
        while trying to get the lease (tunable via -t), and will grab
        the lease if no other process owns it or no other process has touched
        it for a period of 10 minutes.  'id' should be some unique
-       string suitable for use as a filename - a uuid is preferred but a 
+       string suitable for use as a filename - a uuid is preferred but a
        pid is usually sufficient.
- 
+
     2) While running, call 'lease.sh -r id' every few minutes
        to renew the lease and retain ownership.   Fail if this fails,
        since that means you may have lost the lease.  Be careful to call
-       this within 10 minutes of last obtaining or renewing the lease, 
+       this within 10 minutes of last obtaining or renewing the lease,
        otherwise the lease expires and another process is free to obtain
        ownership.  Use the same value of 'id' as was specified in step (1).
-  
+
     3) Finally, call 'lease.sh -d id' to delete and give up your
        lease.  This will fail if you are not the current owner.  Use the same
-       value of 'id' as was specified in step (1).  If you forget to 
+       value of 'id' as was specified in step (1).  If you forget to
        call it, no one else will be able to get the lease for up to 10
        minutes.
- 
+
     To show who currently owns a lease, call 'lease.sh -s' or call
     'kubectl get cm acceptance-test-lease -o yaml -n default'.
-   
+
     To force delete a lease no matter who owns the lease, call
-    'lease.sh -f' or 'kubectl delete cm acceptance-test-lease -n default'.  
+    'lease.sh -f' or 'kubectl delete cm acceptance-test-lease -n default'.
     This should only be done if you're sure there's
     no current processe that owns the lease.
 
   Warnings:
- 
+
     This is a 'best effort' script -- it still has potential race
-    conditions.  They should be pretty rare, but don't bet the farm on 
+    conditions.  They should be pretty rare, but don't bet the farm on
     them not happening!
 
     This script requires cooperation among all users of the k8s cluster that
@@ -69,15 +69,15 @@ cat << EOF
     doesn't directly grant exclusive access, it just can be used to make sure
     that no other callers of the same script have the lease.
 
- 
+
   Internals:
- 
+
     This script maintains a local file '[tmp_root]/acceptance-test-lease'
     which is a potential copy of the remote lease. tmp_root is /tmp/lease
     by default.  You can optionally specify a different tmp_root
     using the hidden '-l' option described above, but always specify the
     same tmp_root across all steps.
- 
+
     This script also maintains a lease record in the remote kubernetes
     cluster in a config map named 'acceptance-test-lease' that's
     located in the default namespace.
@@ -92,7 +92,7 @@ EOF
 
 
 function main {
-  # 
+  #
   # The main for this script.  Arguments come from the command line.
   #
 
@@ -110,7 +110,7 @@ function main {
   # Dir location of potential local copy of remote lease (see hidden '-l' option):
   LOCAL_ROOT=${LOCAL_ROOT:-/tmp}
 
-  # File name of potential copy of remote lease.  
+  # File name of potential copy of remote lease.
   # WARNING:  Do not include a '.' in the file name, this messes up jsonpath parsing.
   LOCAL_FILE=acceptance-test-lease-props
 
@@ -242,7 +242,7 @@ function getLocalLease {
 
 function makeLocalLease {
 #
-# Make a candidate lease file in ${LOCAL_ROOT}/[expired/]${LOCAL_FILE} 
+# Make a candidate lease file in ${LOCAL_ROOT}/[expired/]${LOCAL_FILE}
 # Embed id, hostname, timestamp, etc to make it easy to identify the owner.
 # returns non-zero if fails
 # Usage: Pass "expired" as $1 to create a lease that immediately expires
@@ -308,18 +308,18 @@ function makeLocalLeaseAndReplaceRemote {
 
   deleteRemoteLeaseUnsafe
   if [ $? -ne 0 ]; then
-    traceError "failed - could not delete remote lease" 
+    traceError "failed - could not delete remote lease"
     return 1
   fi
 
   kubectl create configmap ${CONFIGMAP_NAME} --from-file ${LOCAL_ROOT}/${LOCAL_FILE} -n default
   if [ $? -ne 0 ]; then
-    traceError "failed - could not replace" 
+    traceError "failed - could not replace"
     return 1
   fi
 
   # finally, check if we now actually own the lease (someone could have been replacing at the same time)
-  checkLease 
+  checkLease
   if [ $? -ne 0 ]; then
     traceError "failed - replaced remote lease, but we somehow lost a race or can no longer communicate with kubernetes"
     return 1
@@ -330,7 +330,7 @@ function makeLocalLeaseAndReplaceRemote {
 function getRemoteLease {
   #
   #  first, if the remote lease configmap doesn't exist
-  #         then try create one with a lease that's already expired 
+  #         then try create one with a lease that's already expired
   #  second, try show the lease in stdout
   #  return non-zero if this fails
   #
@@ -434,7 +434,7 @@ function obtainLease {
     local mnow=`date +%s`
     if [ $((mnow)) -gt $((remote_ts + ABANDON_TIMEOUT_SECONDS)) ]; then
       # the remote lease hasn't been updated for ABANDON_TIMEOUT_SECONDS
-      # so assume it can be replaced and we can try takeover the lease 
+      # so assume it can be replaced and we can try takeover the lease
 
       # first make a local candidate lease
       makeLocalLeaseAndReplaceRemote
@@ -468,7 +468,7 @@ function renewLease {
   #
 
   # first, check if we actually still own the lease (someone could have replaced it or deleted it)
-  checkLease 
+  checkLease
   if [ $? -ne 0 ]; then
     traceError "failed - we no longer own the lease"
     return 1
@@ -489,9 +489,9 @@ function deleteRemoteLeaseSafe {
   # delete the remote lease if we determine we're the local owner
   # fail otherwise
   #
-  checkLease 
+  checkLease
   if [ $? -ne 0 ]; then
-    traceError "failed - could not delete lease, we are not the owner or kubectl is not working" 
+    traceError "failed - could not delete lease, we are not the owner or kubectl is not working"
     return 1
   fi
   kubectl delete cm ${CONFIGMAP_NAME} -n default
