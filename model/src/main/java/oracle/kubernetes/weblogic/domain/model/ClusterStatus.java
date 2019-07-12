5a293c52a39453658ecf19dc7a8f52074c48e6e3
Ryan Eberhard <ryan.eberhard@oracle.com>
2019-07-12 19:55:06 -0400

    Add cluster status and other corrections

---
+++ model/src/main/java/oracle/kubernetes/weblogic/domain/model/ClusterStatus.java
@@ -0,0 +1,129 @@
+// Copyright 2019, Oracle Corporation and/or its affiliates.  All rights reserved.
+// Licensed under the Universal Permissive License v 1.0 as shown at
+// http://oss.oracle.com/licenses/upl.
+
+package oracle.kubernetes.weblogic.domain.model;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import oracle.kubernetes.json.Description;
+import oracle.kubernetes.json.Range;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+/** ServerStatus describes the current status of a specific WebLogic Server. */
+public class ClusterStatus implements Comparable<ClusterStatus> {
+
+  @Description("WebLogic cluster name. Required.")
+  @SerializedName("clusterName")
+  @Expose
+  private String clusterName;
+
+  /** The number of intended cluster members. Required. */
+  @Description("The number of intended cluster members. Required.")
+  @Range(minimum = 0)
+  private Integer replicas;
+
+  /** The number of ready cluster members. Required. */
+  @Description("The number of ready cluster members. Required.")
+  @Range(minimum = 0)
+  private Integer readyReplicas;
+
+  /**
+   * WebLogic cluster name. Required.
+   *
+   * @return cluster name
+   */
+  public String getClusterName() {
+    return clusterName;
+  }
+
+  /**
+   * WebLogic cluster name. Required.
+   *
+   * @param clusterName cluster name
+   */
+  public void setClusterName(String clusterName) {
+    this.clusterName = clusterName;
+  }
+
+  /**
+   * WebLogic cluster name. Required.
+   *
+   * @param clusterName cluster name
+   * @return this
+   */
+  public ClusterStatus withClusterName(String clusterName) {
+    this.clusterName = clusterName;
+    return this;
+  }
+
+  public Integer getReplicas() {
+    return replicas;
+  }
+
+  public void setReplicas(Integer replicas) {
+    this.replicas = replicas;
+  }
+
+  public ClusterStatus withReplicas(Integer replicas) {
+    this.replicas = replicas;
+    return this;
+  }
+
+  public Integer getReadyReplicas() {
+    return readyReplicas;
+  }
+
+  public void setReadyReplicas(Integer readyReplicas) {
+    this.readyReplicas = readyReplicas;
+  }
+
+  public ClusterStatus withReadyReplicas(Integer readyReplicas) {
+    this.readyReplicas = readyReplicas;
+    return this;
+  }
+
+  @Override
+  public String toString() {
+    return new ToStringBuilder(this)
+        .append("clusterName", clusterName)
+        .append("replicas", replicas)
+        .append("readyReplicas", readyReplicas)
+        .toString();
+  }
+
+  @Override
+  public int hashCode() {
+    return new HashCodeBuilder()
+        .append(clusterName)
+        .append(replicas)
+        .append(readyReplicas)
+        .toHashCode();
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    if (other == this) {
+      return true;
+    }
+    if (!(other instanceof ClusterStatus)) {
+      return false;
+    }
+    ClusterStatus rhs = ((ClusterStatus) other);
+    return new EqualsBuilder()
+        .append(clusterName, rhs.clusterName)
+        .append(replicas, rhs.replicas)
+        .append(readyReplicas, rhs.readyReplicas)
+        .isEquals();
+  }
+
+  @Override
+  public int compareTo(ClusterStatus o) {
+    return clusterName.compareTo(o.clusterName);
+  }
+}
