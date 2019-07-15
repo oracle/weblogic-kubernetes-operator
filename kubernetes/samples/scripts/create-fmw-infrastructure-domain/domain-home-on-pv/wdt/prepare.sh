06d44fb17229cc755586d84f0b93090241f41d3e
ANTARYAMI.PANIGRAHI@ORACLE.COM <antaryami.panigrahi@oracle.com>
2019-07-15 11:44:54 -0700

    JRF Domain Sample Enhancement

---
+++ kubernetes/samples/scripts/create-fmw-infrastructure-domain/domain-home-on-pv/wdt/prepare.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Copyright 2018, 2019, Oracle Corporation and/or its affiliates.  All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+#
+
+# Perform preparation based on the specified cluster type
+
+function usage {
+  echo usage: ${script} -i file [-h]
+  echo "  -i Directory, must be specified."
+  echo "  -h Help"
+  exit $1
+}
+
+
+while getopts "hi:" opt; do
+  case $opt in
+    i) externalFilesTmpDir="${OPTARG}"
+    ;;
+    h) usage 0
+    ;;
+    *) usage 1
+    ;;
+  esac
+done
+
+echo Preparing the model script
+# JRF does not support Dynamic Cluster Model 
+cp ${externalFilesTmpDir}/wdt_model_configured.yaml ${externalFilesTmpDir}/wdt_model.yaml
