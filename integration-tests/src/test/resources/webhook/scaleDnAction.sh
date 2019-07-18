c1411a3913039e005f190d25e14518bd23b9c0ab
Marina Kogan <marina.kogan@oracle.com>
2019-07-18 23:46:14 +0000

    fixed ns and cluster name

--- integration-tests/src/test/resources/webhook/scaleDnAction.sh
+++ integration-tests/src/test/resources/webhook/scaleDnAction.sh
@@ -2,4 +2,4 @@
 echo scale down  action
 MASTER=https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT
 echo Kubernetes master is $MASTER
-source /var/scripts/scalingAction.sh --action=scaleDown --domain_uid=domain1 --cluster_name=DockerCluster --kubernetes_master=$MASTER --wls_domain_namespace=weblogic-domain
+source /var/scripts/scalingAction.sh --action=scaleDown --domain_uid=test5 --cluster_name=cluster-1 --kubernetes_master=$MASTER --wls_domain_namespace=test5
