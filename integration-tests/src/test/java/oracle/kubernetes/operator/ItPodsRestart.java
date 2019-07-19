8fc9f70f0ed87cf570405035c384728d83a42fe1
Maggie He <maggie.he@oracle.com>
2019-07-19 11:35:50 -0700

    turn on QUICKTEST

--- integration-tests/src/test/java/oracle/kubernetes/operator/ItPodsRestart.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/ItPodsRestart.java
@@ -52,7 +52,7 @@ public class ItPodsRestart extends BaseTest {
   @BeforeClass
   public static void staticPrepare() throws Exception {
     // initialize test properties and create the directories
-    if (!QUICKTEST) {
+    if (QUICKTEST) {
       initialize(APP_PROPS_FILE);
 
       logger.info("Checking if operator1 and domain are running, if not creating");
@@ -79,7 +79,7 @@ public class ItPodsRestart extends BaseTest {
    */
   @AfterClass
   public static void staticUnPrepare() throws Exception {
-    if (!QUICKTEST) {
+    if (QUICKTEST) {
       logger.info("+++++++++++++++++++++++++++++++++---------------------------------+");
       logger.info("BEGIN");
       logger.info("Run once, release cluster lease");
@@ -213,7 +213,7 @@ public class ItPodsRestart extends BaseTest {
    */
   @Test
   public void testServerPodsRestartByChangingZImage() throws Exception {
-    Assume.assumeFalse(QUICKTEST);
+    Assume.assumeTrue(QUICKTEST);
     String testMethodName = new Object() {}.getClass().getEnclosingMethod().getName();
     logTestBegin(testMethodName);
 
