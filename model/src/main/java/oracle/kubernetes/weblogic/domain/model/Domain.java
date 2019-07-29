9b313654650287d352a21326b3124905904bf20e
David Zhao <liang.x.zhao@oracle.com>
2019-07-29 13:55:30 +0800

    make istio object in Domain yaml work

--- model/src/main/java/oracle/kubernetes/weblogic/domain/model/Domain.java
+++ model/src/main/java/oracle/kubernetes/weblogic/domain/model/Domain.java
@@ -369,6 +369,10 @@ public class Domain {
     return spec.istioEnabled();
   }
 
+  public int getIstioReadinessPort() {
+    return spec.getIstioReadinessPort();
+  }
+
   /**
    * Returns the domain home.
    *
