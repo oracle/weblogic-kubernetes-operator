153ab165c13c33879a41d05e1f40e6d500961884
Marina Kogan <marina.kogan@oracle.com>
2019-07-31 22:30:52 +0000

    added copyrights

--- integration-tests/src/test/resources/webhook/scaleUpAction.sh
+++ integration-tests/src/test/resources/webhook/scaleUpAction.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Copyright 2019, Oracle Corporation and/or its affiliates.  All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upload
 echo scale up action > scaleUpAction.log
 MASTER=https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT
 echo Kubernetes master is $MASTER >> scaleUpAction.log
