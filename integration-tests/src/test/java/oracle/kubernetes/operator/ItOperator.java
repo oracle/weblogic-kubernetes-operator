7a056ffc9d7a6e825f74a8e6ffc6a67795f7c725
Vanajakshi Mukkara <VANAJAKSHI.MUKKARA@ORACLE.COM>
2019-08-07 20:20:47 -0700

    fix NPE

--- integration-tests/src/test/java/oracle/kubernetes/operator/ItOperator.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/ItOperator.java
@@ -312,8 +312,12 @@ public class ItOperator extends BaseTest {
       if (!domainUidsToBeDeleted.equals("")) {
         logger.info("About to delete domains: " + domainUidsToBeDeleted);
         TestUtils.deleteWeblogicDomainResources(domainUidsToBeDeleted);
-        TestUtils.verifyAfterDeletion(domain1);
+        if(domain1 != null) {
+          TestUtils.verifyAfterDeletion(domain1);
+        }
+        if(domain2 != null) {
         TestUtils.verifyAfterDeletion(domain2);
+        }
       }
     }
     logger.info("SUCCESS - " + testMethodName);
