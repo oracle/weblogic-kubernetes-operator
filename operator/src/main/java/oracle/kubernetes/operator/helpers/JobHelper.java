3028211370f5fc2342a4aecfe38b16c8615093ee
Russell Gold <russell.gold@oracle.com>
2019-08-08 14:36:30 -0400

    Update domain status for image fetch or instrospector job failures

--- operator/src/main/java/oracle/kubernetes/operator/helpers/JobHelper.java
+++ operator/src/main/java/oracle/kubernetes/operator/helpers/JobHelper.java
@@ -4,11 +4,6 @@
 
 package oracle.kubernetes.operator.helpers;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.StringReader;
-import java.util.List;
-
 import io.kubernetes.client.models.V1DeleteOptions;
 import io.kubernetes.client.models.V1EnvVar;
 import io.kubernetes.client.models.V1Job;
@@ -16,6 +11,8 @@ import io.kubernetes.client.models.V1Pod;
 import io.kubernetes.client.models.V1PodList;
 import io.kubernetes.client.models.V1Volume;
 import io.kubernetes.client.models.V1VolumeMount;
+import java.util.ArrayList;
+import java.util.List;
 import oracle.kubernetes.operator.JobWatcher;
 import oracle.kubernetes.operator.LabelConstants;
 import oracle.kubernetes.operator.ProcessingConstants;
@@ -37,14 +34,14 @@ import oracle.kubernetes.weblogic.domain.model.Domain;
 import oracle.kubernetes.weblogic.domain.model.DomainSpec;
 import oracle.kubernetes.weblogic.domain.model.ManagedServer;
 
-
 public class JobHelper {
 
   static final String START_TIME = "WlsRetriever-startTime";
   private static final LoggingFacade LOGGER = LoggingFactory.getLogger("Operator", "Operator");
+  static final String INTROSPECTOR_LOG_PREFIX = "Introspector Job Log: ";
+  static final String EOL_PATTERN = "\\r?\\n";
 
-  private JobHelper() {
-  }
+  private JobHelper() {}
 
   static String createJobName(String domainUid) {
     return LegalNames.toJobIntrospectorName(domainUid);
@@ -133,14 +130,11 @@ public class JobHelper {
   /**
    * Factory for {@link Step} that deletes WebLogic domain introspector job.
    *
-   * @param domainUid The unique identifier assigned to the Weblogic domain when it was registered
-   * @param namespace Namespace
    * @param next Next processing step
    * @return Step for deleting the domain introsepctor jod
    */
-  public static Step deleteDomainIntrospectorJobStep(
-      String domainUid, String namespace, Step next) {
-    return new DeleteIntrospectorJobStep(domainUid, namespace, next);
+  public static Step deleteDomainIntrospectorJobStep(Step next) {
+    return new DeleteIntrospectorJobStep(next);
   }
 
   private static Step createWatchDomainIntrospectorJobReadyStep(Step next) {
@@ -155,7 +149,7 @@ public class JobHelper {
    */
   private static Step readDomainIntrospectorPodLogStep(Step next) {
     return createWatchDomainIntrospectorJobReadyStep(
-        readDomainIntrospectorPodStep(new ReadDomainIntrospectorPodLogStep(next)));
+        readDomainIntrospectorPodStep(readDomainIntrospectorPodLog(next)));
   }
 
   /**
@@ -249,7 +243,8 @@ public class JobHelper {
         return doNext(
             context.createNewJob(
                 readDomainIntrospectorPodLogStep(
-                    ConfigMapHelper.createSitConfigMapStep(getNext()))),
+                    deleteDomainIntrospectorJobStep(
+                        ConfigMapHelper.createSitConfigMapStep(getNext())))),
             packet);
       }
 
@@ -258,18 +253,14 @@ public class JobHelper {
   }
 
   private static class DeleteIntrospectorJobStep extends Step {
-    private String domainUid;
-    private String namespace;
 
-    DeleteIntrospectorJobStep(String domainUid, String namespace, Step next) {
+    DeleteIntrospectorJobStep(Step next) {
       super(next);
-      this.domainUid = domainUid;
-      this.namespace = namespace;
     }
 
     @Override
     public NextAction apply(Packet packet) {
-      return doNext(deleteJob(getNext()), packet);
+      return doNext(deleteJob(packet, getNext()), packet);
     }
 
     String getJobDeletedMessageKey() {
@@ -280,16 +271,23 @@ public class JobHelper {
       LOGGER.info(getJobDeletedMessageKey(), domainUid, namespace, jobName);
     }
 
-    private Step deleteJob(Step next) {
-      String jobName = JobHelper.createJobName(this.domainUid);
-      logJobDeleted(this.domainUid, namespace, jobName);
+    private Step deleteJob(Packet packet, Step next) {
+      DomainPresenceInfo info = packet.getSpi(DomainPresenceInfo.class);
+      java.lang.String domainUid = info.getDomain().getDomainUid();
+      java.lang.String namespace = info.getNamespace();
+      String jobName = JobHelper.createJobName(domainUid);
+      logJobDeleted(domainUid, namespace, jobName);
       return new CallBuilder()
           .deleteJobAsync(
               jobName,
-              this.namespace,
+              namespace,
               new V1DeleteOptions().propagationPolicy("Foreground"),
               new DefaultResponseStep<>(next));
     }
+  }
+
+  static ReadDomainIntrospectorPodLogStep readDomainIntrospectorPodLog(Step next) {
+    return new ReadDomainIntrospectorPodLogStep(next);
   }
 
   private static class ReadDomainIntrospectorPodLogStep extends Step {
@@ -303,7 +301,7 @@ public class JobHelper {
       DomainPresenceInfo info = packet.getSpi(DomainPresenceInfo.class);
       String namespace = info.getNamespace();
 
-      String jobPodName = (String) packet.get(ProcessingConstants.JOB_POD_NAME);
+      String jobPodName = (String) packet.remove(ProcessingConstants.JOB_POD_NAME);
 
       return doNext(readDomainIntrospectorPodLog(jobPodName, namespace, getNext()), packet);
     }
@@ -316,51 +314,102 @@ public class JobHelper {
   }
 
   private static class ReadDomainIntrospectorPodLogResponseStep extends ResponseStep<String> {
+    private StringBuilder logMessage = new StringBuilder();
+    private List<String> severeStatuses = new ArrayList<>();
+
     ReadDomainIntrospectorPodLogResponseStep(Step nextStep) {
       super(nextStep);
     }
 
     @Override
-    public NextAction onFailure(Packet packet, CallResponse<String> callResponse) {
-      cleanupJobArtifacts(packet);
-      return super.onFailure(packet, callResponse);
-    }
-
-    @Override
     public NextAction onSuccess(Packet packet, CallResponse<String> callResponse) {
       String result = callResponse.getResult();
-
-      // Log output to Operator log
       LOGGER.fine("+++++ ReadDomainIntrospectorPodLogResponseStep: \n" + result);
 
-      // Parse out each job log message and log to Operator as SEVERE, FINE, etc...
       if (result != null) {
         convertJobLogsToOperatorLogs(result);
+        if (!severeStatuses.isEmpty()) updateStatus(packet.getSpi(DomainPresenceInfo.class));
+        packet.put(ProcessingConstants.DOMAIN_INTROSPECTOR_LOG_RESULT, result);
       }
 
-      V1Job domainIntrospectorJob = (V1Job) packet.get(ProcessingConstants.DOMAIN_INTROSPECTOR_JOB);
-      if (domainIntrospectorJob != null && JobWatcher.isComplete(domainIntrospectorJob)) {
-        if (result != null) {
-          packet.put(ProcessingConstants.DOMAIN_INTROSPECTOR_LOG_RESULT, result);
-        }
+      V1Job domainIntrospectorJob =
+          (V1Job) packet.remove(ProcessingConstants.DOMAIN_INTROSPECTOR_JOB);
+      if (isNotComplete(domainIntrospectorJob)) return onFailure(packet, callResponse);
 
-        // Delete the job once we've successfully read the result
-        DomainPresenceInfo info = packet.getSpi(DomainPresenceInfo.class);
-        java.lang.String domainUid = info.getDomain().getDomainUid();
-        java.lang.String namespace = info.getNamespace();
-
-        cleanupJobArtifacts(packet);
-
-        return doNext(
-            JobHelper.deleteDomainIntrospectorJobStep(domainUid, namespace, getNext()), packet);
-      }
-
-      return onFailure(packet, callResponse);
+      return doNext(packet);
     }
 
-    private void cleanupJobArtifacts(Packet packet) {
-      packet.remove(ProcessingConstants.JOB_POD_NAME);
-      packet.remove(ProcessingConstants.DOMAIN_INTROSPECTOR_JOB);
+    private boolean isNotComplete(V1Job domainIntrospectorJob) {
+      return domainIntrospectorJob == null || !JobWatcher.isComplete(domainIntrospectorJob);
+    }
+
+    // Parse log messages out of a Job Log
+    //  - assumes each job log message starts with '@['
+    //  - assumes any lines that don't start with '@[' are part
+    //    of the previous log message
+    //  - ignores all lines in the log up to the first line that starts with '@['
+    private void convertJobLogsToOperatorLogs(String jobLogs) {
+      for (String line : jobLogs.split(EOL_PATTERN)) {
+        if (line.startsWith("@[")) {
+          logToOperator();
+          logMessage = new StringBuilder(INTROSPECTOR_LOG_PREFIX).append(line.trim());
+        } else if (logMessage.length() > 0) {
+          logMessage.append(System.lineSeparator()).append(line.trim());
+        }
+      }
+      logToOperator();
+    }
+
+    private void logToOperator() {
+      if (logMessage.length() == 0) return;
+
+      String logMsg = logMessage.toString();
+      switch (getLogLevel(logMsg)) {
+        case "SEVERE":
+          addSevereStatus(logMsg); // fall through
+        case "ERROR":
+          LOGGER.severe(logMsg);
+          break;
+        case "WARNING":
+          LOGGER.warning(logMsg);
+          break;
+        case "INFO":
+          LOGGER.info(logMsg);
+          break;
+        case "FINER":
+          LOGGER.finer(logMsg);
+          break;
+        case "FINEST":
+          LOGGER.finest(logMsg);
+          break;
+        case "FINE":
+        default:
+          LOGGER.fine(logMsg);
+          break;
+      }
+    }
+
+    private void addSevereStatus(String logMsg) {
+      int index = logMsg.toUpperCase().lastIndexOf("[SEVERE]") + "[SEVERE]".length();
+      severeStatuses.add(logMsg.substring(index).trim());
+    }
+
+    private String getLogLevel(String logMsg) {
+      String regExp = ".*\\[(SEVERE|ERROR|WARNING|INFO|FINE|FINER|FINEST)].*";
+      return getFirstLine(logMsg).toUpperCase().replaceAll(regExp, "$1");
+    }
+
+    private String getFirstLine(String logMsg) {
+      return logMsg.split(EOL_PATTERN)[0];
+    }
+
+    private void updateStatus(DomainPresenceInfo domainPresenceInfo) {
+      KubernetesUtils.updateStatus(
+          domainPresenceInfo.getDomain(), "ErrIntrospector", onSeparateLines(severeStatuses));
+    }
+
+    private String onSeparateLines(List<String> lines) {
+      return String.join(System.lineSeparator(), lines);
     }
   }
 
@@ -382,14 +431,14 @@ public class JobHelper {
     private Step readDomainIntrospectorPod(String domainUid, String namespace, Step next) {
       return new CallBuilder()
           .withLabelSelectors(LabelConstants.JOBNAME_LABEL)
-          .listPodAsync(namespace, new PodListStep(domainUid, namespace, next));
+          .listPodAsync(namespace, new PodListStep(domainUid, next));
     }
   }
 
   private static class PodListStep extends ResponseStep<V1PodList> {
     private final String domainUid;
 
-    PodListStep(String domainUid, String ns, Step next) {
+    PodListStep(String domainUid, Step next) {
       super(next);
       this.domainUid = domainUid;
     }
@@ -413,70 +462,6 @@ public class JobHelper {
       }
 
       return doNext(packet);
-    }
-  }
-
-  // Convert a job log message to an operator log message
-  //   - assumes the messages contins a string like '[SEVERE]', etc, if
-  //     not, then assumes the log level is 'FINE'.
-  private static void logToOperator(StringBuffer logStr, StringBuffer extraStr) {
-    String logMsg = "Introspector Job Log: " 
-                    + logStr 
-                    + (extraStr.length() == 0 ? "" : "\n")
-                    + extraStr;
-    String regExp = ".*\\[(SEVERE|ERROR|WARNING|INFO|FINE|FINER|FINEST)\\].*";
-    String logLevel = logStr.toString().toUpperCase().replaceAll(regExp,"$1");
-    switch (logLevel) {
-      case "ERROR":   LOGGER.severe(logMsg);  
-                      break;
-      case "SEVERE":  LOGGER.severe(logMsg);  
-                      break;
-      case "WARNING": LOGGER.warning(logMsg); 
-                      break;
-      case "INFO":    LOGGER.info(logMsg);    
-                      break;
-      case "FINE":    LOGGER.fine(logMsg);    
-                      break;
-      case "FINER":   LOGGER.finer(logMsg);   
-                      break;
-      case "FINEST":  LOGGER.finest(logMsg);  
-                      break;
-      default:        LOGGER.fine(logMsg);    
-                      break;
-    }
-  }
-
-  // Parse log messages out of a Job Log 
-  //  - assumes each job log message starts with '@['
-  //  - assumes any lines that don't start with '@[' are part 
-  //    of the previous log message
-  //  - ignores all lines in the log up to the first line that starts with '@['
-  private static void convertJobLogsToOperatorLogs(String jobLogs) {
-    try { 
-      StringBuffer logString = new StringBuffer(1000);
-      StringBuffer logExtra = new StringBuffer(1000);
-      try (BufferedReader reader = new BufferedReader(new StringReader(jobLogs))) {
-        for (String line = reader.readLine(); line != null; line = reader.readLine()) {
-          if (line.startsWith("@[")) {
-            if (logString.length() > 0) { 
-              logToOperator(logString, logExtra);
-              logString.setLength(0);
-              logExtra.setLength(0);
-            }
-            logString.append(line);
-          } else if (logString.length() != 0) {
-            if (logExtra.length() > 0) { 
-              logExtra.append('\n');
-            }
-            logExtra.append(line);
-          }
-        }
-        if (logString.length() > 0) { 
-          logToOperator(logString, logExtra);
-        }
-      }
-    } catch (IOException ioe) {
-      LOGGER.fine(MessageKeys.JOB_LOG_PARSE_FAILURE, ioe.toString(), jobLogs);
     }
   }
 }
