8962503c1a6112c5539c65c7a2b012540f7c6300
sankar <sankarpn@gmail.com>
2019-08-19 14:53:08 -0700

    remove the namespace from host name

--- integration-tests/src/test/java/oracle/kubernetes/operator/SitConfig.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/SitConfig.java
@@ -99,7 +99,7 @@ public class SitConfig extends BaseTest {
               + "/domain.yaml";
       // copy the jmx test client file the administratioin server weblogic server pod
       ADMINPODNAME = domain.getDomainUid() + "-" + domain.getAdminServerName();
-      fqdn = domain.getDomainNs() + "-" + ADMINPODNAME;
+      fqdn = ADMINPODNAME;
       TestUtils.copyFileViaCat(
           TEST_RES_DIR + "sitconfig/java/SitConfigTests.java",
           "SitConfigTests.java",
