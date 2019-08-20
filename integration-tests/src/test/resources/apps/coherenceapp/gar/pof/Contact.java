ae81d3fac640d16ccf0ef62c6779ba1369545f4a
BHAVANI.RAVICHANDRAN@ORACLE.COM <bravicha@den01smd.us.oracle.com>
2019-08-20 10:54:12 -0700

    making changes after review

--- integration-tests/src/test/resources/apps/coherenceapp/gar/pof/Contact.java
+++ integration-tests/src/test/resources/apps/coherenceapp/gar/pof/Contact.java
@@ -1,6 +1,6 @@
-<!-- Copyright 2019, Oracle Corporation and/or its affiliates.  All rights reserved.
-     Licensed under the Universal Permissive License v 1.0 as shown at
-          http://oss.oracle.com/licenses/upl.-->
+// Copyright 2019, Oracle Corporation and/or its affiliates.  All rights reserved.
+// Licensed under the Universal Permissive License v 1.0 as shown at
+// http://oss.oracle.com/licenses/upl.
 
 package com.examples.pof;
 
@@ -10,120 +10,101 @@ import com.tangosol.io.pof.PortableObject;
 
 import com.tangosol.util.Base;
 
-import java.io.IOException;
-
 import java.util.Date;
 
 /**
- * * @author Sanjay Mantoor *
  *  POF serialization implementation for coherence cache
  */
 public class Contact implements PortableObject {
 
-	private String m_sFirstName;
-	private String m_sLastName;
+  // The POF index for the FirstName property.
+  public static final int FIRSTNAME = 0;
 
-	public Contact() {
+  // The POF index for the LastName property.
+  public static final int LASTNAME = 1;
 
-	}
+  private String m_sFirstName;
+  private String m_sLastName;
 
-	public Contact(String firstName, String lastName) {
-		m_sFirstName = firstName;
-		m_sLastName = lastName;
-	}
+  public Contact() {
+  }
 
-	/**
-	 * * Return the first name. * * @return the first name
-	 */
-	public String getFirstName() {
-		return m_sFirstName;
-	}
+  public Contact(String firstName, String lastName) {
+    m_sFirstName = firstName;
+    m_sLastName = lastName;
+  }
 
-	/**
-	 * * Set the first name. * * @param sFirstName the first name
-	 */
-	public void setFirstName(String sFirstName) {
-		m_sFirstName = sFirstName;
-	}
+  /**
+   * Return the first name. 
+   */
+  public String getFirstName() {
+    return m_sFirstName;
+  }
 
-	/**
-	 * * Return the last name. * * @return the last name
-	 */
-	public String getLastName() {
-		return m_sLastName;
-	}
+  /**
+   * Set the first name.
+   * @param sFirstName the first name
+   */
+  public void setFirstName(String sFirstName) {
+    m_sFirstName = sFirstName;
+  }
 
-	/**
-	 * * Set the last name. * * @param sLastName the last name
-	 */
-	public void setLastName(String sLastName) {
-		m_sLastName = sLastName;
-	}
+  /**
+   * Return the last name.
+   */
+  public String getLastName() {
+    return m_sLastName;
+  }
 
-	/**
-	 * * {@inheritDoc}
-	 */
-	public void readExternal(PofReader reader) throws IOException {
-		m_sFirstName = reader.readString(FIRSTNAME);
-		m_sLastName = reader.readString(LASTNAME);
-	}
+  /**
+   * Set the last name
+   * @param sLastName the last name
+   */
+  public void setLastName(String sLastName) {
+    m_sLastName = sLastName;
+  }
 
-	/**
-	 * * {@inheritDoc}
-	 */
-	public void writeExternal(PofWriter writer) throws IOException {
-		writer.writeString(FIRSTNAME, m_sFirstName);
-		writer.writeString(LASTNAME, m_sLastName);
-	}
+  public void readExternal(PofReader reader) throws IOException {
+    m_sFirstName = reader.readString(FIRSTNAME);
+    m_sLastName = reader.readString(LASTNAME);
+  }
 
-	/**
-	 * * {@inheritDoc}
-	 */
-	public String toString() {
-		StringBuilder sb = new StringBuilder(getFirstName()).append(" ").append(getLastName());
-		return sb.toString();
-	}
+  public void writeExternal(PofWriter writer) throws IOException {
+    writer.writeString(FIRSTNAME, m_sFirstName);
+    writer.writeString(LASTNAME, m_sLastName);
+  }
 
-	/**
-	 * * Returns the hashCode of this {@link Contact}. * * @return the hashCode
-	 * of this {@link Contact}
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((m_sFirstName == null) ? 0 : m_sFirstName.hashCode());
-		result = prime * result + ((m_sLastName == null) ? 0 : m_sLastName.hashCode());
-		return result;
-	}
+  public String toString() {
+    StringBuilder sb = new StringBuilder(getFirstName()).append(" ").append(getLastName());
+    return sb.toString();
+  }
 
-	/**
-	 * * Compares this {@link Contact} with another. * * @param obj
-	 * {@link Contact} to compare to * * @return if the contacts are equal
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
+  /**
+   * Returns the hashCode of the Contact
+   */
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
 
-		if (obj == null) {
-			return false;
-		}
+    result = prime * result + ((m_sFirstName == null) ? 0 : m_sFirstName.hashCode());
+    result = prime * result + ((m_sLastName == null) ? 0 : m_sLastName.hashCode());
+    return result;
+  }
 
-		Contact that = (Contact) obj;
-
-		return Base.equals(that.getFirstName(), getFirstName()) && Base.equals(that.getLastName(), getLastName());
-	}
-
-	/**
-	 * * The POF index for the FirstName property.
-	 */
-	public static final int FIRSTNAME = 0;
-
-	/**
-	 * * The POF index for the LastName property.
-	 */
-	public static final int LASTNAME = 1;
-
+  /**
+   * Compares this Contact with another
+   * @param obj
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    Contact that = (Contact) obj;
+    return Base.equals(that.getFirstName(), getFirstName()) && Base.equals(that.getLastName(), getLastName());
+  }
 }
