a10f424b8ed0a73107bd332b1f5fe8fcd1d65622
Johnny Shum <johnny.shum@oracle.com>
2019-09-17 15:16:30 -0500

    stage changes

--- kubernetes/samples/scripts/create-weblogic-domain/model-in-image/build.sh
+++ kubernetes/samples/scripts/create-weblogic-domain/model-in-image/build.sh
@@ -57,35 +57,7 @@ cd ${WORKDIR}
 #
 ./build_baseimage.sh
 
-# # unzip weblogic-image-tool.zip
-# # #
-# # echo Setting up imagetool
-# #
-# IMGTOOL_BIN=${WORKDIR}/imagetool-*/bin/imagetool.sh
-# #source ${WORKDIR}/imagetool-*/bin/setup.sh
-# #
-# mkdir cache
-# export WLSIMG_CACHEDIR=`pwd`/cache
-# export WLSIMG_BLDDIR=`pwd`
-# #
-# unzip ${SERVER_JRE}.zip
-
-# ${IMGTOOL_BIN} cache addInstaller --type jdk --version 8u221 --path `pwd`/server-jre-8u221-linux-x64.tar.gz
-# if [ "${DOMAINTYPE}" == "WLS" ] ; then
-#     ${IMGTOOL_BIN} cache addInstaller --type wls --version 12.2.1.3.0 --path `pwd`/${WLS_INSTALLER}
-#     IMGTYPE=wls
-# else 
-#     ${IMGTOOL_BIN} cache addInstaller --type fmw --version 12.2.1.3.0 --path `pwd`/${FMW_INSTALLER}
-#     IMGTYPE=fmw
-# fi
-# ${IMGTOOL_BIN} cache addInstaller --type wdt --version latest --path `pwd`/weblogic-deploy.zip
-# #
-# echo Creating base image with patches
-# #
-# ${IMGTOOL_BIN} create --tag model-in-image:x0 --user ${USERID} --password ${USERPWD} --patches 29135930_12.2.1.3.190416,29016089 --jdkVersion 8u221 --type ${IMGTYPE}
 #
-
-
 # Building sample app ear file
 #
 ./build_app.sh
@@ -94,20 +66,16 @@ cd ${WORKDIR}
 if [ "${DOMAINTYPE}" == "JRF" ] ; then
     cp image/model1.yaml.jrf image/model1.yaml
 fi
-
+#
 # Build sample image for deploying to the Kubernetes Cluster
-
+#
 ./build_sample_image.sh
-
-# echo Creating deploy image with wdt models
-# #
-# cd image
-# ${IMGTOOL_BIN} update --tag model-in-image:x1 --fromImage model-in-image:x0 --wdtModel model1.yaml --wdtVariables model1.10.properties --wdtArchive archive1.zip --wdtModelOnly --wdtDomainType ${DOMAINTYPE}
-# cd ..
 
 echo Setting Domain Type in domain.yaml
 #
 sed -i s/@@DOMTYPE@@/${DOMAINTYPE}/ domain.yaml
+
+echo "Sample image build completed, please change directory into ${WORKDIR}, continue to setup Kubernetes resources"
 
 
 
