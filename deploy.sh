0ebdff1cbbc4a08949fa5894001140ebc5e75c52
Ryan Eberhard <ryan.eberhard@oracle.com>
2019-09-27 10:22:15 -0400

    Update license format (#1272)

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#  Copyright 2017, 2019, Oracle Corporation and/or affiliates.  All rights reserved.
-# Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+#  Copyright (c) 2017, 2019, Oracle Corporation and/or affiliates.  All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 IMAGE="container-registry.oracle.com/middleware/weblogic-kubernetes-operator:latest"
 
--- src/integration-tests/bash/archive.sh
+++ src/integration-tests/bash/archive.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Copyright 2017, 2019, Oracle Corporation and/or its affiliates. All rights reserved.
-# Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+# Copyright (c) 2017, 2019, Oracle Corporation and/or its affiliates. All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 #
 # archive.sh <source_dir> <target_dir>
