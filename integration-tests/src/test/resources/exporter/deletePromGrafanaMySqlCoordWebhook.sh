c7659731f774fbfb5d8c5e42ec4adcbf1aea18ec
Marina Kogan <marina.kogan@oracle.com>
2019-11-13 01:28:43 +0000

    fixed test

--- integration-tests/src/test/resources/exporter/deletePromGrafanaMySqlCoordWebhook.sh
+++ integration-tests/src/test/resources/exporter/deletePromGrafanaMySqlCoordWebhook.sh
@@ -14,10 +14,17 @@ helm delete --purge grafana
 kubectl -n monitoring delete secret grafana-secret --ignore-not-found
 kubectl delete -f ${monitoringExporterEndToEndDir}/grafana/persistence.yaml --ignore-not-found
 
+export appname=grafana
+for p in `kubectl get po -l app=$appname -o name -n monitoring `;do echo $p; kubectl delete ${p} -n monitoring --force --grace-period=0 --ignore-not-found; done
+
 #delete prometheus
 helm delete --purge prometheus
 kubectl delete -f ${monitoringExporterEndToEndDir}/prometheus/persistence.yaml --ignore-not-found
 kubectl delete -f ${monitoringExporterEndToEndDir}/prometheus/alert-persistence.yaml --ignore-not-found
+
+export appname=prometheus
+for p in `kubectl get po -l app=$appname -o name -n monitoring `;do echo $p; kubectl delete ${p} -n monitoring --force --grace-period=0 --ignore-not-found; done
+
 
 
 #delete coordinator
