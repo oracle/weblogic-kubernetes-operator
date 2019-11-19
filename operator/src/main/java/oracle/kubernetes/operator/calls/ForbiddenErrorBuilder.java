0487cef25d8cd03ee7fe84c2c63d7d9a8ff643d2
Russell Gold <russell.gold@oracle.com>
2019-11-19 11:26:54 -0500

    Handle forbidden status from Async call (#1341)

---
+++ operator/src/main/java/oracle/kubernetes/operator/calls/ForbiddenErrorBuilder.java
@@ -0,0 +1,42 @@
+// Copyright (c) 2019, Oracle Corporation and/or its affiliates.  All rights reserved.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package oracle.kubernetes.operator.calls;
+
+import java.net.HttpURLConnection;
+
+import io.kubernetes.client.ApiException;
+
+/**
+ * A builder for 'forbidden' async results.
+ */
+public class ForbiddenErrorBuilder implements FailureStatusSource {
+  private static final String FORBIDDEN_REASON = "Forbidden";
+
+  private String message;
+
+  private ForbiddenErrorBuilder(ApiException e) {
+    this.message = e.getMessage();
+  }
+
+  public static boolean isForbiddenOperation(ApiException e) {
+    return e.getCode() == HttpURLConnection.HTTP_FORBIDDEN;
+  }
+
+  public static ForbiddenErrorBuilder fromException(ApiException exception) {
+    if (!isForbiddenOperation(exception))
+      throw new IllegalArgumentException("Is not forbidden exception");
+
+    return new ForbiddenErrorBuilder(exception);
+  }
+
+  @Override
+  public String getMessage() {
+    return message;
+  }
+
+  @Override
+  public String getReason() {
+    return FORBIDDEN_REASON;
+  }
+}
