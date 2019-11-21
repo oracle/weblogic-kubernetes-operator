b106ef5e902e9a71948cd7478108d58e4444846f
Ryan Eberhard <ryan.eberhard@oracle.com>
2019-11-21 21:36:57 +0000

    Schema v7

--- operator/src/main/java/oracle/kubernetes/operator/KubernetesConstants.java
+++ operator/src/main/java/oracle/kubernetes/operator/KubernetesConstants.java
@@ -21,8 +21,8 @@ public interface KubernetesConstants {
   String DOMAIN_PLURAL = "domains";
   String DOMAIN_SINGULAR = "domain";
   String DOMAIN_SHORT = "dom";
-  String DOMAIN_VERSION = "v6";
-  String[] DOMAIN_ALTERNATE_VERSIONS = {"v2", "v3", "v4", "v5"};
+  String DOMAIN_VERSION = "v7";
+  String[] DOMAIN_ALTERNATE_VERSIONS = {"v2", "v3", "v4", "v5", "v6"};
 
   boolean DEFAULT_INCLUDE_SERVER_OUT_IN_POD_LOG = true;
 
