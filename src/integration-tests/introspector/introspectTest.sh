2cdecb633e19a26c10484a8c1aef8e57d0b9905f
Lenny Phan <33355669+lennyphan@users.noreply.github.com>
2020-01-08 14:40:35 -0800

    Nm mem args (#1382)
    * set -Djava.security.egd=file:/dev/./urandom for Node Manager, if not defined
    
    * update user guide to include defaulting information about java security property
    
    * default memory and Java security values if NODEMGR_MEM_ARGS environment variable not defined

--- src/integration-tests/introspector/introspectTest.sh
+++ src/integration-tests/introspector/introspectTest.sh
@@ -522,7 +522,7 @@ function deployPod() {
       export LOCAL_SERVER_DEFAULT_PORT=$ADMIN_PORT
       export KEEP_DEFAULT_DATA_HOME="true"
       export EXPERIMENTAL_LINK_SERVER_DEFAULT_DATA_DIR=""
-      export NODEMGR_MEM_ARGS="-Xms32m -Xmx200m "
+      export NODEMGR_MEM_ARGS="-Xms32m -Xmx200m -Djava.security.egd=file:/dev/./urandom"
     else
       export LOCAL_SERVER_DEFAULT_PORT=$MANAGED_SERVER_PORT
       export KEEP_DEFAULT_DATA_HOME=""
@@ -779,6 +779,22 @@ function checkNodeManagerMemArg() {
 
   if [ "$adminLinecount" != "1" ]; then
     trace "Error: The latest log from 'kubectl -n ${NAMESPACE} logs ${DOMAIN_UID}-${ADMIN-NAME?}' does not contain exactly 1 line that match ' grep '-Xms32m -Xmx200m' ', this probably means that it's reporting NODEMGR_MEM_ARGS not applied"
+    logstatus=1
+  fi
+
+  if [ $logstatus -ne 0 ]; then
+    exit 1
+  fi
+
+  # Verify that NODEMGR_MEM_ARGS environment value contains "-Djava.security.egd=file:/dev/./urandom" in the Node Manager
+  # command line of the Managed Server pod.
+  adminLinecount="`kubectl exec -it -n ${NAMESPACE} ${DOMAIN_UID}-${MANAGED_SERVER_NAME_BASE?}1 \
+       grep "urandom" /shared/logs/${MANAGED_SERVER_NAME_BASE?}1_nodemanager.out \
+       | grep -v "NODEMGR_MEM_ARGS"  |  grep -v "JAVA_OPTIONS" | wc -l`"
+  logstatus=0
+
+  if [ "$adminLinecount" != "1" ]; then
+    trace "Error: The latest log from 'kubectl -n ${NAMESPACE} logs ${DOMAIN_UID}-${ADMIN-NAME?}' does not contain exactly 1 line that match ' grep '-Djava.security.egd=file:/dev/./urandom' ', this probably means that it's reporting NODEMGR_MEM_ARGS not applied"
     logstatus=1
   fi
 
