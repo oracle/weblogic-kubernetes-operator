50418af42078a172025bbf1e1b7129c36d12d89a
Marina Kogan <marina.kogan@oracle.com>
2020-01-17 09:07:41 -0800

    Made some changes to run MonExporter test in external Jenkins  (#1392)
    * added changes to support extjen runs
    
    * added changes to support extjen runs1
    
    * fixed image policy
    
    * fixed image policy1
    
    * corrected secret gen

--- integration-tests/src/test/resources/exporter/buildMonitoringExporter.sh
+++ integration-tests/src/test/resources/exporter/buildMonitoringExporter.sh
@@ -28,4 +28,13 @@ bash get${monitoringExporterVersion}.sh ${resourceExporterDir}/rest_webapp.yml
 
 cd ${monitoringExporterSrcDir}/config_coordinator
 docker build -t config_coordinator .
+if [ ${SHARED_CLUSTER} = "true" ]; then
+    docker login $REPO_REGISTRY -u $REPO_USERNAME -p $REPO_PASSWORD
+    docker tag config_coordinator:latest $REPO_REGISTRY/weblogick8s/config_coordinator:latest
+    docker push $REPO_REGISTRY/weblogick8s/config_coordinator:latest
+    if [ ! "$?" = "0" ] ; then
+       echo "Error: Could not push the image to $REPO_REGISTRY".
+      #exit 1
+    fi
+fi
 echo "Run the script [buildMonitoringExporter.sh] ..."
