042dd7314f301d5f965a4260e7e238790e7605d4
Marina Kogan <marina.kogan@oracle.com>
2020-01-17 23:30:50 +0000

    cleanup

--- integration-tests/src/test/java/oracle/kubernetes/operator/ItMonitoringExporter.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/ItMonitoringExporter.java
@@ -1140,7 +1140,6 @@ public class ItMonitoringExporter extends BaseTest {
 
   static void checkPromGrafana(String searchKey, String expectedVal) throws Exception {
     String podName = getPodName("app=prometheus", "monitoring");
-    //TestUtils.checkPodReady(podName, "monitoring", "2/2");
 
     String crdCmd = "kubectl -n monitoring get pods -l app=prometheus";
     ExecResult resultStatus = ExecCommand.exec(crdCmd);
