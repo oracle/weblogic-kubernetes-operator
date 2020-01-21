c47e0ef60b534f21eeb87480b0bf06d0f83bd28a
Marina Kogan <marina.kogan@oracle.com>
2020-01-21 12:05:33 -0800

    deleted debug commands  (#1397)
    *  comment out get pods k8s comm
    
    *  comment out get pods k8s comm1
    
    * cleanup

--- integration-tests/src/test/resources/exporter/createPromGrafanaMySqlCoordWebhook.sh
+++ integration-tests/src/test/resources/exporter/createPromGrafanaMySqlCoordWebhook.sh
@@ -90,10 +90,7 @@ kubectl apply -f ${resourceExporterDir}/server.yaml --validate=false
 kubectl get pods -n webhook
 POD_NAME=$(kubectl get pod -l app=webhook -o jsonpath="{.items[0].metadata.name}" -n webhook )
 
-kubectl describe pods ${POD_NAME} -n webhook
-kubectl logs ${POD_NAME} -n webhook
 echo "Getting info about webhook"
-kubectl get pods -n webhook
 
 #create coordinator
 kubectl create secret docker-registry ocirsecret -n ${domainNS} \
@@ -103,10 +100,6 @@ kubectl create secret docker-registry ocirsecret -n ${domainNS} \
                     --docker-email=$REPO_EMAIL  \
                     --dry-run -o yaml | kubectl apply -f -
 
-cat ${resourceExporterDir}/coordinator_${domainNS}.yaml
-kubectl apply -f ${resourceExporterDir}/coordinator_${domainNS}.yaml
-kubectl get pods -n ${domainNS}
-kubectl get pods -n monitoring
-kubectl get pods -n webhook
 
+kubectl apply -f ${resourceExporterDir}/coordinator_${domainNS}.yaml
 echo "Run the script [createPromGrafanaMySqlCoordWebhook.sh] ..."
