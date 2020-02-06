f59a09e2165ba397552b05605dbb4a1df0da89a9
Marina Kogan <marina.kogan@oracle.com>
2020-02-06 03:29:56 +0000

    added extra check

--- integration-tests/src/test/java/oracle/kubernetes/operator/ItMonitoringExporter.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/ItMonitoringExporter.java
@@ -148,7 +148,9 @@ public class ItMonitoringExporter extends BaseTest {
         myhost = domain.getHostNameForCurl();
         exporterUrl = "http://" + myhost + ":" + domain.getLoadBalancerWebPort() + "/wls-exporter/";
         boolean isTraefik = ((System.getenv("LB_TYPE") != null
-            && System.getenv("LB_TYPE").equalsIgnoreCase("TRAEFIK")));
+            && System.getenv("LB_TYPE").equalsIgnoreCase("TRAEFIK"))
+            || (domain.getDomainMap().containsKey("loadBalancer")
+            && ((String) domain.getDomainMap().get("loadBalancer")).equalsIgnoreCase("TRAEFIK")));
         if (isTraefik) {
           LoggerHelper.getLocal().log(Level.INFO, "Upgrading Traefik");
           upgradeTraefikHostName();
