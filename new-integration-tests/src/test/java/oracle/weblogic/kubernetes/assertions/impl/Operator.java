b55ebd25278dbb55c9d0da9833549a6a8a4e5c71
Mark Nelson <mark.x.nelson@oracle.com>
2020-03-14 16:44:40 -0400

    some more experimenting and thinking

--- new-integration-tests/src/test/java/oracle/weblogic/kubernetes/assertions/impl/Operator.java
+++ new-integration-tests/src/test/java/oracle/weblogic/kubernetes/assertions/impl/Operator.java
@@ -3,12 +3,22 @@ package oracle.weblogic.kubernetes.assertions.impl;
 import java.util.Random;
 import java.util.concurrent.Callable;
 
+import static oracle.weblogic.kubernetes.extensions.LoggedTest.logger;
+
 public class Operator {
 
     public static Callable<Boolean> isRunning() {
-        // this uses a rand, to simulate that this opertion can take
+        // this uses a rand, to simulate that this operation can take
         // variable amounts of time to complete
-        return () -> new Random().nextInt(3) == 1 ? true : false;
+        return () -> {
+            int outcome = new Random().nextInt(3);
+            if (outcome == 1) {
+                logger.info("Operator pod is in 'Running' state");
+                return true;
+            } else {
+                logger.info("Operator is not in 'Running' state yet, current state is 'ImagePull'");
+                return false;
+            }
+        };
     }
-
 }
