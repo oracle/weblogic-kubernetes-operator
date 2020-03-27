0caae237aa9cb0597e939ad4c6b56a9d0aad64ed
Derek Sharpe <derek.sharpe@oracle.com>
2020-03-27 17:13:52 -0500

    added IntegrationTest annotation

--- new-integration-tests/src/test/java/oracle/weblogic/kubernetes/annotations/IntegrationTest.java
+++ new-integration-tests/src/test/java/oracle/weblogic/kubernetes/annotations/IntegrationTest.java
@@ -1,17 +1,20 @@
-// Copyright 2020, Oracle Corporation and/or its affiliates.
+// Copyright (c) 2020, Oracle Corporation and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-package oracle.weblogic.kubernetes.extensions.tags;
+package oracle.weblogic.kubernetes.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import oracle.weblogic.kubernetes.extensions.Timing;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-@Target({ElementType.TYPE, ElementType.METHOD})
+@Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@Tag("slow")
-public @interface Slow {
+@Tag("integration")
+@ExtendWith(Timing.class)
+public @interface IntegrationTest {
 }
