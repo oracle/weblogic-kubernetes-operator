65ab3c880c62a2fad8cdf315906e0dc1f2501177
doxiao <dongbo.xiao@oracle.com>
2020-03-27 22:50:21 +0000

    Add actions for download and WIT
    Signed-off-by: doxiao <dongbo.xiao@oracle.com>

--- new-integration-tests/src/test/java/oracle/weblogic/kubernetes/actions/TestActions.java
+++ new-integration-tests/src/test/java/oracle/weblogic/kubernetes/actions/TestActions.java
@@ -3,9 +3,15 @@
 
 package oracle.weblogic.kubernetes.actions;
 
+import java.util.List;
+
 import oracle.weblogic.kubernetes.actions.impl.Domain;
 import oracle.weblogic.kubernetes.actions.impl.Namespace;
 import oracle.weblogic.kubernetes.actions.impl.Operator;
+import oracle.weblogic.kubernetes.actions.impl.primitive.Installer;
+import oracle.weblogic.kubernetes.actions.impl.primitive.InstallParams;
+import oracle.weblogic.kubernetes.actions.impl.primitive.WebLogicImageTool;
+import oracle.weblogic.kubernetes.actions.impl.primitive.WITParams;
 
 // this class essentially delegates to the impl classes, and "hides" all of the
 // detail impl classes - tests would only ever call methods in here, never
@@ -38,6 +44,37 @@ public class TestActions {
         return Namespace.createUniqueNamespace();
     }
 
-    // etc...
+    // ------------------------ Docker image  -------------------------
 
+    public static boolean createMIIImage(
+        String baseImageName, 
+        String imageTag,
+        List<String> modelFiles,
+        List<String> modelVariableFiles,
+        List<String> modelArchiveFiles,
+        String wdtVersion,
+        String domainType) {
+  
+    	return 
+            new WebLogicImageTool()
+                .with(new WITParams()
+                     .baseName(baseImageName).tag(imageTag).modelFiles(modelFiles)
+    	             .modelVariableFiles(modelVariableFiles).modelArchiveFiles(modelArchiveFiles)
+    		     .wdtVersion(wdtVersion).domainType(domainType)).updateImage();
+    }
+ 
+ 
+    // ------------------------ Installer  -------------------------
+
+    public static boolean verifyAndInstallWIT(String version, String location) {
+        return new Installer().with(new InstallParams().type("WIT").version(version).location(location))
+        		      .download();
+    }
+ 
+    public static boolean verifyAndInstallWDT(String version, String location) {
+        return new Installer().with(new InstallParams().type("WDT").version(version).location(location))
+  		      .download();
+
+    }
+ 
 }
---
+++ new-integration-tests/src/test/java/oracle/weblogic/kubernetes/actions/impl/primitive/InstallParams.java
@@ -0,0 +1,33 @@
+// Copyright 2020, Oracle Corporation and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package oracle.weblogic.kubernetes.actions.impl.primitive;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+// Presents all parameters that the test InstallParams needs.
+
+public class InstallParams {
+
+    private String type;
+    private String version;
+    private String location;
+
+    public InstallParams type(String type) {
+        this.type = type;
+        return this;
+    }
+
+    public InstallParams version(String version) {
+        this.version = version;
+        return this;
+    }
+
+    public InstallParams location(String location) {
+        this.location = location;
+        return this;
+    }
+
+}
