8512964df8b1315953b8180f48ed3b5ec4a35877
sankar <sankarpn@gmail.com>
2020-03-31 11:47:30 -0700

    working on the pod status

--- new-integration-tests/src/test/java/oracle/weblogic/kubernetes/assertions/impl/Kubernetes.java
+++ new-integration-tests/src/test/java/oracle/weblogic/kubernetes/assertions/impl/Kubernetes.java
@@ -5,7 +5,32 @@ package oracle.weblogic.kubernetes.assertions.impl;
 
 import java.util.concurrent.Callable;
 
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.apis.CustomObjectsApi;
+import io.kubernetes.client.openapi.models.V1Pod;
+import io.kubernetes.client.openapi.models.V1PodList;
+import io.kubernetes.client.util.ClientBuilder;
+import java.io.IOException;
+
 public class Kubernetes {
+
+  private static ApiClient apiClient = null;
+  private static CoreV1Api coreV1Api = null;
+  private static CustomObjectsApi customObjectsApi = null;
+
+  static {
+    try {
+      Configuration.setDefaultApiClient(ClientBuilder.defaultClient());
+      apiClient = Configuration.getDefaultApiClient();
+      coreV1Api = new CoreV1Api();
+      customObjectsApi = new CustomObjectsApi();
+    } catch (IOException ioex) {
+      throw new ExceptionInInitializerError(ioex);
+    }
+  }
 
   public static Callable<Boolean> podExists(String podName, String domainUID, String namespace) {
     return () -> {
@@ -13,7 +38,11 @@ public class Kubernetes {
     };
   }
 
-  public static Callable<Boolean> podRunning(String podName, String domainUID, String namespace) {
+  public static Callable<Boolean> podRunning(String podName, String domainUID, String namespace) throws ApiException {
+    V1PodList list = coreV1Api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
+    for (V1Pod item : list.getItems()) {
+      System.out.println(item.getMetadata().getName());
+    }
     return () -> {
       return true;
     };
