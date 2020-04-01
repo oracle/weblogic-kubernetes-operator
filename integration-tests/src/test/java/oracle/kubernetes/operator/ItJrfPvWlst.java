44a0c5ee90ff6da65b95c07b75262a23b6c9a585
MAGGIE.HE@ORACLE.COM <maggie.he@oracle.com>
2020-04-01 02:25:52 +0000

    remove commented out code

--- integration-tests/src/test/java/oracle/kubernetes/operator/ItJrfPvWlst.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/ItJrfPvWlst.java
@@ -58,7 +58,6 @@ public class ItJrfPvWlst extends BaseTest {
   public void prepare() throws Exception {
     if (QUICKTEST) {
       createResultAndPvDirs(testClassName);
-      //setMaxIterationsPod(80);
       
       TestUtils.exec(
           "cp -rf " 
