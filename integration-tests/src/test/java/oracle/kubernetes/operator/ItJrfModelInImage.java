5babf9ff1c37f03e7cca13555aed61a84c1dbd07
MAGGIE.HE@ORACLE.COM <maggie.he@oracle.com>
2020-04-16 01:20:23 +0000

    increase pod loop count

--- integration-tests/src/test/java/oracle/kubernetes/operator/ItJrfModelInImage.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/ItJrfModelInImage.java
@@ -177,7 +177,7 @@ public class ItJrfModelInImage extends MiiBaseTest {
       
       jrfdomain = new JrfDomain(domainMap);
       
-      jrfdomain.verifyDomainCreated();
+      jrfdomain.verifyDomainCreated(80);
       
       //save and restore walletFile secret
       saveWalletFileSecret(getResultDir(), domainUid, namespace);
@@ -208,7 +208,7 @@ public class ItJrfModelInImage extends MiiBaseTest {
       ExecResult exec = TestUtils.exec("kubectl apply -f " + path.toString());
       LoggerHelper.getLocal().log(Level.INFO, exec.stdout());
       
-      jrfdomain.verifyDomainCreated();
+      jrfdomain.verifyDomainCreated(80);
       testCompletedSuccessfully = true;
 
     } catch (Exception ex) {
