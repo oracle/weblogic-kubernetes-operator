a6b402bd210d4ee220d29aa1a8f0cbeaa111f058
Marina Kogan <marina.kogan@oracle.com>
2020-04-19 19:31:58 -0700

    reset domainuid

--- integration-tests/src/test/java/oracle/kubernetes/operator/ItPodsShutdown.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/ItPodsShutdown.java
@@ -134,7 +134,6 @@ public class ItPodsShutdown extends BaseTest {
 
     Map<String, Object> domainMap = createDomainMap(getNewSuffixCount(), testClassName);
     domainMap.put("namespace", domainNS);
-    domainMap.put("domainUID", domainNS);
     domainMap.put("initialManagedServerReplicas", new Integer("1"));
     domainMap.put("shutdownOptionsOverrides",shutdownProps);
     LoggerHelper.getLocal().log(Level.INFO, "Creating and verifying the domain creation with domainUid: " + domainNS);
