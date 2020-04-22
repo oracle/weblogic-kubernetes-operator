c823a649b88e94db50c7b23cb4ae49ccf5378333
Tom Barnes <tom.barnes@oracle.com>
2020-04-22 08:51:01 +0000

    MII Sample: Minor change to output in the 'util-patch-restart-version.sh' script.

--- kubernetes/samples/scripts/create-weblogic-domain/model-in-image/util-patch-restart-version.sh
+++ kubernetes/samples/scripts/create-weblogic-domain/model-in-image/util-patch-restart-version.sh
@@ -65,16 +65,19 @@ kubectl -n ${DOMAIN_NAMESPACE} patch domain ${DOMAIN_UID} --type='json' \
 
 cat << EOF
 @@
-@@ Info: Domain '${DOMAIN_UID}' in namespace '${DOMAIN_NAMESPACE}' successfully patched!"
+@@ Info: Domain '${DOMAIN_UID}' in namespace '${DOMAIN_NAMESPACE}' successfully patched with restartVersion '${nextRV}'!"
 
-   To wait until pods start and/or get their status:
+   To wait until pods reach the new restart version and/or to get their status:
 
       kubectl get pods -n ${DOMAIN_NAMESPACE} --watch
        # (ctrl-c once all pods are running and ready)
-             -or-
-      'SCRIPTDIR/util-wl-pod-wait.sh -n $DOMAIN_NAMESPACE -d $DOMAIN_UID -p 3 -v'
 
-   Expect the operator to restart the domain's pods until all of them have label 'weblogic.domainRestartVersion=\"$nextRV\"."
+             -or-
+
+      SCRIPTDIR/util-wl-pod-wait.sh -n $DOMAIN_NAMESPACE -d $DOMAIN_UID -p 3 -v
+
+   Expect the operator to restart the domain's pods until all of them
+   have label 'weblogic.domainRestartVersion="$nextRV"."
 
 @@ Done.
 EOF
