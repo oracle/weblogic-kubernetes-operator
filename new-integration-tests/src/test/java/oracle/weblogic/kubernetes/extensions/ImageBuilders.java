f0d92bb5b66bb22155330d3b9b63678a7a15796b
Ryan Eberhard <ryan.eberhard@oracle.com>
2020-04-22 16:52:37 -0400

    Synchronization for one-time initialization logic

--- new-integration-tests/src/test/java/oracle/weblogic/kubernetes/extensions/ImageBuilders.java
+++ new-integration-tests/src/test/java/oracle/weblogic/kubernetes/extensions/ImageBuilders.java
@@ -3,25 +3,38 @@
 
 package oracle.weblogic.kubernetes.extensions;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import oracle.weblogic.kubernetes.actions.impl.Operator;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
 import static oracle.weblogic.kubernetes.extensions.LoggedTest.logger;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.extension.ExtensionContext.Namespace.GLOBAL;
+
+//import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Class to build the required images for the tests.
  */
 public class ImageBuilders implements BeforeAllCallback, ExtensionContext.Store.CloseableResource {
-  private static boolean started = false;
+  private static final AtomicBoolean started = new AtomicBoolean(false);
+  private static final CountDownLatch initializationLatch = new CountDownLatch(1);
 
   @Override
   public void beforeAll(ExtensionContext context) {
-    if (!started) {
-      started = true;
+    /* The pattern is that we have initialization code that we want to run once to completion
+     * before any tests are executed. This method will be called before every test method. Therefore, the
+     * very first time this method is called we will do the initialization. Since we assume that the tests
+     * will be run concurrently in many threads, we need a guard to ensure that only the first thread arriving
+     * attempts to do the initialization *and* that any other threads *wait* for that initialization to complete
+     * before running their tests.
+     */
+    if (!started.getAndSet(true)) {
+      // Only the first thread will enter this block.
+
       logger.info("Building docker Images before any integration test classes are run");
       context.getRoot().getStore(GLOBAL).put("BuildSetup", this);
 
@@ -32,6 +45,17 @@ public class ImageBuilders implements BeforeAllCallback, ExtensionContext.Store.
       /* assertFalse(operatorImage.isEmpty(), "Image name can not be empty");
       assertTrue(Operator.buildImage(operatorImage)); */
 
+      // Initialization is done. Release all waiting other threads. The latch is now disabled so other threads
+      // arriving later will immediately proceed.
+      initializationLatch.countDown();
+    } else {
+      // Other threads will enter here and wait on the latch. Once the latch is released, any threads arriving
+      // later will immediately proceed.
+      try {
+        initializationLatch.await();
+      } catch (InterruptedException e) {
+        throw new IllegalStateException(e);
+      }
     }
   }
 
