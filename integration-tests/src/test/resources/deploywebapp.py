f0d41cf5e99d478a020be1dcb37d8feae13dc6dc
Marina Kogan <marina.kogan@oracle.com>
2020-04-30 16:59:38 +0000

    added debug to deploy app

--- integration-tests/src/test/resources/deploywebapp.py
+++ integration-tests/src/test/resources/deploywebapp.py
@@ -1,22 +1,12 @@
 # Copyright (c) 2018, 2020, Oracle Corporation and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
-
-import time as systime
-
 connect(sys.argv[1],sys.argv[2],sys.argv[3])
-deploy(sys.argv[4],sys.argv[5],sys.argv[6],upload='false',remote='false')
-#systime.sleep(15)
-
-#cd ('AppDeployments')
-#myapps=cmo.getAppDeployments()
- 
-#for appName in myapps:
-#       domainConfig()
-#       cd ('/AppDeployments/'+appName.getName()+'/Targets')
-#       mytargets = ls(returnMap='true')
-#       domainRuntime()
-#       cd('AppRuntimeStateRuntime')
-#       cd('AppRuntimeStateRuntime')
-#       for targetinst in mytargets:
-#             curstate4=cmo.getCurrentState(appName.getName(),targetinst)
-#             print '-----------', curstate4, '-----------', appName.getName()
+appName=sys.argv[4]
+appPath=sys.argv[5]
+clusterName=sys.argv[6]
+print 'Deploying application[%s] to the Cluster[%s] located @ [%s] ' %(appName,clusterName,appPath)
+print 'Checking the server status on the Cluster[%s] ' %(clusterName)
+domainRuntime()
+state(clusterName,'Cluster')
+#deploy(sys.argv[4],sys.argv[5],sys.argv[6],upload='false',remote='false')
+deploy(appName=appName, path=appPath, targets=clusterName)
