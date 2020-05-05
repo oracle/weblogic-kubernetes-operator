2b6a094fda3f19e20a3d966475cfbced5d9be152
sankar <sankarpn@gmail.com>
2020-05-05 11:23:18 -0700

    Address review comments

--- new-integration-tests/src/test/java/oracle/weblogic/kubernetes/ItSimpleValidation.java
+++ new-integration-tests/src/test/java/oracle/weblogic/kubernetes/ItSimpleValidation.java
@@ -93,60 +93,39 @@ class ItSimpleValidation implements LoggedTest {
   String serviceAccountName;
 
   /**
-   * Setup for test suite. Creates service account, namespace, and persistent volumes.
-   * @param namespaces injected by Junit extension
+   * Setup for test suite. Creates service account, persistent volume
+   * and persistent volume claim.
+   * @param namespaces injected by JUnit extension
    */
   @BeforeAll
   public void setup(@Namespaces(2) List<String> namespaces) {
 
-    // get a new unique namespace
-    logger.info("Assigning unique namespace for Operator");
+    logger.info("Assigning unique namespace for operator");
     assertNotNull(namespaces.get(0), "Namespace is null");
     opNamespace = namespaces.get(0);
 
-    logger.info("Assigning unique namespace for Domain");
+    logger.info("Assigning unique namespace for domain");
     assertNotNull(namespaces.get(0), "Namespace is null");
     domainNamespace1 = namespaces.get(1);
 
 
-    // Create a service account for the unique namespace
+    logger.info("Creating a service account");
     serviceAccountName = opNamespace + "-sa";
     V1ServiceAccount serviceAccount = assertDoesNotThrow(
         () -> Kubernetes.createServiceAccount(new V1ServiceAccount()
             .metadata(new V1ObjectMeta().namespace(opNamespace).name(serviceAccountName))));
     logger.info("Created service account: {0}", serviceAccount.getMetadata().getName());
 
-    // create persistent volume and persistent volume claim
-    String pvcName = domainUid + "-pvc"; // name of the persistent volume claim
+    logger.info("creating persistent volume and persistent volume claim");
     String pvName = domainUid + "-pv"; // name of the persistent volume
-
-    V1PersistentVolumeClaim v1pvc = new V1PersistentVolumeClaim()
-        .spec(new V1PersistentVolumeClaimSpec()
-            .addAccessModesItem("ReadWriteMany")
-            .storageClassName(domainUid + "-weblogic-domain-storage-class")
-            .volumeName(pvName)
-            .resources(new V1ResourceRequirements()
-                .putRequestsItem("storage", Quantity.fromString("10Gi"))))
-        .metadata(new V1ObjectMetaBuilder()
-            .withName(pvcName)
-            .withNamespace(domainNamespace1)
-            .build()
-            .putLabelsItem("weblogic.resourceVersion", "domain-v2")
-            .putLabelsItem("weblogic.domainUid", domainUid));
-
-    boolean success = assertDoesNotThrow(
-        () -> TestActions.createPersistentVolumeClaim(v1pvc),
-        "Persistent volume claim creation failed, "
-        + "look at the above console log messages for failure reason in ApiException responsebody"
-    );
-    assertTrue(success, "PersistentVolumeClaim creation failed");
+    String pvcName = domainUid + "-pvc"; // name of the persistent volume claim
 
     V1PersistentVolume v1pv = new V1PersistentVolume()
         .spec(new V1PersistentVolumeSpec()
             .addAccessModesItem("ReadWriteMany")
             .storageClassName(domainUid + "-weblogic-domain-storage-class")
             .volumeMode("Filesystem")
-            .putCapacityItem("storage", Quantity.fromString("10Gi"))
+            .putCapacityItem("storage", Quantity.fromString("5Gi"))
             .persistentVolumeReclaimPolicy("Recycle")
             .hostPath(new V1HostPathVolumeSource()
                 .path(TestConstants.PV_ROOT + "/" + domainUid + "-persistentVolume")))
@@ -156,12 +135,34 @@ class ItSimpleValidation implements LoggedTest {
             .build()
             .putLabelsItem("weblogic.resourceVersion", "domain-v2")
             .putLabelsItem("weblogic.domainUid", domainUid));
-    success = assertDoesNotThrow(
+    boolean success = assertDoesNotThrow(
         () -> TestActions.createPersistentVolume(v1pv),
         "Persistent volume creation failed, "
         + "look at the above console log messages for failure reason in ApiException responsebody"
     );
     assertTrue(success, "PersistentVolume creation failed");
+
+    V1PersistentVolumeClaim v1pvc = new V1PersistentVolumeClaim()
+        .spec(new V1PersistentVolumeClaimSpec()
+            .addAccessModesItem("ReadWriteMany")
+            .storageClassName(domainUid + "-weblogic-domain-storage-class")
+            .volumeName(pvName)
+            .resources(new V1ResourceRequirements()
+                .putRequestsItem("storage", Quantity.fromString("5Gi"))))
+        .metadata(new V1ObjectMetaBuilder()
+            .withName(pvcName)
+            .withNamespace(domainNamespace1)
+            .build()
+            .putLabelsItem("weblogic.resourceVersion", "domain-v2")
+            .putLabelsItem("weblogic.domainUid", domainUid));
+
+    success = assertDoesNotThrow(
+        () -> TestActions.createPersistentVolumeClaim(v1pvc),
+        "Persistent volume claim creation failed, "
+        + "look at the above console log messages for failure reason in ApiException responsebody"
+    );
+    assertTrue(success, "PersistentVolumeClaim creation failed");
+
   }
 
   @Test
@@ -174,7 +175,7 @@ class ItSimpleValidation implements LoggedTest {
     // build the model file list
     final List<String> modelList = Collections.singletonList(MODEL_DIR + "/" + WDT_MODEL_FILE);
 
-    // build an application archive using what is in resources/apps/APP_NAME
+    logger.info("Building an application archive using what is in resources/apps/APP_NAME");
     boolean archiveBuilt = buildAppArchive(
         defaultAppParams()
             .srcDir(APP_NAME));
@@ -184,16 +185,16 @@ class ItSimpleValidation implements LoggedTest {
         .withFailMessage("Failed to create app archive for " + APP_NAME)
         .isTrue();
 
-    // build the archive list
+    logger.info("Building the archive list");
     String zipFile = String.format("%s/%s.zip", ARCHIVE_DIR, APP_NAME);
     List<String> archiveList = Collections.singletonList(zipFile);
 
-    // Set additional environment variables for WIT
+    logger.info("Setting additional environment variables for WIT");
     checkDirectory(WIT_BUILD_DIR);
     Map<String, String> env = new HashMap();
     env.put("WLSIMG_BLDDIR", WIT_BUILD_DIR);
 
-    // build an image using WebLogic Image Tool
+    logger.info("Building an image using WebLogic Image Tool");
     boolean success = createMiiImage(
         defaultWitParams()
             .modelImageName(IMAGE_NAME)
@@ -226,13 +227,13 @@ class ItSimpleValidation implements LoggedTest {
     assertFalse(image.isEmpty(), "Operator image name can not be empty");
     logger.info("Operator image name {0}", image);
 
-    // Create docker registry secret in the operator namespace to pull the image from repository
-    logger.info("Creating docker registry secret in namespace {0}", opNamespace);
+    logger.info("Creating Docker registry secret in the operator namespace "
+        + "{0} to pull the image from repository", opNamespace);
     JsonObject dockerConfigJsonObject = createDockerConfigJson(
         REPO_USERNAME, REPO_PASSWORD, REPO_EMAIL, REPO_REGISTRY);
     String dockerConfigJson = dockerConfigJsonObject.toString();
 
-    // Create the V1Secret configuration
+    logger.info("Creating the V1Secret configuration");
     V1Secret repoSecret = new V1Secret()
         .metadata(new V1ObjectMeta()
             .name(REPO_SECRET_NAME)
@@ -248,13 +249,13 @@ class ItSimpleValidation implements LoggedTest {
     Map<String, Object> secretNameMap = new HashMap<>();
     secretNameMap.put("name", REPO_SECRET_NAME);
 
-    // helm install parameters
+    logger.info("Creating helm install parameters object");
     HelmParams opHelmParams = new HelmParams()
         .releaseName(OPERATOR_RELEASE_NAME)
         .namespace(opNamespace)
         .chartDir(OPERATOR_CHART_DIR);
 
-    // Operator chart values to override
+    logger.info("Creating operator chart values to override object");
     OperatorParams opParams =
         new OperatorParams()
             .helmParams(opHelmParams)
@@ -264,13 +265,13 @@ class ItSimpleValidation implements LoggedTest {
             .serviceAccount(serviceAccountName);
 
     // install Operator
-    logger.info("Installing Operator in namespace {0}", opNamespace);
+    logger.info("Installing operator in namespace {0}", opNamespace);
     assertTrue(installOperator(opParams),
         String.format("Operator install failed in namespace %s", opNamespace));
     logger.info("Operator installed in namespace {0}", opNamespace);
 
     // list helm releases matching Operator release name in operator namespace
-    logger.info("Checking Operator release {0} status in namespace {1}",
+    logger.info("Checking operator release {0} status in namespace {1}",
         OPERATOR_RELEASE_NAME, opNamespace);
     assertTrue(isHelmReleaseDeployed(OPERATOR_RELEASE_NAME, opNamespace),
         String.format("Operator release %s is not in deployed status in namespace %s",
@@ -279,7 +280,7 @@ class ItSimpleValidation implements LoggedTest {
         OPERATOR_RELEASE_NAME, opNamespace);
 
     // check operator is running
-    logger.info("Check Operator pod is running in namespace {0}", opNamespace);
+    logger.info("Check operator pod is running in namespace {0}", opNamespace);
     with().pollDelay(2, SECONDS)
         .and().with().pollInterval(10, SECONDS)
         .atMost(5, MINUTES).await()
@@ -290,11 +291,12 @@ class ItSimpleValidation implements LoggedTest {
                 condition.getElapsedTimeInMS(),
                 condition.getRemainingTimeInMS()))
         .until(operatorIsReady(opNamespace));
+    logger.info("Operator is ready in namespace {0}", opNamespace);
 
   }
 
   /**
-   * Create a simple domain and checks if pods are coming up.
+   * Create a simple domain.
    */
   @Test
   @Order(3)
@@ -302,7 +304,7 @@ class ItSimpleValidation implements LoggedTest {
   @Slow
   public void testCreatingDomain() {
 
-    // create the domain CR
+    logger.info("Creating domain CR configuration object");
     V1ObjectMeta metadata = new V1ObjectMetaBuilder()
         .withName(domainUid)
         .withNamespace(domainNamespace1)
@@ -319,7 +321,7 @@ class ItSimpleValidation implements LoggedTest {
         .spec(domainSpec);
     boolean success = assertDoesNotThrow(
         () -> createDomainCustomResource(domain),
-        "Domain failed to be created, "
+        "Failed to create domain, "
         + "look at the above console log messages for failure reason in ApiException responsebody"
     );
     assertTrue(success);
