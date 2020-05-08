90c17fd85551eb98cf393f1db75551a86c87b276
Ryan Eberhard <ryan.eberhard@oracle.com>
2020-05-08 15:57:26 -0400

    Version to 3.0.0-rc1 (#1633)

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -33,7 +33,7 @@ while getopts "t:" optname; do
   esac
 done
 
-IMAGE_NAME=${name:-oracle/weblogic-kubernetes-operator:3.0.0}
+IMAGE_NAME=${name:-oracle/weblogic-kubernetes-operator:3.0.0-rc1}
 SCRIPTPATH="$( cd "$(dirname "$0")" > /dev/null 2>&1 ; pwd -P )"
 
 # Proxy settings
