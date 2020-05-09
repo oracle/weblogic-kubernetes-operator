582f28abd23f0ed8aaf0f998e51f5a9df4c79d4e
Vanajakshi Mukkara <VANAJAKSHI.MUKKARA@ORACLE.COM>
2020-05-08 20:15:36 -0700

    creating mii basic image in extension class

--- new-integration-tests/src/test/java/oracle/weblogic/kubernetes/extensions/ImageBuilders.java
+++ new-integration-tests/src/test/java/oracle/weblogic/kubernetes/extensions/ImageBuilders.java
@@ -3,6 +3,10 @@
 
 package oracle.weblogic.kubernetes.extensions;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -10,13 +14,27 @@ import oracle.weblogic.kubernetes.actions.impl.Operator;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
+import static oracle.weblogic.kubernetes.TestConstants.MII_BASIC_APP_NAME;
+import static oracle.weblogic.kubernetes.TestConstants.MII_BASIC_IMAGE_NAME;
+import static oracle.weblogic.kubernetes.TestConstants.MII_BASIC_IMAGE_TAG;
+import static oracle.weblogic.kubernetes.TestConstants.MII_BASIC_WDT_MODEL_FILE;
 import static oracle.weblogic.kubernetes.TestConstants.REPO_DUMMY_VALUE;
 import static oracle.weblogic.kubernetes.TestConstants.REPO_PASSWORD;
 import static oracle.weblogic.kubernetes.TestConstants.REPO_REGISTRY;
 import static oracle.weblogic.kubernetes.TestConstants.REPO_USERNAME;
+import static oracle.weblogic.kubernetes.actions.ActionConstants.ARCHIVE_DIR;
+import static oracle.weblogic.kubernetes.actions.ActionConstants.MODEL_DIR;
+import static oracle.weblogic.kubernetes.actions.ActionConstants.WDT_VERSION;
+import static oracle.weblogic.kubernetes.actions.ActionConstants.WIT_BUILD_DIR;
+import static oracle.weblogic.kubernetes.actions.TestActions.buildAppArchive;
+import static oracle.weblogic.kubernetes.actions.TestActions.createMiiImage;
+import static oracle.weblogic.kubernetes.actions.TestActions.defaultAppParams;
+import static oracle.weblogic.kubernetes.actions.TestActions.defaultWitParams;
 import static oracle.weblogic.kubernetes.actions.TestActions.dockerLogin;
 import static oracle.weblogic.kubernetes.actions.TestActions.dockerPush;
+import static oracle.weblogic.kubernetes.assertions.TestAssertions.doesImageExist;
 import static oracle.weblogic.kubernetes.extensions.LoggedTest.logger;
+import static oracle.weblogic.kubernetes.utils.FileUtils.checkDirectory;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.extension.ExtensionContext.Namespace.GLOBAL;
@@ -47,20 +65,34 @@ public class ImageBuilders implements BeforeAllCallback, ExtensionContext.Store.
         // build operator image
         String operatorImage = Operator.getImageName();
         logger.info("Operator image name {0}", operatorImage);
-        // assertFalse(true);
         assertFalse(operatorImage.isEmpty(), "Image name can not be empty");
+        assertTrue(Operator.buildImage(operatorImage));
 
-        // push the image to OCIR to make the test work in multi node cluster
+        // build MII basic image
+        String miiBasicImage = MII_BASIC_IMAGE_NAME + ":" + MII_BASIC_IMAGE_TAG;
+        assertTrue(createMiiBasicImage(MII_BASIC_IMAGE_NAME, MII_BASIC_IMAGE_TAG),
+            String.format("Failed to create the image %s using WebLogic Image Tool",
+                miiBasicImage));
+
+        /* Check image exists using docker images | grep image tag.
+         * Tag name is unique as it contains date and timestamp.
+         * This is a workaround for the issue on Jenkins machine
+         * as docker images imagename:imagetag is not working and
+         * the test fails even though the image exists.
+         */
+        assertTrue(doesImageExist(MII_BASIC_IMAGE_TAG),
+            String.format("Image %s doesn't exist", miiBasicImage));
+
+        // push the image to registry to make the test work in multi node cluster
         if (!REPO_USERNAME.equals(REPO_DUMMY_VALUE)) {
-          assertTrue(Operator.buildImage(operatorImage));
-
           logger.info("docker login");
           assertTrue(dockerLogin(REPO_REGISTRY, REPO_USERNAME, REPO_PASSWORD), "docker login failed");
 
-          logger.info("docker push image {0} to OCIR", operatorImage);
+          logger.info("docker push operator image {0} to registry", operatorImage);
           assertTrue(dockerPush(operatorImage), String.format("docker push failed for image %s", operatorImage));
-        } else {
-          assertTrue(Operator.buildImage(operatorImage));
+
+          logger.info("docker push mii basic image {0} to registry", miiBasicImage);
+          assertTrue(dockerPush(miiBasicImage), String.format("docker push failed for image %s", miiBasicImage));
         }
         
       } finally {
@@ -83,5 +115,40 @@ public class ImageBuilders implements BeforeAllCallback, ExtensionContext.Store.
   @Override
   public void close() {
     logger.info("Cleanup images after all test suites are run");
+  }
+
+
+  private boolean createMiiBasicImage(String imageName, String imageTag) {
+
+    final String image = imageName + ":" + imageTag;
+
+    // build the model file list
+    final List<String> modelList = Collections.singletonList(MODEL_DIR + "/" + MII_BASIC_WDT_MODEL_FILE);
+
+    // build an application archive using what is in resources/apps/APP_NAME
+    assertTrue(buildAppArchive(defaultAppParams()
+        .srcDir(MII_BASIC_APP_NAME)), String.format("Failed to create app archive for %s", MII_BASIC_APP_NAME));
+
+    // build the archive list
+    String zipFile = String.format("%s/%s.zip", ARCHIVE_DIR, MII_BASIC_APP_NAME);
+    final List<String> archiveList = Collections.singletonList(zipFile);
+
+    // Set additional environment variables for WIT
+    checkDirectory(WIT_BUILD_DIR);
+    Map<String, String> env = new HashMap<>();
+    env.put("WLSIMG_BLDDIR", WIT_BUILD_DIR);
+
+    // build an image using WebLogic Image Tool
+    logger.info("Create image {0} using model directory {1}", image, MODEL_DIR);
+    return createMiiImage(
+        defaultWitParams()
+            .modelImageName(imageName)
+            .modelImageTag(MII_BASIC_IMAGE_TAG)
+            .modelFiles(modelList)
+            .modelArchiveFiles(archiveList)
+            .wdtVersion(WDT_VERSION)
+            .env(env)
+            .redirect(true));
+
   }
 }
