b3b8d8da9229bee6434d93b2817fc9142f66d6e3
Russell Gold <russell.gold@oracle.com>
2020-05-12 11:53:39 -0400

    added initial tests

--- operator/src/main/java/oracle/kubernetes/operator/calls/http/HttpResponseStep.java
+++ operator/src/main/java/oracle/kubernetes/operator/calls/http/HttpResponseStep.java
@@ -3,4 +3,36 @@
 
 package oracle.kubernetes.operator.calls.http;
 
-public class HttpResponseStep {}
+import java.net.http.HttpResponse;
+
+import oracle.kubernetes.operator.work.NextAction;
+import oracle.kubernetes.operator.work.Packet;
+import oracle.kubernetes.operator.work.Step;
+
+public abstract class HttpResponseStep extends Step {
+
+  public HttpResponseStep(Step next) {
+    super(next);
+  }
+
+  @Override
+  public NextAction apply(Packet packet) {
+    return null;
+  }
+
+  /**
+   * Processes a successful response.
+   * @param packet the packet from the fiber
+   * @param response the response from the server
+   * @return the next action for the fiber to take
+   */
+  public abstract NextAction onSuccess(Packet packet, HttpResponse<String> response);
+
+  /**
+   * Processes a failure response.
+   * @param packet the packet from the fiber
+   * @param response the response from the server
+   * @return the next action for the fiber to take
+   */
+  public abstract NextAction onFailure(Packet packet, HttpResponse<String> response);
+}
--- operator/src/test/java/oracle/kubernetes/operator/calls/http/HttpResponseStepTest.java
+++ operator/src/test/java/oracle/kubernetes/operator/calls/http/HttpResponseStepTest.java
@@ -3,14 +3,53 @@
 
 package oracle.kubernetes.operator.calls.http;
 
-import org.junit.Test;
+import java.net.http.HttpResponse;
+
+import io.sundr.shaded.com.github.javaparser.ast.body.ModifierSet;
+import oracle.kubernetes.operator.work.Packet;
 import oracle.kubernetes.operator.work.Step;
+import oracle.kubernetes.operator.work.TerminalStep;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.sameInstance;
+import static org.hamcrest.Matchers.typeCompatibleWith;
+import static org.hamcrest.junit.MatcherAssert.assertThat;
 
 public class HttpResponseStepTest {
+	private TerminalStep terminalStep = new TerminalStep();
+	private HttpResponseStepImpl responseStep = new HttpResponseStepImpl(terminalStep);
+
 	@Test
 	public void classImplementsStep() {
-		
-		assertThat(HttpResponseStep.class, typeCompatibleWith(Step.class)); 
-		
+		assertThat(HttpResponseStep.class, typeCompatibleWith(Step.class));
 	}
+
+	@Test
+	public void classIsAbstract() {
+		assertThat(ModifierSet.isAbstract(HttpResponseStep.class.getModifiers()), is(true));
+	}
+
+	@Test
+	public void constructorSetsNextStep() {
+		assertThat(responseStep.getNext(), sameInstance(terminalStep));
+	}
+
+	@Test
+	public void classHasOnSuccessMethod() throws NoSuchMethodException {
+		assertThat(
+					HttpResponseStep.class.getDeclaredMethod("onSuccess", Packet.class, HttpResponse.class),
+					notNullValue());
+	}
+
+	@Test
+	public void classHasOnFailureMethod() throws NoSuchMethodException {
+		assertThat(
+					HttpResponseStep.class.getDeclaredMethod("onFailure", Packet.class, HttpResponse.class),
+					notNullValue());
+	}
+
+	// todo when response is success, invoke onSuccess
+	// todo when response is failure, invoke onFailure
 }
