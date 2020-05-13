892c7f6f10adce78cfec6c0a89c750018189bd6e
sankar <sankarpn@gmail.com>
2020-05-13 11:08:59 -0700

    wip

--- new-integration-tests/src/test/resources/python-scripts/wlst-create-domain-onpv.py
+++ new-integration-tests/src/test/resources/python-scripts/wlst-create-domain-onpv.py
@@ -21,7 +21,7 @@ def create_domain():
 
   print('Configuring the Administration Server')
   cd('/Servers/AdminServer')
-  set('ListenPort', int(admin_port))
+  set('ListenPort', int(admin_server_port))
   set('Name', admin_server_name)
 
   print('Configuring network access point')
