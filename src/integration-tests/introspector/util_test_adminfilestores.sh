1a49d26eb8fd06cbb294a8150588d8338cc1776f
Ryan Eberhard <ryan.eberhard@oracle.com>
2020-05-18 18:21:02 -0400

    More integration tests

--- src/integration-tests/introspector/util_test_adminfilestores.sh
+++ src/integration-tests/introspector/util_test_adminfilestores.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-
-# Copyright 2019, Oracle Corporation and/or its affiliates. All rights reserved.
-# Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+# Copyright (c) 2019, 2020, Oracle Corporation and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 #
 # Description:
