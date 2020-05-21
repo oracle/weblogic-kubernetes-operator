30e52d91651363a7e11fae0c6b6a5be4d2d6cc12
sankar <sankarpn@gmail.com>
2020-05-20 17:16:30 -0700

    Set a default value in non kind environment

--- new-integration-tests/src/test/java/oracle/weblogic/kubernetes/ItDomainOnPV.java
+++ new-integration-tests/src/test/java/oracle/weblogic/kubernetes/ItDomainOnPV.java
@@ -79,7 +79,6 @@ import static oracle.weblogic.kubernetes.TestConstants.OPERATOR_CHART_DIR;
 import static oracle.weblogic.kubernetes.TestConstants.OPERATOR_RELEASE_NAME;
 import static oracle.weblogic.kubernetes.TestConstants.PV_ROOT;
 import static oracle.weblogic.kubernetes.TestConstants.REPO_EMAIL;
-import static oracle.weblogic.kubernetes.TestConstants.REPO_NAME;
 import static oracle.weblogic.kubernetes.TestConstants.REPO_PASSWORD;
 import static oracle.weblogic.kubernetes.TestConstants.REPO_REGISTRY;
 import static oracle.weblogic.kubernetes.TestConstants.REPO_SECRET_NAME;
@@ -175,7 +174,7 @@ public class ItDomainOnPV implements LoggedTest {
 
     String image = WLS_BASE_IMAGE_NAME + ":" + WLS_BASE_IMAGE_TAG;
     boolean isUseSecret = false;
-    if (!REPO_NAME.isEmpty()) {
+    if (!KIND_REPO.isEmpty()) {
       // We can't figure out why the kind clusters can't pull images from OCR using the image pull secret. There
       // is some evidence it may be a containerd bug. Therefore, we are going to "give up" and workaround the issue.
       // The workaround will be to:
