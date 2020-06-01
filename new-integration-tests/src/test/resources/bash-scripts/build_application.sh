c52a1e3a2022f7326178633aa1aacc1c2d35372a
sankar <sankarpn@gmail.com>
2020-06-01 10:21:28 -0700

    Fix parameters

--- new-integration-tests/src/test/resources/bash-scripts/build_application.sh
+++ new-integration-tests/src/test/resources/bash-scripts/build_application.sh
@@ -7,28 +7,9 @@ set -o errexit
 script="${BASH_SOURCE[0]}"
 
 function usage {
-  echo "usage: ${script} [-a <archive>] [-s <script_file>] [-p <properties_file>]"
-  echo "  -a Application name "
-  echo "  -u Script file "
-  echo "  -p Properties file "
-  echo "  -h Help"
+  echo "usage: ${script}"
   exit $1
 }
-
-while getopts ":h:a:s:p:" opt; do
-  case $opt in
-    a) archive="${OPTARG}"
-    ;;
-    s) script_file="${OPTARG}"
-    ;;
-    p) properties_file="${OPTARG}"
-    ;;
-    h) usage 0
-    ;;
-    *) usage 1
-    ;;
-  esac
-done
 
 MW_HOME=/u01/oracle
 WL_HOME=${MW_HOME}/wlserver
