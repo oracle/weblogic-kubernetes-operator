551d170c334fdf283f9848e646fd775ba3397b36
Sankar Periyathambi Neelakandan <45743425+sankarpn@users.noreply.github.com>
2020-06-02 06:32:00 -0700

    Support for building appplications (#1696)
    * Adding infra support to build J2EE applications
    
    * Add targets and system properties support
    
    * Remove comment
    
    * Fix parameters

---
+++ new-integration-tests/src/test/resources/bash-scripts/build_application.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Copyright (c) 2020, Oracle Corporation and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+set -o errexit
+
+script="${BASH_SOURCE[0]}"
+
+function usage {
+  echo "usage: ${script}"
+  exit $1
+}
+
+MW_HOME=/u01/oracle
+WL_HOME=${MW_HOME}/wlserver
+
+
+# source the WLS env
+echo "Setting up build environment"
+source ${WL_HOME}/server/bin/setWLSEnv.sh
+
+# call ant all to build
+cd /application
+ant ${sysprops} {targets}
