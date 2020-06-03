384b1d7640508ff21af87a299747a4008b56ff3f
sankar <sankarpn@gmail.com>
2020-06-03 07:37:17 -0700

    Use admin host and port

--- new-integration-tests/src/test/java/oracle/weblogic/kubernetes/ItDomainInPV.java
+++ new-integration-tests/src/test/java/oracle/weblogic/kubernetes/ItDomainInPV.java
@@ -764,8 +764,11 @@ public class ItDomainInPV implements LoggedTest {
     File wlstPropertiesFile = assertDoesNotThrow(() -> File.createTempFile("wlst", "properties"),
         "Creating WLST properties file failed");
     Properties p1 = new Properties();
+    p1.setProperty("admin_host", K8S_NODEPORT_HOST);
+    p1.setProperty("admin_port", Integer.toString(t3ChannelPort));
     p1.setProperty("admin_username", ADMIN_USERNAME_DEFAULT);
     p1.setProperty("admin_password", ADMIN_PASSWORD_DEFAULT);
+
     p1.setProperty("t3url", t3url);
 
     assertDoesNotThrow(() -> p.store(new FileOutputStream(wlstPropertiesFile), "wlst properties file"),
--- new-integration-tests/src/test/resources/python-scripts/change-server-count.py
+++ new-integration-tests/src/test/resources/python-scripts/change-server-count.py
@@ -4,7 +4,7 @@
 def change_server_count():
   try:
     print 'connecting to admin server'
-    connect(admin_username, admin_password, t3url)
+    connect(admin_username, admin_password, 't3://' + admin_host + ':' + admin_port)
     edit()
     startEdit()
     cd('/Clusters/' + cluster_name + '/DynamicServers/' + cluster_name)
@@ -16,7 +16,6 @@ def change_server_count():
   except NameError, e:
     print('Apparently properties not set.')
     print('Please check the property: ', sys.exc_info()[0], sys.exc_info()[1])
-    usage()
     exit(exitcode=1)
   except:
     print 'Deployment failed'
