0d7e02447d905abb0b6756c1a7a538707b295b8e
doxiao <dongbo.xiao@oracle.com>
2020-06-04 19:03:59 +0000

    More update to the image name

--- new-integration-tests/src/test/java/oracle/weblogic/kubernetes/ItMiiMultiModel.java
+++ new-integration-tests/src/test/java/oracle/weblogic/kubernetes/ItMiiMultiModel.java
@@ -160,7 +160,7 @@ class ItMiiMultiModel implements LoggedTest {
 
     logger.info("Create an image with two model files");
     miiImageMultiModel = createMiiImageAndVerify(
-        String.format("%s-%s", "mii-basic-image", "test-multi-model-image"),
+        "mii-test-multi-model-image",
         Arrays.asList(
             MODEL_DIR + "/" + MII_BASIC_WDT_MODEL_FILE, 
             MODEL_DIR + "/" + modelFileName2, 
