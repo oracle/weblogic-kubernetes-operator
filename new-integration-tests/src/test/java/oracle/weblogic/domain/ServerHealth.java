fd14de21a0923812d68c2be1c26fe0a4077e72b7
Ryan Eberhard <ryan.eberhard@oracle.com>
2020-06-11 16:10:10 -0400

    Backport new integration test suite

---
+++ new-integration-tests/src/test/java/oracle/weblogic/domain/ServerHealth.java
@@ -0,0 +1,132 @@
+// Copyright (c) 2020, Oracle Corporation and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package oracle.weblogic.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.joda.time.DateTime;
+
+@ApiModel(
+    description =
+        "ServerHealth describes the current status and health of a specific WebLogic Server.")
+public class ServerHealth {
+
+  @ApiModelProperty("RFC 3339 date and time at which the server started.")
+  private DateTime activationTime;
+
+  @ApiModelProperty(
+      "Server health of this WebLogic Server. If the value is \"Not available\", the operator has "
+          + "failed to read the health. If the value is \"Not available (possibly overloaded)\", the "
+          + "operator has failed to read the health of the server possibly due to the server is "
+          + "in overloaded state.")
+  private String overallHealth;
+
+  @ApiModelProperty("Status of unhealthy subsystems, if any.")
+  private List<SubsystemHealth> subsystems = new ArrayList<>();
+
+  public ServerHealth activationTime(DateTime activationTime) {
+    this.activationTime = activationTime;
+    return this;
+  }
+
+  public DateTime activationTime() {
+    return activationTime;
+  }
+
+  public DateTime getActivationTime() {
+    return activationTime;
+  }
+
+  public void setActivationTime(DateTime activationTime) {
+    this.activationTime = activationTime;
+  }
+
+  public ServerHealth overallHealth(String overallHealth) {
+    this.overallHealth = overallHealth;
+    return this;
+  }
+
+  public String overallHealth() {
+    return overallHealth;
+  }
+
+  public String getOverallHealth() {
+    return overallHealth;
+  }
+
+  public void setOverallHealth(String overallHealth) {
+    this.overallHealth = overallHealth;
+  }
+
+  public ServerHealth subsystems(List<SubsystemHealth> subsystems) {
+    this.subsystems = subsystems;
+    return this;
+  }
+
+  public List<SubsystemHealth> subsystems() {
+    return subsystems;
+  }
+
+  /**
+   * Adds subsystems health item.
+   * @param subsystemsItem Subsystem health
+   * @return this
+   */
+  public ServerHealth addSubsystemsItem(SubsystemHealth subsystemsItem) {
+    if (subsystems == null) {
+      subsystems = new ArrayList<>();
+    }
+    subsystems.add(subsystemsItem);
+    return this;
+  }
+
+  public List<SubsystemHealth> getSubsystems() {
+    return subsystems;
+  }
+
+  public void setSubsystems(List<SubsystemHealth> subsystems) {
+    this.subsystems = subsystems;
+  }
+
+  @Override
+  public String toString() {
+    return new ToStringBuilder(this)
+        .append("activationTime", activationTime)
+        .append("overallHealth", overallHealth)
+        .append("subsystems", subsystems)
+        .toString();
+  }
+
+  @Override
+  public int hashCode() {
+    return new HashCodeBuilder()
+        .append(overallHealth)
+        .append(activationTime)
+        .append(subsystems)
+        .toHashCode();
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    if (this == other) {
+      return true;
+    }
+
+    if (other == null || getClass() != other.getClass()) {
+      return false;
+    }
+    ServerHealth rhs = (ServerHealth) other;
+    return new EqualsBuilder()
+        .append(overallHealth, rhs.overallHealth)
+        .append(activationTime, rhs.activationTime)
+        .append(subsystems, rhs.subsystems)
+        .isEquals();
+  }
+}
---
+++ new-integration-tests/src/test/java/oracle/weblogic/kubernetes/actions/impl/AppParams.java
@@ -0,0 +1,56 @@
+// Copyright (c) 2020, Oracle Corporation and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package oracle.weblogic.kubernetes.actions.impl;
+
+import java.util.List;
+
+/**
+ * Contains the parameters for creating an application archive.
+ */
+
+public class AppParams {
+
+  // A list of directories under resources/apps that are part of the application.
+  // Note: the order of the directory names is significant. Files are copied into
+  // the staging directory in this order. 
+  private List<String> srcDirList;
+  
+  // The name of the final archive file.
+  // The name of the first dir in srcDirList will be used if the appName is absent.
+  private String appName;
+  
+  // Whether the output of the command is redirected to system out
+  private boolean redirect = true;
+  
+  public AppParams defaults() {
+    return this;
+  }
+
+  public AppParams srcDirList(List<String> srcDirList) {
+    this.srcDirList = srcDirList;
+    return this;
+  }
+
+  public List<String> srcDirList() {
+    return srcDirList;
+  }
+
+  public AppParams appName(String appName) {
+    this.appName = appName;
+    return this;
+  }
+
+  public String appName() {
+    return appName;
+  }
+
+  public AppParams redirect(boolean redirect) {
+    this.redirect = redirect;
+    return this;
+  }
+
+  public boolean redirect() {
+    return redirect;
+  }
+}
