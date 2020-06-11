89a77ae18a2079b1849730c59fd0c72398ee7911
doxiao <dongbo.xiao@oracle.com>
2020-06-11 22:09:56 +0000

    Add a new file

---
+++ operator/src/main/java/oracle/kubernetes/operator/logging/LoggingContext.java
@@ -0,0 +1,58 @@
+// Copyright (c) 2020, Oracle Corporation and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package oracle.kubernetes.operator.logging;
+
+/** Necessary additional context information for Operator log messages. */
+public class LoggingContext {
+  public static final String LOGGING_CONTEXT_KEY = "LoggingContextComponent";
+
+  private static final ThreadLocal<LoggingContext> currentContext = new ThreadLocal<>();
+
+  private String namespace;
+  private String domainUid;
+  
+  /**
+   * Gets the current logging context on the thread.
+   *
+   * @return current logging context
+   */
+  public static LoggingContext context() {
+    return currentContext.get();
+  }
+
+  /**
+   * Set the current LoggingContext on the thread.
+   *
+   * @param loggingContext instance of {@link LoggingContext}
+   */
+  public static void context(LoggingContext loggingContext) {
+    currentContext.set(loggingContext);
+  }
+
+  /**
+   * Remove the current LoggingContext on the thread.
+   *
+   */
+  public static void remove() {
+    currentContext.remove();
+  }
+  
+  public LoggingContext namespace(String namespace) {
+    this.namespace = namespace;
+    return this;
+  }
+  
+  public String namespace() {
+    return namespace;
+  }
+  
+  public LoggingContext domainUid(String domainUid) {
+    this.domainUid = domainUid;
+    return this;
+  }
+  
+  public String domainUid() {
+    return domainUid;
+  }
+}
