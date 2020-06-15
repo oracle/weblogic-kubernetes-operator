25eba07959fcdd54a97733351f5fb4faf0e0cf2f
Russell Gold <russell.gold@oracle.com>
2020-06-15 14:30:58 -0400

    Correct introspect version (#1730)
    * use label on introspector config map to track previous introspectVersion value
    
    * Add REST functionality for introspectDomain and restartDomain
    
    * Undo Javadoc editting error
    
    * rename POST actions

---
+++ operator/src/main/java/oracle/kubernetes/operator/rest/model/DomainActionType.java
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Oracle Corporation and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package oracle.kubernetes.operator.rest.model;
+
+public enum DomainActionType {
+  UNKNOWN, INTROSPECT, RESTART
+}
