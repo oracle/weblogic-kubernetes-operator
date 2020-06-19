ef5ae92848ff2481bba556db1201c124a36686ef
Xian Cao <xian.cao@oracle.com>
2020-06-19 11:33:51 -0700

    Scale a cluster using WLDF Policy (#1724)
    * initial commit
    
    * scale cluster with WLDF
    
    * refactor the code
    
    * add more logging information
    
    * add copyright
    
    * resolve merge conflicts
    
    * get the latest develop

---
+++ new-integration-tests/src/test/resources/bash-scripts/callpyscript.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Copyright (c) 2018, 2020, Oracle Corporation and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+ARG="$*"
+ENVSCRIPT=`find /u01 . -name setDomainEnv.sh -print`
+echo Sourcing $ENVSCRIPT
+. $ENVSCRIPT || exit 1
+echo "$@"
+echo Calling java weblogic.WLST $ARG
+eval java weblogic.WLST $ARG || exit 1
+
