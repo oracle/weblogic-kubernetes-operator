9aef94dec633dfd6bfa04d1187a72f00e890dff4
Ryan Eberhard <ryan.eberhard@oracle.com>
2020-06-30 15:08:07 -0400

    Parse all resource versions using BigInteger (#1774)
    * Parse all resource versions using BigInteger
    
    * Clarify behavior when failing to parse resource version
    
    * Watcher use resource version directly

--- operator/src/test/java/oracle/kubernetes/operator/helpers/KubernetesUtilsTest.java
+++ operator/src/test/java/oracle/kubernetes/operator/helpers/KubernetesUtilsTest.java
@@ -3,6 +3,8 @@
 
 package oracle.kubernetes.operator.helpers;
 
+import java.math.BigInteger;
+
 import io.kubernetes.client.openapi.models.V1ObjectMeta;
 import org.joda.time.DateTime;
 import org.junit.Test;
@@ -37,5 +39,54 @@ public class KubernetesUtilsTest {
 
     assertThat(KubernetesUtils.isFirstNewer(meta2, meta1), is(false));
     assertThat(KubernetesUtils.isFirstNewer(meta1, meta2), is(false));
+  }
+
+  @Test
+  public void whenHaveLargeResourceVersionsAndSameTime_succeedIsFirstNewer() {
+    DateTime now = DateTime.now();
+
+    // This needs to be a value bigger than 2147483647
+    String resVersion = "2733280673";
+    String evenBiggerResVersion = "2733280673000";
+
+    V1ObjectMeta first = new V1ObjectMeta().creationTimestamp(now).resourceVersion(resVersion);
+    V1ObjectMeta second = new V1ObjectMeta().creationTimestamp(now).resourceVersion(evenBiggerResVersion);
+
+    assertThat(KubernetesUtils.isFirstNewer(first, second), is(false));
+  }
+
+  @Test
+  public void whenHaveNonParsableResourceVersionsAndSameTime_succeedIsFirstNewer() {
+    DateTime now = DateTime.now();
+
+    String resVersion = "ThisIsNotANumber";
+    String differentResVersion = "SomeOtherValueAlsoNotANumber";
+
+    V1ObjectMeta first = new V1ObjectMeta().creationTimestamp(now).resourceVersion(resVersion);
+    V1ObjectMeta second = new V1ObjectMeta().creationTimestamp(now).resourceVersion(differentResVersion);
+
+    assertThat(KubernetesUtils.isFirstNewer(first, second), is(false));
+  }
+
+  @Test
+  public void whenHaveSmallResourceVersion_parseCorrectly() {
+    String resVersion = "1";
+
+    BigInteger bigInteger = KubernetesUtils.getResourceVersion(resVersion);
+    assertThat(bigInteger, is(BigInteger.ONE));
+  }
+
+  @Test
+  public void whenHaveNullResourceVersion_parseCorrectly() {
+    BigInteger bigInteger = KubernetesUtils.getResourceVersion((String) null);
+    assertThat(bigInteger, is(BigInteger.ZERO));
+  }
+
+  @Test
+  public void whenHaveOpaqueResourceVersion_parseCorrectly() {
+    String resVersion = "123NotANumber456";
+
+    BigInteger bigInteger = KubernetesUtils.getResourceVersion(resVersion);
+    assertThat(bigInteger, is(BigInteger.ZERO));
   }
 }
