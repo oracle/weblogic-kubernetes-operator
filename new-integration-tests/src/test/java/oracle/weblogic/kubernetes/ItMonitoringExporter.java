589c7672f89db3fb4c2e43fe33eb835799f2a527
Marina Kogan <marina.kogan@oracle.com>
2020-07-24 22:23:29 +0000

    switched to master branch

--- new-integration-tests/src/test/java/oracle/weblogic/kubernetes/ItMonitoringExporter.java
+++ new-integration-tests/src/test/java/oracle/weblogic/kubernetes/ItMonitoringExporter.java
@@ -1178,7 +1178,7 @@ class ItMonitoringExporter {
     assertDoesNotThrow(() -> FileUtils.deleteDirectory(monitoringAppNoRestPort.toFile()));
     assertDoesNotThrow(() -> Files.createDirectories(monitoringAppNoRestPort));
     String monitoringExporterBranch = Optional.ofNullable(System.getenv("MONITORING_EXPORTER_BRANCH"))
-        .orElse("repackage");
+        .orElse("master");
 
 
 
