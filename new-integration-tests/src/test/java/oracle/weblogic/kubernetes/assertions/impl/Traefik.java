fe3971c8d290aa2fba58234b32f7f1d8af778062
Sankar Periyathambi Neelakandan <45743425+sankarpn@users.noreply.github.com>
2020-07-30 07:19:02 -0700

    Add tests to verify loadbalancing with treafik lb for multiple domains (#1831)
    * Adding utilities for creating Traefik load balancer
    
    * Log the assertion errors in IT*.out files
    
    * Create ingress resource for traefik
    
    * Fix the traefik pod name
    
    * do kind repo check
    
    * uninstall traefik
    
    * Add application building
    
    * Deploy application using REST and access it through traefik LB
    
    * create secret for WebLogic domain
    
    * remove the testwebapp
    
    * Print JVMID from clusterview app
    
    * Remove the domain creation in PV
    
    * cleanup code
    
    * cleanup code
    
    * remove the domainUid from url
    
    * verify loadbalancing after creating 2 domains
    
    * cleanup javadocs
    
    * Add verification checks to determine host routing is done correctly
    
    * fix comments
    
    * Add a delay before accessing the app
    
    * Fix the curl command
    
    * iAddressed the review comments
    
    * Fix service name
    
    * fix namespace
    
    * add more wait time
    
    * Adding TLS test
    
    * Fix CN
    
    * Add https usecase
    
    * Encode it as normal String
    
    * Fix file names
    
    * include -k option to ignore host name verification
    
    * Add cert and key as String
    
    * wip
    
    * use same namespace for traefik
    
    * Added more tests
    
    * Fix the ingress rules creation  command
    
    * Fix getNodePort
    
    * Access console in a loop
    
    * Adding voyager tests
    
    * Renamed testclass to be generic for all loadbalancers
    
    * order the tests
    
    * Add break statement once reponse is found
    
    * bind domain names

---
+++ new-integration-tests/src/test/java/oracle/weblogic/kubernetes/assertions/impl/Traefik.java
@@ -0,0 +1,32 @@
+// Copyright (c) 2020, Oracle Corporation and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package oracle.weblogic.kubernetes.assertions.impl;
+
+import java.util.concurrent.Callable;
+
+/**
+ * Assertions for traefik ingress controller.
+ */
+public class Traefik {
+
+  /**
+   * Check if the traefik pod is running in the specified namespace.
+   *
+   * @param namespace in which to check for the running traefik pod
+   * @return true if the traefik pod is running, false otherwise
+   */
+  public static Callable<Boolean> isRunning(String namespace) {
+    return () -> Kubernetes.isTraefikPodRunning(namespace);
+  }
+
+  /**
+   * Check if the traefik pod is ready in the specified namespace.
+   *
+   * @param namespace in which to check for the traefik pod readiness
+   * @return true if the traefik pod is in ready state, false otherwise
+   */
+  public static Callable<Boolean> isReady(String namespace) {
+    return () -> Kubernetes.isTraefikPodReady(namespace);
+  }
+}
