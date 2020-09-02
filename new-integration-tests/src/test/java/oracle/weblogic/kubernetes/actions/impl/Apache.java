0a926c3215e22fee529b70757e418301efb1f124
Xian Cao <xian.cao@oracle.com>
2020-09-02 12:15:27 -0700

    create test infrastructure for Apache load balancer (#1891)
    * initial commit
    
    * apache lb
    
    * add apache tests to ItTwoDomainsLoadBalancers
    
    * fix apache-webtier chart imagePullSecret
    
    * get the ItPodsShutdownOption from shutdown3 branch
    
    * change imagePullPolicy to Always
    
    * push the apache image to Kind repo
    
    * add debug info for REPO_REGISTRY etc in Kind new
    
    * remoe pull image from ocir and push to kind repo
    
    * enable pull apache image from ocir and push to kind new
    
    * cleanup
    
    * move OCR login to test
    
    * address Vanaja's comments
    
    * address Pani's comments
    
    * address Vanaja's comments
    
    Co-authored-by: vanajamukkara <vanajakshi.mukkara@oracle.com>

---
+++ new-integration-tests/src/test/java/oracle/weblogic/kubernetes/actions/impl/Apache.java
@@ -0,0 +1,45 @@
+// Copyright (c) 2020, Oracle Corporation and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package oracle.weblogic.kubernetes.actions.impl;
+
+import oracle.weblogic.kubernetes.actions.impl.primitive.Helm;
+import oracle.weblogic.kubernetes.actions.impl.primitive.HelmParams;
+
+/**
+ * Utility class for Apache load balancer.
+ */
+public class Apache {
+
+  /**
+   * Install Apache Helm chart.
+   *
+   * @param params the parameters to Helm install command such as release name, namespace, repo url or chart dir,
+   *               chart name and chart values
+   * @return true on success, false otherwise
+   */
+  public static boolean install(ApacheParams params) {
+    return Helm.install(params.getHelmParams(), params.getValues());
+  }
+
+  /**
+   * Upgrade Apache Helm release.
+   *
+   * @param params the parameters to Helm upgrade command such as release name, namespace and chart values to override
+   * @return true on success, false otherwise
+   */
+  public static boolean upgrade(ApacheParams params) {
+    return Helm.upgrade(params.getHelmParams(), params.getValues());
+  }
+
+  /**
+   * Uninstall Apache Helm release.
+   *
+   * @param params the parameters to Helm uninstall command such as release name and namespace
+   * @return true on success, false otherwise
+   */
+  public static boolean uninstall(HelmParams params) {
+    return Helm.uninstall(params);
+  }
+
+}
