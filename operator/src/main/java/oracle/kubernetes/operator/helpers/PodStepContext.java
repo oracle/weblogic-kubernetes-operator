604c987e33e1801313cc9434ea9cb037c94085a0
Dongbo Xiao <dongbo.xiao@oracle.com>
2020-09-17 11:06:58 -0400

    OWLS-80038 and OWLS-80090: fix mountPath validation and token substitution (#1911)
    * Skip volume mount path validation if it contains valid tokens
    
    * Check admin serverPod's additional volume mount paths too in domain validation
    
    * Check cluster serverPod's additional mount paths too in domain validation
    
    * check mountpath validation after token substitution is performed
    
    * cleanup
    
    * fine tuning
    
    * minor cleanup
    
    * In progress
    
    * WIP
    
    * minor change
    
    * Modify a test name

--- operator/src/main/java/oracle/kubernetes/operator/helpers/PodStepContext.java
+++ operator/src/main/java/oracle/kubernetes/operator/helpers/PodStepContext.java
@@ -267,7 +267,9 @@ public abstract class PodStepContext extends BasePodStepContext {
    * @return a step to be scheduled.
    */
   Step verifyPod(Step next) {
-    return new VerifyPodStep(next);
+    return Step.chain(
+        DomainValidationSteps.createAdditionalDomainValidationSteps(podModel.getSpec()),
+        new VerifyPodStep(next));
   }
 
   /**
