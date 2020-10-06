81b5a2730ad4782b705089893b75ddf18a8c39fd
Anil Kedia <37935279+ankedia@users.noreply.github.com>
2020-10-06 13:32:53 -0400

    OWLS-84786 - Use Kubernetes Java Client 10.0.0 (#1972)
    * Second attempt at using the Kubernetes Java Client 10.0.0
    
    This reverts commit e4b5117b482baddeec32783476558f294fdc6150.
    
    * Diagnostics
    
    * Remove duplicated dependencies
    
    * More diagnostics
    
    * Update charts
    
    * changes for OWLS-84786
    
    * removed diagnostics messages and code cleanup for OWLS-84786
    
    * minor cleanup
    
    * Back out changes to docs/charts dir.
    
    Co-authored-by: Ryan Eberhard <ryan.eberhard@oracle.com>

--- operator/src/main/java/oracle/kubernetes/operator/helpers/PodStepContext.java
+++ operator/src/main/java/oracle/kubernetes/operator/helpers/PodStepContext.java
@@ -30,7 +30,6 @@ import io.kubernetes.client.openapi.models.V1PodReadinessGate;
 import io.kubernetes.client.openapi.models.V1PodSpec;
 import io.kubernetes.client.openapi.models.V1Probe;
 import io.kubernetes.client.openapi.models.V1SecretVolumeSource;
-import io.kubernetes.client.openapi.models.V1Status;
 import io.kubernetes.client.openapi.models.V1Volume;
 import io.kubernetes.client.openapi.models.V1VolumeMount;
 import oracle.kubernetes.operator.DomainSourceType;
@@ -428,7 +427,7 @@ public abstract class PodStepContext extends BasePodStepContext {
     return new CreateResponseStep(next);
   }
 
-  private ResponseStep<V1Status> deleteResponse(Step next) {
+  private ResponseStep<V1Pod> deleteResponse(Step next) {
     return new DeleteResponseStep(next);
   }
 
@@ -901,7 +900,7 @@ public abstract class PodStepContext extends BasePodStepContext {
     }
   }
 
-  private class DeleteResponseStep extends ResponseStep<V1Status> {
+  private class DeleteResponseStep extends ResponseStep<V1Pod> {
     DeleteResponseStep(Step next) {
       super(next);
     }
@@ -911,7 +910,7 @@ public abstract class PodStepContext extends BasePodStepContext {
     }
 
     @Override
-    public NextAction onFailure(Packet packet, CallResponse<V1Status> callResponses) {
+    public NextAction onFailure(Packet packet, CallResponse<V1Pod> callResponses) {
       if (callResponses.getStatusCode() == CallBuilder.NOT_FOUND) {
         return onSuccess(packet, callResponses);
       }
@@ -919,8 +918,9 @@ public abstract class PodStepContext extends BasePodStepContext {
     }
 
     @Override
-    public NextAction onSuccess(Packet packet, CallResponse<V1Status> callResponses) {
-      return doNext(replacePod(getNext()), packet);
+    public NextAction onSuccess(Packet packet, CallResponse<V1Pod> callResponses) {
+      PodAwaiterStepFactory pw = packet.getSpi(PodAwaiterStepFactory.class);
+      return doNext(pw.waitForDelete(callResponses.getResult(), replacePod(getNext())), packet);
     }
   }
 
