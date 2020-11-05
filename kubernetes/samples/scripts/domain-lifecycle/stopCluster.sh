e4907a3d71c45047dabf6ee95aa55c9f647d93d0
Anil Kedia <37935279+ankedia@users.noreply.github.com>
2020-11-05 11:25:30 -0500

    Owls83995 - Sample scripts to shutdown and start a specific managed server/cluster/domain (#2002)
    * owls-83995 - Scripts to start/stop a managed server/cluster/domain
    
    * fix method comments
    
    * Minor changes
    
    * Address review comments and fix script comments/usages.
    
    * Added integration tests, made few doc changes based on review comments and minor fix.
    
    * Clarify script usage, updated README file and minor changes.
    
    * Changes to add script usage details
    
    * Address PR review comments
    
    * Review comment and cleanup.
    
    * Documentation changes based on PR review comments.
    
    * Fully qualified replica value as per review comments
    
    * edit docs
    
    * edit README
    
    * Address PR review comments
    
    * Changes to address PR review comments and removed ItLifecycleSampleScripts class by adding methods in ItSamples
    
    * fix indentation
    
    * fix comment and typo
    
    * Added validation as per review comment.
    
    * changes to address review comment and minor cleanup
    
    * PR review comment - changes to assume default policy is IF_NEEDED if policy is not set at domain level.
    
    * changes for new algorithm as documented on http://aseng-wiki.us.oracle.com/asengwiki/pages/viewpage.action?pageId=5280694898
    
    * More changes for new algorithm.
    
    * code refactoring and minor doc update.
    
    * Minor change for dynamic server name validation
    
    * Changes to address review comments.
    
    * More review comment changes and cleanup.
    
    * Unset policy to start independent (stadalone) maanged server instead of ALWAYS.
    
    * Latest review comment changes.
    
    * More changes based on review comments.
    
    * Chnages for latest review comments.
    
    * Remove unused action variable and assignments.
    
    * Fix the logic to display error when config map not found and return policy without quotes.
    
    * Changes for latest review comments.
    
    * Changes for latest round of review comments.
    
    * use printError instead of echo
    
    * Changes to remove integration tests and doc review comments.
    
    Co-authored-by: Rosemary Marano <rosemary.marano@oracle.com>

---
+++ kubernetes/samples/scripts/domain-lifecycle/stopCluster.sh
@@ -0,0 +1,115 @@
+# !/bin/sh
+# Copyright (c) 2020, Oracle Corporation and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+#
+
+script="${BASH_SOURCE[0]}"
+scriptDir="$( cd "$( dirname "${script}" )" && pwd )"
+source ${scriptDir}/helper.sh
+if [ "${debug}" == "true" ]; then set -x; fi;
+
+function usage() {
+
+  cat << EOF
+
+  This script stops a WebLogic cluster in a domain by patching
+  'spec.clusters[<cluster-name>].serverStartPolicy' attribute of the domain
+  resource to 'NEVER'. This change will cause the operator to initiate shutdown
+  of cluster's WebLogic server instance pods if the pods are running.
+ 
+  Usage:
+ 
+    $(basename $0) -c mycluster [-n mynamespace] [-d mydomainuid] [-m kubecli]
+  
+    -c <cluster-name>   : Cluster name parameter is required.
+
+    -d <domain_uid>     : Domain unique-id. Default is 'sample-domain1'.
+
+    -n <namespace>      : Domain namespace. Default is 'sample-domain1-ns'.
+
+    -m <kubernetes_cli> : Kubernetes command line interface. Default is 'kubectl' if KUBERNETES_CLI env
+                          variable is not set. Otherwise default is the value of KUBERNETES_CLI env variable.
+
+    -v <verbose_mode>   : Enables verbose mode. Default is 'false'.
+
+    -h                  : This help.
+   
+EOF
+exit $1
+}
+
+kubernetesCli=${KUBERNETES_CLI:-kubectl}
+clusterName=""
+domainUid="sample-domain1"
+domainNamespace="sample-domain1-ns"
+verboseMode=false
+patchJson=""
+
+while getopts "vc:n:m:d:h" opt; do
+  case $opt in
+    c) clusterName="${OPTARG}"
+    ;;
+    n) domainNamespace="${OPTARG}"
+    ;;
+    d) domainUid="${OPTARG}"
+    ;;
+    m) kubernetesCli="${OPTARG}"
+    ;;
+    v) verboseMode=true;
+    ;;
+    h) usage 0
+    ;;
+    *) usage 1
+    ;;
+  esac
+done
+
+set -eu
+
+#
+# Function to perform validations, read files and initialize workspace
+#
+function initialize {
+
+  validateErrors=false
+
+  validateKubernetesCliAvailable
+  validateJqAvailable
+
+  if [ -z "${clusterName}" ]; then
+    validationError "Please specify cluster name using '-c' parameter e.g. '-c cluster-1'."
+  fi
+
+  isValidCluster=""
+  validateClusterName "${domainUid}" "${domainNamespace}" "${clusterName}" isValidCluster
+
+  if [ "${isValidCluster}" != 'true' ]; then
+    validationError "cluster ${clusterName} is not part of domain ${domainUid} in namespace ${domainNamespace}. "
+  fi
+
+  failIfValidationErrors
+}
+
+initialize
+
+# Get the domain in json format
+domainJson=$(${kubernetesCli} get domain ${domainUid} -n ${domainNamespace} -o json)
+
+# Get server start policy for this server
+getClusterPolicy "${domainJson}" "${clusterName}" startPolicy
+if [ -z "${startPolicy}" ]; then
+  getDomainPolicy "${domainJson}" startPolicy
+fi
+
+if [[ "${startPolicy}" == 'NEVER' || "${startPolicy}" == 'ADMIN_ONLY' ]]; then 
+  printInfo "No changes needed, exiting. The cluster '${clusterName}' is already stopped or stopping. The effective value of spec.clusters[?(clusterName="${clusterName}"].serverStartPolicy attribute on the domain resource is 'NEVER' or 'ADMIN_ONLY'."
+  exit 0
+fi
+
+# Set policy value to NEVER
+printInfo "Patching start policy of cluster '${clusterName}' from '${startPolicy}' to 'NEVER'."
+createPatchJsonToUpdateClusterPolicy "${domainJson}" "${clusterName}" "NEVER" patchJson
+
+executePatchCommand "${kubernetesCli}" "${domainUid}" "${domainNamespace}" "${patchJson}" "${verboseMode}"
+
+printInfo "Successfully patched cluster '${clusterName}' with 'NEVER' start policy!"
