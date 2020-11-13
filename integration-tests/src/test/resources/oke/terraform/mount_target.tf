92f97f62cd7b255ee211e89575172d8a6a5a0b6c
Marina Kogan <marina.kogan@oracle.com>
2020-11-13 01:09:06 +0000

    mt

--- integration-tests/src/test/resources/oke/terraform/mount_target.tf
+++ integration-tests/src/test/resources/oke/terraform/mount_target.tf
@@ -6,8 +6,8 @@ resource "oci_file_storage_mount_target" "oketest_mount_target" {
   #Required
   availability_domain = "${var.availability_domain_name}"
   compartment_id      = "${var.compartment_ocid}"
-  #subnet_id           = "${oci_core_subnet.oke-subnet-worker-2.id}"
-  subnet_id           = "${lookup(data.oci_core_subnets.oke_subnets.subnets[var.subnet_worker],"id")}"
+  subnet_id           = "${oci_core_subnet.oke-subnet-worker-2.id}"
+  #subnet_id           = "${lookup(data.oci_core_subnets.oke_subnets.subnets[var.subnet_worker],"id")}"
 
   #Optional
   display_name = "${var.cluster_name}-mt"
