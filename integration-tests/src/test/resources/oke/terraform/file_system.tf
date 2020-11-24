65d7e90386778c3e6c056ce12c91a38dfd884883
Marina Kogan <marina.kogan@oracle.com>
2020-11-24 08:57:59 -0800

    Added support for domainOnPV using NFS in Oke cluster (#2053)
    * added nfs
    
    * added antiaffity
    
    * added syncronized
    
    * fixed typo
    
    * fixed typo1
    
    * fixed domain crd for itpodtemplate
    
    * style
    
    * added storageclass to pv
    
    * added storageclass to pvc
    
    * added storageclass to pvc1
    
    * style
    
    * fixed hostpath
    
    * added support for domain job fix-pvc for oke
    
    * added nfs to pv
    
    * added fix for antiaffinity
    
    * fixed pv creation
    
    * added terraform config
    
    * mt
    
    * fixed mt
    
    * added copyrights
    
    * added fixpvc common util
    
    * style
    
    * style1
    
    * fixed chown command
    
    * fixed chown command1
    
    * consolidated pv and pvc creation for nfs and hostpath
    
    * style
    
    * fixed owner change
    
    * fixed typos

---
+++ integration-tests/src/test/resources/oke/terraform/file_system.tf
@@ -0,0 +1,10 @@
+/*
+# Copyright (c) 2020, Oracle Corporation and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+*/
+
+resource "oci_file_storage_file_system" "oketest_fs" {
+  #Required
+  availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[1],"name")}"
+  compartment_id      = "${var.compartment_ocid}"
+}
