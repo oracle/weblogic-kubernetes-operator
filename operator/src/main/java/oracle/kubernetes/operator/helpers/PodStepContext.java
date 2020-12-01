cad0c306067ef3650d61a9194a560443bb3e1e24
Anil Kedia <anil.kedia@oracle.com>
2020-12-01 21:28:26 +0000

    Change to set higher precedence for env var configured in initContainer over those configured in server or cluster as per review comments.

--- operator/src/main/java/oracle/kubernetes/operator/helpers/PodStepContext.java
+++ operator/src/main/java/oracle/kubernetes/operator/helpers/PodStepContext.java
@@ -599,10 +599,11 @@ public abstract class PodStepContext extends BasePodStepContext {
   }
 
   private List<V1EnvVar> createEnv(V1Container c, TuningParameters tuningParameters) {
-    List<V1EnvVar> envVars = new ArrayList<>();
-    Optional.ofNullable(c.getEnv()).ifPresent(envVars::addAll);
-    envVars.addAll(getEnvironmentVariables(tuningParameters));
-    return envVars;
+    List<V1EnvVar> initContainerEnvVars = new ArrayList<>();
+    Optional.ofNullable(c.getEnv()).ifPresent(initContainerEnvVars::addAll);
+    getEnvironmentVariables(tuningParameters).forEach(envVar ->
+            addDefaultEnvVarIfMissing(initContainerEnvVars, envVar.getName(), envVar.getValue()));
+    return initContainerEnvVars;
   }
 
   // ---------------------- model methods ------------------------------
