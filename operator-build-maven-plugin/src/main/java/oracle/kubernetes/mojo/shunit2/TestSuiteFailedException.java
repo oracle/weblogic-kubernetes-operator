8e3fb5ddd16228a7bd51a5a5b1eff07016e0da6e
Russell Gold <russ@russgold.net>
2020-12-10 10:55:44 -0500

    Create plugin for running bash unit tests

---
+++ operator-build-maven-plugin/src/main/java/oracle/kubernetes/mojo/shunit2/TestSuiteFailedException.java
@@ -0,0 +1,11 @@
+// Copyright (c) 2020, Oracle Corporation and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package oracle.kubernetes.mojo.shunit2;
+
+class TestSuiteFailedException extends RuntimeException {
+
+  public TestSuiteFailedException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
