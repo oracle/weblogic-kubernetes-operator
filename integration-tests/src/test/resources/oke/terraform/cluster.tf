e585f3ebc72a325889523c045062dbc7d68a9cfe
Ryan Eberhard <ryan.eberhard@oracle.com>
2021-01-04 16:25:34 -0500

    Update copyright statements for 2021 and adjust language (#2122)
    * Update copyright statements for 2021 and language change
    
    * Revert changes to previous doc sets
    
    * Update chart build
    
    * Fix copyright
    
    * Update JavaDoc copyright source

--- integration-tests/src/test/resources/oke/terraform/cluster.tf
+++ integration-tests/src/test/resources/oke/terraform/cluster.tf
@@ -1,5 +1,5 @@
 /*
-# Copyright (c) 2020, Oracle Corporation and/or its affiliates.
+# Copyright (c) 2020, 2021, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 */
 variable "cluster_kubernetes_version" { default = "v1.15.7" }
