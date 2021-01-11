be73de798625f59204c792317fd3c54fa7f1f096
Huiling Zhao <41090416+hzhao-github@users.noreply.github.com>
2021-01-11 12:02:55 -0800

    Added Operator Upgrade Integration tests for FMW Domain (#2102)
    * Added Operator Upgrade Integration tests for FMW Domain
    
    * Moved helpers to commom util class
    
    * Added more test cases
    
    * fixed the copyright
    
    Co-authored-by: huizhao <huizhao@huizhao-1.subnet2ad1phx.devweblogicphx.oraclevcn.com>
    Co-authored-by: huizhao <huizhao@phx3187a26.subnet4ad3phx.devweblogicphx.oraclevcn.com>

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/CommonTestUtils.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/CommonTestUtils.java
@@ -86,6 +86,8 @@ import static java.nio.file.Files.createDirectories;
 import static java.nio.file.Files.readString;
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static java.util.concurrent.TimeUnit.SECONDS;
+import static oracle.weblogic.kubernetes.TestConstants.ADMIN_PASSWORD_DEFAULT;
+import static oracle.weblogic.kubernetes.TestConstants.ADMIN_USERNAME_DEFAULT;
 import static oracle.weblogic.kubernetes.TestConstants.APACHE_RELEASE_NAME;
 import static oracle.weblogic.kubernetes.TestConstants.APACHE_SAMPLE_CHART_DIR;
 import static oracle.weblogic.kubernetes.TestConstants.APPSCODE_REPO_NAME;
@@ -148,6 +150,7 @@ import static oracle.weblogic.kubernetes.TestConstants.WEBLOGIC_IMAGE_NAME;
 import static oracle.weblogic.kubernetes.TestConstants.WEBLOGIC_IMAGE_TAG;
 import static oracle.weblogic.kubernetes.TestConstants.WEBLOGIC_IMAGE_TO_USE_IN_SPEC;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.ARCHIVE_DIR;
+import static oracle.weblogic.kubernetes.actions.ActionConstants.ITTESTS_DIR;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.MODEL_DIR;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.WDT_VERSION;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.WIT_BUILD_DIR;
@@ -196,6 +199,7 @@ import static oracle.weblogic.kubernetes.actions.TestActions.uninstallKibana;
 import static oracle.weblogic.kubernetes.actions.TestActions.upgradeOperator;
 import static oracle.weblogic.kubernetes.actions.impl.primitive.Kubernetes.listSecrets;
 import static oracle.weblogic.kubernetes.assertions.TestAssertions.appAccessibleInPod;
+import static oracle.weblogic.kubernetes.assertions.TestAssertions.checkHelmReleaseRevision;
 import static oracle.weblogic.kubernetes.assertions.TestAssertions.credentialsNotValid;
 import static oracle.weblogic.kubernetes.assertions.TestAssertions.credentialsValid;
 import static oracle.weblogic.kubernetes.assertions.TestAssertions.doesImageExist;
@@ -1160,8 +1164,13 @@ public class CommonTestUtils {
    *
    * @param domain the oracle.weblogic.domain.Domain object to create domain custom resource
    * @param domainNamespace namespace in which the domain will be created
+   * @param domVersion custom resource's version
    */
-  public static void createDomainAndVerify(Domain domain, String domainNamespace) {
+  public static void createDomainAndVerify(Domain domain, 
+                                           String domainNamespace,
+                                           String... domVersion) {
+    String domainVersion = (domVersion.length == 0) ? DOMAIN_VERSION : domVersion[0];
+
     LoggingFacade logger = getLogger();
     // create the domain CR
     assertNotNull(domain, "domain is null");
@@ -1170,7 +1179,7 @@ public class CommonTestUtils {
 
     logger.info("Creating domain custom resource for domainUid {0} in namespace {1}",
         domainUid, domainNamespace);
-    assertTrue(assertDoesNotThrow(() -> createDomainCustomResource(domain),
+    assertTrue(assertDoesNotThrow(() -> createDomainCustomResource(domain, domainVersion),
         String.format("Create domain custom resource failed with ApiException for %s in namespace %s",
             domainUid, domainNamespace)),
         String.format("Create domain custom resource failed with ApiException for %s in namespace %s",
@@ -1186,7 +1195,7 @@ public class CommonTestUtils {
                 domainNamespace,
                 condition.getElapsedTimeInMS(),
                 condition.getRemainingTimeInMS()))
-        .until(domainExists(domainUid, DOMAIN_VERSION, domainNamespace));
+        .until(domainExists(domainUid, domainVersion, domainNamespace));
   }
 
   /**
@@ -1657,7 +1666,7 @@ public class CommonTestUtils {
    * @param domainName Name of domain to which cluster belongs
    * @param namespace cluster's namespace
    * @param replicaCount replica count value to match
-   * @return true, if the cluster's replica count matches the input parameter value.
+   * @return true if matches false if not
    */
   public static boolean checkClusterReplicaCountMatches(String clusterName, String domainName,
                                                         String namespace, Integer replicaCount) throws ApiException {
@@ -3442,5 +3451,117 @@ public class CommonTestUtils {
         .until(assertDoesNotThrow(() -> podDoesNotExist(podName, domainUid, domNamespace),
             String.format("podDoesNotExist failed with ApiException for %s in namespace in %s",
                 podName, domNamespace)));
+  }
+
+  /**
+   * Deploy application and access the application once to make sure the app is accessible.
+   * @param domainNamespace namespace where domain exists
+   * @param domainUid the domain to which the cluster belongs
+   * @param clusterName the WebLogic cluster name that the app deploys to
+   * @param adminServerName the WebLogic admin server name that the app deploys to
+   * @param adminServerPodName WebLogic admin pod prefix
+   * @param managedServerPodNamePrefix WebLogic managed server pod prefix
+   * @param replicaCount replica count of the cluster
+   * @param adminInternalPort admin server's internal port
+   * @param msInternalPort managed server's internal port
+   */
+  public static void deployAndAccessApplication(String domainNamespace,
+                                                String domainUid,
+                                                String clusterName,
+                                                String adminServerName,
+                                                String adminServerPodName,
+                                                String managedServerPodNamePrefix,
+                                                int replicaCount,
+                                                String adminInternalPort,
+                                                String msInternalPort) {
+    final LoggingFacade logger = getLogger();
+
+    Path archivePath = Paths.get(ITTESTS_DIR, "../src/integration-tests/apps/testwebapp.war");
+    logger.info("Deploying application {0} to domain {1} cluster target cluster-1 in namespace {2}",
+        archivePath, domainUid, domainNamespace);
+    logger.info("Deploying webapp {0} to admin server and cluster", archivePath);
+    DeployUtil.deployUsingWlst(adminServerPodName,
+                               adminInternalPort,
+                               ADMIN_USERNAME_DEFAULT,
+                               ADMIN_PASSWORD_DEFAULT,
+                               clusterName + "," + adminServerName,
+                               archivePath,
+                               domainNamespace);
+
+    // check if the application is accessible inside of a server pod using quick retry policy
+    logger.info("Check and wait for the application to become ready");
+    for (int i = 1; i <= replicaCount; i++) {
+      checkAppIsRunning(withQuickRetryPolicy, domainNamespace, managedServerPodNamePrefix + i,
+          msInternalPort, "testwebapp/index.jsp", managedServerPodNamePrefix + i);
+    }
+    checkAppIsRunning(withQuickRetryPolicy, domainNamespace, adminServerPodName,
+        adminInternalPort, "testwebapp/index.jsp", adminServerPodName);
+  }
+
+  /**
+   * Check application availability while the operator upgrade is happening and once the ugprade is complete
+   * by accessing the application inside the managed server pods.
+   * @param domainNamespace namespace where domain exists
+   * @param operatorNamespace namespace where operator exists
+   * @param appAvailability application's availability
+   * @param adminPodName WebLogic admin server pod name
+   * @param managedServerPodNamePrefix WebLogic managed server pod prefix
+   * @param replicaCount replica count of the cluster
+   * @param adminInternalPort admin server's internal port
+   * @param msInternalPort managed server's internal port
+   * @param appPath application path
+   */
+  public static void collectAppAvailability(String domainNamespace,
+                                            String operatorNamespace,
+                                            List<Integer> appAvailability,
+                                            String adminPodName,
+                                            String managedServerPodNamePrefix,
+                                            int replicaCount,
+                                            String adminInternalPort,
+                                            String msInternalPort,
+                                            String appPath) {
+    final LoggingFacade logger = getLogger();
+
+    // Access the pod periodically to check application's availability during
+    // upgrade and after upgrade is complete.
+    // appAccessedAfterUpgrade is used to access the app once after upgrade is complete
+    boolean appAccessedAfterUpgrade = false;
+    while (!appAccessedAfterUpgrade) {
+      boolean isUpgradeComplete = checkHelmReleaseRevision(OPERATOR_RELEASE_NAME, operatorNamespace, "2");
+      // upgrade is not complete or app is not accessed after upgrade
+      if (!isUpgradeComplete || !appAccessedAfterUpgrade) {
+        // Check application accessibility on admin server
+        if (appAccessibleInPod(domainNamespace,
+                               adminPodName,
+                               adminInternalPort,
+                               appPath,
+                               adminPodName)) {
+          appAvailability.add(1);
+          logger.info("application accessible in admin pod " + adminPodName);
+        } else {
+          appAvailability.add(0);
+          logger.info("application not accessible in admin pod " + adminPodName);
+        }
+
+        // Check application accessibility on managed servers
+        for (int i = 1; i <= replicaCount; i++) {
+          if (appAccessibleInPod(domainNamespace,
+                       managedServerPodNamePrefix + i,
+                                 msInternalPort,
+                                 appPath,
+                                managedServerPodNamePrefix + i)) {
+            appAvailability.add(1);
+            logger.info("application is accessible in pod " + managedServerPodNamePrefix + i);
+          } else {
+            appAvailability.add(0);
+            logger.info("application is not accessible in pod " + managedServerPodNamePrefix + i);
+          }
+        }
+      }
+      if (isUpgradeComplete) {
+        logger.info("Upgrade is complete and app is accessed after upgrade");
+        appAccessedAfterUpgrade = true;
+      }
+    }
   }
 }
