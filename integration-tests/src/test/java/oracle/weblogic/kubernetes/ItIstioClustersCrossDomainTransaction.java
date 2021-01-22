8f43d38526d11b099522203bc435c21d9a003c21
Marina Kogan <marina.kogan@oracle.com>
2021-01-22 23:44:00 +0000

    fixed host

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItIstioClustersCrossDomainTransaction.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItIstioClustersCrossDomainTransaction.java
@@ -55,7 +55,6 @@ import static oracle.weblogic.kubernetes.TestConstants.ADMIN_USERNAME_DEFAULT;
 import static oracle.weblogic.kubernetes.TestConstants.DB_IMAGE_TO_USE_IN_SPEC;
 import static oracle.weblogic.kubernetes.TestConstants.DOMAIN_API_VERSION;
 import static oracle.weblogic.kubernetes.TestConstants.DOMAIN_VERSION;
-import static oracle.weblogic.kubernetes.TestConstants.K8S_NODEPORT_HOST;
 import static oracle.weblogic.kubernetes.TestConstants.OCIR_SECRET_NAME;
 import static oracle.weblogic.kubernetes.TestConstants.RESULTS_ROOT;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.APP_DIR;
