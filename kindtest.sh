81bb6859b03013f95f128a069a0e96e3467ac596
Marina Kogan <marina.kogan@oracle.com>
2021-01-27 11:33:30 -0800

    Removed cross cluster test from kindest.sh sequential run  (#2167)
    * added new test
    
    * styling
    
    * fixed logic
    
    * fixed test logic
    
    * fixed test logic1
    
    * test1
    
    * added verbose
    
    * added verbose
    
    * revert later to return false
    
    * revert later to return false
    
    * revert later to return false
    
    * modified k8s host
    
    * modified k8s host
    
    * modified k8s host
    
    * fixed test
    
    * fixed test
    
    * fixed test
    
    * fixed test1
    
    * fixed test2
    
    * fixed test3
    
    * fixed test4
    
    * fixed test5
    
    * fixed test
    
    * fixed test
    
    * unset final var
    
    * added more
    
    * added more
    
    * added more
    
    * added more
    
    * fixed test
    
    * split on two classes
    
    * split on two classes
    
    * fixed port
    
    * added files
    
    * style
    
    * corrected file
    
    * fixed host
    
    * fixed host
    
    * added files
    
    * fixed tests
    
    * fixed test
    
    * fixed build
    
    * fixed build1
    
    * fixed style
    
    * excluded tests from regular exec
    
    * added TWOCLUSTER var
    
    * added Assumption
    
    * added Assumption
    
    * added tag
    
    * fixed style
    
    * added disable
    
    * fixed test
    
    * fixed test
    
    * fixed test
    
    * fixed typo
    
    * fixed test1
    
    * added template for tfvars file
    
    * docs corrections
    
    * removed CrossCluster from seq run

--- kindtest.sh
+++ kindtest.sh
@@ -275,8 +275,8 @@ if [ "${maven_profile_name}" = "wls-image-cert" ] || [ "${maven_profile_name}" =
   time mvn -Dwdt.download.url="${wdt_download_url}" -Dwit.download.url="${wit_download_url}" -pl integration-tests -P ${maven_profile_name} verify 2>&1 | tee "${RESULT_ROOT}/kindtest.log"
 else
   if [ "${test_filter}" = "ItOperatorUpgrade" ] || [ "${parallel_run}" = "false" ]; then
-    echo "Running mvn -Dit.test=${test_filter} -Dwdt.download.url=${wdt_download_url} -Dwit.download.url=${wit_download_url} -pl integration-tests -P ${maven_profile_name} verify"
-    time mvn -Dit.test="${test_filter}" -Dwdt.download.url="${wdt_download_url}" -Dwit.download.url="${wit_download_url}" -pl integration-tests -P ${maven_profile_name} verify 2>&1 | tee "${RESULT_ROOT}/kindtest.log"
+    echo "Running mvn -Dit.test=${test_filter}, !ItIstioCrossClusters -Dwdt.download.url=${wdt_download_url} -Dwit.download.url=${wit_download_url} -pl integration-tests -P ${maven_profile_name} verify"
+    time mvn -Dit.test="${test_filter}, !ItIstioCrossClusters*" -Dwdt.download.url="${wdt_download_url}" -Dwit.download.url="${wit_download_url}" -pl integration-tests -P ${maven_profile_name} verify 2>&1 | tee "${RESULT_ROOT}/kindtest.log"
   else
     echo "Running mvn -Dit.test=${test_filter}, !ItOperatorUpgrade, !ItDedicatedMode, !ItT3Channel, !ItOpUpgradeFmwDomainInPV, !ItIstioCrossCLusters* -Dwdt.download.url=${wdt_download_url} -Dwit.download.url=${wit_download_url} -DPARALLEL_CLASSES=${parallel_run} -DNUMBER_OF_THREADS=${threads}  -pl integration-tests -P ${maven_profile_name} verify"
     time mvn -Dit.test="${test_filter}, !ItOperatorUpgrade, !ItDedicatedMode, !ItT3Channel, !ItOpUpgradeFmwDomainInPV, !ItIstioCrossClusters*" -Dwdt.download.url="${wdt_download_url}" -Dwit.download.url="${wit_download_url}" -DPARALLEL_CLASSES="${parallel_run}" -DNUMBER_OF_THREADS="${threads}" -pl integration-tests -P ${maven_profile_name} verify 2>&1 | tee "${RESULT_ROOT}/kindtest.log"
