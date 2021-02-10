1f6223709fef3060ef36b25545307fac31932cf5
Tom Barnes <tom.barnes@oracle.com>
2021-02-10 10:16:31 -0500

    default WLST_PROPERTIES to include WLST_EXTRA_PROPERTIES which in turn defaults to setting 'urandom' (#2183)

--- operator/src/main/resources/scripts/wlst.sh
+++ operator/src/main/resources/scripts/wlst.sh
@@ -45,6 +45,19 @@ wlst_loc2="${MW_HOME}/oracle_common/common/bin/wlst.sh"
 [ -f "$wlst_loc1" ]   && wlst_sh="$wlst_loc1"
 [ -z "$wlst_sh" ] && trace SEVERE "'${wlst_loc1}' or '${wlst_loc2}' not found, make sure ORACLE_HOME, WL_HOME, or MW_HOME is set correctly." && exit 1
 
+urandom_prop="-Djava.security.egd=file:/dev/./urandom"
+if [ ! -v WLST_EXTRA_PROPERTIES ]; then
+  trace "Env var WLST_EXTRA_PROPERTIES not set, defaulting to '$urandom_prop'."
+  WLST_EXTRA_PROPERTIES="$urandom_prop"
+else
+  trace "Env var WLST_EXTRA_PROPERTIES has been externally set to '$WLST_EXTRA_PROPERTIES' (it will not default to '$urandom_prop')."
+fi
+export WLST_PROPERTIES="$WLST_PROPERTIES $WLST_EXTRA_PROPERTIES"
+trace "Env var WLST_PROPERTIES='$WLST_PROPERTIES' (includes WLST_EXTRA_PROPERTIES)."
+if [ "${WLST_PROPERTIES/$urandom_prop/}" = "${WLST_PROPERTIES}" ]; then
+  trace "Env var WLST_PROPERTIES does not include '$urandom_prop'; this may significantly slow any use of the WLST encrypt command on low entropy hosts."
+fi
+
 trace "Running wlst script '${wlst_script}'"
 
 ${wlst_sh} -skipWLSModuleScanning ${wlst_script} "${@:2}"
