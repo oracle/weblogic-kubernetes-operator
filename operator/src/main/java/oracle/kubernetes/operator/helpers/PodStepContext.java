bf7307aa8376dbbfdf9095c6563f80ad0bae75c4
Ryan Eberhard <ryan.eberhard@oracle.com>
2021-02-16 15:25:52 -0500

    First phase updating of dependencies

--- operator/src/main/java/oracle/kubernetes/operator/helpers/PodStepContext.java
+++ operator/src/main/java/oracle/kubernetes/operator/helpers/PodStepContext.java
@@ -370,7 +370,7 @@ public abstract class PodStepContext extends BasePodStepContext {
 
     return new CallBuilder()
             .patchPodAsync(getPodName(), getNamespace(), getDomainUid(),
-            new V1Patch(patchBuilder.build().toString()), patchResponse(next));
+              new V1Patch(patchBuilder.build().toString()), patchResponse(next));
   }
 
   private Map<String, String> getNonHashedPodLabels() {
