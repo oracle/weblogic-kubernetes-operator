35d309d1e5ed2a810cee256c2b8308ceeaeaef2f
Ryan Eberhard <ryan.eberhard@oracle.com>
2021-03-09 12:01:39 -0500

    Obfuscate

--- owaspDependencyCheck.sh
+++ owaspDependencyCheck.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2020, 2021, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-# Generate report of publicly disclosed vulnerabilities
+# Generate report of known issues in dependencies
 
 set -e
 
