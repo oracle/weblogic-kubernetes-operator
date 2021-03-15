10e6412f88d0b5e8f66cd5a3de2003e9ef4e8593
Anil Kedia <anil.kedia@oracle.com>
2021-03-15 04:51:00 +0000

    Integration test to verify that domain processing completed event is generated after a scaling operation

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItKubernetesEvents.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItKubernetesEvents.java
@@ -169,6 +169,7 @@ public class ItKubernetesEvents {
     // create pull secrets for WebLogic image when running in non Kind Kubernetes cluster
     // this secret is used only for non-kind cluster
     createSecretForBaseImages(domainNamespace1);
+
   }
 
   /**
@@ -182,6 +183,7 @@ public class ItKubernetesEvents {
   @DisplayName("Test domain events for various successful domain life cycle changes")
   public void testDomainK8SEventsSuccess() {
     DateTime timestamp = new DateTime(Instant.now().getEpochSecond() * 1000L);
+
     logger.info("Creating domain");
     createDomain();
 
@@ -375,10 +377,45 @@ public class ItKubernetesEvents {
   }
 
   /**
+   * Scale down and scale up the domain and verify that
+   * DomainProcessingCompleted normal event is generated.
+   */
+  @Order(7)
+  @Test
+  public void testK8sEventsScaleDomainAndVerifyDomainProcessingCompleted() {
+    try {
+      scaleDomainAndVerifyCompletedEvent(1, "scale down", true);
+      scaleDomainAndVerifyCompletedEvent(2, "scale up", true);
+    } finally {
+      scaleDomain(2);
+    }
+  }
+
+  private void scaleDomainAndVerifyCompletedEvent(int replicaCount, String testType, boolean verify) {
+    DateTime timestamp = new DateTime(Instant.now().getEpochSecond() * 1000L);
+    String patchStr
+            = "["
+            + "{\"op\": \"replace\", \"path\": \"/spec/clusters/0/replicas\", \"value\": " + replicaCount + "}"
+            + "]";
+    logger.info("Updating replicas in cluster {0} using patch string: {1}", cluster1Name, patchStr);
+    V1Patch patch = new V1Patch(patchStr);
+    assertTrue(patchDomainCustomResource(domainUid, domainNamespace1, patch, V1Patch.PATCH_FORMAT_JSON_PATCH),
+            "Failed to patch domain");
+    if (verify) {
+      logger.info("Verify the DomainProcessingCompleted event is generated after " + testType);
+      checkEvent(opNamespace, domainNamespace1, domainUid, DOMAIN_PROCESSING_COMPLETED, "Normal", timestamp);
+    }
+  }
+
+  private void scaleDomain(int replicaCount) {
+    scaleDomainAndVerifyCompletedEvent(replicaCount, null, false);
+  }
+
+  /**
    * Scale the cluster below minimum dynamic cluster size and verify the DomainValidationError
    * warning event is generated.
    */
-  @Order(7)
+  @Order(8)
   @Test
   public void testDomainK8sEventsScaleBelowMin() {
     DateTime timestamp = new DateTime(Instant.now().getEpochSecond() * 1000L);
@@ -413,7 +450,7 @@ public class ItKubernetesEvents {
    * Replace the pv and pvc in the domain resource with a pv/pvc not containing any WebLogic domain
    * and verify the DomainProcessingFailed warning event is generated.
    */
-  @Order(8)
+  @Order(9)
   @Test
   public void testDomainK8sEventsProcessingFailed() {
     DateTime timestamp = new DateTime(Instant.now().getEpochSecond() * 1000L);
@@ -462,7 +499,7 @@ public class ItKubernetesEvents {
   /**
    * Test DomainDeleted event is logged when domain resource is deleted.
    */
-  @Order(9)
+  @Order(10)
   @Test
   @DisplayName("Test domain events for various domain life cycle changes")
   public void testDomainK8SEventsDelete() {
@@ -496,7 +533,7 @@ public class ItKubernetesEvents {
    * </p>
    * Test verifies NamespaceWatchingStarted event is logged when operator starts watching an another domain namespace.
    */
-  @Order(10)
+  @Order(11)
   @Test
   public void testK8SEventsStartWatchingNS() {
     DateTime timestamp = new DateTime(Instant.now().getEpochSecond() * 1000L);
@@ -525,7 +562,7 @@ public class ItKubernetesEvents {
    * </p>
    * Test verifies NamespaceWatchingStopped event is logged when operator stops watching a domain namespace.
    */
-  @Order(11)
+  @Order(12)
   @Test
   @Disabled("Bug - OWLS-87181")
   public void testK8SEventsStopWatchingNS() {
