b53bd9b4c79ded0dc81317cc259c345a78a8d176
Marina Kogan <marina.kogan@oracle.com>
2021-03-18 19:56:11 +0000

    created fmwUtil file and moved dublicated methods from fwm classes

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItFmwBigCMMiiDomain.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItFmwBigCMMiiDomain.java
@@ -13,25 +13,13 @@ import java.util.Collections;
 import java.util.List;
 
 import io.kubernetes.client.openapi.models.V1ConfigMap;
-import io.kubernetes.client.openapi.models.V1EnvVar;
-import io.kubernetes.client.openapi.models.V1LocalObjectReference;
-import io.kubernetes.client.openapi.models.V1ObjectMeta;
-import io.kubernetes.client.openapi.models.V1SecretReference;
-import oracle.weblogic.domain.AdminServer;
-import oracle.weblogic.domain.AdminService;
-import oracle.weblogic.domain.Channel;
-import oracle.weblogic.domain.Cluster;
-import oracle.weblogic.domain.Configuration;
 import oracle.weblogic.domain.Domain;
-import oracle.weblogic.domain.DomainSpec;
-import oracle.weblogic.domain.Model;
-import oracle.weblogic.domain.Opss;
-import oracle.weblogic.domain.ServerPod;
 import oracle.weblogic.kubernetes.actions.impl.primitive.Kubernetes;
 import oracle.weblogic.kubernetes.annotations.IntegrationTest;
 import oracle.weblogic.kubernetes.annotations.Namespaces;
 import oracle.weblogic.kubernetes.logging.LoggingFacade;
 import oracle.weblogic.kubernetes.utils.DbUtils;
+import oracle.weblogic.kubernetes.utils.FmwUtils;
 import org.awaitility.core.ConditionFactory;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -40,7 +28,6 @@ import org.junit.jupiter.api.Test;
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static oracle.weblogic.kubernetes.TestConstants.DB_IMAGE_TO_USE_IN_SPEC;
-import static oracle.weblogic.kubernetes.TestConstants.DOMAIN_API_VERSION;
 import static oracle.weblogic.kubernetes.TestConstants.FMWINFRA_IMAGE_NAME;
 import static oracle.weblogic.kubernetes.TestConstants.FMWINFRA_IMAGE_TAG;
 import static oracle.weblogic.kubernetes.TestConstants.FMWINFRA_IMAGE_TO_USE_IN_SPEC;
@@ -49,8 +36,6 @@ import static oracle.weblogic.kubernetes.TestConstants.MII_BASIC_APP_NAME;
 import static oracle.weblogic.kubernetes.TestConstants.OCIR_SECRET_NAME;
 import static oracle.weblogic.kubernetes.TestConstants.RESULTS_ROOT;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.MODEL_DIR;
-import static oracle.weblogic.kubernetes.actions.TestActions.getServiceNodePort;
-import static oracle.weblogic.kubernetes.utils.CommonTestUtils.checkPodReadyAndServiceExists;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.createDomainAndVerify;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.createMiiImageAndVerify;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.createOcirRepoSecret;
@@ -58,11 +43,9 @@ import static oracle.weblogic.kubernetes.utils.CommonTestUtils.createOpsswalletp
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.createRcuAccessSecret;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.createSecretWithUsernamePassword;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.dockerLoginAndPushImageToRegistry;
-import static oracle.weblogic.kubernetes.utils.CommonTestUtils.getExternalServicePodName;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.installAndVerifyOperator;
 import static oracle.weblogic.kubernetes.utils.DbUtils.setupDBandRCUschema;
 import static oracle.weblogic.kubernetes.utils.FileUtils.replaceStringInFile;
-import static oracle.weblogic.kubernetes.utils.TestUtils.callWebAppAndWaitTillReady;
 import static oracle.weblogic.kubernetes.utils.ThreadSafeLogger.getLogger;
 import static org.awaitility.Awaitility.with;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
@@ -71,7 +54,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Test to creat a FMW domain in model in image with over 1Mb data
- * to test that generated introspector Config Maps will be splitted to smaller than 1Mg
+ * to test that generated introspector Config Maps will be splitted to smaller than 1Mb
  * and domain will be started and running.
  */
 @DisplayName("Test to a create FMW model in image domain "
@@ -247,7 +230,7 @@ public class ItFmwBigCMMiiDomain {
     dockerLoginAndPushImageToRegistry(jrfMii1Image);
 
     // create the domain object
-    Domain domain = createDomainResource(domainUid,
+    Domain domain = FmwUtils.createDomainResource(domainUid,
         jrfDomainNamespace,
         adminSecretName,
         OCIR_SECRET_NAME,
@@ -258,7 +241,7 @@ public class ItFmwBigCMMiiDomain {
         jrfMii1Image);
 
     createDomainAndVerify(domain, jrfDomainNamespace);
-    verifyDomainReady(jrfDomainNamespace, domainUid);
+    FmwUtils.verifyDomainReady(jrfDomainNamespace, domainUid, replicaCount);
     // check if multiple configmaps are created
     try {
       if (!Kubernetes.listConfigMaps(jrfDomainNamespace).getItems().isEmpty()) {
@@ -280,99 +263,5 @@ public class ItFmwBigCMMiiDomain {
     } catch (Exception ex) {
       throw new RuntimeException("Failed to process config maps" + ex.getMessage());
     }
-  }
-
-  /**
-   * Construct a domain object with the given parameters that can be used to create a domain resource.
-   * @param domainUid unique Uid of the domain
-   * @param domNamespace  namespace where the domain exists
-   * @param adminSecretName  name of admin secret
-   * @param repoSecretName name of repository secret
-   * @param encryptionSecretName name of encryption secret
-   * @param rcuAccessSecretName name of RCU access secret
-   * @param opssWalletPasswordSecretName name of opss wallet password secret
-   * @param replicaCount count of replicas
-   * @param miiImage name of model in image
-   * @return Domain WebLogic domain
-   */
-  private Domain createDomainResource(
-      String domainUid, String domNamespace, String adminSecretName,
-      String repoSecretName, String encryptionSecretName, String rcuAccessSecretName,
-      String opssWalletPasswordSecretName, int replicaCount, String miiImage) {
-    // create the domain CR
-    Domain domain = new Domain()
-        .apiVersion(DOMAIN_API_VERSION)
-        .kind("Domain")
-        .metadata(new V1ObjectMeta()
-            .name(domainUid)
-            .namespace(domNamespace))
-        .spec(new DomainSpec()
-            .domainUid(domainUid)
-            .domainHomeSourceType("FromModel")
-            .image(miiImage)
-            .imagePullPolicy("IfNotPresent")
-            .addImagePullSecretsItem(new V1LocalObjectReference()
-                .name(repoSecretName))
-            .webLogicCredentialsSecret(new V1SecretReference()
-                .name(adminSecretName)
-                .namespace(domNamespace))
-            .includeServerOutInPodLog(true)
-            .serverStartPolicy("IF_NEEDED")
-            .serverPod(new ServerPod()
-                .addEnvItem(new V1EnvVar()
-                    .name("JAVA_OPTIONS")
-                    .value("-Dweblogic.StdoutDebugEnabled=false"))
-                .addEnvItem(new V1EnvVar()
-                    .name("USER_MEM_ARGS")
-                    .value("-Djava.security.egd=file:/dev/./urandom ")))
-            .adminServer(new AdminServer()
-                .serverStartState("RUNNING")
-                .adminService(new AdminService()
-                    .addChannelsItem(new Channel()
-                        .channelName("default")
-                        .nodePort(0))))
-            .addClustersItem(new Cluster()
-                .clusterName("cluster-1")
-                .replicas(replicaCount)
-                .serverStartState("RUNNING"))
-            .configuration(new Configuration()
-                .opss(new Opss()
-                    .walletPasswordSecret(opssWalletPasswordSecretName))
-                .model(new Model()
-                    .domainType("JRF")
-                    .runtimeEncryptionSecret(encryptionSecretName))
-                .addSecretsItem(rcuAccessSecretName)
-                .introspectorJobActiveDeadlineSeconds(600L)));
-
-    return domain;
-  }
-
-  /**
-   * Verify Pod is ready and service exists for both admin server and managed servers.
-   * Verify EM console is accessible.
-   */
-  private void verifyDomainReady(String domainNamespace, String domainUid) {
-    String adminServerPodName = domainUid + "-admin-server";
-    String managedServerPrefix = domainUid + "-managed-server";
-    checkPodReadyAndServiceExists(adminServerPodName, domainUid, domainNamespace);
-
-    for (int i = 1; i <= replicaCount; i++) {
-      logger.info("Checking managed server service {0} is created in namespace {1}",
-          managedServerPrefix + i + "-c1", domainNamespace);
-      checkPodReadyAndServiceExists(managedServerPrefix + i + "-c1", domainUid, domainNamespace);
-    }
-
-    //check access to the em console: http://hostname:port/em
-    int nodePort = getServiceNodePort(
-        domainNamespace, getExternalServicePodName(adminServerPodName), "default");
-    assertTrue(nodePort != -1,
-        "Could not get the default external service node port");
-    logger.info("Found the default service nodePort {0}", nodePort);
-    String curlCmd1 = "curl -s -L --show-error --noproxy '*' "
-        + " http://" + K8S_NODEPORT_HOST + ":" + nodePort
-        + "/em --write-out %{http_code} -o /dev/null";
-    logger.info("Executing default nodeport curl command {0}", curlCmd1);
-    assertTrue(callWebAppAndWaitTillReady(curlCmd1, 5), "Calling web app failed");
-    logger.info("EM console is accessible thru default service");
   }
 }
--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItFmwMiiDomain.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItFmwMiiDomain.java
@@ -9,24 +9,12 @@ import java.util.Collections;
 import java.util.List;
 
 import io.kubernetes.client.custom.V1Patch;
-import io.kubernetes.client.openapi.models.V1EnvVar;
-import io.kubernetes.client.openapi.models.V1LocalObjectReference;
-import io.kubernetes.client.openapi.models.V1ObjectMeta;
-import io.kubernetes.client.openapi.models.V1SecretReference;
-import oracle.weblogic.domain.AdminServer;
-import oracle.weblogic.domain.AdminService;
-import oracle.weblogic.domain.Channel;
-import oracle.weblogic.domain.Cluster;
-import oracle.weblogic.domain.Configuration;
 import oracle.weblogic.domain.Domain;
-import oracle.weblogic.domain.DomainSpec;
-import oracle.weblogic.domain.Model;
-import oracle.weblogic.domain.Opss;
-import oracle.weblogic.domain.ServerPod;
 import oracle.weblogic.kubernetes.actions.impl.primitive.Command;
 import oracle.weblogic.kubernetes.annotations.IntegrationTest;
 import oracle.weblogic.kubernetes.annotations.Namespaces;
 import oracle.weblogic.kubernetes.logging.LoggingFacade;
+import oracle.weblogic.kubernetes.utils.FmwUtils;
 import org.awaitility.core.ConditionFactory;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -38,20 +26,16 @@ import org.junit.jupiter.api.TestMethodOrder;
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static oracle.weblogic.kubernetes.TestConstants.DB_IMAGE_TO_USE_IN_SPEC;
-import static oracle.weblogic.kubernetes.TestConstants.DOMAIN_API_VERSION;
 import static oracle.weblogic.kubernetes.TestConstants.FMWINFRA_IMAGE_NAME;
 import static oracle.weblogic.kubernetes.TestConstants.FMWINFRA_IMAGE_TAG;
 import static oracle.weblogic.kubernetes.TestConstants.FMWINFRA_IMAGE_TO_USE_IN_SPEC;
-import static oracle.weblogic.kubernetes.TestConstants.K8S_NODEPORT_HOST;
 import static oracle.weblogic.kubernetes.TestConstants.MII_BASIC_APP_NAME;
 import static oracle.weblogic.kubernetes.TestConstants.OCIR_SECRET_NAME;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.MODEL_DIR;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.RESOURCE_DIR;
-import static oracle.weblogic.kubernetes.actions.TestActions.getServiceNodePort;
 import static oracle.weblogic.kubernetes.actions.TestActions.patchDomainCustomResource;
 import static oracle.weblogic.kubernetes.actions.impl.primitive.Command.defaultCommandParams;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.checkPodDeleted;
-import static oracle.weblogic.kubernetes.utils.CommonTestUtils.checkPodReadyAndServiceExists;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.createDomainAndVerify;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.createMiiImageAndVerify;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.createOcirRepoSecret;
@@ -59,11 +43,9 @@ import static oracle.weblogic.kubernetes.utils.CommonTestUtils.createOpsswalletp
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.createRcuAccessSecret;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.createSecretWithUsernamePassword;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.dockerLoginAndPushImageToRegistry;
-import static oracle.weblogic.kubernetes.utils.CommonTestUtils.getExternalServicePodName;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.installAndVerifyOperator;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.patchServerStartPolicy;
 import static oracle.weblogic.kubernetes.utils.DbUtils.setupDBandRCUschema;
-import static oracle.weblogic.kubernetes.utils.TestUtils.callWebAppAndWaitTillReady;
 import static oracle.weblogic.kubernetes.utils.ThreadSafeLogger.getLogger;
 import static org.awaitility.Awaitility.with;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
@@ -214,7 +196,7 @@ public class ItFmwMiiDomain {
     dockerLoginAndPushImageToRegistry(jrfMiiImage);
 
     // create the domain object
-    Domain domain = createDomainResource(domainUid,
+    Domain domain = FmwUtils.createDomainResource(domainUid,
         jrfDomainNamespace,
         adminSecretName,
         OCIR_SECRET_NAME,
@@ -225,7 +207,7 @@ public class ItFmwMiiDomain {
         jrfMiiImage);
 
     createDomainAndVerify(domain, jrfDomainNamespace);
-    verifyDomainReady();
+    FmwUtils.verifyDomainReady(jrfDomainNamespace, domainUid, replicaCount);
   }
 
   /**
@@ -244,7 +226,7 @@ public class ItFmwMiiDomain {
     shutdownDomain();
     patchDomainWithWalletFileSecret(opsswalletfileSecretName);
     startupDomain();
-    verifyDomainReady();
+    FmwUtils.verifyDomainReady(jrfDomainNamespace, domainUid, replicaCount);
   }
 
   /**
@@ -327,96 +309,5 @@ public class ItFmwMiiDomain {
     V1Patch patch = new V1Patch(new String(patchStr));
 
     return patchDomainCustomResource(domainUid, jrfDomainNamespace, patch, V1Patch.PATCH_FORMAT_JSON_PATCH);
-  }
-
-  /**
-   * Construct a domain object with the given parameters that can be used to create a domain resource.
-   * @param domainUid unique Uid of the domain
-   * @param domNamespace  namespace where the domain exists
-   * @param adminSecretName  name of admin secret
-   * @param repoSecretName name of repository secret
-   * @param encryptionSecretName name of encryption secret
-   * @param rcuAccessSecretName name of RCU access secret
-   * @param opssWalletPasswordSecretName name of opss wallet password secret
-   * @param replicaCount count of replicas
-   * @param miiImage name of model in image
-   * @return Domain WebLogic domain
-   */
-  private Domain createDomainResource(
-      String domainUid, String domNamespace, String adminSecretName,
-      String repoSecretName, String encryptionSecretName, String rcuAccessSecretName,
-      String opssWalletPasswordSecretName, int replicaCount, String miiImage) {
-    // create the domain CR
-    Domain domain = new Domain()
-            .apiVersion(DOMAIN_API_VERSION)
-            .kind("Domain")
-            .metadata(new V1ObjectMeta()
-                .name(domainUid)
-                .namespace(domNamespace))
-            .spec(new DomainSpec()
-                .domainUid(domainUid)
-                .domainHomeSourceType("FromModel")
-                .image(miiImage)
-                .imagePullPolicy("IfNotPresent")
-                .addImagePullSecretsItem(new V1LocalObjectReference()
-                    .name(repoSecretName))
-                .webLogicCredentialsSecret(new V1SecretReference()
-                    .name(adminSecretName)
-                    .namespace(domNamespace))
-                .includeServerOutInPodLog(true)
-                .serverStartPolicy("IF_NEEDED")
-                .serverPod(new ServerPod()
-                    .addEnvItem(new V1EnvVar()
-                        .name("JAVA_OPTIONS")
-                        .value("-Dweblogic.StdoutDebugEnabled=false"))
-                    .addEnvItem(new V1EnvVar()
-                        .name("USER_MEM_ARGS")
-                        .value("-Djava.security.egd=file:/dev/./urandom ")))
-                .adminServer(new AdminServer()
-                    .serverStartState("RUNNING")
-                    .adminService(new AdminService()
-                        .addChannelsItem(new Channel()
-                            .channelName("default")
-                            .nodePort(0))))
-                .addClustersItem(new Cluster()
-                    .clusterName("cluster-1")
-                    .replicas(replicaCount)
-                    .serverStartState("RUNNING"))
-                .configuration(new Configuration()
-                    .opss(new Opss()
-                        .walletPasswordSecret(opssWalletPasswordSecretName))
-                    .model(new Model()
-                        .domainType("JRF")
-                        .runtimeEncryptionSecret(encryptionSecretName))
-                    .addSecretsItem(rcuAccessSecretName)
-                    .introspectorJobActiveDeadlineSeconds(600L)));
-
-    return domain;
-  }
-
-  /**
-   * Verify Pod is ready and service exists for both admin server and managed servers.
-   * Verify EM console is accessible.
-   */
-  private void verifyDomainReady() {
-    checkPodReadyAndServiceExists(adminServerPodName, domainUid, jrfDomainNamespace);
-    for (int i = 1; i <= replicaCount; i++) {
-      logger.info("Checking managed server service {0} is created in namespace {1}",
-          managedServerPrefix + i + "-c1", jrfDomainNamespace);
-      checkPodReadyAndServiceExists(managedServerPrefix + i + "-c1", domainUid, jrfDomainNamespace);
-    }
-
-    //check access to the em console: http://hostname:port/em
-    int nodePort = getServiceNodePort(
-           jrfDomainNamespace, getExternalServicePodName(adminServerPodName), "default");
-    assertTrue(nodePort != -1,
-        "Could not get the default external service node port");
-    logger.info("Found the default service nodePort {0}", nodePort);
-    String curlCmd1 = "curl -s -L --show-error --noproxy '*' "
-        + " http://" + K8S_NODEPORT_HOST + ":" + nodePort
-        + "/em --write-out %{http_code} -o /dev/null";
-    logger.info("Executing default nodeport curl command {0}", curlCmd1);
-    assertTrue(callWebAppAndWaitTillReady(curlCmd1, 5), "Calling web app failed");
-    logger.info("EM console is accessible thru default service");
   }
 }
