f97ffc5566171bc0994432effb5597e684810981
Ryan Eberhard <ryan.eberhard@oracle.com>
2021-03-25 16:14:57 -0400

    Simplify script

--- documentation/publish.sh
+++ documentation/publish.sh
@@ -41,17 +41,11 @@ else
 fi
 
 echo "Building documentation for current version and for selected archived versions..."
-cd latest
-hugo -d "${outdir}" -b https://oracle.github.io/weblogic-kubernetes-operator
-cd ../2.5
-hugo -d "${outdir}/2.5" -b https://oracle.github.io/weblogic-kubernetes-operator/2.5
-cd ../2.6
-hugo -d "${outdir}/2.6" -b https://oracle.github.io/weblogic-kubernetes-operator/2.6
-cd ../3.0
-hugo -d "${outdir}/3.0" -b https://oracle.github.io/weblogic-kubernetes-operator/2.5
-cd ../3.1
-hugo -d "${outdir}/3.1" -b https://oracle.github.io/weblogic-kubernetes-operator/2.5
-cd ..
+hugo -s latest -d "${outdir}" -b https://oracle.github.io/weblogic-kubernetes-operator
+hugo -s 2.5 -d "${outdir}/2.5" -b https://oracle.github.io/weblogic-kubernetes-operator/2.5
+hugo -s 2.6 -d "${outdir}/2.6" -b https://oracle.github.io/weblogic-kubernetes-operator/2.6
+hugo -s 3.0 -d "${outdir}/3.0" -b https://oracle.github.io/weblogic-kubernetes-operator/2.5
+hugo -s 3.1 -d "${outdir}/3.1" -b https://oracle.github.io/weblogic-kubernetes-operator/2.5
 
 echo "Copying static files into place..."
 cp -R charts domains swagger "${outdir}"
