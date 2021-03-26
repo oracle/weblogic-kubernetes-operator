eef12ad4b530714d1ca44c06534a5946d60a15d2
Anil Kedia <37935279+ankedia@users.noreply.github.com>
2021-03-26 10:48:49 -0400

    OWLS-69993: Scripts to perform rolling restart of a WebLogic domain or cluster. (#2276)
    * OWLS-69993: Scripts to perform rolling restart of a WebLogic Cluster and a Domain.

--- kubernetes/samples/scripts/domain-lifecycle/stopCluster.sh
+++ kubernetes/samples/scripts/domain-lifecycle/stopCluster.sh
@@ -21,7 +21,7 @@ function usage() {
  
     $(basename $0) -c mycluster [-n mynamespace] [-d mydomainuid] [-m kubecli]
   
-    -c <cluster-name>   : Cluster name parameter is required.
+    -c <cluster-name>   : Cluster name (required parameter).
 
     -d <domain_uid>     : Domain unique-id. Default is 'sample-domain1'.
 
