adcf005403afc0994ebe574a8bb5bfc564219dc4
Anil Kedia <37935279+ankedia@users.noreply.github.com>
2021-04-06 20:10:17 -0400

    Owls 88574 - Remove internal network name and "www-proxy-hqdc" from the sample and test scripts (#2298)
    * OWLS-88574 - Remove usage of www-proxy-hqdc and us.oracle.com in sample scripts and integration tests.

--- integration-tests/src/test/resources/bash-scripts/wdt-create-domain-onpv.sh
+++ integration-tests/src/test/resources/bash-scripts/wdt-create-domain-onpv.sh
@@ -48,7 +48,7 @@
 #                  default:  https://github.com/oracle/weblogic-deploy-tooling/releases/download/release-1.9.7/$WDT_INSTALL_ZIP_FILE
 #
 #   https_proxy    Proxy for downloading WDT_INSTALL_ZIP_URL.
-#                  default: "http://www-proxy-hqdc.us.oracle.com:80"
+#                  default: ""
 #                  (If set to empty the script will try with no proxy.)
 #
 #   https_proxy2   Alternate proxy for downloading WDT_INSTALL_ZIP_URL
@@ -69,7 +69,7 @@
 
 # using "-" instead of ":-" in case proxy vars are explicitly set to "".
 https_proxy=${https_proxy-""}
-https_proxy2=${https_proxy2-"http://www-proxy-hqdc.us.oracle.com:80"}
+https_proxy2=${https_proxy2-""}
 
 export ORACLE_HOME=${ORACLE_HOME:-/u01/oracle}
 
