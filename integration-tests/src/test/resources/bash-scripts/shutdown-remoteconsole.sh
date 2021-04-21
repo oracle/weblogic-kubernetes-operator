6002bc0c519783f665d6d5471d68f789be9a7ec3
MAGGIE.HE@ORACLE.COM <maggie.he@oracle.com>
2021-04-21 03:01:16 +0000

    remove shutdown script file

--- integration-tests/src/test/resources/bash-scripts/shutdown-remoteconsole.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2021, Oracle and/or its affiliates.
-# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
-
-kill -9 `jps | grep console.jar | awk '{print $1}'`
