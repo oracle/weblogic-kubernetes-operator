bbb4f4d9aac7663cf3378a57ab99b6181d449763
Johnny Shum <johnny.shum@oracle.com>
2021-05-04 10:55:47 -0500

    Fix missing error output from WDT compareModel (#2351)
    * compareModel.sh does not generate compare_model_stdout if there is a code failure.
    
    * use stdout for readibility
    
    * Update modelInImage.sh
    
    Co-authored-by: Tom Barnes <tom.barnes@oracle.com>

--- operator/src/main/resources/scripts/modelInImage.sh
+++ operator/src/main/resources/scripts/modelInImage.sh
@@ -603,12 +603,11 @@ function diff_model() {
   export __WLSDEPLOY_STORE_MODEL__=1
   # $1 - new model, $2 original model
 
-  # no need to redirect output, -output_dir for compareModel will generate the output file
-  ${WDT_BINDIR}/compareModel.sh -oracle_home ${ORACLE_HOME} -output_dir /tmp $1 $2 > /dev/null 2>&1
+  ${WDT_BINDIR}/compareModel.sh -oracle_home ${ORACLE_HOME} -output_dir /tmp $1 $2 > ${WDT_OUTPUT} 2>&1
   ret=$?
   if [ $ret -ne 0 ]; then
     trace SEVERE "WDT Compare Model failed:"
-    cat /tmp/compare_model_stdout
+    cat ${WDT_OUTPUT}
     exitOrLoop
   fi
 
