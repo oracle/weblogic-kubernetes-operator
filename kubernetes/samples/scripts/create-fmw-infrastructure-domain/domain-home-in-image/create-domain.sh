bb6df5409151ef3756b17eaeab750e62eba70b62
Anthony Lai <anthony.lai@oracle.com>
2021-05-25 07:18:40 -0700

    OWLS-89562 - Add option to create-domain.sh in FMW domain home in image sample for use in imagetool --buildNetwork parameter (#2377)
    * add buildNetwork option to be passed into imageTool update
    
    * support buildNetwork for both wlst and wdt modes
    
    * add -b option to create-domain.sh for passing buildNetwork paramater to image tool
    
    * add Troubleshooting section for failed to build JDBC Connection object

--- kubernetes/samples/scripts/create-fmw-infrastructure-domain/domain-home-in-image/create-domain.sh
+++ kubernetes/samples/scripts/create-fmw-infrastructure-domain/domain-home-in-image/create-domain.sh
@@ -26,7 +26,7 @@ source ${scriptDir}/../../common/wdt-and-wit-utility.sh
 source ${scriptDir}/../../common/validate.sh
 
 function usage {
-  echo usage: ${script} -o dir -i file -u username -p password [-q rcuSchemaPassword] [-e] [-v] [-n] [-h]
+  echo usage: ${script} -o dir -i file -u username -p password [-q rcuSchemaPassword] [-b buildNetworkParam] [-n encryption-key] [-e] [-v] [-h]
   echo "  -i Parameter inputs file, must be specified."
   echo "  -o Output directory for the generated properties and YAML files, must be specified."
   echo "  -u WebLogic administrator user name for the WebLogic domain."
@@ -35,6 +35,7 @@ function usage {
   echo "  -e Also create the resources in the generated YAML files, optional."
   echo "  -v Validate the existence of persistentVolumeClaim, optional."
   echo "  -n Encryption key for encrypting passwords in the WDT model and properties files, optional."
+  echo "  -b Value to be used in the buildNetwork parameter when invoking WebLogic Image Tool, optional."
   echo "  -h Help"
   exit $1
 }
@@ -44,7 +45,7 @@ function usage {
 #
 doValidation=false
 executeIt=false
-while getopts "evhi:o:u:p:n:q:" opt; do
+while getopts "evhi:o:u:p:n:b:q:" opt; do
   case $opt in
     i) valuesInputFile="${OPTARG}"
     ;;
@@ -61,6 +62,8 @@ while getopts "evhi:o:u:p:n:q:" opt; do
     q) rcuSchemaPassword="${OPTARG}"
     ;;
     n) wdtEncryptKey="${OPTARG}"
+    ;;
+    b) buildNetwork="${OPTARG}"
     ;;
     h) usage 0
     ;;
@@ -277,6 +280,11 @@ function createDomainHome {
       cmd="$cmd  --wdtEncryptionKeyFile \"${wdtEncryptionKeyFile}\"
       "
     fi
+  fi
+
+  if [ -n "${buildNetwork}" ]; then
+    cmd="$cmd    --buildNetwork ${buildNetwork}
+    "
   fi
 
   echo @@ "Info: About to run the following WIT command:"
