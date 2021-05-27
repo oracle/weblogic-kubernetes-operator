c026181e5168b5a7fb3ea1e0017247ed429854c5
Ryan Eberhard <ryan.eberhard@oracle.com>
2021-05-27 17:50:40 -0400

    Support clusterService.sessionAffinity (#2383)
    * Support clusterService.sessionAffinity

---
+++ operator/src/main/java/oracle/kubernetes/weblogic/domain/model/SessionAffinity.java
@@ -0,0 +1,9 @@
+// Copyright (c) 2021, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package oracle.kubernetes.weblogic.domain.model;
+
+public enum SessionAffinity {
+  ClientIP,
+  None
+}
