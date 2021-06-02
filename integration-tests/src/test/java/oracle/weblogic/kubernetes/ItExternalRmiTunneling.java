5187f05a9d3e9575e7a053cb12480f8f05d54ad6
ANTARYAMI.PANIGRAHI@ORACLE.COM <anpanigr@anpanigr-1.subnet1ad2phx.devweblogicphx.oraclevcn.com>
2021-06-02 17:49:13 +0000

    remove cluster service configuration

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItExternalRmiTunneling.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItExternalRmiTunneling.java
@@ -35,6 +35,7 @@ import org.awaitility.core.ConditionFactory;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
@@ -366,6 +367,59 @@ class ItExternalRmiTunneling {
     logger.info("External RMI tunneling works for Traefik");
   }
 
+  @Order(3)
+  @Test
+  @DisplayName("Verify RMI access WLS through Nginx LoadBalancer")
+  public void testExternalRmiAccessThruNginx() {
+
+    assumeFalse(WEBLOGIC_SLIM, "Skipping RMI Tunnelling Test for slim image");
+    // Build the standalone JMS Client to send and receive messages
+    buildClient();
+    buildClientOnPod();
+
+    // Prepare the voyager ingress file from the template file by replacing
+    // domain namespace, domain UID, cluster service name and tls secret
+    Map<String, String> templateMap  = new HashMap();
+    templateMap.put("DOMAIN_NS", domainNamespace);
+    templateMap.put("DOMAIN_UID", domainUid);
+    templateMap.put("CLUSTER", clusterName);
+    templateMap.put("INGRESS_HOST", K8S_NODEPORT_HOST);
+
+    Path srcNginxHttpFile = Paths.get(RESOURCE_DIR,
+        "tunneling", "nginx.tunneling.template.yaml");
+    Path targetNginxHttpFile = assertDoesNotThrow(
+        () -> generateFileFromTemplate(srcNginxHttpFile.toString(),
+        "nginx.tunneling.yaml", templateMap));
+    logger.info("Generated Nginx Http Tunneling file {0}", targetNginxHttpFile);
+
+    StringBuffer deployIngress = new StringBuffer("kubectl apply -f ");
+    deployIngress.append(Paths.get(RESULTS_ROOT, "nginx.tunneling.yaml"));
+    // Deploy the nginx ingress controller
+    ExecResult result = assertDoesNotThrow(
+        () -> exec(new String(deployIngress), true));
+
+    // Unlike Voyager, there is no such service to check for tunneling
+    logger.info("kubectl apply returned {0}", result.toString());
+
+    // Get the ingress service nodeport corresponding to non-tls service
+    // Get the Traefik Service Name ginx-release-etqg-ingress-nginx-controller
+    String service = 
+        NGINX_RELEASE_NAME + "-" 
+        + nginxNamespace.substring(3) + "-ingress-nginx-controller";
+ 
+    logger.info("NGINX_SERVICE {0} in {1}", service, nginxNamespace);
+    int httpTunnelingPort =
+        getServiceNodePort(nginxNamespace, service, "http");
+    assertTrue(httpTunnelingPort != -1,
+        "Could not get the Nginx HttpTunnelingPort service node port");
+    logger.info("HttpTunnelingPort for Nginx {0}", httpTunnelingPort);
+
+    // Make sure the JMS Connection LoadBalancing and message LoadBalancing
+    // works from RMI client outside of k8s cluster 
+    runExtClient(httpTunnelingPort, 2, false);
+    logger.info("External RMI tunneling works for Nginx");
+  }
+
   /**
    * The external JMS client sends 300 messages to a Uniform Distributed
    * Queue using load balancer HTTPS url which maps to custom channel on
@@ -374,7 +428,7 @@ class ItExternalRmiTunneling {
    * The test is skipped for slim images, beacuse wlthint3client.jar is not 
    * available to download to build the external rmi JMS Client. 
    */
-  @Order(3)
+  @Order(4)
   @Test
   @DisplayName("Verify tls RMI access WLS through Voyager loadBalancer")
   public void testExternalRmiAccessThruVoyagerHttpsTunneling() {
@@ -415,7 +469,7 @@ class ItExternalRmiTunneling {
     runExtHttpsClient(httpsTunnelingPort, 2, false);
   }
 
-  @Order(4)
+  @Order(5)
   @Test
   @DisplayName("Verify tls RMI access WLS through Traefik loadBalancer")
   public void testExternalRmiAccessThruTraefikHttpsTunneling() {
@@ -459,6 +513,54 @@ class ItExternalRmiTunneling {
     assertTrue(httpsTunnelingPort != -1,
         "Could not get the Traefik HttpsTunnelingPort service node port");
     logger.info("HttpsTunnelingPort for Traefik {0}", httpsTunnelingPort);
+    runExtHttpsClient(httpsTunnelingPort, 2, false);
+  }
+
+  @Disabled("NGNIX tls ingress yaml file not ready")
+  @Order(6)
+  @Test
+  @DisplayName("Verify tls RMI access WLS through Nginx loadBalancer")
+  public void testExternalRmiAccessThruNginxHttpsTunneling() {
+
+    assumeFalse(WEBLOGIC_SLIM, "Skipping RMI Tunnelling Test for slim image");
+
+    // Build the standalone JMS Client to send and receive messages
+    buildClient();
+
+    // Prepare the voyager ingress file from the template file by replacing
+    // domain namespace, domain UID, cluster service name and tls secret
+    Map<String, String> templateMap  = new HashMap();
+    templateMap.put("DOMAIN_NS", domainNamespace);
+    templateMap.put("DOMAIN_UID", domainUid);
+    templateMap.put("CLUSTER", clusterName);
+    templateMap.put("TLS_CERT", tlsSecretName);
+    templateMap.put("INGRESS_HOST", K8S_NODEPORT_HOST);
+
+    Path srcNginxHttpsFile  = Paths.get(RESOURCE_DIR,
+        "tunneling", "nginx.tls.tunneling.template.yaml");
+    Path targetNginxHttpsFile = assertDoesNotThrow(
+        () -> generateFileFromTemplate(srcNginxHttpsFile.toString(),
+            "nginx.tls.tunneling.yaml", templateMap));
+    logger.info("Generated Nginx Https Tunneling file {0}", targetNginxHttpsFile);
+
+    // Deploy nginx ingress controller with tls enabled service with SSL
+    // terminating at Ingress.
+    StringBuffer deployNginxIngress = new StringBuffer("kubectl apply -f ");
+    deployNginxIngress.append(Paths.get(RESULTS_ROOT, "nginx.tls.tunneling.yaml"));
+    ExecResult result = assertDoesNotThrow(
+        () -> exec(new String(deployNginxIngress), true));
+    logger.info("kubectl apply returned {0}", result.toString());
+
+    // Get the ingress service nodeport corresponding to tls service
+    // Get the Nginx Service Name nginx-release-{ns}
+    String service = 
+         NGINX_RELEASE_NAME + "-" + nginxNamespace.substring(3);
+    logger.info("NGINX_SERVICE {0} in {1}", service, nginxNamespace);
+    int httpsTunnelingPort =
+        getServiceNodePort(nginxNamespace, service, "websecure");
+    assertTrue(httpsTunnelingPort != -1,
+        "Could not get the Nginx HttpsTunnelingPort service node port");
+    logger.info("HttpsTunnelingPort for Nginx {0}", httpsTunnelingPort);
     runExtHttpsClient(httpsTunnelingPort, 2, false);
   }
 
