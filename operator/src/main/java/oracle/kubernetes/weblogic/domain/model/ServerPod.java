6deec2949136b019ae3f6eb6d5689d1a964730a9
Anil Kedia <37935279+ankedia@users.noreply.github.com>
2021-07-16 17:52:35 -0400

    OWLS-90857 - Create copy of init container env variables for each sever pod to avoid the roll issue (#2458)
    * OWLS-90857 - Create copy of init container env variables for each server to avoid the roll issue.
    
    * Added debug and javadoc.
    
    * Fix java doc and rename method to clarify the intent.
    
    * Changed the log level for pod dump message in case of roll and message for loading scripts in config map.
    
    * Changes as per PR review comments.

--- operator/src/main/java/oracle/kubernetes/weblogic/domain/model/ServerPod.java
+++ operator/src/main/java/oracle/kubernetes/weblogic/domain/model/ServerPod.java
@@ -17,6 +17,7 @@ import io.kubernetes.client.custom.Quantity;
 import io.kubernetes.client.openapi.models.V1Affinity;
 import io.kubernetes.client.openapi.models.V1Capabilities;
 import io.kubernetes.client.openapi.models.V1Container;
+import io.kubernetes.client.openapi.models.V1ContainerBuilder;
 import io.kubernetes.client.openapi.models.V1EnvVar;
 import io.kubernetes.client.openapi.models.V1HostPathVolumeSource;
 import io.kubernetes.client.openapi.models.V1NodeAffinity;
@@ -43,6 +44,7 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import static java.util.Collections.emptyList;
+import static oracle.kubernetes.operator.helpers.PodHelper.createCopy;
 
 class ServerPod extends KubernetesResource {
 
@@ -443,7 +445,7 @@ class ServerPod extends KubernetesResource {
       addIfMissing(var);
     }
     for (V1Container c : serverPod1.getInitContainers()) {
-      addInitContainerIfMissing(c);
+      addInitContainerIfMissing(createWithEnvCopy(c));
     }
     for (V1Container c : serverPod1.getContainers()) {
       addContainerIfMissing(c);
@@ -490,6 +492,10 @@ class ServerPod extends KubernetesResource {
                 .collect(Collectors.toList());
       }
     }
+  }
+
+  private V1Container createWithEnvCopy(V1Container c) {
+    return new V1ContainerBuilder(c).withEnv(createCopy(c.getEnv())).build();
   }
 
   private void addIfMissing(V1Volume var) {
