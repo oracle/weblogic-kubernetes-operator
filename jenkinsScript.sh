72534ed34213a44f93add1ce3995ab544be4f7aa
Ryan Eberhard <ryan.eberhard@oracle.com>
2021-07-22 10:25:22 -0400

    Collect journalctl logs from Kind nodes

--- jenkinsScript.sh
+++ jenkinsScript.sh
@@ -127,6 +127,6 @@ echo "Info: Run tests.."
 sh -x ./kindtest.sh -t "${IT_TEST}" -v ${KUBE_VERSION} -p ${PARALLEL_RUN} -d ${WDT_DOWNLOAD_URL} -i ${WIT_DOWNLOAD_URL} -x ${NUMBER_OF_THREADS} -m ${MAVEN_PROFILE_NAME}
 
 mkdir -m777 -p "${WORKSPACE}/logdir/${BUILD_TAG}/wl_k8s_test_results"
-journalctl --utc --dmesg --system --since "$start_time" > "${WORKSPACE}/logdir/${BUILD_TAG}/wl_k8s_test_results/journalctl.out"
+journalctl --utc --dmesg --system --since "$start_time" > "${WORKSPACE}/logdir/${BUILD_TAG}/wl_k8s_test_results/journalctl-compute.out"
 
 
