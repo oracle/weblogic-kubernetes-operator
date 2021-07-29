4c26b1d175a992790fa67c602217120f5e492a41
Maggie He <maggie.he@oracle.com>
2021-07-29 17:57:47 -0400

    Add test to verify the restarted operator would detect the existing introspector and wait for the job to complete  (#2482)
    * Add test to verify the restarted operator would detect the existing introspector and wait for the job to complete

---
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/CommonOperatorUtils.java
@@ -0,0 +1,72 @@
+// Copyright (c) 2021, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package oracle.weblogic.kubernetes.utils;
+
+import oracle.weblogic.kubernetes.logging.LoggingFacade;
+import org.awaitility.core.ConditionFactory;
+
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static oracle.weblogic.kubernetes.TestConstants.OPERATOR_RELEASE_NAME;
+import static oracle.weblogic.kubernetes.actions.TestActions.getOperatorPodName;
+import static oracle.weblogic.kubernetes.actions.TestActions.startOperator;
+import static oracle.weblogic.kubernetes.actions.TestActions.stopOperator;
+import static oracle.weblogic.kubernetes.assertions.TestAssertions.operatorIsReady;
+import static oracle.weblogic.kubernetes.utils.CommonTestUtils.checkPodDoesNotExist;
+import static oracle.weblogic.kubernetes.utils.ThreadSafeLogger.getLogger;
+import static org.awaitility.Awaitility.with;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+/**
+ * The common utility class for Operator.
+ */
+public class CommonOperatorUtils {
+
+  private static final ConditionFactory withStandardRetryPolicy
+      = with().pollDelay(2, SECONDS)
+      .and().with().pollInterval(10, SECONDS)
+      .atMost(5, MINUTES).await();
+
+
+  /**
+   * Restart Operator by changing replica to 0 in operator deployment to stop Operator
+   * and changing replica back to 1 to start Operator.
+   * @param opNamespace namespace where Operator exists
+   */
+  public static void restartOperator(String opNamespace) {
+    LoggingFacade logger = getLogger();
+    // get operator pod name
+    String operatorPodName = assertDoesNotThrow(
+        () -> getOperatorPodName(OPERATOR_RELEASE_NAME, opNamespace));
+    assertNotNull(operatorPodName, "Operator pod name returned is null");
+    logger.info("Operator pod name {0}", operatorPodName);
+
+    // stop operator by changing replica to 0 in operator deployment
+    assertTrue(stopOperator(opNamespace), "Couldn't stop the Operator");
+
+    // check operator pod is not running
+    checkPodDoesNotExist(operatorPodName, null, opNamespace);
+
+    // start operator by changing replica to 1 in operator deployment
+    assertTrue(startOperator(opNamespace), "Couldn't start the Operator");
+
+    // check operator is running
+    logger.info("Check Operator pod is running in namespace {0}", opNamespace);
+    withStandardRetryPolicy
+        .conditionEvaluationListener(
+            condition -> logger.info("Waiting for operator to be running in namespace {0} "
+                    + "(elapsed time {1}ms, remaining time {2}ms)",
+                opNamespace,
+                condition.getElapsedTimeInMS(),
+                condition.getRemainingTimeInMS()))
+        .until(operatorIsReady(opNamespace));
+
+    logger.info("Operator pod is restarted in namespace {0}", opNamespace);
+
+  }
+
+}
