88894a9fb7b7ede3ab81084dd05f1b9059c8f4bc
xiancao <xian.cao@oracle.com>
2021-11-19 16:25:23 +0000

    uninstall elasticsearch

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItElasticLogging.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItElasticLogging.java
@@ -36,8 +36,16 @@ import org.junit.jupiter.api.Test;
 import static oracle.weblogic.kubernetes.TestConstants.COPY_WLS_LOGGING_EXPORTER_FILE_NAME;
 import static oracle.weblogic.kubernetes.TestConstants.DOMAIN_API_VERSION;
 import static oracle.weblogic.kubernetes.TestConstants.ELASTICSEARCH_HOST;
+import static oracle.weblogic.kubernetes.TestConstants.ELASTICSEARCH_HTTPS_PORT;
 import static oracle.weblogic.kubernetes.TestConstants.ELASTICSEARCH_HTTP_PORT;
+import static oracle.weblogic.kubernetes.TestConstants.ELASTICSEARCH_IMAGE;
+import static oracle.weblogic.kubernetes.TestConstants.ELASTICSEARCH_NAME;
+import static oracle.weblogic.kubernetes.TestConstants.ELKSTACK_NAMESPACE;
+import static oracle.weblogic.kubernetes.TestConstants.KIBANA_IMAGE;
 import static oracle.weblogic.kubernetes.TestConstants.KIBANA_INDEX_KEY;
+import static oracle.weblogic.kubernetes.TestConstants.KIBANA_NAME;
+import static oracle.weblogic.kubernetes.TestConstants.KIBANA_PORT;
+import static oracle.weblogic.kubernetes.TestConstants.KIBANA_TYPE;
 import static oracle.weblogic.kubernetes.TestConstants.LOGSTASH_INDEX_KEY;
 import static oracle.weblogic.kubernetes.TestConstants.MII_BASIC_APP_NAME;
 import static oracle.weblogic.kubernetes.TestConstants.OCIR_SECRET_NAME;
@@ -195,6 +203,20 @@ class ItElasticLogging {
   @AfterAll
   void tearDown() {
     // uninstall ELK Stack
+    elasticsearchParams = new LoggingExporterParams()
+        .elasticsearchName(ELASTICSEARCH_NAME)
+        .elasticsearchImage(ELASTICSEARCH_IMAGE)
+        .elasticsearchHttpPort(ELASTICSEARCH_HTTP_PORT)
+        .elasticsearchHttpsPort(ELASTICSEARCH_HTTPS_PORT)
+        .loggingExporterNamespace(ELKSTACK_NAMESPACE);
+
+    kibanaParams = new LoggingExporterParams()
+        .kibanaName(KIBANA_NAME)
+        .kibanaImage(KIBANA_IMAGE)
+        .kibanaType(KIBANA_TYPE)
+        .loggingExporterNamespace(ELKSTACK_NAMESPACE)
+        .kibanaContainerPort(KIBANA_PORT);
+
     if (elasticsearchParams != null) {
       logger.info("Uninstall Elasticsearch pod");
       assertDoesNotThrow(() -> uninstallAndVerifyElasticsearch(elasticsearchParams),
@@ -202,7 +224,7 @@ class ItElasticLogging {
     }
 
     if (kibanaParams != null) {
-      logger.info("Uninstall Elasticsearch pod");
+      logger.info("Uninstall Kibana pod");
       assertDoesNotThrow(() -> uninstallAndVerifyKibana(kibanaParams),
           "uninstallAndVerifyKibana failed with ApiException");
     }
