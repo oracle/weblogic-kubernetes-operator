79d1bd4d334741669baa1f3ed43936726375bd8d
Ryan Eberhard <ryan.eberhard@oracle.com>
2021-11-22 09:44:54 -0500

    Temporarily disable trap in diff_model as logFileRotate or testLogFileRotate may return non-zero code

--- operator/src/main/resources/scripts/modelInImage.sh
+++ operator/src/main/resources/scripts/modelInImage.sh
@@ -62,6 +62,12 @@ SCRIPT_ERROR=255
 WDT_ONLINE_MIN_VERSION="1.9.9"
 WDT_OFFLINE_MIN_VERSION="1.7.3"
 
+FATAL_JRF_INTROSPECTOR_ERROR_MSG="Model In Image JRF domain creation and schema initialization encountered an unrecoverable error.
+ If it is a database credential related error such as wrong password, schema prefix, or database connect
+ string, then correct the error and patch the domain resource 'domain.spec.introspectVersion' with a new
+ value. If the error is not related to a database credential, then you must also drop and recreate the
+ JRF schemas before patching the domain resource. Introspection Error: "
+
 export WDT_MODEL_SECRETS_DIRS="/weblogic-operator/config-overrides-secrets"
 [ ! -d ${WDT_MODEL_SECRETS_DIRS} ] && unset WDT_MODEL_SECRETS_DIRS
 
@@ -74,7 +80,6 @@ if [ ! -d "${WDT_OUTPUT_DIR}" ]; then
   trace "Creating WDT standard output directory: '${WDT_OUTPUT_DIR}'"
   createFolder "${WDT_OUTPUT_DIR}"
 fi
-
 
 # sort_files  sort the files according to the names and naming conventions and write the result to stdout
 #    $1  directory
@@ -621,6 +626,9 @@ function diff_model_v1() {
 
 function diff_model() {
   trace "Entering diff_model"
+  # wdt shell script or logFileRotate may return non-zero code if trap is on, then it will go to trap instead
+  # temporarily disable it
+  stop_trap
 
   export __WLSDEPLOY_STORE_MODEL__=1
   # $1 - new model, $2 original model
@@ -672,6 +680,9 @@ function diff_model() {
   fi
 
   wdtRotateAndCopyLogFile "${WDT_COMPARE_MODEL_LOG}"
+
+  # restore trap
+  start_trap
 
   trace "Exiting diff_model"
 }
@@ -902,9 +913,7 @@ function wdtCreatePrimordialDomain() {
     #  output configmap and save it for reuse.
 
     ${WDT_BINDIR}/createDomain.sh ${wdtArgs} > ${WDT_OUTPUT} 2>&1
-
   else
-
     # We get here only for JRF domain 'second time' (or more) case.
 
     # JRF wallet reuse note:
@@ -928,7 +937,8 @@ function wdtCreatePrimordialDomain() {
     # without admin intervention (retrying can compound the problem and obscure the original issue).
     #
     if [ "JRF" == "$WDT_DOMAIN_TYPE" ] && [ -z "${OPSS_FLAGS}" ] ; then
-      trace SEVERE "Model in Image: FatalIntrospectorError: WDT Create Primordial Domain Failed, ret=${ret}"
+      trace SEVERE "Model in Image: FatalIntrospectorError: WDT Create Domain Failed, return ${ret}. " \
+        ${FATAL_JRF_INTROSPECTOR_ERROR_MSG}
     else
       trace SEVERE "Model in Image: WDT Create Primordial Domain Failed, ret=${ret}"
     fi
@@ -937,6 +947,9 @@ function wdtCreatePrimordialDomain() {
   else
     trace "WDT Create Domain Succeeded, ret=${ret}:"
     cat ${WDT_OUTPUT}
+    if [ "JRF" == "$WDT_DOMAIN_TYPE" ]; then
+      CREATED_JRF_PRIMODIAL="true"
+    fi
   fi
 
   wdtRotateAndCopyLogFile "${WDT_CREATE_DOMAIN_LOG}"
@@ -974,7 +987,12 @@ function wdtUpdateModelDomain() {
   ret=$?
 
   if [ $ret -ne 0 ]; then
-    trace SEVERE "WDT Update Domain command Failed:"
+    if [ "true" == "${CREATED_JRF_PRIMODIAL}" ] ; then
+      trace SEVERE "Model in Image: FatalIntrospectorError: WDT Update Domain Failed, return ${ret}. " \
+        ${FATAL_JRF_INTROSPECTOR_ERROR_MSG}
+    else
+      trace SEVERE "WDT Update Domain command Failed:"
+    fi
     cat ${WDT_OUTPUT}
     exitOrLoop
   fi
@@ -1037,7 +1055,7 @@ function wdtHandleOnlineUpdate() {
   for file in $(sort_files ${IMG_ARCHIVES_ROOTDIR} "*.zip")
     do
         # expand the archive domain libraries to the domain lib
-        cd ${DOMAIN_HOME}/lib || return exitOrLoop
+        cd ${DOMAIN_HOME}/lib || exitOrLoop
         ${JAVA_HOME}/bin/jar xf ${IMG_ARCHIVES_ROOTDIR}/${file} wlsdeploy/domainLibraries/
 
         if [ $? -ne 0 ] ; then
@@ -1339,10 +1357,8 @@ function logSevereAndExit() {
 #   1 - Name of the log file to rotate and copy to WDT output directory.
 function wdtRotateAndCopyLogFile() {
   local logFileName=$1
-  testLogFileRotate "${WDT_OUTPUT_DIR}/${logFileName}"
-  [ $? -ne 0 ] && trace SEVERE "Error accessing '${WDT_OUTPUT_DIR}'. See previous log messages." && exit 1
 
-  logFileRotate ${WDT_OUTPUT_DIR}/${logFileName} ${WDT_LOG_FILE_MAX:-11}
+  logFileRotate "${WDT_OUTPUT_DIR}/${logFileName}" "${WDT_LOG_FILE_MAX:-11}"
 
   cp ${WDT_ROOT}/logs/${logFileName} ${WDT_OUTPUT_DIR}/
 }
