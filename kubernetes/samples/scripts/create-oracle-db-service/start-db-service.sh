6508e48ac8208eb9dad7ad0ad1bac31f332f7f9e
Marina Kogan <marina.kogan@oracle.com>
2021-12-03 00:39:47 +0000

    create unique db yaml

--- kubernetes/samples/scripts/create-oracle-db-service/start-db-service.sh
+++ kubernetes/samples/scripts/create-oracle-db-service/start-db-service.sh
@@ -95,11 +95,10 @@ kubectl apply -f ${dbYaml}
 
 dbpod=$(detectPod ${namespace})
 
-echo " DEBUG1 ${dbpod} running in ${namespace}" 
 echo "Is going to check dbpod: ${dbpod} in the namespace: ${namespace} "
-
 checkPod ${dbpod} ${namespace}
-echo " DEBUG2 ${dbpod} running in ${namespace}" 
+
+echo " checking pod state for pod ${dbpod} running in ${namespace}"
 checkPodState ${dbpod} ${namespace} "1/1"
 checkService oracle-db ${namespace}
 
