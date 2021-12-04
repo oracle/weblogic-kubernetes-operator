e025f487461ced5042f08839a59bbcfa7782662a
MAGGIE.HE@ORACLE.COM <maggie.he@oracle.com>
2021-12-04 05:08:13 +0000

    wip

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItParameterizedDomain.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItParameterizedDomain.java
@@ -382,13 +382,13 @@ class ItParameterizedDomain {
     }
 
     final String hostName = "localhost";
-      String forwardedPortNo = startPortForwardProcess(hostName, domainNamespace, domainUid, ADMIN_SERVER_PORT);
-      verifyAdminConsoleAccessible(domainNamespace, hostName, forwardedPortNo, false);
+    String forwardedPortNo = startPortForwardProcess(hostName, domainNamespace, domainUid, ADMIN_SERVER_PORT);
+    verifyAdminConsoleAccessible(domainNamespace, hostName, forwardedPortNo, false);
 
-      forwardedPortNo = startPortForwardProcess(hostName, domainNamespace, domainUid, ADMIN_SERVER_SECURE_PORT);
-      verifyAdminConsoleAccessible(domainNamespace, hostName, forwardedPortNo, true);
+    forwardedPortNo = startPortForwardProcess(hostName, domainNamespace, domainUid, ADMIN_SERVER_SECURE_PORT);
+    verifyAdminConsoleAccessible(domainNamespace, hostName, forwardedPortNo, true);
 
-      stopPortForwardProcess(domainNamespace);
+    stopPortForwardProcess(domainNamespace);
 
     /*if (!OKD) {
       // Test that `kubectl port-foward` is able to forward a local port to default channel port (7001 in this test)
