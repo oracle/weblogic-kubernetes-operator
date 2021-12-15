9d9e9523cf8c0620d48fcee7d84fd26d6ff85cf0
Dongbo Xiao <dongbo.xiao@oracle.com>
2021-12-15 15:51:18 -0500

    Minor change

--- operator/src/main/java/oracle/kubernetes/operator/DomainStatusUpdater.java
+++ operator/src/main/java/oracle/kubernetes/operator/DomainStatusUpdater.java
@@ -411,6 +411,9 @@ public class DomainStatusUpdater {
     }
 
     DomainStatus getCachedNewStatus() {
+      if (newStatus == null) {
+        return getNewStatus();
+      }
       return newStatus;
     }
 
