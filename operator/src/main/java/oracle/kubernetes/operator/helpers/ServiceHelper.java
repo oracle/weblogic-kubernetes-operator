59e883f774f575092b1ac67f7f06907e210f2b18
Dongbo Xiao <dongbo.xiao@oracle.com>
2021-12-15 15:17:00 -0500

    Doc update and cleanup

--- operator/src/main/java/oracle/kubernetes/operator/helpers/ServiceHelper.java
+++ operator/src/main/java/oracle/kubernetes/operator/helpers/ServiceHelper.java
@@ -690,7 +690,7 @@ public class ServiceHelper {
         if (UnrecoverableErrorBuilder.isAsyncCallUnrecoverableFailure(callResponse)) {
           return updateDomainStatus(packet, callResponse);
         } else {
-          return getNextFailureAction(conflictStep, packet, callResponse);
+          return onFailure(conflictStep, packet, callResponse);
         }
       }
 
