49f7e36757c796d17561578d1a1f117485ae0dc0
xiancao <xian.cao@oracle.com>
2021-12-16 02:50:32 +0000

    change monitoring exporter branch to main

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/MonitoringUtils.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/MonitoringUtils.java
@@ -142,7 +142,7 @@ public class MonitoringUtils {
     assertDoesNotThrow(() -> Files.createDirectories(monitoringTemp));
 
     String monitoringExporterBranch = Optional.ofNullable(System.getenv("MONITORING_EXPORTER_BRANCH"))
-        .orElse("master");
+        .orElse("main");
     CommandParams params = Command.defaultCommandParams()
         .command("git clone -b "
             + monitoringExporterBranch
