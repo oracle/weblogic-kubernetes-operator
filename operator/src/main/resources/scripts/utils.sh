0f41dbd09cfa895737de9401603af5081c6bb2e5
Anil Kedia <anil.kedia@oracle.com>
2021-12-22 16:07:34 +0000

    Minor change to mention the domain spec stanza in the comment.

--- operator/src/main/resources/scripts/utils.sh
+++ operator/src/main/resources/scripts/utils.sh
@@ -548,8 +548,8 @@ function waitForShutdownMarker() {
     if [ -e ${SHUTDOWN_MARKER_FILE} ] ; then
       exit 0
     fi
-    # Set the SERVER_SLEEP_INTERVAL_SECONDS environment variable in the domain specs to
-    # override the default sleep interval value of 1 second.
+    # Set the SERVER_SLEEP_INTERVAL_SECONDS environment variable in the domain specs in
+    # `spec.serverPod.env` stanza to override the default sleep interval value of 1 second.
     sleep ${SERVER_SLEEP_INTERVAL_SECONDS:-1}
   done
 }
