8dd790900676869221c4adef18b4f9761e2f40f2
Johnny Shum <johnny.shum@oracle.com>
2022-01-28 15:53:14 -0600

    update copyrights

--- operator/src/test/python/test_wdt_mii_filter.py
+++ operator/src/test/python/test_wdt_mii_filter.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2021, 2022, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
 import ast
 import os
 import unittest
