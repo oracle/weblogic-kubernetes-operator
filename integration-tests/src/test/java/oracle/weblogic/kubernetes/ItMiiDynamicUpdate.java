65256b3696a316ac67687e1703df9c6599d11eb6
ANTARYAMI.PANIGRAHI@ORACLE.COM <anpanigr@phx3182b0a.subnet4ad3phx.devweblogicphx.oraclevcn.com>
2022-01-29 00:08:30 +0000

    Test modification due to WDT 2.0 update

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItMiiDynamicUpdate.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItMiiDynamicUpdate.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2020, 2021, Oracle and/or its affiliates.
+// Copyright (c) 2020, 2022, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package oracle.weblogic.kubernetes;
@@ -612,7 +612,7 @@ class ItMiiDynamicUpdate {
     Path pathToUndeployAppYaml = Paths.get(WORK_DIR + "/undeployapp.yaml");
     String yamlToUndeployApp = "appDeployments:\n"
         + "  Application:\n"
-        + "    !myear:";
+        + "    '!myear':";
 
     assertDoesNotThrow(() -> Files.write(pathToUndeployAppYaml, yamlToUndeployApp.getBytes()));
 
