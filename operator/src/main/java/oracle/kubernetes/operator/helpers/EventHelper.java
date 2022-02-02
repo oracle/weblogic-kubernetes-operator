7f4797731fce0d3f048cd646fdf8d36e1d66ed37
Dongbo Xiao <dongbo.xiao@oracle.com>
2022-02-01 22:04:11 -0500

    Backport PR#2720 for owls-93995 to release 3.3 (#2743)
    * Backport PR#2720 for owls-93995 to release 3.3

--- operator/src/main/java/oracle/kubernetes/operator/helpers/EventHelper.java
+++ operator/src/main/java/oracle/kubernetes/operator/helpers/EventHelper.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2020, 2021, Oracle and/or its affiliates.
+// Copyright (c) 2020, 2022, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package oracle.kubernetes.operator.helpers;
@@ -91,7 +91,7 @@ public class EventHelper {
   /**
    * Factory for {@link Step} that asynchronously create an event.
    *
-   * @param domainNamespaces DomainSpaces instance
+   * @param domainNamespaces DomainNamespaces instance
    * @param eventData event item
    * @param next next step
    * @return Step for creating an event
