c5dd2097ddf66b0043c4ca234fa4eaf2eb14b68a
Maggie He <maggie.he@oracle.com>
2022-02-14 13:23:21 -0500

    okdtest script and okdJenkins script (#2780)
    * v1 okdtest.sh
    
    * wip1
    
    * wip2
    
    * wip3
    
    * wip4
    
    * wip5
    
    * minor change
    
    * chmod for okdtest.sh
    
    * debugging
    
    * wip6 with debugging msg
    
    * wip7
    
    * wip8
    
    Co-authored-by: Oracle Public Cloud User <opc@api.c1.okd4.weblogick8s.org>
    Co-authored-by: Oracle Public Cloud User <opc@api-int.c1.okd4.weblogick8s.org>

---
+++ okdJenkins.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+# Copyright (c) 2022, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+#
+# This script checks for the below required environment variables on Jenkins and runs the integration tests
+# MVN_HOME
+# #IIT_TEST
+# WDT_DOWNLOAD_URL
+# WIT_DOWNLOAD_URL
+# NUMBER_OF_THREADS
+# JAVA_HOME
+# OCR_PASSWORD
+# OCR_USERNAME
+# OCIR_USERNAME
+# OCIR_PASSWORD
+# OCIR_EMAIL
+# NFS_SERVER
+# PV_ROOT
+
+whoami
+
+uname -a
+
+set -o errexit
+set -o pipefail
+
+checkEnvVars() {
+  local has_errors=false
+  while [ ! -z "${1}" ]; do
+    if [ -z "${!1}" ]; then
+      echo "Error: '${1}' env variable is not set"
+      has_errors=true
+    else
+      if [ "${1/PASSWORD//}" = "${1}" ]; then
+        echo "Info: env var ${1}='${!1}'"
+      else
+        echo "Info: env var ${1}='***'"
+      fi
+    fi
+    shift
+  done
+  if [ ! "$has_errors" = "false" ]; then
+    echo "Error: Missing env vars, exiting."
+    exit 1
+  fi
+}
+ver() { printf %02d%02d%02d%02d%02d $(echo "$1" | tr '.' ' '); }
+checkJavaVersion() {
+  java_version=`java -version 2>&1 >/dev/null | grep 'java version' | awk '{print $3}'`
+  echo "Info: java version ${java_version}"
+  if [ $(ver $java_version) -lt $(ver "11.0.10") ]; then
+    echo "Error: Java version should be 11.0.10 or higher"
+    exit 1
+  fi
+}
+
+echo "WORKSPACE ${WORKSPACE}"
+
+checkEnvVars  \
+   MVN_HOME  \
+   IT_TEST  \
+   WDT_DOWNLOAD_URL  \
+   WIT_DOWNLOAD_URL  \
+   NUMBER_OF_THREADS  \
+   JAVA_HOME  \
+   OCR_PASSWORD  \
+   OCR_USERNAME  \
+   OCIR_USERNAME  \
+   OCIR_PASSWORD  \
+   OCIR_EMAIL  \
+   NFS_SERVER  \
+   PV_ROOT  
+
+export PATH=${JAVA_HOME}/bin:${MVN_HOME}/bin:$PATH
+echo "PATH=$PATH"
+
+which java
+java -version
+checkJavaVersion
+
+which mvn
+mvn --version
+
+docker version
+
+export RESULT_ROOT=${WORKSPACE}/RESULT_ROOT
+export BRANCH_NAME=${BRANCH}
+
+if [[ -z "${WORKSPACE}" ]]; then
+  export WORKSPACE=${PWD}
+fi
+
+cd $WORKSPACE
+[ -d ${WORKSPACE}/logdir ] && rm -rf ${WORKSPACE}/logdir && mkdir -p ${WORKSPACE}/logdir
+pwd
+ls
+
+echo "Info: Run tests.."
+echo " PWD is: ${PWD}"
+chmod 777 ${WORKSPACE}/okdtest.sh
+
+sh -x ./okdtest.sh -t "${IT_TEST}" -p ${PARALLEL_RUN} -d ${WDT_DOWNLOAD_URL} -i ${WIT_DOWNLOAD_URL} -x ${NUMBER_OF_THREADS} -m ${MAVEN_PROFILE_NAME}
