0e8aa14ac3cd07b7cd19cfbc77773df18e8b583e
sankar <sankarpn@gmail.com>
2022-02-28 13:27:23 -0800

    wip

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItIntrospectVersion.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItIntrospectVersion.java
@@ -944,6 +944,8 @@ class ItIntrospectVersion {
             domainUid, domainNamespace));
     assertNotNull(domain1, "Got null domain resource");
     assertNotNull(domain1.getSpec(), domain1 + " /spec is null");
+
+
     logger.info("Getting timestamps for the following pods");
     for (int i = 1; i <= 3; i++) {
       String managedServerPodName = cl2managedServerPodNamePrefix + i;
@@ -952,12 +954,15 @@ class ItIntrospectVersion {
     // get the map with server pods and their original creation timestamps
     cl2podsWithTimeStamps = getPodsWithTimeStamps(domainNamespace, adminServerPodName, cl2managedServerPodNamePrefix,
         replicaCount);
+
+
     logger.info("Getting timestamps for the following pods");
     for (int i = 1; i <= 3; i++) {
       String managedServerPodName = myclustermanagedServerPodNamePrefix + i;
       logger.info(managedServerPodName);
     }
     // get the map with server pods and their original creation timestamps
+    myclusterpodsWithTimeStamps = new LinkedHashMap<>();
     for (int i = 1; i <= 3; i++) {
       String managedServerPodName = myclustermanagedServerPodNamePrefix + i;
       myclusterpodsWithTimeStamps.put(managedServerPodName,
