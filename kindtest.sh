425792ec7673058ff8d19ef9775c705e5c9cc1d5
sankar <sankarpn@gmail.com>
2022-03-07 12:42:17 -0800

    debug

--- kindtest.sh
+++ kindtest.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 # Copyright (c) 2020, 2022, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
