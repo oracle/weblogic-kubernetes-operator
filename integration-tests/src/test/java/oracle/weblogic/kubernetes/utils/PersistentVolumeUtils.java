03cf1cb11f2aa359e7275c5e9dc71b9c5da48393
Sankar Periyathambi Neelakandan <45743425+sankarpn@users.noreply.github.com>
2022-03-11 10:23:38 -0800

    Delete pv if it exists before creating (#2826)
    * check pv doesn't exist and delete if it exists
    
    * debug
    
    * modify tests to use unique pv name
    
    * remove debugging flag
    
    * fix missed change

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/PersistentVolumeUtils.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/PersistentVolumeUtils.java
@@ -36,7 +36,10 @@ import static oracle.weblogic.kubernetes.TestConstants.PV_ROOT;
 import static oracle.weblogic.kubernetes.TestConstants.WEBLOGIC_IMAGE_TO_USE_IN_SPEC;
 import static oracle.weblogic.kubernetes.actions.TestActions.createPersistentVolume;
 import static oracle.weblogic.kubernetes.actions.TestActions.createPersistentVolumeClaim;
+import static oracle.weblogic.kubernetes.actions.TestActions.deletePersistentVolume;
+import static oracle.weblogic.kubernetes.actions.impl.UniqueName.random;
 import static oracle.weblogic.kubernetes.assertions.TestAssertions.pvExists;
+import static oracle.weblogic.kubernetes.assertions.TestAssertions.pvNotExists;
 import static oracle.weblogic.kubernetes.assertions.TestAssertions.pvcExists;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.testUntil;
 import static oracle.weblogic.kubernetes.utils.ThreadSafeLogger.getLogger;
@@ -157,6 +160,13 @@ public class PersistentVolumeUtils {
   public static void createPV(String pvName, String domainUid, String className) {
 
     LoggingFacade logger = getLogger();
+
+    logger.info("deleting persistent volume pvName {0} if it exists", pvName);
+    deletePersistentVolume(pvName);
+    testUntil(
+        assertDoesNotThrow(() -> pvNotExists(pvName, null),
+            String.format("pvNotExists failedfor pv %s", pvName)), logger, "pv {0} to be deleted", pvName);
+
     logger.info("creating persistent volume for pvName {0}, domainUid: {1}, className: {2}",
         pvName, domainUid, className);
     Path pvHostPath = null;
@@ -363,4 +373,20 @@ public class PersistentVolumeUtils {
 
     createPVPVCAndVerify(v1pv,v1pvc, labelSelector, namespace);
   }
+
+  /**
+   * Get a unique name for pv or pvc with a supplied prefix.
+   * @param prefix prefix for pv or pvc name
+   * @return full pv or pvc name
+   */
+  public static String getUniquePvOrPvcName(String prefix) {
+    char[] name = new char[6];
+    for (int i = 0; i < name.length; i++) {
+      name[i] = (char) (random.nextInt(25) + (int) 'a');
+    }
+    String pvOrPvcName = prefix + new String(name);
+    getLogger().info("Creating unique pv|pvc name {0}", pvOrPvcName);
+    return pvOrPvcName;
+  }
+
 }
