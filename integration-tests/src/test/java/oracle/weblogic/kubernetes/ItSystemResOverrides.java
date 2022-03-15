82f80a43b7b54a26b931475e81f30d02c1789571
Sankar Periyathambi Neelakandan <45743425+sankarpn@users.noreply.github.com>
2022-03-15 10:25:38 -0700

    create pv and pvc objects with unique names to reduce failures due to (#2837)
    leftover resources

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItSystemResOverrides.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItSystemResOverrides.java
@@ -71,6 +71,7 @@ import static oracle.weblogic.kubernetes.utils.JobUtils.getIntrospectJobName;
 import static oracle.weblogic.kubernetes.utils.OperatorUtils.installAndVerifyOperator;
 import static oracle.weblogic.kubernetes.utils.PersistentVolumeUtils.createPV;
 import static oracle.weblogic.kubernetes.utils.PersistentVolumeUtils.createPVC;
+import static oracle.weblogic.kubernetes.utils.PersistentVolumeUtils.getUniquePvOrPvcName;
 import static oracle.weblogic.kubernetes.utils.PodUtils.checkPodDoesNotExist;
 import static oracle.weblogic.kubernetes.utils.PodUtils.checkPodExists;
 import static oracle.weblogic.kubernetes.utils.PodUtils.checkPodReady;
@@ -102,8 +103,8 @@ class ItSystemResOverrides {
   final String managedServerNameBase = "ms-";
   final int managedServerPort = 8001;
   int t3ChannelPort;
-  final String pvName = domainUid + "-pv"; // name of the persistent volume
-  final String pvcName = domainUid + "-pvc"; // name of the persistent volume claim
+  final String pvName = getUniquePvOrPvcName(domainUid + "-pv-");
+  final String pvcName = getUniquePvOrPvcName(domainUid + "-pvc-");
   final String wlSecretName = "weblogic-credentials";
   final String managedServerPodNamePrefix = domainUid + "-" + managedServerNameBase;
   int replicaCount = 2;
