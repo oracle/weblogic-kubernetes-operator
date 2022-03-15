acd25c1bc7709f5400c0122f25c70164de9986e0
Sankar Periyathambi Neelakandan <45743425+sankarpn@users.noreply.github.com>
2022-03-15 10:25:38 -0700

    create pv and pvc objects with unique names to reduce failures due to (#2837)
    leftover resources

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItMiiServiceMigration.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItMiiServiceMigration.java
@@ -57,6 +57,7 @@ import static oracle.weblogic.kubernetes.utils.ImageUtils.createSecretForBaseIma
 import static oracle.weblogic.kubernetes.utils.OperatorUtils.installAndVerifyOperator;
 import static oracle.weblogic.kubernetes.utils.PersistentVolumeUtils.createPV;
 import static oracle.weblogic.kubernetes.utils.PersistentVolumeUtils.createPVC;
+import static oracle.weblogic.kubernetes.utils.PersistentVolumeUtils.getUniquePvOrPvcName;
 import static oracle.weblogic.kubernetes.utils.PodUtils.checkPodDoesNotExist;
 import static oracle.weblogic.kubernetes.utils.PodUtils.getExternalServicePodName;
 import static oracle.weblogic.kubernetes.utils.ThreadSafeLogger.getLogger;
@@ -97,8 +98,8 @@ class ItMiiServiceMigration {
   private static ConditionFactory withStandardRetryPolicy = null;
   private static int replicaCount = 2;
   private static final String domainUid = "mii-jms-recovery";
-  private static String pvName = domainUid + "-pv";
-  private static String pvcName = domainUid + "-pvc";
+  private static final String pvName = getUniquePvOrPvcName(domainUid + "-pv-");
+  private static final String pvcName = getUniquePvOrPvcName(domainUid + "-pvc-");
   private static final String adminServerPodName = domainUid + "-admin-server";
   private static final String managedServerPrefix = domainUid + "-managed-server";
   private static LoggingFacade logger = null;
