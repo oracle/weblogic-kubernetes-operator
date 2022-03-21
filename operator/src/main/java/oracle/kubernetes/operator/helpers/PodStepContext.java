0a4572fd0e73fa37b7cfc35ff31bec6cf9e8804b
Anil Kedia <anil.kedia@oracle.com>
2022-03-21 17:03:23 +0000

    Minor cleanup.

--- operator/src/main/java/oracle/kubernetes/operator/helpers/PodStepContext.java
+++ operator/src/main/java/oracle/kubernetes/operator/helpers/PodStepContext.java
@@ -94,6 +94,7 @@ import static oracle.kubernetes.operator.helpers.CompatibilityCheck.Compatibilit
 import static oracle.kubernetes.operator.helpers.EventHelper.EventItem.DOMAIN_ROLL_STARTING;
 import static oracle.kubernetes.operator.helpers.EventHelper.EventItem.POD_CYCLE_STARTING;
 import static oracle.kubernetes.operator.helpers.LegalNames.LEGAL_CONTAINER_PORT_NAME_MAX_LENGTH;
+import static oracle.kubernetes.utils.OperatorUtils.isNullOrEmpty;
 import static oracle.kubernetes.weblogic.domain.model.Model.DEFAULT_WDT_INSTALL_HOME;
 import static oracle.kubernetes.weblogic.domain.model.Model.DEFAULT_WDT_MODEL_HOME;
 
@@ -964,11 +965,11 @@ public abstract class PodStepContext extends BasePodStepContext {
     addEnvVar(vars, ServerEnvVars.AS_SERVICE_NAME, LegalNames.toServerServiceName(getDomainUid(), getAsName()));
     Optional.ofNullable(getDataHome()).ifPresent(v -> addEnvVar(vars, ServerEnvVars.DATA_HOME, v));
     String wdtInstallHome = getWdtInstallHome();
-    if (wdtInstallHome != null && !wdtInstallHome.isEmpty() && !wdtInstallHome.equals(DEFAULT_WDT_INSTALL_HOME)) {
+    if (!isNullOrEmpty(wdtInstallHome) && !wdtInstallHome.equals(DEFAULT_WDT_INSTALL_HOME)) {
       addEnvVar(vars, IntrospectorJobEnvVars.WDT_INSTALL_HOME, wdtInstallHome);
     }
     String modelHome = getModelHome();
-    if (modelHome != null && !modelHome.isEmpty() && !modelHome.equals(DEFAULT_WDT_MODEL_HOME)) {
+    if (!isNullOrEmpty(modelHome) && !modelHome.equals(DEFAULT_WDT_MODEL_HOME)) {
       addEnvVar(vars, IntrospectorJobEnvVars.WDT_MODEL_HOME, modelHome);
     }
     Optional.ofNullable(getServerSpec().getAuxiliaryImages()).ifPresent(cm -> addAuxiliaryImageEnv(cm, vars));
