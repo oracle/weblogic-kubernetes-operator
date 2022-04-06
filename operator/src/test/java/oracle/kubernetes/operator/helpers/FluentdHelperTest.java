b93ce3c448237b4701adab8867674adb60f14e54
jshum2479 <johnny.shum@oracle.com>
2022-04-06 13:33:39 -0500

    add unit test for FluentdHelper

--- operator/src/test/java/oracle/kubernetes/operator/helpers/FluentdHelperTest.java
+++ operator/src/test/java/oracle/kubernetes/operator/helpers/FluentdHelperTest.java
@@ -1,25 +1,29 @@
-// Copyright (c) 2018, 2022, Oracle and/or its affiliates.
+// Copyright (c) 2022, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package oracle.kubernetes.operator.helpers;
+
+import com.meterware.simplestub.Memento;
+import io.kubernetes.client.openapi.models.V1ConfigMap;
+import io.kubernetes.client.openapi.models.V1ObjectMeta;
+import oracle.kubernetes.operator.DomainProcessorTestSetup;
+import oracle.kubernetes.operator.LabelConstants;
+import oracle.kubernetes.operator.calls.UnrecoverableCallException;
+import oracle.kubernetes.operator.work.Packet;
+import oracle.kubernetes.operator.work.Step;
+import oracle.kubernetes.utils.TestUtils;
+import oracle.kubernetes.weblogic.domain.DomainConfigurator;
+import oracle.kubernetes.weblogic.domain.DomainConfiguratorFactory;
+import oracle.kubernetes.weblogic.domain.model.Domain;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
-
-import com.meterware.simplestub.Memento;
-import io.kubernetes.client.openapi.models.V1ConfigMap;
-import io.kubernetes.client.openapi.models.V1ObjectMeta;
-import oracle.kubernetes.operator.LabelConstants;
-import oracle.kubernetes.operator.calls.UnrecoverableCallException;
-import oracle.kubernetes.operator.work.Packet;
-import oracle.kubernetes.operator.work.Step;
-import oracle.kubernetes.utils.TestUtils;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 import static com.meterware.simplestub.Stub.createStrictStub;
 import static oracle.kubernetes.common.logging.MessageKeys.CM_CREATED;
@@ -31,42 +35,23 @@ import static oracle.kubernetes.operator.KubernetesConstants.SCRIPT_CONFIG_MAP_N
 import static oracle.kubernetes.operator.ProcessingConstants.SCRIPT_CONFIG_MAP;
 import static oracle.kubernetes.operator.helpers.KubernetesTestSupport.CONFIG_MAP;
 import static oracle.kubernetes.operator.helpers.NamespaceHelper.getOperatorNamespace;
+import static oracle.kubernetes.operator.helpers.StepContextConstants.FLUENTD_CONFIG_DATA_NAME;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.sameInstance;
+import static org.hamcrest.Matchers.startsWith;
 import static org.hamcrest.junit.MatcherAssert.assertThat;
 
-class ConfigMapHelperTest {
+class FluentdHelperTest {
   private static final String DOMAIN_NS = "namespace";
 
-  private static final SemanticVersion PRODUCT_VERSION = new SemanticVersion(3, 0, 0);
-  private static final SemanticVersion PRODUCT_VERSION_OLD = new SemanticVersion(2, 4, 0);
-  private static final SemanticVersion PRODUCT_VERSION_FUTURE = new SemanticVersion(3, 1, 0);
-
-  private final V1ConfigMap defaultConfigMap = defineConfigMap(PRODUCT_VERSION);
-  private final RetryStrategyStub retryStrategy = createStrictStub(RetryStrategyStub.class);
   private final KubernetesTestSupport testSupport = new KubernetesTestSupport();
   private final List<Memento> mementos = new ArrayList<>();
   private final List<LogRecord> logRecords = new ArrayList<>();
+  private final Domain newDomain = DomainProcessorTestSetup.createTestDomain(2L);
+  private final DomainConfigurator domainConfigurator = configureDomain(newDomain);
 
-
-  private V1ConfigMap defineConfigMap(SemanticVersion productVersion) {
-    Map<String, String> data = ConfigMapHelper.loadScriptsFromClasspath(DOMAIN_NS);
-    return AnnotationHelper.withSha256Hash(new V1ConfigMap()
-        .apiVersion("v1")
-        .kind("ConfigMap")
-        .metadata(createMetadata(productVersion))
-        .data(data), data);
-  }
-
-  private V1ObjectMeta createMetadata(SemanticVersion productVersion) {
-    return new V1ObjectMeta()
-        .name(SCRIPT_CONFIG_MAP_NAME)
-        .namespace(DOMAIN_NS)
-        .putLabelsItem(LabelConstants.OPERATORNAME_LABEL, getOperatorNamespace())
-        .putLabelsItem(LabelConstants.CREATEDBYOPERATOR_LABEL, "true")
-        .putLabelsItem(LabelConstants.OPERATOR_VERSION, productVersion.toString());
-  }
 
   @BeforeEach
   public void setUp() throws Exception {
@@ -85,59 +70,33 @@ class ConfigMapHelperTest {
   }
 
   @Test
-  void whenUnableToReadConfigMap_reportFailure() {
-    testSupport.failOnResource(CONFIG_MAP, SCRIPT_CONFIG_MAP_NAME, DOMAIN_NS, 401);
+  void whenUserSpecifyOwnFluentdConfig() {
+    String data = "<match>me</match>";
+    domainConfigurator
+        .withFluentdConfiguration(true, "fluentd-cred",
+            data);
+    V1ConfigMap configMap = FluentdHelper.getFluentdConfigMap(newDomain.getFluentdSpecification(),
+        newDomain.getDomainUid(),
+        newDomain.getNamespace());
 
-    Step scriptConfigMapStep = ConfigMapHelper.createScriptConfigMapStep(DOMAIN_NS, null);
-    testSupport.runSteps(scriptConfigMapStep);
-
-    testSupport.verifyCompletionThrowable(UnrecoverableCallException.class);
+    assertThat(configMap.getData().get(FLUENTD_CONFIG_DATA_NAME), equalTo(data));
   }
 
   @Test
-  void whenNoConfigMap_createIt() {
-    testSupport.runSteps(ConfigMapHelper.createScriptConfigMapStep(DOMAIN_NS, null));
+  void whenUserNotSpecifyOwnFluentdConfig() {
+    domainConfigurator
+        .withFluentdConfiguration(true, "fluentd-cred",
+            null);
+    V1ConfigMap configMap = FluentdHelper.getFluentdConfigMap(newDomain.getFluentdSpecification(),
+        newDomain.getDomainUid(),
+        newDomain.getNamespace());
 
-    assertThat(testSupport.getResources(CONFIG_MAP), notNullValue());
-    assertThat(logRecords, containsInfo(CM_CREATED));
+    assertThat(configMap.getData().get(FLUENTD_CONFIG_DATA_NAME), notNullValue());
+    assertThat(configMap.getData().get(FLUENTD_CONFIG_DATA_NAME), startsWith("   <match fluent.**>"));
   }
 
-  @Test
-  void whenNoConfigMap_retryOnFailure() {
-    testSupport.addRetryStrategy(retryStrategy);
-    testSupport.failOnCreate(CONFIG_MAP, DOMAIN_NS, 401);
-
-    Step scriptConfigMapStep = ConfigMapHelper.createScriptConfigMapStep(DOMAIN_NS, null);
-    testSupport.runSteps(scriptConfigMapStep);
-
-    testSupport.verifyCompletionThrowable(UnrecoverableCallException.class);
-    assertThat(retryStrategy.getConflictStep(), sameInstance(scriptConfigMapStep));
-  }
-
-  @Test
-  void whenMatchingConfigMapExists_addToPacket() {
-    testSupport.defineResources(defaultConfigMap);
-
-    Packet packet = testSupport.runSteps(ConfigMapHelper.createScriptConfigMapStep(DOMAIN_NS, PRODUCT_VERSION));
-
-    assertThat(logRecords, containsFine(CM_EXISTS));
-    assertThat(packet, hasEntry(SCRIPT_CONFIG_MAP, defaultConfigMap));
-  }
-
-  @Test
-  void whenExistingConfigMapHasOldVersion_replaceIt() {
-    testSupport.defineResources(defineConfigMap(PRODUCT_VERSION_OLD));
-
-    testSupport.runSteps(ConfigMapHelper.createScriptConfigMapStep(DOMAIN_NS, PRODUCT_VERSION));
-
-    assertThat(logRecords, containsInfo(CM_REPLACED));
-  }
-
-  @Test
-  void whenExistingConfigMapHasFutureVersion_dontReplaceIt() {
-    testSupport.defineResources(defineConfigMap(PRODUCT_VERSION_FUTURE));
-
-    testSupport.runSteps(ConfigMapHelper.createScriptConfigMapStep(DOMAIN_NS, PRODUCT_VERSION));
+  private DomainConfigurator configureDomain(Domain domain) {
+    return DomainConfiguratorFactory.forDomain(domain);
   }
 
 }
