7ac1c5653b19798a2eaf66208cafb405d502bd2c
Robert Patrick <robert.patrick@oracle.com>
2022-04-07 08:42:15 -0500

    changing script to require bash

--- operator/src/main/resources/scripts/auxImage.sh
+++ operator/src/main/resources/scripts/auxImage.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # Copyright (c) 2021, 2022, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
