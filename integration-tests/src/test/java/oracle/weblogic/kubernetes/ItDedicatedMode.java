cc905d3ef47c84ea04866f37741fb417916bc166
Antaryami Panigrahi <31135902+anpanigr@users.noreply.github.com>
2022-04-11 17:44:16 -0400

    Initial check-in (#2940)
    Co-authored-by: ANTARYAMI.PANIGRAHI@ORACLE.COM <anpanigr@phx3182b0a.subnet4ad3phx.devweblogicphx.oraclevcn.com>

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItDedicatedMode.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItDedicatedMode.java
@@ -23,14 +23,9 @@ import oracle.weblogic.kubernetes.actions.impl.primitive.HelmParams;
 import oracle.weblogic.kubernetes.annotations.IntegrationTest;
 import oracle.weblogic.kubernetes.annotations.Namespaces;
 import oracle.weblogic.kubernetes.logging.LoggingFacade;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 
 import static oracle.weblogic.kubernetes.TestConstants.ADMIN_PASSWORD_DEFAULT;
 import static oracle.weblogic.kubernetes.TestConstants.ADMIN_SERVER_NAME_BASE;
@@ -50,6 +45,8 @@ import static oracle.weblogic.kubernetes.actions.TestActions.uninstallOperator;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.checkPodReadyAndServiceExists;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.scaleAndVerifyCluster;
 import static oracle.weblogic.kubernetes.utils.DomainUtils.createDomainAndVerify;
+import static oracle.weblogic.kubernetes.utils.OKDUtils.createRouteForOKD;
+import static oracle.weblogic.kubernetes.utils.OKDUtils.setTlsTerminationForRoute;
 import static oracle.weblogic.kubernetes.utils.OperatorUtils.installAndVerifyOperator;
 import static oracle.weblogic.kubernetes.utils.PodUtils.checkPodDoesNotExist;
 import static oracle.weblogic.kubernetes.utils.PodUtils.setPodAntiAffinity;
@@ -58,15 +55,13 @@ import static oracle.weblogic.kubernetes.utils.ThreadSafeLogger.getLogger;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 /**
- * The current class verifies various use cases related to domainNamespaceSelectionStrategy.
+ * The current class verifies various use cases related to Dedicated 
+ * domainNamespaceSelectionStrategy applicable to Operator Helm Chart.
  * For more detail regarding the feature, please refer to
- * https://github.com/oracle/weblogic-kubernetes-operator/blob/develop/docs-source/content/
- * userguide/managing-operators/using-the-operator/using-helm.md#overall-operator-information.
+ * https://oracle.github.io/weblogic-kubernetes-operator/userguide/managing-operators/using-helm/#weblogic-domain-management
  */
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DisplayName("Test Operator and WebLogic domain with Dedicated set to true")
 @IntegrationTest
 class ItDedicatedMode {
@@ -78,11 +73,13 @@ class ItDedicatedMode {
   private static final String CRD_V16 = "domain-crd.yaml";
 
   // domain constants
-  private final String domainUid = "dedicated-domain-1";
+  private final String domainUid = "dedicated-domain1";
   private final String clusterName = "cluster-1";
   private final int replicaCount = 2;
-  private final String adminServerPodName = domainUid + "-" + ADMIN_SERVER_NAME_BASE;
-  private final String managedServerPodPrefix = domainUid + "-" + MANAGED_SERVER_NAME_BASE;
+  private final String adminServerPodName = 
+       domainUid + "-" + ADMIN_SERVER_NAME_BASE;
+  private final String managedServerPodPrefix = 
+       domainUid + "-" + MANAGED_SERVER_NAME_BASE;
 
   // operator constants
   private static HelmParams opHelmParams;
@@ -92,20 +89,22 @@ class ItDedicatedMode {
   private static LoggingFacade logger = null;
 
   /**
-   * Get namespaces for operator and domain2. Create CRD based on the k8s version.
-   *
+   * Get namespaces for operator and domain. 
+   * Create CRD based on the k8s version.
    * @param namespaces list of namespaces created by the IntegrationTestWatcher by the
    *                   JUnit engine parameter resolution mechanism.
    */
   @BeforeAll
   public static void initAll(@Namespaces(2) List<String> namespaces) {
     logger = getLogger();
+
     // get a unique operator namespace
     logger.info("Getting a unique namespace for operator");
     assertNotNull(namespaces.get(0), "Namespace list is null");
     opNamespace = namespaces.get(0);
 
-    // in the dedicated mode, the operator only manages domains in the operator's own namespace
+    // in the dedicated mode, the operator only manages domains in the 
+    // operator's own namespace
     domain1Namespace = opNamespace;
 
     // get a new unique domainNamespace
@@ -121,7 +120,7 @@ class ItDedicatedMode {
             .chartDir(OPERATOR_CHART_DIR);
 
     // delete existing CRD
-    new Command()
+    Command
         .withParams(new CommandParams()
             .command("kubectl delete crd domains.weblogic.oracle --ignore-not-found"))
         .execute();
@@ -129,72 +128,58 @@ class ItDedicatedMode {
     // install CRD
     String createCrdCommand = "kubectl create -f " + ITTESTS_DIR + "/../kubernetes/crd/" + CRD_V16;
     logger.info("Creating CRD with command {0}", createCrdCommand);
-    new Command()
+    Command
         .withParams(new CommandParams().command(createCrdCommand))
         .execute();
-  }
 
-  @AfterAll
-  public void tearDownAll() {
-  }
-
-  /**
-   * When installing the Operator via helm install,
-   * set the Operator Helm Chart parameter domainNamespaceSelectionStrategy to Dedicated and
-   * set domainNamespaces to something that is different from the operator's namespace.
-   * Make sure that the domain which is not in the operator's target namespaces do not come up.
-   *   Install an Operator with a namespace and set domainNamespaces in a different namespace
-   *     from the Operator's namespace, also set domainNamespaceSelectionStrategy to Dedicated
-   *     for the Operator Helm Chart.
-   *   Verify the Operator is up and running.
-   *   Create WebLogic Domain in a namespace that is different from the Operator's namespace.
-   *   Verify that the domain does not come up.
-   */
-  @Test
-  @Order(1)
-  @DisplayName("Set domainNamespaceSelectionStrategy to Dedicated for the Operator Helm Chart and "
-      + "verify that a domain not deployed in operator's namespace doesn't come up")
-  void testDedicatedModeDiffNamespace() {
-    // install and verify operator
+    // Install the Operator in a ns (say op) with helm parameter 
+    // domainNamespaceSelectionStrategy set to Dedicated and set 
+    // domainNamespaces parameter to something other than Operator ns (say wls)
     logger.info("Installing and verifying operator");
     installAndVerifyOperator(opNamespace, opNamespace + "-sa",
         true, 0, opHelmParams, domainNamespaceSelectionStrategy,
         false, domain2Namespace);
+    logger.info("Operator installed on namespace {0} and domainNamespaces set to {1} ", opNamespace, domain2Namespace);
 
+  }
+
+  /**
+   * Create WebLogic Domain in a namespace (say wls) that is different 
+   * from the Operator's namespace. Verify that the domain does not come up.
+   */
+  @Test
+  @DisplayName("Verify in Dedicated NamespaceSelectionStrategy domain on non-operator namespace does not started")
+  void testDedicatedModeDiffNamespace() {
     // create and verify the domain
-    logger.info("Creating and verifying model in image domain");
+    logger.info("Creating a domain in non-operator namespace {1}", domain2Namespace);
     createDomain(domain2Namespace);
     verifyDomainNotRunning(domain2Namespace);
+    logger.info("WebLogic domain is not managed in non-operator namespace");
   }
 
   /**
-   * When installing the Operator via helm install,
-   * set domainNamespaceSelectionStrategy to Dedicated for the Operator Helm Chart.
-   * Make sure that the domains in the operator's target namespaces comes up.
-   *   Operator is installed in the test case testDedicatedModeDiffNamespace.
-   *   Create a WebLogic Domain with the same namespace as Operator's namespace.
-   *   Verify that the WebLogic domain whose namespace is same as Operator's namespace comes up.
+   * Create WebLogic Domain in a namespace (say op) that is same as  
+   * Operator's namespace. Verify that the domain does come up and can be 
+   * scaled up using Operator
    */
   @Test
-  @Order(2)
-  @DisplayName("Set domainNamespaceSelectionStrategy to Dedicated for the Operator Helm Chart and "
-      + "verify that the domain deployed in the operator's namespace comes up")
+  @DisplayName("Verify in Dedicated NamespaceSelectionStrategy domain on operator namespace gets started")
   void testDedicatedModeSameNamespace() {
-    // create and verify the domain
-    logger.info("Creating and verifying model in image domain");
+
+    // This test uses the operator restAPI to scale the doamin. 
+    // To do this in OKD cluster, we need to expose the external service as 
+    // route and set tls termination to  passthrough 
+    String opExternalSvc = 
+        createRouteForOKD("external-weblogic-operator-svc", opNamespace);
+    // Patch the route just created to set tls termination to passthrough
+    setTlsTerminationForRoute("external-weblogic-operator-svc", opNamespace);
+    
+    logger.info("Creating a domain in perator namespace {1}", domain1Namespace);
     createDomain(domain1Namespace);
     verifyDomainRunning(domain1Namespace);
-  }
+    logger.info("WebLogic domain is managed in operator namespace Only");
 
-  /**
-   * Test that when domainNamespaceSelectionStrategy is set to Dedicated for the Operator Helm Chart,
-   * scaling up cluster-1 in domain1Namespace succeeds.
-   */
-  @Test
-  @Order(3)
-  @DisplayName("Scale up cluster-1 in domain1Namespace and verify it succeeds")
-  void testDedicatedModeSameNamespaceScale() {
-    // scale the cluster and check domain can be managed from the operator
+    // Scale up cluster-1 in domain1Namespace and verify it succeeds
     int externalRestHttpsPort = getServiceNodePort(opNamespace, "external-weblogic-operator-svc");
     logger.info("externalRestHttpsPort {0}", externalRestHttpsPort);
 
@@ -206,84 +191,12 @@ class ItDedicatedMode {
         "", null, null);
   }
 
-  /**
-   * Test when domainNamespaceSelectionStrategy is set to Dedicated for the Operator Helm Chart and
-   * the CRD with a lower than expected version is present, Operator fails with error
-   * if it has no permission to overwrite the CRD.
-   */
-  @Test
-  @Order(4)
-  @Disabled("Disable the test because the Operator has permission to overwrite the CRD")
-  @DisplayName("Create a CRD with a lower than expected version and verify that Operator fails with error")
-  void testDedicatedModeNlowerVersionCrd() {
-    // delete existing CRD
-    new Command()
-        .withParams(new CommandParams()
-            .command("kubectl delete crd domains.weblogic.oracle --ignore-not-found"))
-        .execute();
-
-    // install a lower version of CRD, v2.6.0
-    new Command()
-        .withParams(new CommandParams()
-            .command("kubectl create -f " + ITTESTS_DIR + "/../kubernetes/crd/domain-v1beta1-crdv7-260.yaml"))
-        .execute();
-
-    try {
-      // install latest version of operator and verify
-      logger.info("Installing and verifying operator");
-      installAndVerifyOperator(opNamespace, opNamespace + "-sa",
-          false, 0, opHelmParams, domainNamespaceSelectionStrategy,
-          false, domain2Namespace);
-
-      // we expect installAndVerifyOperator fails with a lower than expected version of CRD
-      fail("Installing the Operator should fail with a lower than expected version of CRD");
-    } catch (Exception ex) {
-      logger.info("Installing the Operator with a lower than expected version of CRD failed as expected");
-    } finally {
-      // restore the test env
-      uninstallOperatorAndVerify();
-    }
-  }
-
-  /**
-   * Test when domainNamespaceSelectionStrategy is set to Dedicated for the Operator Helm Chart and
-   * the CRD is not present or is deleted, Operator fails with error if it has no permission to create the CRD.
-   */
-  @Test
-  @Order(5)
-  @Disabled("Disable the test because the Operator has permission to create the CRD")
-  @DisplayName("Delete the CRD and verify that Operator fails with error")
-  void testDedicatedModeNoCrd() {
-    // delete existing CRD
-    logger.info("Delete existing CRD");
-    new Command()
-        .withParams(new CommandParams()
-            .command("kubectl delete crd domains.weblogic.oracle --ignore-not-found"))
-        .execute();
-
-    try {
-      // install and verify operator
-      logger.info("Installing and verifying operator");
-      installAndVerifyOperator(opNamespace, opNamespace + "-sa",
-          false, 0, opHelmParams, domainNamespaceSelectionStrategy,
-          false, domain2Namespace);
-
-      // we expect installAndVerifyOperator fails when the CRD misses
-      fail("Installing the Operator should fail when the CRD misses");
-    } catch (Exception ex) {
-      logger.info("Installing the Operator failed as expected when the CRD misses");
-    } finally {
-      // restore the test env
-      uninstallOperatorAndVerify();
-    }
-  }
-
   private void createDomain(String domainNamespace) {
     // create secret for admin credentials
     logger.info("Create secret for admin credentials");
     String adminSecretName = "weblogic-credentials";
-    assertDoesNotThrow(() -> createSecretWithUsernamePassword(adminSecretName, 
-        domainNamespace, ADMIN_USERNAME_DEFAULT, ADMIN_PASSWORD_DEFAULT),
+    assertDoesNotThrow(() -> createSecretWithUsernamePassword(adminSecretName, domainNamespace,
+        ADMIN_USERNAME_DEFAULT, ADMIN_PASSWORD_DEFAULT),
           String.format("create secret for admin credentials failed for %s", adminSecretName));
 
     // create encryption secret
@@ -347,7 +260,7 @@ class ItDedicatedMode {
                     .runtimeEncryptionSecret(encryptionSecretName))));
     setPodAntiAffinity(domain);
     // create model in image domain
-    logger.info("Creating model in image domain {0} in namespace {1} using docker image {2}",
+    logger.info("Creating mii domain {0} in namespace {1} using image {2}",
         domainUid, domainNamespace, miiImage);
     createDomainAndVerify(domain, domainNamespace);
   }
@@ -396,7 +309,7 @@ class ItDedicatedMode {
         String.format("Delete service acct %s failed in namespace %s", opServiceAccount, opNamespace));
 
     // delete secret/ocir-secret
-    new Command()
+    Command
         .withParams(new CommandParams()
             .command("kubectl delete secret/ocir-secret -n " + opNamespace + " --ignore-not-found"))
         .execute();
