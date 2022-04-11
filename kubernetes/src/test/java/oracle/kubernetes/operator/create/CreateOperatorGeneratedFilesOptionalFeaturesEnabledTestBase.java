b3c77cf174bf689634cec6db256fba4ab38f3a7b
Ryan Eberhard <ryan.eberhard@oracle.com>
2022-04-11 09:20:31 -0400

    Fix and enable Helm installation tests and correct a few other Sonar bugs (#2939)
    * Fix and enable Helm installation tests and correct a few other Sonar bugs

--- kubernetes/src/test/java/oracle/kubernetes/operator/create/CreateOperatorGeneratedFilesOptionalFeaturesEnabledTestBase.java
+++ kubernetes/src/test/java/oracle/kubernetes/operator/create/CreateOperatorGeneratedFilesOptionalFeaturesEnabledTestBase.java
@@ -1,10 +1,12 @@
-// Copyright (c) 2018, 2021, Oracle and/or its affiliates.
+// Copyright (c) 2018, 2022, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package oracle.kubernetes.operator.create;
 
+import io.kubernetes.client.openapi.models.V1ConfigMapVolumeSource;
 import io.kubernetes.client.openapi.models.V1Container;
 import io.kubernetes.client.openapi.models.V1Deployment;
+import io.kubernetes.client.openapi.models.V1KeyToPath;
 import io.kubernetes.client.openapi.models.V1Service;
 import oracle.kubernetes.operator.utils.OperatorYamlFactory;
 
@@ -53,6 +55,9 @@ public abstract class CreateOperatorGeneratedFilesOptionalFeaturesEnabledTestBas
   @Override
   public V1Deployment getExpectedWeblogicOperatorDeployment() {
     V1Deployment expected = super.getExpectedWeblogicOperatorDeployment();
+
+    boolean isElkIntegrationEnabled = Boolean.parseBoolean(getInputs().getElkIntegrationEnabled());
+
     V1Container operatorContainer =
         expected.getSpec().getTemplate().getSpec().getContainers().get(0);
     operatorContainer.addVolumeMountsItem(
@@ -62,25 +67,37 @@ public abstract class CreateOperatorGeneratedFilesOptionalFeaturesEnabledTestBas
         .getSpec()
         .getTemplate()
         .getSpec()
-        .addContainersItem(
-            newContainer()
-                .name("logstash")
-                .image(getInputs().getLogStashImage())
-                .addArgsItem("-f")
-                .addArgsItem("/logs/logstash.conf")
-                .addEnvItem(
-                    newEnvVar()
-                        .name("ELASTICSEARCH_HOST")
-                        .value(getInputs().getElasticSearchHost()))
-                .addEnvItem(
-                    newEnvVar()
-                        .name("ELASTICSEARCH_PORT")
-                        .value(getInputs().getElasticSearchPort()))
-                .addVolumeMountsItem(newVolumeMount().name("log-dir").mountPath("/logs")))
         .addVolumesItem(
             newVolume().name("log-dir").emptyDir(newEmptyDirVolumeSource().medium("Memory")))
         .addImagePullSecretsItem(
             newLocalObjectReference().name(getInputs().getWeblogicOperatorImagePullSecretName()));
+
+    V1Container logstashContainer = newContainer()
+        .name("logstash")
+        .image(getInputs().getLogStashImage())
+        .addEnvItem(
+            newEnvVar()
+                .name("ELASTICSEARCH_HOST")
+                .value(getInputs().getElasticSearchHost()))
+        .addEnvItem(
+            newEnvVar()
+                .name("ELASTICSEARCH_PORT")
+                .value(getInputs().getElasticSearchPort()))
+        .addVolumeMountsItem(newVolumeMount().name("log-dir").mountPath("/logs"));
+
+    expected.getSpec().getTemplate().getSpec().addContainersItem(logstashContainer);
+
+    if (isElkIntegrationEnabled) {
+      expected.getSpec().getTemplate().getSpec()
+          .addVolumesItem(newVolume()
+              .name("logstash-config-cm-volume")
+              .configMap(new V1ConfigMapVolumeSource()
+                  .name("weblogic-operator-logstash-cm")
+                  .addItemsItem(new V1KeyToPath().key("logstash.conf").path("logstash.conf"))));
+
+      logstashContainer.addVolumeMountsItem(
+          newVolumeMount().name("logstash-config-cm-volume").mountPath("/usr/share/logstash/pipeline"));
+    }
     return expected;
   }
 }
