45cfd65a8526eaaa18f230d9e636a54061f3d397
Ryan Eberhard <ryan.eberhard@oracle.com>
2022-05-02 18:22:06 -0400

    Temporarily disable unit-test while we investigate

--- operator/src/test/java/oracle/kubernetes/operator/helpers/CrdHelperTest.java
+++ operator/src/test/java/oracle/kubernetes/operator/helpers/CrdHelperTest.java
@@ -34,6 +34,7 @@ import oracle.kubernetes.utils.TestUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static com.meterware.simplestub.Stub.createStrictStub;
@@ -258,6 +259,7 @@ class CrdHelperTest {
   }
 
   @Test
+  @Disabled("Temporarily disable while investigating")
   void whenExistingCrdHasCompatibleConversionWebhook_dontReplaceIt() {
     fileSystem.defineFile(WEBHOOK_CERTIFICATE, "asdf");
     V1CustomResourceDefinition existing = defineCrd(PRODUCT_VERSION, HASH);
