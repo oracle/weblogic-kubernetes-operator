b8b7a6e2b8efef6fca5a2191fdd3410f22ce5bde
ANTARYAMI.PANIGRAHI@ORACLE.COM <anpanigr@anpanigr-2.subnet1ad3phx.devweblogicphx.oraclevcn.com>
2022-05-07 02:46:41 +0000

    initial check-in

---
+++ integration-tests/src/test/resources/bash-scripts/syncTestRepo.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Copyright (c) 2022, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/up
+
+SOURCE_REPO=container-registry.oracle.com
+SOURCE_USER=${OCR_USERNAME}
+SOURCE_PASSWORD=${OCR_PASSWORD}
+
+TARGET_REPO=phx.ocir.io
+TARGET_USER=${OCIR_USERNAME}
+TARGET_PASSWORD=${OCIR_PASSWORD}
+
+echo ${SOURCE_PASSWORD} > pwd.txt
+cat pwd.txt | docker login ${SOURCE_REPO} -u ${SOURCE_USER} --password-stdin
+rm -rf pwd.txt
+# Alternatively use 
+# docker login ${SOURCE_REPO} -u ${SOURCE_USER} -p  ${SOURCE_PASSWORD}
+
+echo ${TARGET_PASSWORD} > pwd.txt
+cat pwd.txt | docker login ${TARGET_REPO} -u ${TARGET_USER} --password-stdin
+rm -rf pwd.txt
+# Alternatively use 
+# docker login ${TARGET_REPO} -u ${TARGET_USER} -p  ${TARGET_PASSWORD}
+
+function dockerPullPushMiddelwareImage() {
+ image=${1}
+ docker pull ${SOURCE_REPO}/middleware/${image}
+ docker tag ${SOURCE_REPO}/middelwarwe/${image} \
+            ${TARGET_REPO}/weblogick8s/test-images/${image}
+ docker push  ${TARGET_REPO}/weblogick8s/test-imgaes/${image}
+ docker rmi -f ${SOURCE_REPO}/middleware/${image}
+}
+
+function dockerPullPushDbImage() {
+ image=${1}
+ docker pull ${SOURCE_REPO}/database/${image}
+ docker tag ${SOURCE_REPO}/database/${image} \
+            ${TARGET_REPO}/weblogick8s/test-images/database/${image}
+ docker push  ${TARGET_REPO}/weblogick8s/test-images/database/${image}
+ docker rmi -f ${SOURCE_REPO}/database/${image}
+}
+
+dockerPullPushMiddelwareImage weblogic:14.1.1.0-11
+dockerPullPushMiddelwareImage fmw-infrastructure:12.2.1.4
+
+dockerPullPushDbImage enterprise:12.2.0.1-slim
