a45f374c2bb6b8fecd70bac6dfb995a903210127
Marina Kogan <marina.kogan@oracle.com>
2022-05-19 17:26:37 +0000

    fixed terraform

--- kubernetes/samples/scripts/terraform/oke.create.sh
+++ kubernetes/samples/scripts/terraform/oke.create.sh
@@ -32,11 +32,11 @@ setupTerraform() {
     mkdir ${terraformDir}
     cd ${terraformDir}
     if [[ "${OSTYPE}" == "darwin"* ]]; then
-      curl -O https://releases.hashicorp.com/terraform/0.11.15/terraform_0.12.31_darwin_amd64.zip
-      unzip terraform_0.12.31_darwin_amd64.zip
+      curl -O https://releases.hashicorp.com/terraform/0.11.15/terraform_1.2.0_darwin_amd64.zip
+      unzip terraform_1.2.0_darwin_amd64.zip
     elif [[ "${OSTYPE}" == "linux"* ]]; then
-       curl -LO --retry 3 https://releases.hashicorp.com/terraform/0.12.31/terraform_0.12.31_linux_amd64.zip
-       unzip -o terraform_0.12.31_linux_amd64.zip -d ${terraformDir}
+       curl -LO --retry 3 https://releases.hashicorp.com/terraform/1.2.0/terraform_1.2.0_linux_amd64.zip
+       unzip -o terraform_1.2.0_linux_amd64.zip -d ${terraformDir}
     else
        echo "Unsupported OS"
     fi
