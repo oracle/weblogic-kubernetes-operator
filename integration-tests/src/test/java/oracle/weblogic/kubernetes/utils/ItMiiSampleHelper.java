4be315d322f018aee24ee9918f83c7d0def04369
MAGGIE.HE@ORACLE.COM <maggie.he@oracle.com>
2022-05-25 16:23:38 +0000

    provide multi secrets for MiiAux sample

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/ItMiiSampleHelper.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/ItMiiSampleHelper.java
@@ -18,6 +18,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 
 import static oracle.weblogic.kubernetes.TestConstants.BASE_IMAGES_REPO;
 import static oracle.weblogic.kubernetes.TestConstants.BASE_IMAGES_REPO_SECRET;
+import static oracle.weblogic.kubernetes.TestConstants.BASE_IMAGES_REPO_SECRET_NAME;
 import static oracle.weblogic.kubernetes.TestConstants.DB_IMAGE_NAME;
 import static oracle.weblogic.kubernetes.TestConstants.DB_IMAGE_TAG;
 import static oracle.weblogic.kubernetes.TestConstants.DOMAIN_IMAGES_REPO;
@@ -122,7 +123,8 @@ public class ItMiiSampleHelper {
     envMap.put("BASE_IMAGE_NAME", WEBLOGIC_IMAGE_TO_USE_IN_SPEC
         .substring(0, WEBLOGIC_IMAGE_TO_USE_IN_SPEC.lastIndexOf(":")));
     envMap.put("BASE_IMAGE_TAG", WEBLOGIC_IMAGE_TAG);
-    envMap.put("IMAGE_PULL_SECRET_NAME", TEST_IMAGES_REPO_SECRET_NAME); //ocir secret
+    envMap.put("IMAGE_PULL_SECRET_NAME", BASE_IMAGES_REPO_SECRET_NAME);
+    envMap.put("DOMAIN_IMAGE_PULL_SECRET_NAME", TEST_IMAGES_REPO_SECRET_NAME);
     envMap.put("K8S_NODEPORT_HOST", K8S_NODEPORT_HOST);
     envMap.put("OKD", "" +  OKD);
     envMap.put("DO_AI", String.valueOf(imageType == ImageType.AUX));
@@ -145,11 +147,17 @@ public class ItMiiSampleHelper {
     execTestScriptAndAssertSuccess(DomainType.WLS, "-traefik", "Traefik deployment failure");
 
     logger.info("Setting up docker secrets");
-    // Create the repo secret to pull the image
+    // Create the repo secret to pull the domain image
     // this secret is used only for non-kind cluster
     createOcirRepoSecret(domainNamespace);
-    logger.info("Docker registry secret {0} created successfully in namespace {1}",
+    logger.info("Docker registry secret {0} created for domain image successfully in namespace {1}",
         TEST_IMAGES_REPO_SECRET_NAME, domainNamespace);
+    // Create the repo secret to pull the base image
+    // this secret is used only for non-kind cluster
+    createSecretForBaseImages(domainNamespace);
+    logger.info("Docker registry secret {0} for base image created successfully in namespace {1}",
+        BASE_IMAGES_REPO_SECRET_NAME, domainNamespace);
+
 
     if (domainType.equals(DomainType.JRF)) {
       // install db for FMW test cases
