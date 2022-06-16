88ae9964ee0864dafc2dd36740a71ba8f9f587b0
Marina Kogan <marina.kogan@oracle.com>
2022-06-16 18:25:40 +0000

    fixed ownership

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItWlsSamples.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItWlsSamples.java
@@ -608,12 +608,17 @@ class ItWlsSamples {
         replaceStringInFile(get(sampleBase.toString(), "create-domain-inputs.yaml").toString(),
             "imagePullPolicy: IfNotPresent", "imagePullPolicy: Always");
       }
-      if (OKE_CLUSTER) {
-        String chownCommand = "chown 1000:0 %DOMAIN_ROOT_DIR%"
+      if (OKE_CLUSTER && sampleBase.toString().contains("domain-home-on-pv")) {
+        String chownCommand1 = "chown 1000:0 %DOMAIN_ROOT_DIR%"
                 + "/. && find %DOMAIN_ROOT_DIR%"
                 + "/. -maxdepth 1 ! -name '.snapshot' ! -name '.' -print0 | xargs -r -0  chown -R 1000:0";
+        String chownCommand2 = "chown 1000:1000 %DOMAIN_ROOT_DIR%"
+                + "/. && find %DOMAIN_ROOT_DIR%"
+                + "/. -maxdepth 1 ! -name '.snapshot' ! -name '.' -print0 | xargs -r -0  chown -R 1000:1000";
         replaceStringInFile(get(sampleBase.toString(), "create-domain-job-template.yaml").toString(),
-                "chown -R 1000:0 %DOMAIN_ROOT_DIR%", chownCommand);
+                "chown -R 1000:0 %DOMAIN_ROOT_DIR%", chownCommand1);
+        replaceStringInFile(get(sampleBase.toString(), "update-domain-job-template.yaml").toString(),
+                "chown -R 1000:1000 %DOMAIN_ROOT_DIR%", chownCommand2);
       }
     });
   }
