f695ff1014a02d22e069724fb8bfb4994990c1f1
Sankar Neelakandan <sankar.periyathambi.neelakand@oracle.com>
2022-06-22 16:04:29 -0700

    wip

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItConfigDistributionStrategy.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItConfigDistributionStrategy.java
@@ -1077,8 +1077,10 @@ class ItConfigDistributionStrategy {
       p.setProperty("dsName", dsName);
       p.setProperty("dsUrl", jdbcDsUrl);
       if (WEBLOGIC_IMAGE_TO_USE_IN_SPEC.contains("12.2.1.3")) {
-        p.setProperty("dsDriver", "com.mysql.cj.jdbc.Driver");
+        logger.info("Using 12.2.1.3 image, driver is com.mysql.jdbc.Driver");
+        p.setProperty("dsDriver", "com.mysql.jdbc.Driver");
       } else {
+        logger.info("Using 12.2.1.4 and later image, driver is com.mysql.cj.jdbc.Driver");
         p.setProperty("dsDriver", "com.mysql.cj.jdbc.Driver");
       }
       p.setProperty("dsUser", user);
