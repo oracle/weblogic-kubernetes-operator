a191b82a54802c6df70bf5b58d7f0c2674d3c30e
Dongbo Xiao <dongbo.xiao@oracle.com>
2022-06-23 09:37:38 -0400

    Fix a command option when running WDT extractDomainResoure.sh

--- kubernetes/samples/scripts/common/wdt-and-wit-utility.sh
+++ kubernetes/samples/scripts/common/wdt-and-wit-utility.sh
@@ -212,7 +212,7 @@ run_wdt() {
   cmd="
   $wdt_bin_dir/extractDomainResource.sh
      -oracle_home $oracle_home
-     -domain_resource_file domain${action}.yaml
+     -output_file domain${action}.yaml
      -domain_home $domain_home_dir
      -model_file $model_final
      -variable_file $inputs_final
