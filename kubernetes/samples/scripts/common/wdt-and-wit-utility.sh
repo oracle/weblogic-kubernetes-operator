cffca112eeb157a1b0c329f5794d0dcce9a6143a
Dongbo Xiao <dongbo.xiao@oracle.com>
2022-06-23 13:12:48 -0400

    Revert a testing change

--- kubernetes/samples/scripts/common/wdt-and-wit-utility.sh
+++ kubernetes/samples/scripts/common/wdt-and-wit-utility.sh
@@ -212,7 +212,7 @@ function run_wdt {
   cmd="
   $wdt_bin_dir/extractDomainResource.sh
      -oracle_home $oracle_home
-     -output_dir domain${action}.yaml
+     -domain_resource_file domain${action}.yaml
      -domain_home $domain_home_dir
      -model_file $model_final
      -variable_file $inputs_final
