1981406def127cfe4966e0d1852d02a037b47f25
Marina Kogan <marina.kogan@oracle.com>
2022-06-28 17:13:48 +0000

    fixed style

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItServerStartPolicy.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItServerStartPolicy.java
@@ -13,9 +13,9 @@ import oracle.weblogic.kubernetes.logging.LoggingFacade;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
