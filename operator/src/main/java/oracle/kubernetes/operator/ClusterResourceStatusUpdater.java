4192fd5807c5ac339cdbd23153b73eb112f3522e
Lenny Phan <lenny.phan@oracle.com>
2022-07-15 10:22:22 -0700

    add missing classes

---
+++ operator/src/main/java/oracle/kubernetes/operator/ClusterResourceStatusUpdater.java
@@ -0,0 +1,241 @@
+// Copyright (c) 2022, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package oracle.kubernetes.operator;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import javax.annotation.Nonnull;
+
+import oracle.kubernetes.operator.calls.CallResponse;
+import oracle.kubernetes.operator.calls.UnrecoverableErrorBuilder;
+import oracle.kubernetes.operator.helpers.CallBuilder;
+import oracle.kubernetes.operator.helpers.DomainPresenceInfo;
+import oracle.kubernetes.operator.helpers.ResponseStep;
+import oracle.kubernetes.operator.logging.LoggingFacade;
+import oracle.kubernetes.operator.logging.LoggingFactory;
+import oracle.kubernetes.operator.steps.DefaultResponseStep;
+import oracle.kubernetes.operator.work.NextAction;
+import oracle.kubernetes.operator.work.Packet;
+import oracle.kubernetes.operator.work.Step;
+import oracle.kubernetes.operator.work.Step.StepAndPacket;
+import oracle.kubernetes.weblogic.domain.model.ClusterResource;
+import oracle.kubernetes.weblogic.domain.model.ClusterStatus;
+
+import static oracle.kubernetes.operator.KubernetesConstants.CLUSTER;
+import static oracle.kubernetes.operator.KubernetesConstants.HTTP_NOT_FOUND;
+
+/**
+ * Updates for status of Cluster resources.
+ */
+@SuppressWarnings("WeakerAccess")
+public class ClusterResourceStatusUpdater {
+
+  private static final LoggingFacade LOGGER = LoggingFactory.getLogger("Operator", "Operator");
+
+  private ClusterResourceStatusUpdater() {
+  }
+
+  /**
+   * Creates an asynchronous step to update cluster status.
+   *
+   * @param next the next step
+   * @return the new step
+   */
+  public static Step createClusterResourceStatusUpdaterStep(Step next) {
+    return new ClusterResourceStatusUpdaterStep(next);
+  }
+
+  private static class ClusterResourceStatusUpdaterStep extends Step {
+
+    ClusterResourceStatusUpdaterStep(Step next) {
+      super(next);
+    }
+
+    /**
+     * Invokes step using the packet as input/output context.
+     *
+     * @param packet Packet
+     * @return Next action
+     */
+    @Override
+    public NextAction apply(Packet packet) {
+      DomainPresenceInfo info = DomainPresenceInfo.fromPacket(packet).orElseThrow();
+      Step step = Optional.ofNullable(info.getDomain())
+          .map(domain -> createUpdateClusterResourceStatusSteps(packet, info))
+          .orElse(null);
+      return doNext(step != null ? step : getNext(), packet);
+    }
+
+    private Step createUpdateClusterResourceStatusSteps(Packet packet, DomainPresenceInfo info) {
+      final List<StepAndPacket> result = info.getClusterResources().stream()
+          .filter(res -> isClusterResourceStatusChanged(packet, res))
+          .map(res -> createReplaceClusterResourceStatusStep(createContext(packet, res))).collect(Collectors.toList());
+      return result.isEmpty() ? null : new RunInParallelStep(result);
+    }
+
+    private boolean isClusterResourceStatusChanged(Packet packet, ClusterResource resource) {
+      ReplaceClusterStatusContext context = createContext(packet, resource);
+      return !Objects.equals(context.getNewStatus(), context.getResource().getStatus());
+    }
+  }
+
+  static StepAndPacket createReplaceClusterResourceStatusStep(ReplaceClusterStatusContext context) {
+    return new StepAndPacket(createReplaceClusterStatusAsyncStep(context), context.getPacket().copy());
+  }
+
+  private static Step createReplaceClusterStatusAsyncStep(ReplaceClusterStatusContext context) {
+    return new CallBuilder()
+        .replaceClusterStatusAsync(
+            context.getClusterResourceName(),
+            context.getNamespace(),
+            context.createReplacementClusterResource(),
+            new ClusterResourceStatusReplaceResponseStep(context));
+  }
+
+  static class ClusterResourceStatusReplaceResponseStep extends DefaultResponseStep<ClusterResource> {
+    private final ReplaceClusterStatusContext context;
+
+    ClusterResourceStatusReplaceResponseStep(ReplaceClusterStatusContext context) {
+      this.context = context;
+    }
+
+    @Override
+    public NextAction onSuccess(Packet packet, CallResponse<ClusterResource> callResponse) {
+      if (callResponse.getResult() != null) {
+        packet.getSpi(DomainPresenceInfo.class).addClusterResource(callResponse.getResult());
+      }
+      return doNext(packet);
+    }
+
+    @Override
+    public NextAction onFailure(Packet packet, CallResponse<ClusterResource> callResponse) {
+      if (UnrecoverableErrorBuilder.isAsyncCallUnrecoverableFailure(callResponse)) {
+        return super.onFailure(packet, callResponse);
+      } else {
+        return onFailure(createRetry(), packet, callResponse);
+      }
+    }
+
+    private Step createRetry() {
+      return Step.chain(
+          createClusterResourceRefreshStep(),
+          new SingleClusterResourceStatusUpdateStep(context.getClusterName()));
+    }
+
+    private Step createClusterResourceRefreshStep() {
+      return new CallBuilder().readClusterAsync(this.context.getClusterResourceName(),
+          this.context.getNamespace(), new ReadClusterResponseStep());
+    }
+  }
+
+  static class RunInParallelStep extends Step {
+    final Collection<StepAndPacket> statusUpdater;
+
+    RunInParallelStep(Collection<StepAndPacket> statusUpdater) {
+      this.statusUpdater = statusUpdater;
+    }
+
+    @Override
+    public NextAction apply(Packet packet) {
+      if (statusUpdater.isEmpty()) {
+        return doNext(getNext(), packet);
+      } else {
+        return doForkJoin(getNext(), packet, statusUpdater);
+      }
+    }
+  }
+
+  static ReplaceClusterStatusContext createContext(Packet packet, ClusterResource resource) {
+    return new ReplaceClusterStatusContext(packet, resource);
+  }
+
+  static class ReplaceClusterStatusContext {
+    private final Packet packet;
+    private final DomainPresenceInfo info;
+    private final ClusterResource resource;
+
+    private ReplaceClusterStatusContext(@Nonnull Packet packet, @Nonnull ClusterResource resource) {
+      this.packet = packet;
+      this.info = DomainPresenceInfo.fromPacket(packet).orElseThrow();
+      this.resource = resource;
+    }
+
+    ClusterStatus getNewStatus() {
+      return getClusterStatus(info.getDomain().getOrCreateStatus().getClusters(), resource.getClusterName());
+    }
+
+    ClusterResource getResource() {
+      return resource;
+    }
+
+    Packet getPacket() {
+      return packet;
+    }
+
+    String getClusterName() {
+      return resource.getClusterName();
+    }
+
+    String getClusterResourceName() {
+      return resource.getMetadata().getName();
+    }
+
+    String getNamespace() {
+      return resource.getNamespace();
+    }
+
+    ClusterResource createReplacementClusterResource() {
+      return new ClusterResource()
+          .withKind(CLUSTER)
+          .withMetadata(resource.getMetadata())
+          .spec(null)
+          .withStatus(getNewStatus());
+    }
+
+    private ClusterStatus getClusterStatus(List<ClusterStatus> clusterStatuses, String clusterName) {
+      for (ClusterStatus clusterStatus : clusterStatuses) {
+        if (clusterName.equals(clusterStatus.getClusterName())) {
+          return clusterStatus;
+        }
+      }
+      return null;
+    }
+  }
+
+  static class ReadClusterResponseStep extends ResponseStep<ClusterResource> {
+    @Override
+    public NextAction onSuccess(Packet packet, CallResponse<ClusterResource> callResponse) {
+      if (callResponse.getResult() != null) {
+        packet.getSpi(DomainPresenceInfo.class).addClusterResource(callResponse.getResult());
+      }
+      return doNext(packet);
+    }
+
+    @Override
+    public NextAction onFailure(Packet packet, CallResponse<ClusterResource> callResponse) {
+      return callResponse.getStatusCode() == HTTP_NOT_FOUND
+          ? doNext(null, packet)
+          : super.onFailure(packet, callResponse);
+    }
+  }
+
+  static class SingleClusterResourceStatusUpdateStep extends Step {
+    private final String clusterName;
+
+    SingleClusterResourceStatusUpdateStep(String clusterName) {
+      this.clusterName = clusterName;
+    }
+
+    @Override
+    public NextAction apply(Packet packet) {
+      DomainPresenceInfo info = DomainPresenceInfo.fromPacket(packet).orElseThrow();
+      ClusterResource res = info.getClusterResource(clusterName);
+      return doNext(createReplaceClusterStatusAsyncStep(createContext(packet, res)), packet);
+    }
+  }
+}
+
---
+++ operator/src/test/java/oracle/kubernetes/operator/ClusterResourceStatusUpdaterTest.java
@@ -0,0 +1,191 @@
+// Copyright (c) 2022, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package oracle.kubernetes.operator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.meterware.simplestub.Memento;
+import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.models.V1ObjectMeta;
+import oracle.kubernetes.operator.calls.UnrecoverableCallException;
+import oracle.kubernetes.operator.helpers.DomainPresenceInfo;
+import oracle.kubernetes.operator.helpers.KubernetesTestSupport;
+import oracle.kubernetes.operator.helpers.RetryStrategyStub;
+import oracle.kubernetes.operator.tuning.TuningParametersStub;
+import oracle.kubernetes.operator.utils.WlsDomainConfigSupport;
+import oracle.kubernetes.operator.wlsconfig.WlsDomainConfig;
+import oracle.kubernetes.operator.work.TerminalStep;
+import oracle.kubernetes.utils.SystemClockTestSupport;
+import oracle.kubernetes.utils.TestUtils;
+import oracle.kubernetes.weblogic.domain.model.ClusterResource;
+import oracle.kubernetes.weblogic.domain.model.ClusterSpec;
+import oracle.kubernetes.weblogic.domain.model.ClusterStatus;
+import oracle.kubernetes.weblogic.domain.model.DomainResource;
+import oracle.kubernetes.weblogic.domain.model.DomainStatus;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.meterware.simplestub.Stub.createStrictStub;
+import static oracle.kubernetes.operator.DomainProcessorTestSetup.NS;
+import static oracle.kubernetes.operator.DomainProcessorTestSetup.UID;
+import static oracle.kubernetes.operator.KubernetesConstants.HTTP_INTERNAL_ERROR;
+import static oracle.kubernetes.operator.KubernetesConstants.HTTP_UNAVAILABLE;
+import static oracle.kubernetes.operator.ProcessingConstants.DOMAIN_TOPOLOGY;
+import static oracle.kubernetes.operator.helpers.KubernetesTestSupport.CLUSTER_STATUS;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.junit.MatcherAssert.assertThat;
+
+class ClusterResourceStatusUpdaterTest {
+  private static final String NAME = UID;
+  public static final String CLUSTER = "cluster-1";
+  private final TerminalStep endStep = new TerminalStep();
+  private final KubernetesTestSupport testSupport = new KubernetesTestSupport();
+  private final List<Memento> mementos = new ArrayList<>();
+  private final ClusterResource cluster = createClusterResource(CLUSTER);
+  private final DomainResource domain = DomainProcessorTestSetup.createTestDomain();
+  private final DomainPresenceInfo info = new DomainPresenceInfo(domain);
+  private final WlsDomainConfigSupport configSupport = new WlsDomainConfigSupport(NAME);
+  private final RetryStrategyStub retryStrategy = createStrictStub(RetryStrategyStub.class);
+
+  @BeforeEach
+  void setUp() throws NoSuchFieldException {
+    mementos.add(TestUtils.silenceOperatorLogger().ignoringLoggedExceptions(ApiException.class));
+    mementos.add(testSupport.install());
+    mementos.add(TuningParametersStub.install());
+    mementos.add(ClientFactoryStub.install());
+    mementos.add(SystemClockTestSupport.installClock());
+
+    domain.setStatus(new DomainStatus());
+    testSupport.addDomainPresenceInfo(info);
+    testSupport.defineResources(domain, cluster);
+  }
+
+  @AfterEach
+  void tearDown() throws Exception {
+    mementos.forEach(Memento::revert);
+
+    testSupport.throwOnCompletionFailure();
+  }
+
+  void addTopologyToPacket(WlsDomainConfig domainConfig) {
+    testSupport.addToPacket(DOMAIN_TOPOLOGY, domainConfig);
+  }
+
+  private void updateClusterResourceStatus() {
+    testSupport.runSteps(ClusterResourceStatusUpdater.createClusterResourceStatusUpdaterStep(endStep));
+  }
+
+  @Test
+  void statusStep_updateClusterResourceStatusFromDomainStatus() {
+    ClusterStatus newStatus = new ClusterStatus().withMinimumReplicas(0).withMaximumReplicas(8)
+        .withClusterName(CLUSTER).withReplicas(2).withReadyReplicas(1).withReplicasGoal(2);
+    domain.getStatus().addCluster(newStatus);
+    cluster.withStatus(null);
+    info.addClusterResource(cluster);
+
+    updateClusterResourceStatus();
+
+    ClusterResource clusterResource = testSupport
+        .getResourceWithName(KubernetesTestSupport.CLUSTER, NAME + '-' + CLUSTER);
+    assertThat(clusterResource,  notNullValue());
+    assertThat(clusterResource.getStatus(), equalTo(newStatus));
+  }
+
+  @Test
+  void whenMultipleStaticClusterResources_statusStep_updatesAllClusterResourceStatusesFromDomainStatus() {
+    ClusterStatus[] newStatuses = new ClusterStatus[3];
+    for (int i = 1; i <= 3; i++) {
+      String clusterName = "cluster" + i;
+      newStatuses[i - 1] = new ClusterStatus().withMinimumReplicas(0).withMaximumReplicas(i)
+          .withClusterName(clusterName).withReplicas(2).withReadyReplicas(1).withReplicasGoal(2);
+      domain.getStatus().addCluster(newStatuses[i - 1]);
+      ClusterResource resource = createClusterResource(clusterName)
+          .withStatus(null);
+      info.addClusterResource(resource);
+      testSupport.defineResources(resource);
+    }
+
+    updateClusterResourceStatus();
+
+    for (int i = 1; i <= 3; i++) {
+      ClusterResource clusterResource = testSupport
+          .getResourceWithName(KubernetesTestSupport.CLUSTER, NAME + "-cluster" + i);
+      assertThat(clusterResource,  notNullValue());
+      assertThat(clusterResource.getStatus(), equalTo(newStatuses[i - 1]));
+    }
+  }
+
+  @Test
+  void whenNoClusterResourceInDomainPresenceInfo_doNothing() {
+    domain.getStatus().addCluster(new ClusterStatus().withMinimumReplicas(0).withMaximumReplicas(5)
+        .withClusterName(CLUSTER).withReplicas(2).withReadyReplicas(1).withReplicasGoal(2));
+
+    updateClusterResourceStatus();
+
+    ClusterResource clusterResource = testSupport
+        .getResourceWithName(KubernetesTestSupport.CLUSTER, NAME + '-' + "cluster-1");
+    assertThat(clusterResource,  notNullValue());
+    assertThat(clusterResource.getStatus(), nullValue());
+  }
+
+  @Test
+  void whenNoClusterStatusInDomainStatus_doNothing() {
+    info.addClusterResource(cluster);
+
+    updateClusterResourceStatus();
+
+    ClusterResource clusterResource = testSupport
+        .getResourceWithName(KubernetesTestSupport.CLUSTER, NAME + '-' + CLUSTER);
+    assertThat(clusterResource,  notNullValue());
+    assertThat(clusterResource.getStatus(), nullValue());
+  }
+
+  @Test
+  void onFailedReplaceStatus_reportUnrecoverableFailure() {
+    cluster.withStatus(new ClusterStatus().withMinimumReplicas(0).withMaximumReplicas(8)
+        .withClusterName(CLUSTER).withReplicas(2).withReadyReplicas(1).withReplicasGoal(2));
+    info.addClusterResource(cluster);
+    domain.getStatus().addCluster(new ClusterStatus());
+    testSupport.failOnReplaceStatus(CLUSTER_STATUS, NAME + '-' + CLUSTER, NS, HTTP_INTERNAL_ERROR);
+
+    updateClusterResourceStatus();
+
+    testSupport.verifyCompletionThrowable(UnrecoverableCallException.class);
+  }
+
+  @Test
+  void onFailedReplaceStatus_retryRequest() {
+    ClusterStatus newStatus = new ClusterStatus().withMinimumReplicas(0).withMaximumReplicas(8)
+        .withClusterName(CLUSTER).withReplicas(2).withReadyReplicas(1).withReplicasGoal(2);
+    domain.getStatus().addCluster(newStatus);
+    cluster.withStatus(null);
+    info.addClusterResource(cluster);
+    retryStrategy.setNumRetriesLeft(1);
+    testSupport.addRetryStrategy(retryStrategy);
+    testSupport.failOnReplaceStatus(CLUSTER_STATUS, NAME + '-' + CLUSTER, NS, HTTP_UNAVAILABLE);
+
+    updateClusterResourceStatus();
+
+    ClusterResource clusterResource = testSupport
+        .getResourceWithName(KubernetesTestSupport.CLUSTER, NAME + '-' + CLUSTER);
+    assertThat(clusterResource,  notNullValue());
+    assertThat(clusterResource.getStatus(), equalTo(newStatus));
+  }
+
+  private void addStaticClusterToTopology(String clusterName, String... servers) {
+    configSupport.addWlsCluster(clusterName, servers);
+    addTopologyToPacket(configSupport.createDomainConfig());
+  }
+
+  private ClusterResource createClusterResource(String clusterName) {
+    return new ClusterResource()
+        .withMetadata(new V1ObjectMeta().namespace(DomainProcessorTestSetup.NS).name(
+            ClusterResourceStatusUpdaterTest.NAME + '-' + clusterName))
+        .spec(new ClusterSpec().withDomainUid(ClusterResourceStatusUpdaterTest.NAME).withClusterName(clusterName));
+  }
+}
