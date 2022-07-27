b2a08a65313216d53f44b71336f9d90aa2c44d2d
MAGGIE.HE@ORACLE.COM <maggie.he@oracle.com>
2022-07-27 21:36:32 +0000

    custome wdt url fix

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/actions/impl/primitive/Installer.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/actions/impl/primitive/Installer.java
@@ -8,27 +8,20 @@ import java.io.File;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.DOWNLOAD_DIR;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.IMAGE_TOOL;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.REMOTECONSOLE;
-import static oracle.weblogic.kubernetes.actions.ActionConstants.REMOTECONSOLE_DOWNLOAD_FILENAME_DEFAULT;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.REMOTECONSOLE_DOWNLOAD_URL;
-import static oracle.weblogic.kubernetes.actions.ActionConstants.REMOTECONSOLE_DOWNLOAD_URL_DEFAULT;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.REMOTECONSOLE_FILE;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.SNAKE;
-import static oracle.weblogic.kubernetes.actions.ActionConstants.SNAKE_DOWNLOADED_FILENAME;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.SNAKE_DOWNLOAD_URL;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.WDT;
-import static oracle.weblogic.kubernetes.actions.ActionConstants.WDT_DOWNLOAD_FILENAME_DEFAULT;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.WDT_DOWNLOAD_URL;
-import static oracle.weblogic.kubernetes.actions.ActionConstants.WDT_DOWNLOAD_URL_DEFAULT;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.WIT;
-import static oracle.weblogic.kubernetes.actions.ActionConstants.WIT_DOWNLOAD_FILENAME_DEFAULT;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.WIT_DOWNLOAD_URL;
-import static oracle.weblogic.kubernetes.actions.ActionConstants.WIT_DOWNLOAD_URL_DEFAULT;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.WLE;
-import static oracle.weblogic.kubernetes.actions.ActionConstants.WLE_DOWNLOAD_FILENAME_DEFAULT;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.WLE_DOWNLOAD_URL;
-import static oracle.weblogic.kubernetes.actions.ActionConstants.WLE_DOWNLOAD_URL_DEFAULT;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.WORK_DIR;
 import static oracle.weblogic.kubernetes.actions.impl.primitive.Command.defaultCommandParams;
+import static oracle.weblogic.kubernetes.utils.CommonTestUtils.getActualLocationIfNeeded;
+import static oracle.weblogic.kubernetes.utils.CommonTestUtils.getInstallerFileName;
 import static oracle.weblogic.kubernetes.utils.FileUtils.checkDirectory;
 import static oracle.weblogic.kubernetes.utils.FileUtils.doesFileExist;
 import static oracle.weblogic.kubernetes.utils.ThreadSafeLogger.getLogger;
@@ -210,119 +203,4 @@ public class Installer {
     return command;
   }
 
-  /**
-   * Figure out the actual version number of the latest release of WDT or WIT if the version
-   * parameter is not specified or is specified as "latest". Otherwise return the passed in
-   * version parameter itself.
-   * 
-   * @return the version number that is determined
-   * @throws RuntimeException if the operation failed for any reason
-   */
-  private String getActualLocationIfNeeded(
-      String location,
-      String type,
-      String downloadDir
-  ) throws RuntimeException {
-    String actualLocation = location;
-    if (needToGetActualLocation(location, type)) {
-      String version = "";
-      String command = String.format(
-          "curl -fL %s -o %s/%s-%s",
-          location,
-          downloadDir,
-          type,
-          TMP_FILE_NAME);
- 
-      CommandParams params = 
-          defaultCommandParams()
-              .command(command)
-              .saveResults(true);
-      if (!Command.withParams(params).execute()) {
-        RuntimeException exception =
-            new RuntimeException(String.format("Failed to get the latest %s release information.", type));
-        getLogger().severe(
-            String.format(
-                "Failed to get the latest %s release information. The stderr is %s",
-                type,
-                params.stderr()),
-            exception);
-        throw exception;
-      }
-
-      command = String.format(
-          "cat %s/%s-%s | grep 'releases/download' | awk '{ split($0,a,/href=\"/);%s | %s", 
-          downloadDir,
-          type, 
-          TMP_FILE_NAME, 
-          " print a[2] }'", 
-          " cut -d/ -f 6"); 
-    
-      params = 
-          defaultCommandParams()
-          .command(command)
-          .saveResults(true)
-          .redirect(true);
- 
-      // the command is considered successful only if we have got back a real version number in params.stdout()
-      if (Command.withParams(params).execute()
-          && params.stdout() != null
-          && params.stdout().length() != 0) {
-        // Because I've updated the name of the logging exporter to remove the version number in the name, but
-        // also preserved the original, there will be two entries located. Take the first.
-        version = params.stdout().lines().findFirst().get().trim();
-      } else {
-        RuntimeException exception =
-            new RuntimeException(String.format("Failed to get the version number of the requested %s release.", type));
-        getLogger().severe(
-            String.format(
-                "Failed to get the version number of the requested %s release. The stderr is %s",
-                type,
-                params.stderr()),
-            exception);
-        throw exception;
-      }
-
-      if (version != null) {
-        actualLocation = location.replace("latest",
-            String.format("download/%s/%s", version, getInstallerFileName(type)));
-      }
-    }
-    getLogger().info("The actual download location for {0} is {1}", params.type(), actualLocation);
-    return actualLocation;
-  }
-
-  private boolean needToGetActualLocation(
-      String location,
-      String type) {
-    switch (type) {
-      case WDT:
-        return WDT_DOWNLOAD_URL_DEFAULT.equals(location);
-      case WIT:
-        return WIT_DOWNLOAD_URL_DEFAULT.equals(location);
-      case WLE:
-        return WLE_DOWNLOAD_URL_DEFAULT.equals(location);
-      case REMOTECONSOLE:
-        return REMOTECONSOLE_DOWNLOAD_URL_DEFAULT.equals(location);
-      default:
-        return false;
-    }
-  }
-
-  private String getInstallerFileName(
-      String type) {
-    switch (type) {
-      case WDT:
-        return WDT_DOWNLOAD_FILENAME_DEFAULT;
-      case WIT:
-        return WIT_DOWNLOAD_FILENAME_DEFAULT;
-      case WLE:
-        return WLE_DOWNLOAD_FILENAME_DEFAULT;
-      case SNAKE:
-        return SNAKE_DOWNLOADED_FILENAME;
-      case REMOTECONSOLE:
-        return REMOTECONSOLE_DOWNLOAD_FILENAME_DEFAULT;
-      default:
-        return "";
-    }
-  }
 }
