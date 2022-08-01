debdbb676639e6984329a407f2558a7be725b06a
Anthony Lai <anthony.lai@oracle.com>
2022-08-01 08:55:22 -0700

    comments

--- operator/src/test/sh/scaling/scalingActionTest.sh
+++ operator/src/test/sh/scaling/scalingActionTest.sh
@@ -2,6 +2,12 @@
 # Copyright (c) 2021, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
+# To run this test:
+# export SHUNIT2_PATH=<path to shunit2>
+# export SCRIPTPATH=<path to scalingAction.sh>
+# sh scaling/scalingActionTest.sh
+#
+
 API_VERSION="v9"
 TEST_OPERATOR_ROOT=/tmp/test/weblogic-operator
 setUp() {
