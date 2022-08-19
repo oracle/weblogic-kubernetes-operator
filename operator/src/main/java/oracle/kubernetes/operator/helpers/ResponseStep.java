b66748eb8463f63433f32a58ba9143321268beb4
Dongbo Xiao <dongbo.xiao@oracle.com>
2022-08-19 15:54:03 -0400

    improve integration test

--- operator/src/main/java/oracle/kubernetes/operator/helpers/ResponseStep.java
+++ operator/src/main/java/oracle/kubernetes/operator/helpers/ResponseStep.java
@@ -179,7 +179,7 @@ public abstract class ResponseStep<T> extends Step {
   private NextAction logNoRetry(Packet packet, CallResponse<T> callResponse) {
     if (callResponse != null) {
       addDomainFailureStatus(packet, callResponse.getRequestParams(), callResponse.getE());
-      
+
       if (callResponse.getStatusCode() != HTTP_NOT_FOUND && LOGGER.isWarningEnabled()) {
         LOGGER.warning(
             MessageKeys.ASYNC_NO_RETRY,
