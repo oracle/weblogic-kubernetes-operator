98f77e6b2b544fe8412f20b460f81a47e673cbef
Marina Kogan <marina.kogan@oracle.com>
2022-09-06 18:24:44 +0000

    aded test to oke

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItConfigDistributionStrategy.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItConfigDistributionStrategy.java
@@ -124,6 +124,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @DisplayName("Verify the overrideDistributionStrategy applies the overrides accordingly to the value set")
 @Tag("kind-parallel")
 @Tag("okd-wls-mrg")
+@Tag("oke-parallel")
 @IntegrationTest
 @Tag("olcne")
 class ItConfigDistributionStrategy {
