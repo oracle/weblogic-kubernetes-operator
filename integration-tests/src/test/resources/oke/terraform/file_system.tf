6fdc050ae4efbe75003e3a7d634da8cac981058e
Marina Kogan <marina.kogan@oracle.com>
2022-09-07 18:17:09 +0000

    added multiple exports

--- integration-tests/src/test/resources/oke/terraform/file_system.tf
+++ integration-tests/src/test/resources/oke/terraform/file_system.tf
@@ -3,9 +3,54 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 */
 
-resource "oci_file_storage_file_system" "oketest_fs" {
+resource "oci_file_storage_file_system" "oketest_fs1" {
+  #Required
+  availability_domain = data.oci_identity_availability_domains.ADs.availability_domains[1]["name"]
+  compartment_id      = var.compartment_ocid
+}
+resource "oci_file_storage_file_system" "oketest_fs2" {
+  #Required
+  availability_domain = data.oci_identity_availability_domains.ADs.availability_domains[1]["name"]
+  compartment_id      = var.compartment_ocid
+}
+resource "oci_file_storage_file_system" "oketest_fs3" {
+  #Required
+  availability_domain = data.oci_identity_availability_domains.ADs.availability_domains[1]["name"]
+  compartment_id      = var.compartment_ocid
+}
+resource "oci_file_storage_file_system" "oketest_fs4" {
+  #Required
+  availability_domain = data.oci_identity_availability_domains.ADs.availability_domains[1]["name"]
+  compartment_id      = var.compartment_ocid
+}
+resource "oci_file_storage_file_system" "oketest_fs5" {
+  #Required
+  availability_domain = data.oci_identity_availability_domains.ADs.availability_domains[1]["name"]
+  compartment_id      = var.compartment_ocid
+}
+resource "oci_file_storage_file_system" "oketest_fs6" {
+  #Required
+  availability_domain = data.oci_identity_availability_domains.ADs.availability_domains[1]["name"]
+  compartment_id      = var.compartment_ocid
+}
+resource "oci_file_storage_file_system" "oketest_fs7" {
   #Required
   availability_domain = data.oci_identity_availability_domains.ADs.availability_domains[1]["name"]
   compartment_id      = var.compartment_ocid
 }
 
+resource "oci_file_storage_file_system" "oketest_fs8" {
+  #Required
+  availability_domain = data.oci_identity_availability_domains.ADs.availability_domains[1]["name"]
+  compartment_id      = var.compartment_ocid
+}
+resource "oci_file_storage_file_system" "oketest_fs9" {
+  #Required
+  availability_domain = data.oci_identity_availability_domains.ADs.availability_domains[1]["name"]
+  compartment_id      = var.compartment_ocid
+}
+resource "oci_file_storage_file_system" "oketest_fs10" {
+  #Required
+  availability_domain = data.oci_identity_availability_domains.ADs.availability_domains[1]["name"]
+  compartment_id      = var.compartment_ocid
+}
--- integration-tests/src/test/resources/oke/terraform/twoclusters/export.tf
+++ integration-tests/src/test/resources/oke/terraform/twoclusters/export.tf
@@ -2,10 +2,64 @@
 # Copyright (c) 2020, 2021, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 */
-resource "oci_file_storage_export" "oketest_export" {
+resource "oci_file_storage_export" "oketest_export1" {
   #Required
   export_set_id  = oci_file_storage_export_set.oketest_export_set.id
-  file_system_id = oci_file_storage_file_system.oketest_fs.id
-  path           = "/oketest"
+  file_system_id = oci_file_storage_file_system.oketest_fs1.id
+  path           = "/oketest1"
+}
+resource "oci_file_storage_export" "oketest_export2" {
+  #Required
+  export_set_id  = oci_file_storage_export_set.oketest_export_set.id
+  file_system_id = oci_file_storage_file_system.oketest_fs2.id
+  path           = "/oketest2"
+}
+resource "oci_file_storage_export" "oketest_export3" {
+  #Required
+  export_set_id  = oci_file_storage_export_set.oketest_export_set.id
+  file_system_id = oci_file_storage_file_system.oketest_fs3.id
+  path           = "/oketest3"
+}
+resource "oci_file_storage_export" "oketest_export4" {
+  #Required
+  export_set_id  = oci_file_storage_export_set.oketest_export_set.id
+  file_system_id = oci_file_storage_file_system.oketest_fs4.id
+  path           = "/oketest4"
+}
+resource "oci_file_storage_export" "oketest_export5" {
+  #Required
+  export_set_id  = oci_file_storage_export_set.oketest_export_set.id
+  file_system_id = oci_file_storage_file_system.oketest_fs5.id
+  path           = "/oketest5"
+}
+resource "oci_file_storage_export" "oketest_export6" {
+  #Required
+  export_set_id  = oci_file_storage_export_set.oketest_export_set.id
+  file_system_id = oci_file_storage_file_system.oketest_fs6.id
+  path           = "/oketest6"
+}
+resource "oci_file_storage_export" "oketest_export7" {
+  #Required
+  export_set_id  = oci_file_storage_export_set.oketest_export_set.id
+  file_system_id = oci_file_storage_file_system.oketest_fs7.id
+  path           = "/oketest7"
+}
+resource "oci_file_storage_export" "oketest_export8" {
+  #Required
+  export_set_id  = oci_file_storage_export_set.oketest_export_set.id
+  file_system_id = oci_file_storage_file_system.oketest_fs8.id
+  path           = "/oketest8"
+}
+resource "oci_file_storage_export" "oketest_export9" {
+  #Required
+  export_set_id  = oci_file_storage_export_set.oketest_export_set.id
+  file_system_id = oci_file_storage_file_system.oketest_fs9.id
+  path           = "/oketest9"
+}
+resource "oci_file_storage_export" "oketest_export10" {
+  #Required
+  export_set_id  = oci_file_storage_export_set.oketest_export_set.id
+  file_system_id = oci_file_storage_file_system.oketest_fs10.id
+  path           = "/oketest10"
 }
 
