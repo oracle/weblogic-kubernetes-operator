b8f488ed3a283123a0eeaf3ca438a6e9ab325ad6
Marina Kogan <marina.kogan@oracle.com>
2022-09-09 13:23:39 -0700

    Added more tests to oke runs (#3392)
    * Added more tests to OKE runs

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItDedicatedMode.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItDedicatedMode.java
@@ -64,6 +64,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  */
 @DisplayName("Test Operator and WebLogic domain with Dedicated set to true")
 @Tag("kind-sequential")
+@Tag("oke-sequential")
 @Tag("okd-wls-mrg")
 @IntegrationTest
 @Tag("olcne")
