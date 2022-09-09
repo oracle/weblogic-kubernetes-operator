c20578e2172d6367a72ff0220ea3b5bbb84c10fd
Lenny Phan <lenny.phan@oracle.com>
2022-09-09 13:08:16 -0700

    cleanup code

--- operator/src/main/java/oracle/kubernetes/weblogic/domain/model/DomainSpec.java
+++ operator/src/main/java/oracle/kubernetes/weblogic/domain/model/DomainSpec.java
@@ -1186,7 +1186,6 @@ public class DomainSpec extends BaseConfiguration {
 
     @Override
     public void setReplicaCount(String clusterName, ClusterSpec clusterSpec, int replicaCount) {
-      //clusterSpec.setReplicas(replicaCount);
       Optional.ofNullable(clusterSpec)
               .ifPresentOrElse(cs -> cs.setReplicas(replicaCount), () -> setReplicas(replicaCount));
     }
