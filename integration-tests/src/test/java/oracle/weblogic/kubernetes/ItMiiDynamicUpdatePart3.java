72af1bac36081ecd86874ff69420b90039150669
Marina Kogan <marina.kogan@oracle.com>
2022-09-14 18:39:45 -0700

    fixed expected msg (#3410)
    * fixed expected msg

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItMiiDynamicUpdatePart3.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItMiiDynamicUpdatePart3.java
@@ -260,7 +260,10 @@ class ItMiiDynamicUpdatePart3 {
     if (!WEBLOGIC_SLIM) {
       assertTrue(operatorPodLog.contains("Version check passed"));
     }
-    assertTrue(operatorPodLog.contains("Job " + domainUid + "-introspector has failed"));
+    String expectedMsg = "The domain " + domainUid + " introspect job failed";
+
+    assertTrue(operatorPodLog.contains(expectedMsg), "Can't find expected msg "
+            + expectedMsg + " in the operator log");
     assertTrue(operatorPodLog.contains(MII_DYNAMIC_UPDATE_EXPECTED_ERROR_MSG));
   }
 
