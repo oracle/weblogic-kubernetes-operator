98da8d4482969d356b1ea8304b43d445c1e53c3c
Anil Kedia <anil.kedia@oracle.com>
2022-09-15 18:30:39 +0000

    PR review comment - sanity check for the DOMAIN_INTROSPECTION_COMPLETE constant value to ensure it isn't empty.

--- validateIntrospectorScript.sh
+++ validateIntrospectorScript.sh
@@ -7,6 +7,10 @@ found_error=0
 
 INTROSPECT_DOMAIN_SCRIPT_FILE=$(find "$(pwd -P)" -type f -name "introspectDomain.sh" | tail -n 1)
 DOMAIN_INTROSPECTION_COMPLETE_STRING=$(grep -Er "DOMAIN_INTROSPECTION_COMPLETE" * | grep ProcessingConstants.java | grep -v html | sed 's/.*= //g' | sed 's/;$//g')
+if [ -z "${DOMAIN_INTROSPECTION_COMPLETE_STRING}" ]; then
+  printf "Failed to find the value of DOMAIN_INTROSPECTION_COMPLETE constant in ProcessingConstants.java file.\n"
+  exit 1
+fi
 RESULT=$(grep -E "${DOMAIN_INTROSPECTION_COMPLETE_STRING}" "$INTROSPECT_DOMAIN_SCRIPT_FILE")
 if [ -z "${RESULT}" ]; then
   printf "The script file '%s' must contain phrase %s that matches the DOMAIN_INTROSPECTION_COMPLETE constant value in the operator.\n" \
