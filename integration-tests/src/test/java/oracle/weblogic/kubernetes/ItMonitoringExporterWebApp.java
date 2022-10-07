a6b7c8f3704e7a3985cf6000923d33066d6299d8
Marina Kogan <marina.kogan@oracle.com>
2022-10-07 12:46:55 -0700

    Enabled administration port test for MonitoringExporter (#3490)
    * Enabled administration port test for MonitoringExporter

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItMonitoringExporterWebApp.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItMonitoringExporterWebApp.java
@@ -34,7 +34,6 @@ import oracle.weblogic.kubernetes.utils.ExecResult;
 import oracle.weblogic.kubernetes.utils.MonitoringUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -308,18 +307,21 @@ class ItMonitoringExporterWebApp {
    * Create Model in Image with admin port and ssl enabled.
    * Check generated monitoring exporter WebLogic metrics via https request.
    */
-  @Disabled("Disabled the test due to oracle/weblogic-monitoring-exporter#138")
   @Test
-  @DisplayName("Test Accesability of Monitoring Exporter dashboard and metrics if admin port is enabled.")
+  @DisplayName("Test Accessibility of Monitoring Exporter dashboard and metrics if admin port is enabled.")
   void testAdminPortEnabled() throws Exception {
     try {
       // create and verify one cluster mii domain with admin port enabled
       logger.info("Create domain and verify that it's running");
-      String miiImage1 = MonitoringUtils.createAndVerifyMiiImage(monitoringExporterAppDir,
+      String monitoringExporterAdministrationRestPortAppDir = Paths.get(monitoringExporterAppDir,
+          "administrationrestport").toString();
+      String miiImage1 = MonitoringUtils.createAndVerifyMiiImage(monitoringExporterAdministrationRestPortAppDir,
           MODEL_DIR + "/model-adminportenabled.yaml",
           SESSMIGR_APP_NAME, MONEXP_IMAGE_NAME);
+
       createAndVerifyDomain(miiImage1, domain2Uid, domain2Namespace,
           "FromModel", 2, false, null, null);
+
       logger.info("checking access to wls metrics via https connection");
 
       assertTrue(verifyMonExpAppAccess("wls-exporter",
