661c5b6d243530dbf933ac08961e961d66e2f7bd
Sankar Neelakandan <sankar.periyathambi.neelakand@oracle.com>
2022-11-09 11:43:00 -0800

    port prs 3533,3579 to main

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/actions/impl/Domain.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/actions/impl/Domain.java
@@ -33,6 +33,7 @@ import oracle.weblogic.kubernetes.utils.FileUtils;
 import static oracle.weblogic.kubernetes.TestConstants.ADMIN_PASSWORD_DEFAULT;
 import static oracle.weblogic.kubernetes.TestConstants.ADMIN_SERVER_NAME_BASE;
 import static oracle.weblogic.kubernetes.TestConstants.ADMIN_USERNAME_DEFAULT;
+import static oracle.weblogic.kubernetes.TestConstants.OKD;
 import static oracle.weblogic.kubernetes.TestConstants.PROJECT_ROOT;
 import static oracle.weblogic.kubernetes.TestConstants.RESULTS_ROOT;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.RBAC_API_GROUP;
@@ -57,6 +58,7 @@ import static oracle.weblogic.kubernetes.utils.OKDUtils.getRouteHost;
 import static oracle.weblogic.kubernetes.utils.ThreadSafeLogger.getLogger;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Domain {
@@ -457,9 +459,11 @@ public class Domain {
         secretName, opServiceAccount, opNamespace, secretToken);
 
     // decode the secret encoded token
-    String decodedToken = new String(Base64.getDecoder().decode(secretToken));
+    String decodedToken = OKD ? secretToken : new String(Base64.getDecoder().decode(secretToken));
     logger.info("Got decoded token for secret {0} associated with service account {1} in namespace {2}: {3}",
         secretName, opServiceAccount, opNamespace, decodedToken);
+    
+    assertNotNull(decodedToken, "Couldn't get secret, token is null");
 
     // build the curl command to scale the cluster
     String command = new StringBuffer()
