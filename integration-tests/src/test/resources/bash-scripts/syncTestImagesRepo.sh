44626855a142c0f8c38e8f07224af44b00e060f6
Antaryami Panigrahi <antaryami.panigrahi@oracle.com>
2022-12-09 22:56:32 +0000

    Modify scripts to upload cpu images

--- integration-tests/src/test/resources/bash-scripts/syncTestImagesRepo.sh
+++ integration-tests/src/test/resources/bash-scripts/syncTestImagesRepo.sh
@@ -5,55 +5,89 @@
 set -eu
 set -o pipefail
 
-repoLogin() {
+dockerLogin() {
 
-echo "${WLSIMG_BUILDER:-docker} login to src ${SOURCE_REPO}"
+echo "docker login to src ${SOURCE_REPO}"
 echo ${SOURCE_PASSWORD} > pwd.txt
-cat pwd.txt | ${WLSIMG_BUILDER:-docker} login ${SOURCE_REPO} -u ${SOURCE_USER} --password-stdin 
+cat pwd.txt | docker login ${SOURCE_REPO} -u ${SOURCE_USER} --password-stdin 
 rm -rf pwd.txt
 
 # Alternatively use 
-# ${WLSIMG_BUILDER:-docker} login ${SOURCE_REPO} -u ${SOURCE_USER} -p  ${SOURCE_PASSWORD}
+# docker login ${SOURCE_REPO} -u ${SOURCE_USER} -p  ${SOURCE_PASSWORD}
 
-echo "${WLSIMG_BUILDER:-docker} login to target ${TARGET_REPO}"
+echo "docker login to target ${TARGET_REPO}"
 echo ${TARGET_PASSWORD} > pwd.txt
-cat pwd.txt | ${WLSIMG_BUILDER:-docker} login ${TARGET_REPO} -u ${TARGET_USER} --password-stdin
+cat pwd.txt | docker login ${TARGET_REPO} -u ${TARGET_USER} --password-stdin
 rm -rf pwd.txt
 
 }
 
-repoPullPushImage() {
+dockerPullPushImage() {
  
  # Here the source image contains absolute image path
  # and source image contains relative path wrt to TARGET_REPO
 
- src_image="${1}"
+ src_image="${SOURCE_REPO}/${1}"
  tgt_image="${TARGET_REPO}/${2}"
 
- if [ ${DRY_RUN} == "true" ]; then 
-   echo "Executing a dry run ..."
-   echo "${WLSIMG_BUILDER:-docker} pull ${src_image} "
-   echo "${WLSIMG_BUILDER:-docker} tag  ${src_image} ${tgt_image} "
-   echo "${WLSIMG_BUILDER:-docker} push ${tgt_image} "
- else 
-   printf 'SRC[%s] TARGET[%s] \n' "${src_image}" "${tgt_image}"
-   ${WLSIMG_BUILDER:-docker} pull ${src_image}
-   ${WLSIMG_BUILDER:-docker} tag  ${src_image} ${tgt_image}
-   ${WLSIMG_BUILDER:-docker} push ${tgt_image}
+ image=$(echo ${src_image} | awk -F"/" '{print $NF}')
 
-   ${WLSIMG_BUILDER:-docker} rmi -f ${src_image} 
-   ${WLSIMG_BUILDER:-docker} rmi -f ${tgt_image} 
+ #printf 'SOURCE[%s] \n' "${src_image}" >> ${OUT}
+ #printf 'TARGET[%s] \n' "${tgt_image}" >> ${OUT}
+
+ docker pull ${src_image} || true
+ sid=$(docker images ${src_image} -q)
+ if [ "x$sid" == "x" ] ; then
+     printf 'Could not download Source Image [%s] \n' ${src_image}
+     exit -1 
+ fi
+
+ docker pull ${tgt_image} || true 
+ tid=$(docker images ${tgt_image} -q)
+ if [ -z ${tid} ]; then
+   printf 'Could not download Target Image [%s] \n' ${tgt_image}
+   if [ ${DRY_RUN} != "true" ]; then
+     docker tag  ${src_image} ${tgt_image}
+     docker push ${tgt_image}
+     docker rmi -f ${src_image} 
+     docker rmi -f ${tgt_image} 
+     printf 'MISSING Uploaded missing [%s] to Target \n' "${image}" >> ${OUT}
+   else
+     printf 'MISSING [%s] image on Target Repositoty \n' "${image}" >> ${OUT}
+   fi 
  fi
    
+ # Compare the images id(s)
+ # Update the image if needed based on id comparision 
+ if [ ! -z ${tid} ] && [ ! -z ${sid} ]; then
+   #printf 'SOURCE_IMAGEID[%s] TARGET_IMAGEID[%s]\n' "${sid}" "${tid}" >> ${OUT}
+   if [ ${tid} == ${sid} ]; then
+      printf 'SKIP [%s] image is up-to-date \n' "${image}"  >> ${OUT}
+   else 
+     td=$(date "+%Y-%m-%d")
+     printf 'UPDATE [%s] image updated \n' "${image}"  >> ${OUT}
+     if [ ${DRY_RUN} != "true" ]; then
+       printf 'Updating  image [%s] on Target \n' "${image}" >> ${OUT}
+       docker rmi -f ${tgt_image} 
+       docker tag  ${src_image} ${tgt_image}
+       docker push ${tgt_image}
+       docker rmi -f ${src_image} 
+       docker rmi -f ${tgt_image} 
+     else 
+      printf 'UPDATE [%s] image updated \n' "${image}"  >> ${OUT}
+     fi
+  fi 
+ fi
+ printf '\n'  >> ${OUT}
 }
 
-repoPullPushImages() {
+dockerPullPushImages() {
  file="images.properties"
- grep -E -v '^#' $file | grep -v "^$" |
+ grep -E -v '^#' ${PROP_FILE} | grep -v "^$" |
    while IFS=";" read -r f1 f2 
    do
-     # printf 'Source Location : [%s], Target: [%s] \n' "$f1" "$f2"
-     repoPullPushImage $f1 $f2 
+     #printf 'Source Location : [%s], Target: [%s] \n' "$f1" "$f2"
+     dockerPullPushImage $f1 $f2 
    done
  }
 
@@ -61,10 +95,13 @@ repoPullPushImages() {
 
 if [ $# -ge 1 ]; then
  echo "Export the following Environment varaiables before running the script"
- echo "  SOURCE_REPO(ocr),  SOURCE_USER, SOURCE_PASSWORD (Base Image repository)"
- echo "  TARGET_REPO(ocir), TARGET_USER, TARGET_PASSWORD (Target Image repository)"
+ echo "  SOURCE_REPO(ocr),SOURCE_USER,SOURCE_PASSWORD (Source Image repository)"
+ echo "  TARGET_REPO(ocir),TARGET_USER,TARGET_PASSWORD(Target Image repository)"
  exit 0
 fi
+
+OUT=update.out
+echo -e "#### Image Upgrade Status on [`date`] \n" > ${OUT}
 
 SOURCE_REPO=${SOURCE_REPO:-container-registry.oracle.com}
 SOURCE_USER=${SOURCE_USER:-oracle}
@@ -75,6 +112,14 @@ TARGET_USER=${TARGET_USER:-oracle}
 TARGET_PASSWORD=${TARGET_PASSWORD:-changeme}
 
 DRY_RUN=${DRY_RUN:-false}
+PROP_FILE=${PROP_FILE:-images.properties}
+
+if [ -f ${PROP_FILE} ]; then
+  echo "Loading the Image properties file [${PROP_FILE}]"
+else
+  echo "Could not load Image properties file [${PROP_FILE}]"
+  exit -1
+fi
 
 echo "SOURCE_REPO[$SOURCE_REPO] and TARGET_REPO[${TARGET_REPO}]"
 
@@ -83,5 +128,6 @@ if [ ${SOURCE_REPO} == ${TARGET_REPO} ]; then
   exit -1
 fi
 
-repoLogin 
-repoPullPushImages
+dockerLogin 
+dockerPullPushImages
+cat ${OUT}
