a87053f0a5a3a2d816a5d25b625ed8160a53a5fa
xiancao <xian.cao@oracle.com>
2022-12-21 19:10:36 +0000

    test callpyscript.sh

--- integration-tests/src/test/resources/bash-scripts/callpyscript.sh
+++ integration-tests/src/test/resources/bash-scripts/callpyscript.sh
@@ -3,9 +3,10 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 ARG="$*"
-ENVSCRIPT=`find /u01 . -name setDomainEnv.sh -print`
-echo Sourcing $ENVSCRIPT
-. $ENVSCRIPT || exit 1
+#ENVSCRIPT=`find /u01 . -name setDomainEnv.sh -print`
+#echo Sourcing $ENVSCRIPT
+#. $ENVSCRIPT || exit 1
+. $DOMAIN_HOME/bin/setDomainEnv.sh
 echo "$@"
 echo Calling java weblogic.WLST $ARG
 eval java weblogic.WLST $ARG || exit 1
