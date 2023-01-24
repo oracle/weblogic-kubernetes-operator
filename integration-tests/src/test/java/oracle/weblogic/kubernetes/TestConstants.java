1f2b97a279df4722ecfc3940332b5720c68d017c
Marina Kogan <marina.kogan@oracle.com>
2023-01-24 04:53:21 -0800

    backort changes from main for  monexp update (#3881)
    * backport changes from main for  WME update

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/TestConstants.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/TestConstants.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2020, 2022, Oracle and/or its affiliates.
+// Copyright (c) 2020, 2023, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package oracle.weblogic.kubernetes;
@@ -262,7 +262,7 @@ public interface TestConstants {
 
   //monitoring constants
   public static final String MONITORING_EXPORTER_WEBAPP_VERSION =
-      getNonEmptySystemProperty("wko.it.monitoring.exporter.webapp.version", "2.1.0");
+      getNonEmptySystemProperty("wko.it.monitoring.exporter.webapp.version", "2.1.1");
   public static final String MONITORING_EXPORTER_BRANCH =
       getNonEmptySystemProperty("wko.it.monitoring.exporter.branch", "main");
   public static final String PROMETHEUS_CHART_VERSION =
