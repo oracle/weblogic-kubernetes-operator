6565b1f271e999117140c2e5066541044f8d4444
Dongbo Xiao <dongbo.xiao@oracle.com>
2023-02-14 16:22:56 -0500

    Ignore NS deleted events for not managed namespaces

--- operator/src/main/java/oracle/kubernetes/operator/DomainNamespaces.java
+++ operator/src/main/java/oracle/kubernetes/operator/DomainNamespaces.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2020, 2022, Oracle and/or its affiliates.
+// Copyright (c) 2020, 2023, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package oracle.kubernetes.operator;
@@ -73,6 +73,10 @@ public class DomainNamespaces {
 
   AtomicBoolean isStopping(String ns) {
     return namespaceStoppingMap.computeIfAbsent(ns, key -> new AtomicBoolean(false));
+  }
+
+  AtomicBoolean getStopping(String ns) {
+    return namespaceStoppingMap.get(ns);
   }
 
   boolean isStarting(String ns) {
