421681462e1dfd2be7ff9721b292b001b7735047
Johnny Shum <johnny.shum@oracle.com>
2023-02-14 18:47:36 -0600

    Port fix model without anyservers (#3978)
    * fix wdt model doesn't have any servers defined

--- operator/src/main/resources/scripts/model-wdt-create-filter.py
+++ operator/src/main/resources/scripts/model-wdt-create-filter.py
@@ -7,32 +7,45 @@
 #  This is a WDT filter for primordial domain creation. It filters out all resources and
 #  apps deployments, leaving only the domainInfo and admin server in topology.
 #
+import sys, traceback
+import utils
+
+
 def filter_model(model):
-	if model and 'topology' in model:
-            topology = model['topology']
-            if 'AdminServerName' in model['topology']:
-                admin_server = topology['AdminServerName']
-            else:
-                # weblogic default
-                admin_server = 'AdminServer'
-            model['topology'] = {}
-            model['topology']['AdminServerName'] = admin_server
-            model['topology']['Server'] = {}
-            if admin_server in topology['Server']:
-                model['topology']['Server'][admin_server] = topology['Server'][admin_server]
-            else:
-                model['topology']['Server'][admin_server] = {}
+  try:
+    if model and 'topology' in model:
+      topology = model['topology']
+      if 'AdminServerName' in model['topology']:
+        admin_server = topology['AdminServerName']
+      else:
+        # weblogic default
+        admin_server = 'AdminServer'
+      model['topology'] = {}
+      model['topology']['AdminServerName'] = admin_server
+      model['topology']['Server'] = {}
 
-            if 'Name' in topology:
-                model['topology']['Name'] = topology['Name']
+      # cover the odd case that the model doesn't have any server!
+      if 'Server' in topology and admin_server in topology['Server']:
+        model['topology']['Server'][admin_server] = topology['Server'][admin_server]
+      else:
+        if 'Server' not in topology:
+          model['topology']['Server'] = {}
+        model['topology']['Server'][admin_server] = {}
 
-            if 'Security' in topology:
-                model['topology']['Security'] = topology['Security']
+      if 'Name' in topology:
+        model['topology']['Name'] = topology['Name']
 
+      if 'Security' in topology:
+        model['topology']['Security'] = topology['Security']
 
-	if model and 'appDeployments' in model:
-            model['appDeployments'] = {}
+    if model and 'appDeployments' in model:
+      model['appDeployments'] = {}
 
-	if model and 'resources' in model:
-            model['resources'] = {}
+    if model and 'resources' in model:
+      model['resources'] = {}
 
+  except:
+    exc_type, exc_obj, exc_tb = sys.exc_info()
+    ee_string = traceback.format_exception(exc_type, exc_obj, exc_tb)
+    utils.trace('SEVERE', 'Error in applying MII filter:\n ' + str(ee_string))
+    raise
