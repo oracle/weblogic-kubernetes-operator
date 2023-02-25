e217eabefc7b1d35e8de7035544cec72f2c1732d
Ryan Eberhard <ryan.eberhard@oracle.com>
2023-02-25 11:18:22 -0500

    Work in progress

--- operator/src/main/resources/scripts/stopServer.sh
+++ operator/src/main/resources/scripts/stopServer.sh
@@ -23,16 +23,33 @@ STOP_OUT_FILE="${serverLogHome}/${SERVER_NAME}.stop.out"
 SHUTDOWN_MARKER_FILE="${serverLogHome}/${SERVER_NAME}.shutdown"
 SERVER_PID_FILE="${serverLogHome}/${SERVER_NAME}.pid"
 
-
 logFileRotate ${STOP_OUT_FILE} ${STOP_OUT_FILE_MAX:-11}
+
+exit_script () {
+  trace "Exit script" &>> ${STOP_OUT_FILE}
+
+  # K8S does not print any logs in the PreStop stage in kubectl logs.
+  # The workaround is to print script's output to the main process' stdout using /proc/1/fd/1
+  # See https://github.com/kubernetes/kubernetes/issues/25766 for more details.
+  trace "Stop Server: === Contents of the script output file ${STOP_OUT_FILE} ===" > /proc/1/fd/1
+  cat ${STOP_OUT_FILE} >> /proc/1/fd/1
+  trace "Stop Server: === End of ${STOP_OUT_FILE} contents. ===" >> /proc/1/fd/1
+
+  touch ${SHUTDOWN_MARKER_FILE}
+
+  if [ -f ${SERVER_PID_FILE} ]; then
+    kill -2 $(< ${SERVER_PID_FILE} )
+  fi
+
+  exit $1
+}
 
 trace "Stop server ${SERVER_NAME}" &> ${STOP_OUT_FILE}
 
 checkEnv SERVER_NAME || exit 1
 
 if [ "${MOCK_WLS}" == 'true' ]; then
-  touch ${SHUTDOWN_MARKER_FILE}
-  exit 0
+  exit_script 0
 fi
 
 # Arguments for shutdown
@@ -46,8 +63,8 @@ export SHUTDOWN_TIMEOUT_ARG=${SHUTDOWN_TIMEOUT:-30}
 # Calculate the wait timeout to issue "kill -9" before the pod is destroyed. 
 # Allow 3 seconds for the NFS v3 manager to detect the process destruction and release file locks.
 export SIGKILL_WAIT_TIMEOUT=$(expr $SHUTDOWN_TIMEOUT_ARG - 3)
-wait_and_kill_after_timeout(){
-  trace "Wait for ${SIGKILL_WAIT_TIMEOUT} seconds for ${SERVER_NAME} to shut down." >> ${STOP_OUT_FILE}
+wait_and_kill_after_timeout () {
+  trace "Wait up to ${SIGKILL_WAIT_TIMEOUT} seconds for ${SERVER_NAME} to shut down." >> ${STOP_OUT_FILE}
   sleep ${SIGKILL_WAIT_TIMEOUT}
   trace "The server ${SERVER_NAME} didn't shut down in ${SIGKILL_WAIT_TIMEOUT} seconds, " \
         "killing the server processes." >> ${STOP_OUT_FILE}
@@ -55,13 +72,13 @@ wait_and_kill_after_timeout(){
   adjustPath
 
   kill -9 `jps -v | grep -v Jps | awk '{ print $1 }'`
-  touch ${SHUTDOWN_MARKER_FILE}
+  exit_script 0
 }
 
 # Wait for the timeout value to issue "kill -9" and then kill the WebLogic server processes.
 wait_and_kill_after_timeout &
 
-check_for_shutdown() {
+check_for_shutdown () {
   [ ! -f "${SCRIPTPATH}/readState.sh" ] && trace SEVERE "Missing file '${SCRIPTPATH}/readState.sh'." && exit 1
 
   state=`${SCRIPTPATH}/readState.sh`
@@ -93,7 +110,6 @@ check_for_shutdown() {
     adjustPath
 
     kill -9 `jps -v | grep " -Dweblogic.Name=${SERVER_NAME} " | awk '{ print $1 }'`
-    touch ${SHUTDOWN_MARKER_FILE}
     return 0
   fi
 
@@ -104,7 +120,7 @@ check_for_shutdown() {
 
 # Check if the server is already shutdown
 check_for_shutdown
-[ $? -eq 0 ] && trace "Server is already shutting down, is shutdown or failed" &>>  ${STOP_OUT_FILE} && exit 0
+[ $? -eq 0 ] && trace "Server is already shutting down, is shutdown or failed" &>>  ${STOP_OUT_FILE} && exit_script 0
 
 # Otherwise, connect to and stop the server instance
 [ ! -f "${SCRIPTPATH}/wlst.sh" ] && trace SEVERE "Missing file '${SCRIPTPATH}/wlst.sh'." && exit 1
@@ -122,17 +138,4 @@ if [ ! -z $pid ]; then
   kill -15 $pid
 fi
 
-touch ${SHUTDOWN_MARKER_FILE}
-
-if [ -f ${SERVER_PID_FILE} ]; then
-  kill -2 $(< ${SERVER_PID_FILE} )
-fi
-
-trace "Exit script" &>> ${STOP_OUT_FILE}
-
-# K8S does not print any logs in the PreStop stage in kubectl logs.
-# The workaround is to print script's output to the main process' stdout using /proc/1/fd/1
-# See https://github.com/kubernetes/kubernetes/issues/25766 for more details.
-trace "Stop Server: === Contents of the script output file ${STOP_OUT_FILE} ===" > /proc/1/fd/1
-cat ${STOP_OUT_FILE} >> /proc/1/fd/1
-trace "Stop Server: === End of ${STOP_OUT_FILE} contents. ===" >> /proc/1/fd/1
+exit_script 0
