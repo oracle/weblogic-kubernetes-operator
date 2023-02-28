7a788102e4243bfdd3f85422a49932bcba546ccd
Xian Cao <xian.cao@oracle.com>
2023-02-27 16:10:23 -0800

    Add Cluster Resource tests into olcne profile (#4005)
    * add cluster resource tests to olcne

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItConsensusLeasingValidation.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItConsensusLeasingValidation.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2022, Oracle and/or its affiliates.
+// Copyright (c) 2022, 2023, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package oracle.weblogic.kubernetes;
@@ -38,6 +38,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @IntegrationTest
 @Tag("kind-parallel")
 @Tag("oke-parallel")
+@Tag("olcne")
 class ItConsensusLeasingValidation {
 
   private static String opNamespace = null;
