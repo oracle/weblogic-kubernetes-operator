406ecec729a45a2cf1b5f209261e6a56621b71e7
Marina Kogan <marina.kogan@oracle.com>
2023-03-01 00:28:00 +0000

    added fix1

--- integration-tests/src/test/resources/bash-scripts/install-istio.sh
+++ integration-tests/src/test/resources/bash-scripts/install-istio.sh
@@ -43,7 +43,7 @@ ${KUBERNETES_CLI} create namespace istio-system
  echo "Set the image registry to gcr.io/istio-release during istio installation"
 ( cd ${istiodir}
   bin/istioctl x precheck
-  bin/istioctl install --set meshConfig.enablePrometheusMerge=false --set values.global.imagePullSecrets[0]=docker-istio-secret --set hub=gcr.io/istio-release --set components.cni.enabled=true --set profile=demo -y
+  bin/istioctl install --set meshConfig.enablePrometheusMerge=true --set values.global.imagePullSecrets[0]=docker-istio-secret --set hub=gcr.io/istio-release --set components.cni.enabled=true --set profile=demo -y
   bin/istioctl verify-install
   bin/istioctl version
 )
