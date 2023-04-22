8086d6b4d8771b186b1cc822e05fd013dbe39201
Robert Patrick <robert.patrick@oracle.com>
2023-04-22 10:34:28 -0500

    checkstyle errors

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/TestConstants.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/TestConstants.java
@@ -118,8 +118,8 @@ public interface TestConstants {
 
   // Get DB_IMAGE_NAME/DB_IMAGE_TAG from env var, if its not provided and
   // if base images repo is OCIR use OCIR default image values
-  public static final String DB_IMAGE_NAME = BASE_IMAGES_REPO + "/" + BASE_IMAGES_TENANCY + "/" +
-      getNonEmptySystemProperty("wko.it.db.image.name", DB_IMAGE_NAME_DEFAULT);
+  public static final String DB_IMAGE_NAME = BASE_IMAGES_REPO + "/" + BASE_IMAGES_TENANCY + "/"
+      + getNonEmptySystemProperty("wko.it.db.image.name", DB_IMAGE_NAME_DEFAULT);
   public static final String DB_IMAGE_TAG = getNonEmptySystemProperty("wko.it.db.image.tag", DB_IMAGE_TAG_DEFAULT);
 
   // WebLogic Base Image with Japanese Locale
