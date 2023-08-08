c63a52208fa8c00e3d42dae17bcbfcec3c765d39
Ryan Eberhard <ryan.eberhard@oracle.com>
2023-08-08 17:59:58 -0400

    Harden liveness probe by checking for server process or any failed state in state file

--- operator/src/main/resources/scripts/livenessProbe.sh
+++ operator/src/main/resources/scripts/livenessProbe.sh
@@ -73,13 +73,13 @@ if [ "${MOCK_WLS}" != 'true' ]; then
   # Adjust PATH if necessary before calling jps
   adjustPath
 
-  if [ `jps -l | grep -c " weblogic.NodeManager"` -eq 0 ]; then
-    trace SEVERE "WebLogic NodeManager process not found."
+  if [ `jps -v | grep -c " -Dweblogic.Name=${SERVER_NAME} "` -eq 0 ]; then
+    trace SEVERE "WebLogic Server instance process not found."
     exit $RETVAL
   fi
 fi
 
-if [ -f ${STATEFILE} ] && [ `grep -c "FAILED_NOT_RESTARTABLE" ${STATEFILE}` -eq 1 ]; then
+if [ -f ${STATEFILE} ] && [ `grep -c "FAILED" ${STATEFILE}` -eq 1 ]; then
   # WARNING: This state file check is essentially a public API and 
   #          must continue to be honored even if we remove the node
   #          manager from the life cycle.
@@ -88,7 +88,7 @@ if [ -f ${STATEFILE} ] && [ `grep -c "FAILED_NOT_RESTARTABLE" ${STATEFILE}` -eq 
   #          the file to FAILED_NOT_RESTARTABLE to force a liveness
   #          failure when the user detects that their applications
   #          are unresponsive.)
-  trace SEVERE "WebLogic Server state is FAILED_NOT_RESTARTABLE."
+  trace SEVERE "WebLogic Server state is " `cat ${STATEFILE} | cut -f 1 -d ':'` "."
   exit $RETVAL
 fi
 
