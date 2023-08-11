2e09d62fd5c7dd46570503a60752f67e59d531dd
ANTARYAMI.PANIGRAHI <antaryami.panigrahi@oracle.com>
2023-08-11 12:13:03 +0000

    Add WKO Upgrade Test from release/4.1.0 and release/4.0.7

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItOperatorWlsUpgrade.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItOperatorWlsUpgrade.java
@@ -196,9 +196,20 @@ class ItOperatorWlsUpgrade {
   }
 
   /**
-   * Operator upgrade from 4.0.6 to current.
+   * Operator upgrade from 4.0.7 to current.
    */
   @Tag("oke-sequential")
+  @ParameterizedTest
+  @DisplayName("Upgrade Operator from 4.0.7 to current")
+  @ValueSource(strings = { "Image", "FromModel" })
+  void testOperatorWlsUpgradeFrom407ToCurrent(String domainType) {
+    logger.info("Starting test testOperatorWlsUpgradeFrom407ToCurrent with domain type {0}", domainType);
+    installAndUpgradeOperator(domainType, "4.0.7", OLD_DOMAIN_VERSION, DEFAULT_EXTERNAL_SERVICE_NAME_SUFFIX);
+  }
+
+  /**
+   * Operator upgrade from 4.0.6 to current.
+   */
   @ParameterizedTest
   @DisplayName("Upgrade Operator from 4.0.6 to current")
   @ValueSource(strings = { "Image", "FromModel" })
@@ -208,15 +219,15 @@ class ItOperatorWlsUpgrade {
   }
 
   /**
-   * Operator upgrade from 4.0.5 to current.
+   * Operator upgrade from 4.1.0 to current.
    */
   @Tag("oke-sequential")
   @ParameterizedTest
-  @DisplayName("Upgrade Operator from 4.0.5 to current")
+  @DisplayName("Upgrade Operator from 4.1.0 to current")
   @ValueSource(strings = { "Image", "FromModel" })
-  void testOperatorWlsUpgradeFrom405ToCurrent(String domainType) {
-    logger.info("Starting test testOperatorWlsUpgradeFrom405ToCurrent with domain type {0}", domainType);
-    installAndUpgradeOperator(domainType, "4.0.5", OLD_DOMAIN_VERSION, DEFAULT_EXTERNAL_SERVICE_NAME_SUFFIX);
+  void testOperatorWlsUpgradeFrom410ToCurrent(String domainType) {
+    logger.info("Starting test testOperatorWlsUpgradeFrom410ToCurrent with domain type {0}", domainType);
+    installAndUpgradeOperator(domainType, "4.1.0", OLD_DOMAIN_VERSION, DEFAULT_EXTERNAL_SERVICE_NAME_SUFFIX);
   }
 
   /**
