691ec5a504582d00e6e943a90cd9ba1834608803
Ryan Eberhard <ryan.eberhard@oracle.com>
2023-08-15 16:17:33 -0400

    Remove member from stateless logging formatter

--- common/src/main/java/oracle/kubernetes/common/logging/CommonLoggingFormatter.java
+++ common/src/main/java/oracle/kubernetes/common/logging/CommonLoggingFormatter.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2022, Oracle and/or its affiliates.
+// Copyright (c) 2022, 2023, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package oracle.kubernetes.common.logging;
@@ -9,16 +9,6 @@ import java.util.logging.LogRecord;
 
 /** Common log formatter to format log messages in JSON format. */
 public class CommonLoggingFormatter extends BaseLoggingFormatter<Object> {
-
-  @Override
-  protected Object getCurrentFiberIfSet() {
-    return null;
-  }
-
-  @Override
-  protected String getFiber() {
-    return null;
-  }
 
   @Override
   protected String getDomainUid(Object fiber) {
