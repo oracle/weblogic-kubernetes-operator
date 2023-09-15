06852c01a04652992fe1a4aecda985787dc06c47
Sankar Periyathambi neelakandan <sankar.periyathambi.neelakand@oracle.com>
2023-09-15 19:00:17 +0000

    Backport MR 4435 to release/4.1

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItWlsDomainOnPVSample.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItWlsDomainOnPVSample.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import oracle.weblogic.kubernetes.actions.impl.UniqueName;
 import oracle.weblogic.kubernetes.actions.impl.primitive.Command;
 import oracle.weblogic.kubernetes.actions.impl.primitive.CommandParams;
 import oracle.weblogic.kubernetes.annotations.IntegrationTest;
@@ -45,7 +46,9 @@ import static oracle.weblogic.kubernetes.actions.ActionConstants.WIT_JAVA_HOME;
 import static oracle.weblogic.kubernetes.actions.TestActions.imagePull;
 import static oracle.weblogic.kubernetes.actions.TestActions.imagePush;
 import static oracle.weblogic.kubernetes.actions.TestActions.imageTag;
+import static oracle.weblogic.kubernetes.utils.CommonTestUtils.backupReports;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.getUniqueName;
+import static oracle.weblogic.kubernetes.utils.CommonTestUtils.restoreReports;
 import static oracle.weblogic.kubernetes.utils.ImageUtils.createBaseRepoSecret;
 import static oracle.weblogic.kubernetes.utils.ImageUtils.createTestRepoSecret;
 import static oracle.weblogic.kubernetes.utils.SampleUtils.createPVHostPathAndChangePermissionInKindCluster;
@@ -152,7 +155,9 @@ class ItWlsDomainOnPVSample {
   @Test
   @Order(1)
   public void testInstallOperator() {
+    String backupReports = backupReports(UniqueName.uniqueName(this.getClass().getSimpleName()));    
     execTestScriptAndAssertSuccess("-oper", "Failed to run -oper");
+    restoreReports(backupReports);
   }
 
   /**
