ac47e606d020dc98dfdc2f2ee640db8fc26eed6e
maggie_he <maggie.he@oracle.com>
2023-10-31 18:28:23 +0000

    Backport remote console installation fix to release/4.0

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/CommonTestUtils.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/CommonTestUtils.java
@@ -64,7 +64,6 @@ import static oracle.weblogic.kubernetes.actions.ActionConstants.APP_DIR;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.MODEL_DIR;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.REMOTECONSOLE;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.REMOTECONSOLE_DOWNLOAD_FILENAME_DEFAULT;
-import static oracle.weblogic.kubernetes.actions.ActionConstants.REMOTECONSOLE_DOWNLOAD_URL_DEFAULT;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.RESOURCE_DIR;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.SNAKE;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.SNAKE_DOWNLOADED_FILENAME;
@@ -1799,6 +1798,8 @@ public class CommonTestUtils {
     String actualLocation = location;
     if (needToGetActualLocation(location, type)) {
       actualLocation = location + "/download/" + getInstallerFileName(type);
+    } else if (!needToGetActualLocation(location, type) && type.equalsIgnoreCase(REMOTECONSOLE)) {
+      actualLocation = location + getInstallerFileName(type);
     }
     getLogger().info("The actual download location for {0} is {1}", type, actualLocation);
     return actualLocation;
@@ -1814,8 +1815,6 @@ public class CommonTestUtils {
         return WIT_DOWNLOAD_URL_DEFAULT.equals(location);
       case WLE:
         return WLE_DOWNLOAD_URL_DEFAULT.equals(location);
-      case REMOTECONSOLE:
-        return REMOTECONSOLE_DOWNLOAD_URL_DEFAULT.equals(location);
       default:
         return false;
     }
