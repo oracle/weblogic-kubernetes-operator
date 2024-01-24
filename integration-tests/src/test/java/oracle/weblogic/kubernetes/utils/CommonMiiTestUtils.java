2116d7d0d9ba2425d30a5d5bd7fbb8c1367c5713
Sankar Periyathambi neelakandan <sankar.periyathambi.neelakand@oracle.com>
2024-01-24 20:38:56 +0000

    add -g option for curl command

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/CommonMiiTestUtils.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/CommonMiiTestUtils.java
@@ -811,7 +811,7 @@ public class CommonMiiTestUtils {
                 .runtimeEncryptionSecret(encryptionSecretName)
                 .onlineUpdate(new OnlineUpdate()
                     .enabled(onlineUpdateEnabled)))
-            .introspectorJobActiveDeadlineSeconds(300L));
+            .introspectorJobActiveDeadlineSeconds(600L));
 
     if (setDataHome) {
       domainSpec.dataHome(uniquePath + "/data");
