7cffccf1fd5aed0eaec7892108b42d8d752426f8
jshum <johnny.shum@oracle.com>
2024-03-13 09:56:53 +0800

    Test EU commission fix and integration test fix

--- operator/src/test/java/oracle/kubernetes/operator/helpers/JobHelperTest.java
+++ operator/src/test/java/oracle/kubernetes/operator/helpers/JobHelperTest.java
@@ -1470,7 +1470,7 @@ class JobHelperTest extends DomainValidationTestBase {
         .withLogHome("/share/logs/domain1")
         .withDomainHomeSourceType(DomainSourceType.PERSISTENT_VOLUME)
         .withAdditionalVolume("volume1", VOLUME_PATH_1)
-        .withAdditionalVolumeMount("volume1Mount", VOLUME_MOUNT_PATH_1)
+        .withAdditionalVolumeMount("dhmountpoint", "/share")
         .withInitializeDomainOnPV(new InitializeDomainOnPV()
             .domain(new DomainOnPV().createMode(CreateIfNotExists.DOMAIN_AND_RCU)
                 .domainType(JRF)
@@ -1487,7 +1487,7 @@ class JobHelperTest extends DomainValidationTestBase {
             .map(V1Container::getVolumeMounts).orElse(Collections.emptyList()).stream()
             .map(V1VolumeMount::getMountPath)
             .collect(Collectors.toList()),
-          hasItems(SCRIPTS_MOUNTS_PATH, "/tmpAuxiliaryImage", VOLUME_MOUNT_PATH_1));
+          hasItems(SCRIPTS_MOUNTS_PATH, "/share"));
 
   }
 
