8ab83839806898e7cf64335c025c9edf94affdb0
xian_cao <xian.cao@oracle.com>
2024-10-16 19:40:51 +0000

    workaround OWLS-122679 - do not create WDT base image in InitializationTask in crio-pipeline and OCNE

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/extensions/InitializationTasks.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/extensions/InitializationTasks.java
@@ -240,7 +240,7 @@ public class InitializationTasks implements BeforeAllCallback, ExtensionContext.
 
         logger.info("Build/Check wdt-basic image with tag {0}", WDT_BASIC_IMAGE_TAG);
         // build WDT basic image if does not exits
-        if (! imageExists(WDT_BASIC_IMAGE_NAME, WDT_BASIC_IMAGE_TAG)) {
+        if (!imageExists(WDT_BASIC_IMAGE_NAME, WDT_BASIC_IMAGE_TAG) && !CRIO && !OCNE) {
           logger.info("Building wdt-basic image {0}", wdtBasicImage);
           testUntil(
                 withVeryLongRetryPolicy,
@@ -249,7 +249,7 @@ public class InitializationTasks implements BeforeAllCallback, ExtensionContext.
                 logger,
                 "createBasicImage to be successful");
         } else {
-          logger.info("!!!! domain image {0} exists !!!!", wdtBasicImage);
+          logger.info("!!!! domain image {0} exists !!!! or env is not OCNE based", wdtBasicImage);
         }
 
         /* Check image exists using WLSIMG_BUILDER images | grep image tag.
@@ -261,8 +261,10 @@ public class InitializationTasks implements BeforeAllCallback, ExtensionContext.
         assertTrue(doesImageExist(MII_BASIC_IMAGE_TAG),
               String.format("Image %s doesn't exist", miiBasicImage));
 
-        assertTrue(doesImageExist(WDT_BASIC_IMAGE_TAG),
+        if (!CRIO && !OCNE) {
+          assertTrue(doesImageExist(WDT_BASIC_IMAGE_TAG),
               String.format("Image %s doesn't exist", wdtBasicImage));
+        }
 
         logger.info(WLSIMG_BUILDER + " login");
         testUntil(withVeryLongRetryPolicy,
@@ -280,7 +282,9 @@ public class InitializationTasks implements BeforeAllCallback, ExtensionContext.
           // add images only if SKIP_BUILD_IMAGES_IF_EXISTS is not set
           if (!SKIP_BUILD_IMAGES_IF_EXISTS) {
             images.add(miiBasicImage);
-            images.add(wdtBasicImage);
+            if (!CRIO && !OCNE) {
+              images.add(wdtBasicImage);
+            }
           }
 
           for (String image : images) {
