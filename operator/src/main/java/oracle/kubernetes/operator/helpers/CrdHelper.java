550417b3b48013ef647757cddfe30501f41cce7c
Ryan Eberhard <ryan.eberhard@oracle.com>
2025-02-27 16:55:48 -0500

    Use WME 2.2.3

--- operator/src/main/java/oracle/kubernetes/operator/helpers/CrdHelper.java
+++ operator/src/main/java/oracle/kubernetes/operator/helpers/CrdHelper.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2018, 2023, Oracle and/or its affiliates.
+// Copyright (c) 2018, 2025, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package oracle.kubernetes.operator.helpers;
@@ -137,7 +137,7 @@ public class CrdHelper {
     try (Writer writer = Files.newBufferedWriter(PathSupport.getPath(outputFileName))) {
       writer.write(
               """
-              # Copyright (c) 2020, 2023, Oracle and/or its affiliates.
+              # Copyright (c) 2020, 2025, Oracle and/or its affiliates.
               # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
               """);
       writer.write("\n");
