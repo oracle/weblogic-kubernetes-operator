2e987d653809187355eb8c401106905480588c31
MARINA.KOGAN <marina.kogan@oracle.com>
2025-03-24 13:16:02 +0000

    Update MonitoringExporterFiltering tests to adjust changes for wls-exporter servlet package name changes

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/TestConstants.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/TestConstants.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2020, 2024, Oracle and/or its affiliates.
+// Copyright (c) 2020, 2025, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package oracle.weblogic.kubernetes;
@@ -311,7 +311,7 @@ public interface TestConstants {
 
   //monitoring constants
   public static final String MONITORING_EXPORTER_WEBAPP_VERSION =
-      getNonEmptySystemProperty("wko.it.monitoring.exporter.webapp.version", "2.1.3");
+      getNonEmptySystemProperty("wko.it.monitoring.exporter.webapp.version", "2.3.0");
   public static final String MONITORING_EXPORTER_BRANCH =
       getNonEmptySystemProperty("wko.it.monitoring.exporter.branch", "main");
   public static final String PROMETHEUS_CHART_VERSION =
