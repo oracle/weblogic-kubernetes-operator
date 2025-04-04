52c0bdc95b6ec72d6d9a2f52e51e5377da2f2a94
xiancao <xian.cao@oracle.com>
2025-04-04 17:12:19 +0000

    update copyright

--- integration-tests/src/test/resources/ocne/terraform/1.9/modules/oci-ocne-images/datasources.tf
+++ integration-tests/src/test/resources/ocne/terraform/1.9/modules/oci-ocne-images/datasources.tf
@@ -1,4 +1,4 @@
-# Copyright (c) 2024 Oracle Corporation and/or affiliates.  All rights reserved.
+# Copyright (c) 2024, 2025, Oracle Corporation and/or affiliates.  All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
 # Gets the OCID of the OS image to use
