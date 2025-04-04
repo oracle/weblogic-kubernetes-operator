3db14260f8020b0819ce0e8df59fae9336263d60
Ryan Eberhard <ryan.eberhard@oracle.com>
2025-04-04 18:45:02 +0000

    Merge branch 'trap' into 'main'
    Cleanup traps and container exit messages
    
    See merge request weblogic-cloud/weblogic-kubernetes-operator!4933
    
    (cherry picked from commit 5166066622a876337202d2078a35eece582072ce)
    
    c6a065fa Cleanup traps and container exit messages

--- deployment/scripts/webhook.sh
+++ deployment/scripts/webhook.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Copyright (c) 2022, Oracle and/or its affiliates.
+# Copyright (c) 2022, 2025, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-echo "Launching the Domain Custom Resource Conversion Webhook for Oracle WebLogic Server Kubernetes Operator..."
+echo "Launching the Schema Conversion Webhook for Oracle WebLogic Server Kubernetes Operator..."
 
 # Relays SIGTERM to all java processes
 relay_SIGTERM() {
@@ -12,6 +12,22 @@ relay_SIGTERM() {
 }
 
 trap relay_SIGTERM SIGTERM
+
+# Relays SIGKILL to all java processes
+relay_SIGKILL() {
+  pid=`grep java /proc/[0-9]*/comm | awk -F / '{ print $3; }'`
+  echo "Sending SIGKILL to java process " $pid
+  kill -SIGKILL $pid
+  exit 0
+}
+
+trap relay_SIGKILL SIGKILL
+
+exitMessage() {
+  echo "Exiting container for the Schema Conversion Webhook for Oracle WebLogic Server Kubernetes Operator..."
+}
+
+trap exitMessage EXIT
 
 if [[ ! -z "$REMOTE_DEBUG_PORT" ]]; then
   DEBUG="-agentlib:jdwp=transport=dt_socket,server=y,suspend=$DEBUG_SUSPEND,address=*:$REMOTE_DEBUG_PORT"
