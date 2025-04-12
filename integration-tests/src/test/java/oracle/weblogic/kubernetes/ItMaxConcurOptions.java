f68763c26bca6c8a009d9644cebda67badb27e7b
Huiling Zhao <huiling.zhao@oracle.com>
2025-04-12 00:40:51 +0000

    debug 2

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItMaxConcurOptions.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItMaxConcurOptions.java
@@ -443,14 +443,12 @@ class ItMaxConcurOptions {
                 .addEnvItem(new V1EnvVar()
                     .name("JAVA_OPTIONS")
                     .value("-Dweblogic.security.SSL.ignoreHostnameVerification=true"))
-                // huizhao added below to fix intermittent issue to have
-               // pod maxconcuroptionsdomain-c1-managed-server3 ready caused by shutDownStatus=137
-                .addEnvItem(new V1EnvVar()
-                    .name("MEM_ARGS")
-                    .value("-Xms1024m -Xmx1024m"))
                 .addEnvItem(new V1EnvVar()
                     .name("USER_MEM_ARGS")
-                    .value("-Djava.security.egd=file:/dev/./urandom ")))
+                    .value("-Djava.security.egd=file:/dev/./urandom "))
+                .addEnvItem(new V1EnvVar()
+                    .name("MEM_ARGS")
+                    .value("-Xms1024m -Xmx1024m")))
             .adminServer(new AdminServer()
                 .adminChannelPortForwardingEnabled(false)
                 .serverService(new ServerService()
