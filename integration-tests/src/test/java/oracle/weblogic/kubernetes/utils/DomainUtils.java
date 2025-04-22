72005c3c77db295661b53399130d29b808739156
MAGGIE.HE@ORACLE.COM <maggie.he@oracle.com>
2025-04-22 20:26:56 +0000

    runAsUser 10001

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/DomainUtils.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/DomainUtils.java
@@ -1047,6 +1047,9 @@ public class DomainUtils {
     V1PodSecurityContext podSecCtxt = new V1PodSecurityContext();
     getLogger().info("DEBUG!!!! before setting runAsUser: " + podSecCtxt.getRunAsUser());
     getLogger().info("DEBUG!!!! before settimg FsGroup: " + podSecCtxt.getFsGroup());
+    podSecCtxt.runAsUser(10001L);
+    getLogger().info("DEBUG!!!! after setting runAsUser: " + podSecCtxt.getRunAsUser());
+    getLogger().info("DEBUG!!!! after setting FsGroup: " + podSecCtxt.getFsGroup());
     
     // create a domain custom resource configuration object
     DomainResource domain = new DomainResource()
