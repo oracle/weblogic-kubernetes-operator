52a1ecfc2eb25377577715b8985ee6e530619504
Huiling Zhao <huiling.zhao@oracle.com>
2025-04-24 16:14:06 +0000

    more change

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItMaxConcurOptions.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItMaxConcurOptions.java
@@ -24,6 +24,7 @@ import oracle.weblogic.domain.Configuration;
 import oracle.weblogic.domain.DomainResource;
 import oracle.weblogic.domain.DomainSpec;
 import oracle.weblogic.domain.Model;
+import oracle.weblogic.domain.ProbeTuning;
 import oracle.weblogic.domain.ServerPod;
 import oracle.weblogic.domain.ServerService;
 import oracle.weblogic.kubernetes.actions.impl.OperatorParams;
