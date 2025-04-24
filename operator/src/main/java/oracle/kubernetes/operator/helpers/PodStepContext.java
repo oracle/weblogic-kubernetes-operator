847fe6f774c7c9cb5056610fdb78c1557bc87045
Ryan Eberhard <ryan.eberhard@oracle.com>
2025-04-24 15:55:37 -0400

    Have monitoring exporter use configured container security

--- operator/src/main/java/oracle/kubernetes/operator/helpers/PodStepContext.java
+++ operator/src/main/java/oracle/kubernetes/operator/helpers/PodStepContext.java
@@ -1668,7 +1668,7 @@ public abstract class PodStepContext extends BasePodStepContext {
             .image(getDomain().getMonitoringExporterImage())
             .imagePullPolicy(getDomain().getMonitoringExporterImagePullPolicy())
             .resources(getDomain().getMonitoringExporterResources())
-            .securityContext(PodSecurityHelper.getDefaultContainerSecurityContext())
+            .securityContext(getServerSpec().getContainerSecurityContext())
             .addEnvItem(new V1EnvVar().name("JAVA_OPTS").value(createJavaOptions()))
             .addPortsItem(new V1ContainerPort()
                 .name("metrics").protocol("TCP").containerPort(getPort()));
