78d3a5b4a3703f8a720a3f78a107b2e2eb0285d3
Sankar Periyathambi neelakandan <sankar.periyathambi.neelakand@oracle.com>
2025-04-27 14:25:49 +0000

    Add Jenkinsfile.openshift job to main branch

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/extensions/InitializationTasks.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/extensions/InitializationTasks.java
@@ -321,7 +321,7 @@ public class InitializationTasks implements BeforeAllCallback, ExtensionContext.
           installTraefikLB();
         }
         //install Oracle Database operator as a one time task
-        if (!OCNE && !OKD && !CRIO && !ARM) {
+        if (!OCNE && !CRIO && !ARM) {
           installOracleDBOperator();
         }
 
@@ -331,7 +331,7 @@ public class InitializationTasks implements BeforeAllCallback, ExtensionContext.
           logger.info("Installing istio before any test suites are run");
           installIstio();
         }
-        if (INSTALL_WEBLOGIC && !OKD && !CRIO && !ARM && !OKE_CLUSTER) {
+        if (INSTALL_WEBLOGIC && !CRIO && !ARM && !OKE_CLUSTER) {
           installOnPremWebLogic();
         }
       } finally {
