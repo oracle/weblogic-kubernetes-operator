e93180fa69ba70c90b3b2ce58257ad6d6e0e299d
Sankar Neelakandan <sankar.periyathambi.neelakand@oracle.com>
2026-01-22 21:19:21 -0800

    wip

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/OperatorUtils.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/OperatorUtils.java
@@ -17,6 +17,7 @@ import oracle.weblogic.kubernetes.actions.impl.primitive.CommandParams;
 import oracle.weblogic.kubernetes.actions.impl.primitive.HelmParams;
 import oracle.weblogic.kubernetes.logging.LoggingFacade;
 
+import static oracle.weblogic.kubernetes.TestConstants.ARM;
 import static oracle.weblogic.kubernetes.TestConstants.DEFAULT_EXTERNAL_REST_IDENTITY_SECRET_NAME;
 import static oracle.weblogic.kubernetes.TestConstants.ELASTICSEARCH_HOST;
 import static oracle.weblogic.kubernetes.TestConstants.ELASTICSEARCH_HTTP_PORT;
@@ -25,6 +26,7 @@ import static oracle.weblogic.kubernetes.TestConstants.KUBERNETES_CLI;
 import static oracle.weblogic.kubernetes.TestConstants.LOGSTASH_IMAGE;
 import static oracle.weblogic.kubernetes.TestConstants.OKD;
 import static oracle.weblogic.kubernetes.TestConstants.OPERATOR_CHART_DIR;
+import static oracle.weblogic.kubernetes.TestConstants.OPERATOR_RELEASE_IMAGE;
 import static oracle.weblogic.kubernetes.TestConstants.OPERATOR_RELEASE_NAME;
 import static oracle.weblogic.kubernetes.TestConstants.TEST_IMAGES_REPO_SECRET_NAME;
 import static oracle.weblogic.kubernetes.actions.TestActions.createServiceAccount;
@@ -450,9 +452,9 @@ public class OperatorUtils {
     }
 
     operatorImage = getOperatorImageName();
-    //if (ARM) {
-    //  operatorImage = OPERATOR_RELEASE_IMAGE;
-    //}
+    if (ARM) {
+      String x = OPERATOR_RELEASE_IMAGE;
+    }
 
     assertFalse(operatorImage.isEmpty(), "operator image name can not be empty");
     logger.info("operator image name {0}", operatorImage);
